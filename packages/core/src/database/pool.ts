9cc58b8af0df5ff4c5c60f119a5c38d4379b1b7b
Gao Sun <gao@logto.io>
2021-06-26 01:39:02 +0800

    Finish adapter

--- packages/core/src/database/pool.ts
+++ packages/core/src/database/pool.ts
@@ -1,11 +1,7 @@
 import { createPool } from 'slonik';
-import { createQueryNormalisationInterceptor } from 'slonik-interceptor-query-normalisation';
-import { createFieldNameTransformationInterceptor } from 'slonik-interceptor-field-name-transformation';
+import { createInterceptors } from 'slonik-interceptor-preset';
 
-const interceptors = [
-  createQueryNormalisationInterceptor(),
-  createFieldNameTransformationInterceptor({ format: 'CAMEL_CASE' }),
-];
+const interceptors = [...createInterceptors()];
 
 const pool = createPool('postgres://localhost/logto', { interceptors });
 
--- packages/core/src/database/utils.ts
+++ packages/core/src/database/utils.ts
@@ -5,10 +5,16 @@ type FieldIdentifiers<Key extends string | number | symbol> = {
   [key in Key]: IdentifierSqlTokenType;
 };
 
-export const convertToIdentifiers = <T extends Table>({ table, fields }: T) => ({
+export const convertToIdentifiers = <T extends Table>(
+  { table, fields }: T,
+  withPrefix = false
+) => ({
   table: sql.identifier([table]),
   fields: Object.entries<string>(fields).reduce(
-    (previous, [key, value]) => ({ ...previous, [key]: sql.identifier([table, value]) }),
+    (previous, [key, value]) => ({
+      ...previous,
+      [key]: sql.identifier(withPrefix ? [table, value] : [value]),
+    }),
     // eslint-disable-next-line @typescript-eslint/prefer-reduce-type-parameter
     {} as FieldIdentifiers<keyof T['fields']>
   ),
