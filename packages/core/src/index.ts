b892c882be50bb0e17ad9a6f9baafb0b9dc6a1dc
Gao Sun <gao@silverhand.io>
2021-06-26 01:39:02 +0800

    Finish adapter

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,12 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import Koa from 'koa';
+import logger from 'koa-logger';
 import mount from 'koa-mount';
 import Router from 'koa-router';
 import { Provider } from 'oidc-provider';
+import postgresAdapter from './oidc/adapter';
 
 const router = new Router();
 
@@ -9,6 +14,10 @@ const app = new Koa();
 const PORT = 3000;
 
 const oidc = new Provider(`http://localhost:${PORT}/oidc`, {
+  adapter: postgresAdapter,
+  renderError: (ctx, out, error) => {
+    console.log(error);
+  },
   cookies: {
     // V2: Rotate this when necessary
     // https://github.com/panva/node-oidc-provider/blob/main/docs/README.md#cookieskeys
@@ -38,7 +47,8 @@ router.get('/callback', (ctx) => {
   ctx.body = 'A callback';
 });
 
-app.use(mount('/oidc', oidc.app)).use(router.routes()).use(router.allowedMethods());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+app.use(logger()).use(mount('/oidc', oidc.app)).use(router.routes()).use(router.allowedMethods());
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
