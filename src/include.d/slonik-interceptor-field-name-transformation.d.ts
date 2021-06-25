d36fa5e38ca35bf9065b618d7fb553f7d82d7a27
Gao Sun <gao@logto.io>
2021-06-26 01:39:02 +0800

    Finish adapter

--- src/include.d/slonik-interceptor-field-name-transformation.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'slonik-interceptor-field-name-transformation' {
-  import { InterceptorType } from 'slonik';
-
-  export const createFieldNameTransformationInterceptor: (config?: {
-    format: 'CAMEL_CASE';
-  }) => InterceptorType;
-}
