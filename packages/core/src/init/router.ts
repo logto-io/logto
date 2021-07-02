0de039453682dc4aa2d704703133ef3971e7d5cd
Gao Sun <gao@silverhand.io>
2021-07-02 22:42:48 +0800

    feat: pipe sign-in routes to playground url

--- packages/core/src/init/router.ts
+++ packages/core/src/init/router.ts
@@ -1,11 +1,17 @@
+import got from 'got';
 import Koa from 'koa';
 import Router from 'koa-router';
+import { promisify } from 'util';
+import stream from 'stream';
 import { signInRoute } from '../consts';
+import { getEnv } from '../utils';
 
+const pipeline = promisify(stream.pipeline);
 const router = new Router();
 
-router.get(signInRoute, (ctx) => {
-  ctx.body = 'Signing in';
+router.get(new RegExp(`^${signInRoute}(?:/|$)`), async (ctx) => {
+  // CAUTION: this is for dev purpose only, add a switch if needed
+  await pipeline(got.stream.get(getEnv('PLAYGROUND_URL')), ctx.res);
 });
 
 export default function initRouter(app: Koa): void {
