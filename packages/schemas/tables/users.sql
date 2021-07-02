02cc1af7f49a21cb0880c43f1b78d810000f17e8
Gao Sun <gao@silverhand.io>
2021-07-02 21:09:08 +0800

    Add user table

---
+++ packages/schemas/tables/users.sql
@@ -0,0 +1,9 @@
+create table users (
+  id varchar(24) not null,
+  user_name varchar(128) unique,
+  primary_email varchar(128) unique,
+  primary_phone varchar(128) unique,
+  password_encrypted varchar(128),
+  password_encryption_method varchar(32),
+  primary key (id)
+);
