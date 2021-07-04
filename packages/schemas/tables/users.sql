0584979dbba48d1ad23b0a01c296d5165397cc0e
Gao Sun <gao@silverhand.io>
2021-07-04 17:38:37 +0800

    refactor: user_name -> username

--- packages/schemas/tables/users.sql
+++ packages/schemas/tables/users.sql
@@ -2,7 +2,7 @@ create type password_encryption_method as enum ('SaltAndPepper');
 
 create table users (
   id varchar(24) not null,
-  user_name varchar(128) unique,
+  username varchar(128) unique,
   primary_email varchar(128) unique,
   primary_phone varchar(128) unique,
   password_encrypted varchar(128),
