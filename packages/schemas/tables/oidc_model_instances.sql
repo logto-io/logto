fca35e69339879fd79600272a16e01403e157e29
Gao Sun <gaosun.dev@gmail.com>
2021-07-04 21:17:55 +0800

    feat: remove redundant fields and add TS type support in generated files

--- packages/schemas/tables/oidc_model_instances.sql
+++ packages/schemas/tables/oidc_model_instances.sql
@@ -1,29 +1,26 @@
 create table oidc_model_instances (
   model_name varchar(64) not null,
   id varchar(128) not null,
-  payload jsonb not null,
+  payload jsonb /* @use OidcModelInstancePayload */ not null,
   expires_at bigint not null,
   consumed_at bigint,
-  user_code varchar(128),
-  uid varchar(128),
-  grant_id varchar(128),
   primary key (model_name, id)
 );
 
-create index oidc_model_instances__model_name_user_code
+create index oidc_model_instances__model_name_payload_user_code
 on oidc_model_instances (
   model_name,
-  user_code
+  (payload->>'userCode')
 );
 
-create index oidc_model_instances__model_name_uid
+create index oidc_model_instances__model_name_payload_uid
 on oidc_model_instances (
   model_name,
-  uid
+  (payload->>'uid')
 );
 
-create index oidc_model_instances__model_name_grant_id
+create index oidc_model_instances__model_name_payload_grant_id
 on oidc_model_instances (
   model_name,
-  grant_id
+  (payload->>'grantId')
 );
