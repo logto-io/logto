91059f431f36b7470b9ad24af1043a72bfcd2dbc
Gao Sun <gao@logto.io>
2021-07-04 15:01:02 +0800

    api: add sign in / consent

--- src/init/router.ts
+++ src/init/router.ts
@@ -2,19 +2,18 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import { Provider } from 'oidc-provider';
 import createSignInRoutes from '@/routes/sign-in';
-import createUIRoutes from '@/routes/ui';
+import createUIProxy from '@/proxies/ui';
 
 const createRouter = (provider: Provider): Router => {
   const router = new Router();
 
-  router.use('/api', createSignInRoutes());
-  router.use(createUIRoutes(provider));
+  router.use('/api', createSignInRoutes(provider));
 
   return router;
 };
 
 export default function initRouter(app: Koa, provider: Provider): Router {
   const router = createRouter(provider);
-  app.use(router.routes()).use(router.allowedMethods());
+  app.use(router.routes()).use(createUIProxy()).use(router.allowedMethods());
   return router;
 }
