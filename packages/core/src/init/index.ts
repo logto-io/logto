cda61a68b41eb3d8855917e96e956ad0132eb7ef
Gao Sun <gao@silverhand.io>
2021-07-06 23:12:35 +0800

    refactor: add guard and error handler middleware

--- packages/core/src/init/index.ts
+++ packages/core/src/init/index.ts
@@ -1,11 +1,13 @@
 import Koa from 'koa';
-import logger from 'koa-logger';
+import koaLogger from 'koa-logger';
 
+import koaErrorHandler from '@/middleware/koa-error-handler';
 import initOidc from './oidc';
 import initRouter from './router';
 
 export default async function initApp(app: Koa, port: number): Promise<void> {
-  app.use(logger());
+  app.use(koaErrorHandler());
+  app.use(koaLogger());
 
   const provider = await initOidc(app, port);
   initRouter(app, provider);
--- packages/core/src/routes/sign-in.ts
+++ packages/core/src/routes/sign-in.ts
@@ -1,51 +1,52 @@
 import assert from 'assert';
 import Router from 'koa-router';
-import koaBody from 'koa-body';
 import { object, string } from 'zod';
 import { encryptPassword } from '@/utils/password';
 import { findUserById } from '@/queries/user';
 import { Provider } from 'oidc-provider';
 import { conditional } from '@logto/essentials';
+import koaGuard from '@/middleware/koa-guard';
 
 export default function createSignInRoutes(provider: Provider) {
   const router = new Router();
 
-  router.post('/sign-in', koaBody(), async (ctx) => {
-    const {
-      prompt: { name },
-    } = await provider.interactionDetails(ctx.req, ctx.res);
+  router.post(
+    '/sign-in',
+    koaGuard({ body: object({ id: string().optional(), password: string().optional() }) }),
+    async (ctx) => {
+      const {
+        prompt: { name },
+      } = await provider.interactionDetails(ctx.req, ctx.res);
 
-    if (name === 'login') {
-      const SignInBody = object({
-        id: string().min(1),
-        password: string().min(1),
-      });
-      const { id, password } = SignInBody.parse(ctx.request.body);
+      if (name === 'login') {
+        const { id, password } = ctx.guard.body;
 
-      const { passwordEncrypted, passwordEncryptionMethod, passwordEncryptionSalt } =
-        await findUserById(id);
+        assert(id && password, 'Insufficent sign-in info.');
+        const { passwordEncrypted, passwordEncryptionMethod, passwordEncryptionSalt } =
+          await findUserById(id);
 
-      assert(passwordEncrypted && passwordEncryptionMethod && passwordEncryptionSalt);
-      assert(
-        encryptPassword(id, password, passwordEncryptionSalt, passwordEncryptionMethod) ===
-          passwordEncrypted
-      );
+        assert(passwordEncrypted && passwordEncryptionMethod && passwordEncryptionSalt);
+        assert(
+          encryptPassword(id, password, passwordEncryptionSalt, passwordEncryptionMethod) ===
+            passwordEncrypted
+        );
 
-      const redirectTo = await provider.interactionResult(
-        ctx.req,
-        ctx.res,
-        {
-          login: { accountId: id },
-        },
-        { mergeWithLastSubmission: false }
-      );
-      ctx.body = { redirectTo };
-    } else if (name === 'consent') {
-      ctx.body = { redirectTo: ctx.request.origin + '/sign-in/consent' };
-    } else {
-      throw new Error(`Prompt not supported: ${name}`);
+        const redirectTo = await provider.interactionResult(
+          ctx.req,
+          ctx.res,
+          {
+            login: { accountId: id },
+          },
+          { mergeWithLastSubmission: false }
+        );
+        ctx.body = { redirectTo };
+      } else if (name === 'consent') {
+        ctx.body = { redirectTo: ctx.request.origin + '/sign-in/consent' };
+      } else {
+        throw new Error(`Prompt not supported: ${name}`);
+      }
     }
-  });
+  );
 
   router.post('/sign-in/consent', async (ctx) => {
     const { session, grantId, params, prompt } = await provider.interactionDetails(
