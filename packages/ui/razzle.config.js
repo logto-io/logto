554efa661aaca8786ac34b917484585ab074de7f
Gao Sun <gaosun.dev@gmail.com>
2021-07-11 21:08:38 +0800

    feat: initial commit

---
+++ packages/ui/razzle.config.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = {
+  options: {
+    buildType: "spa",
+  },
+  plugins: ["scss"],
+  modifyWebpackConfig: ({ webpackConfig }) => {
+    const config = { ...webpackConfig };
+
+    config.resolve.alias = {
+      "@": path.resolve("src/"),
+    };
+
+    return config;
+  },
+};
