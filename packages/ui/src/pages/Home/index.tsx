6f88d1d1a51c60c0c9605ac1637e416aa638da9a
Gao Sun <gao@silverhand.io>
2021-07-18 17:22:17 +0800

    feat: init input and i18n

--- packages/ui/src/pages/Home/index.tsx
+++ packages/ui/src/pages/Home/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import Input from '@/components/Input';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import styles from './index.module.scss';
 
 const Home = () => {
+  const { t } = useTranslation();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.title}>Logto</div>
+      <div className={styles.title}>登录 Logto</div>
+      <Input placeholder={t('sign-in.username')} value={username} onChange={setUsername} />
+      <Input
+        placeholder={t('sign-in.password')}
+        type="password"
+        value={password}
+        onChange={setPassword}
+      />
     </div>
   );
 };
