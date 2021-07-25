eedd1f22ee9087195f893f479a598357aebb6343
Gao Sun <gao@silverhand.io>
2021-07-25 17:35:01 +0800

    refactor: refine sign-in errors

--- packages/core/src/errors/RequestError/message.ts
+++ packages/core/src/errors/RequestError/message.ts
@@ -3,10 +3,12 @@ import { guardErrorMessage } from './collection/guard-errors';
 import { oidcErrorMessage } from './collection/oidc-errors';
 import { registerErrorMessage } from './collection/register-errors';
 import { swaggerErrorMessage } from './collection/swagger-errors';
+import { signInErrorMessage } from './collection/sign-in-errors';
 
 export const requestErrorMessage: Record<RequestErrorCode, string> = {
   ...guardErrorMessage,
   ...oidcErrorMessage,
   ...registerErrorMessage,
   ...swaggerErrorMessage,
+  ...signInErrorMessage,
 };
