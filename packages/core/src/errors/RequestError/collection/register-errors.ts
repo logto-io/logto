78d40242487b93bc4a81300e6f6bf954694adcd3
Gao Sun <gaosun.dev@gmail.com>
2021-07-28 01:13:51 +0800

    feat(phrases): add package and refactor error code

--- packages/core/src/errors/RequestError/collection/register-errors.ts
+++
@@ -1,7 +0,0 @@
-export enum RegisterErrorCode {
-  UsernameExists = 'register.username_exists',
-}
-
-export const registerErrorMessage: Record<RegisterErrorCode, string> = {
-  [RegisterErrorCode.UsernameExists]: 'The username already exists.',
-};
---
+++ packages/phrases/src/locales/zh-cn.ts
@@ -0,0 +1,41 @@
+import en from './en';
+
+const translation = {
+  sign_in: {
+    title: '登录',
+    loading: '登录中...',
+    error: '用户名或密码错误。',
+    username: '用户名',
+    password: '密码',
+  },
+  register: {
+    create_account: '新用户注册',
+  },
+};
+
+const errors = {
+  guard: {
+    invalid_input: '请求内容有误。',
+  },
+  oidc: {
+    aborted: '用户终止了交互。',
+  },
+  register: {
+    username_exists: '用户名已存在。',
+  },
+  sign_in: {
+    invalid_credentials: '用户名或密码错误，请检查您的输入。',
+    invalid_sign_in_method: '当前登录方式不可用。',
+    insufficient_info: '登录信息缺失，请检查您的输入。',
+  },
+  swagger: {
+    invalid_zod_type: '无效的 Zod 类型，请检查路由 guard 配置。',
+  },
+};
+
+const zhCN: typeof en = Object.freeze({
+  translation,
+  errors,
+});
+
+export default zhCN;
