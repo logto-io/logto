95c3b1e824f96493849f6fdc22fd45dda868e5aa
simeng_li <simeng.li@airbnb.com>
2021-08-01 23:03:14 +0800

    :sparkles: Add register moweb page

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -4,15 +4,17 @@ import AppContent from './components/AppContent';
 import initI18n from './init/i18n';
 import Consent from './pages/Consent';
 import SignIn from './pages/SignIn';
+import Register from './pages/Register';
 import './scss/normalized.scss';
 
 initI18n();
 
 const App = () => (
-  <AppContent theme="dark">
+  <AppContent theme="light">
     <Switch>
       <Route exact path="/sign-in" component={SignIn} />
       <Route exact path="/sign-in/consent" component={Consent} />
+      <Route exact path="/Register" component={Register} />
     </Switch>
   </AppContent>
 );
