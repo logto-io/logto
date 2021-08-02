a245f5f1e6913a40c39735df97a899a90b21ad01
Gao Sun <gao@silverhand.io>
2021-08-02 21:11:38 +0800

    feat(ui): set theme according to system configuration

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import AppContent from './components/AppContent';
+import useTheme from './hooks/use-theme';
 import initI18n from './init/i18n';
 import Consent from './pages/Consent';
 import SignIn from './pages/SignIn';
@@ -9,14 +10,18 @@ import './scss/normalized.scss';
 
 initI18n();
 
-const App = () => (
-  <AppContent theme="dark">
-    <Switch>
-      <Route exact path="/sign-in" component={SignIn} />
-      <Route exact path="/sign-in/consent" component={Consent} />
-      <Route exact path="/Register" component={Register} />
-    </Switch>
-  </AppContent>
-);
+const App = () => {
+  const theme = useTheme();
+
+  return (
+    <AppContent theme={theme}>
+      <Switch>
+        <Route exact path="/sign-in" component={SignIn} />
+        <Route exact path="/sign-in/consent" component={Consent} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </AppContent>
+  );
+};
 
 export default App;
