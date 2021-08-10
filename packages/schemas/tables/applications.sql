2998dece515948b6cd253fcfa5f062c88a074112
Gao Sun <gao@silverhand.io>
2021-08-10 17:21:34 +0800

    refactor(schemas): oidc_clients -> applications

---
+++ packages/schemas/tables/applications.sql
@@ -0,0 +1,16 @@
+create type application_type as enum ('Native', 'SPA', 'Traditional');
+
+create table applications (
+  id varchar(128) not null,
+  name varchar(256) not null,
+  type application_type not null,
+  oidc_client_id varchar(128) not null,
+  oidc_client_metadata jsonb /* @use OidcClientMetadata */ not null,
+  created_at bigint not null default(extract(epoch from now())),
+  primary key (id)
+);
+
+create index applications__oidc_client_id
+on applications (
+  oidc_client_id
+);
