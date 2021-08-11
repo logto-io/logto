b457d95a9e8fbf41034905c538f59f121498a7a3
Gao Sun <gao@silverhand.io>
2021-08-11 22:13:08 +0800

    refactor: conform RESTful convention (#77)
    * refactor: conform RESTful convention
    
    * refactor: update tests

--- packages/core/src/routes/session.ts
+++ packages/core/src/routes/session.ts
@@ -9,9 +9,9 @@ import koaGuard from '@/middleware/koa-guard';
 import RequestError from '@/errors/RequestError';
 import { LogtoErrorCode } from '@logto/phrases';
 
-export default function signInRoutes(router: Router, provider: Provider) {
+export default function sessionRoutes(router: Router, provider: Provider) {
   router.post(
-    '/sign-in',
+    '/session',
     koaGuard({ body: object({ username: string().optional(), password: string().optional() }) }),
     async (ctx, next) => {
       const {
@@ -21,7 +21,7 @@ export default function signInRoutes(router: Router, provider: Provider) {
       if (name === 'login') {
         const { username, password } = ctx.guard.body;
 
-        assert(username && password, new RequestError('sign_in.insufficient_info'));
+        assert(username && password, new RequestError('session.insufficient_info'));
 
         try {
           const { id, passwordEncrypted, passwordEncryptionMethod, passwordEncryptionSalt } =
@@ -29,12 +29,12 @@ export default function signInRoutes(router: Router, provider: Provider) {
 
           assert(
             passwordEncrypted && passwordEncryptionMethod && passwordEncryptionSalt,
-            new RequestError('sign_in.invalid_sign_in_method')
+            new RequestError('session.invalid_sign_in_method')
           );
           assert(
             encryptPassword(id, password, passwordEncryptionSalt, passwordEncryptionMethod) ===
               passwordEncrypted,
-            new RequestError('sign_in.invalid_credentials')
+            new RequestError('session.invalid_credentials')
           );
 
           const redirectTo = await provider.interactionResult(
@@ -48,13 +48,13 @@ export default function signInRoutes(router: Router, provider: Provider) {
           ctx.body = { redirectTo };
         } catch (error: unknown) {
           if (!(error instanceof RequestError)) {
-            throw new RequestError('sign_in.invalid_credentials');
+            throw new RequestError('session.invalid_credentials');
           }
 
           throw error;
         }
       } else if (name === 'consent') {
-        ctx.body = { redirectTo: ctx.request.origin + '/sign-in/consent' };
+        ctx.body = { redirectTo: ctx.request.origin + '/session/consent' };
       } else {
         throw new Error(`Prompt not supported: ${name}`);
       }
@@ -63,7 +63,7 @@ export default function signInRoutes(router: Router, provider: Provider) {
     }
   );
 
-  router.post('/sign-in/consent', async (ctx, next) => {
+  router.post('/session/consent', async (ctx, next) => {
     const { session, grantId, params, prompt } = await provider.interactionDetails(
       ctx.req,
       ctx.res
@@ -99,7 +99,7 @@ export default function signInRoutes(router: Router, provider: Provider) {
     return next();
   });
 
-  router.post('/sign-in/abort', async (ctx, next) => {
+  router.delete('/session', async (ctx, next) => {
     await provider.interactionDetails(ctx.req, ctx.res);
     const error: LogtoErrorCode = 'oidc.aborted';
     const redirectTo = await provider.interactionResult(ctx.req, ctx.res, {
