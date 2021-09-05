2bcb540a0e5e5e0418d34358493c49b8b9fd01db
Gao Sun <gao@silverhand.io>
2021-09-05 20:57:51 +0800

    feat(core): add status api and health check after deployment

---
+++ packages/core/src/routes/status.ts
@@ -0,0 +1,8 @@
+import { AnonymousRouter } from './types';
+
+export default function statusRoutes<T extends AnonymousRouter>(router: T) {
+  router.get('/status', async (ctx, next) => {
+    ctx.status = 204;
+    return next();
+  });
+}
