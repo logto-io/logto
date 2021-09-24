4ea5f65982e7c79f741748327fa146cd2893fbce
simeng-li <36393111+simeng-li@users.noreply.github.com>
2021-09-24 17:00:26 +0800

    fix(core):  add redirectURI to the response body of register endpoint /user (#119)
    * fix(ui): hide loading status
    
    should hide loading status while a http error is thrown
    
    * fix(ui): use api-hooks for consent request
    
    integrate api-hooks for the consent page request
    
    * fix(ui): add redirectURI to the response body of register endpoint
    
    Fix the use registration flow, should redirect after a success registration callback

--- packages/ui/src/pages/Consent/index.tsx
+++ packages/ui/src/pages/Consent/index.tsx
@@ -2,17 +2,21 @@ import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { consent } from '@/apis/consent';
+import useApi from '@/hooks/use-api';
 
 const Consent = () => {
   const { t } = useTranslation();
+  const { result, run: asyncConsent } = useApi(consent);
 
   useEffect(() => {
-    const autoConsent = async () => {
-      window.location.assign((await consent()).redirectTo);
-    };
-
-    void autoConsent();
+    void asyncConsent();
   }, []);
+
+  useEffect(() => {
+    if (result?.redirectTo) {
+      window.location.assign(result.redirectTo);
+    }
+  }, [result]);
 
   return <div>{t('sign_in.loading')}</div>;
 };
