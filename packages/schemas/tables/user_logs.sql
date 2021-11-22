c55471770b441f215d5934c028979c5093971e60
wangsijie <wangsijie94@gmail.com>
2021-11-22 11:33:35 +0800

    fix(core): add log result type

--- packages/schemas/tables/user_logs.sql
+++ packages/schemas/tables/user_logs.sql
@@ -1,9 +1,12 @@
-create type log_type as enum ('SignInSuccess', 'SignInFailedWrongPassword', 'ExchangeAccessTokenSuccess');
+create type log_type as enum ('SignInUsernameAndPassword', 'ExchangeAccessToken');
+
+create type log_result as enum ('Success', 'Failed');
 
 create table user_logs (
   id varchar(24) not null,
   user_id varchar(24) not null,
   type log_type not null,
+  type log_result not null, /* not using boolean, may have more result types in the future */
   payload jsonb /* @use UserLogPayload */ not null,
   created_at timestamptz not null default(now()),
   primary key (id)
