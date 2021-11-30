838ae3fad97248c64d455887cf5312070cfbc37d
Wang Sijie <wangsijie@silverhand.io>
2021-11-30 11:06:50 +0800

    feat: add koa-pagination (#143)
    * feat: add koa-pagination
    
    * fix: pr fix

---
+++ packages/core/src/middleware/koa-pagination.test.ts
@@ -0,0 +1,139 @@
+import { createMockContext } from '@shopify/jest-koa-mocks';
+import { Context } from 'koa';
+
+import koaPagination, { WithPaginationContext } from './koa-pagination';
+
+const next = jest.fn();
+const setHeader = jest.fn();
+const links = new Set<string>();
+const appendHeader = jest.fn((key: string, value: string) => {
+  if (key === 'Link') {
+    links.add(value);
+  }
+});
+
+const createContext = (query: Record<string, string>): WithPaginationContext<Context> => {
+  const baseContext = createMockContext();
+  const context = {
+    ...baseContext,
+    request: {
+      ...baseContext.request,
+      origin: '',
+      path: '',
+      query,
+    },
+    pagination: { limit: 0, offset: 0 },
+    set: setHeader,
+    append: appendHeader,
+  };
+  return context;
+};
+
+afterEach(() => {
+  setHeader.mockClear();
+  appendHeader.mockClear();
+  links.clear();
+});
+
+describe('request', () => {
+  it('should get limit and offset from queries', async () => {
+    const context = createContext({ page: '1', page_size: '30' });
+    await koaPagination()(context, next);
+    expect(context.pagination.limit).toEqual(30);
+    expect(context.pagination.offset).toEqual(0);
+  });
+
+  it('should set default page to 1 (offset to 0) if non is provided', async () => {
+    const context = createContext({});
+    await koaPagination()(context, next);
+    expect(context.pagination.offset).toEqual(0);
+  });
+
+  it('should set default pageSize(limit) to 20', async () => {
+    const context = createContext({});
+    await koaPagination({ defaultPageSize: 20 })(context, next);
+    expect(context.pagination.limit).toEqual(20);
+  });
+
+  it('throw when page value is not number-like', async () => {
+    const context = createContext({ page: 'invalid_number' });
+    await expect(koaPagination()(context, next)).rejects.toThrow();
+  });
+
+  it('throw when page number is 0', async () => {
+    const context = createContext({ page: '0' });
+    await expect(koaPagination()(context, next)).rejects.toThrow();
+  });
+
+  it('throw when page_size value is not number-like', async () => {
+    const context = createContext({ page_size: 'invalid_number' });
+    await expect(koaPagination()(context, next)).rejects.toThrow();
+  });
+
+  it('throw when page_size number is 0', async () => {
+    const context = createContext({ page_size: '0' });
+    await expect(koaPagination()(context, next)).rejects.toThrow();
+  });
+
+  it('throw when page_size number exceeds max', async () => {
+    const context = createContext({ page_size: '200' });
+    await expect(koaPagination({ maxPageSize: 100 })(context, next)).rejects.toThrow();
+  });
+});
+
+describe('response', () => {
+  it('should add Total-Number to response header', async () => {
+    const context = createContext({});
+    await koaPagination()(context, async () => {
+      // eslint-disable-next-line @silverhand/fp/no-mutation
+      context.pagination.totalCount = 100;
+    });
+    expect(setHeader).toHaveBeenCalledWith('Total-Number', '100');
+  });
+
+  describe('Link in response header', () => {
+    it('should append `first` and `last` in 1 of 1', async () => {
+      const context = createContext({});
+      await koaPagination({ defaultPageSize: 20 })(context, async () => {
+        // eslint-disable-next-line @silverhand/fp/no-mutation
+        context.pagination.totalCount = 10;
+      });
+      expect(links.has('<?page=1>; rel="first"')).toBeTruthy();
+      expect(links.has('<?page=1>; rel="last"')).toBeTruthy();
+    });
+
+    it('should append `first`, `next`, `last` in 1 of 2', async () => {
+      const context = createContext({});
+      await koaPagination({ defaultPageSize: 20 })(context, async () => {
+        // eslint-disable-next-line @silverhand/fp/no-mutation
+        context.pagination.totalCount = 30;
+      });
+      expect(links.has('<?page=1>; rel="first"')).toBeTruthy();
+      expect(links.has('<?page=2>; rel="next"')).toBeTruthy();
+      expect(links.has('<?page=2>; rel="last"')).toBeTruthy();
+    });
+
+    it('should append `first`, `prev`, `last` in 2 of 2', async () => {
+      const context = createContext({ page: '2' });
+      await koaPagination({ defaultPageSize: 20 })(context, async () => {
+        // eslint-disable-next-line @silverhand/fp/no-mutation
+        context.pagination.totalCount = 30;
+      });
+      expect(links.has('<?page=1>; rel="first"')).toBeTruthy();
+      expect(links.has('<?page=1>; rel="prev"')).toBeTruthy();
+      expect(links.has('<?page=2>; rel="last"')).toBeTruthy();
+    });
+
+    it('should append `first`, `prev`, `next`, `last` in 2 of 3', async () => {
+      const context = createContext({ page: '2' });
+      await koaPagination({ defaultPageSize: 20 })(context, async () => {
+        // eslint-disable-next-line @silverhand/fp/no-mutation
+        context.pagination.totalCount = 50;
+      });
+      expect(links.has('<?page=1>; rel="first"')).toBeTruthy();
+      expect(links.has('<?page=1>; rel="prev"')).toBeTruthy();
+      expect(links.has('<?page=3>; rel="next"')).toBeTruthy();
+      expect(links.has('<?page=3>; rel="last"')).toBeTruthy();
+    });
+  });
+});
---
+++ packages/core/src/middleware/koa-pagination.ts
@@ -0,0 +1,67 @@
+import { MiddlewareType } from 'koa';
+import { number } from 'zod';
+
+import RequestError from '@/errors/RequestError';
+import { buildLink } from '@/utils/pagination';
+
+export interface Pagination {
+  offset: number;
+  limit: number;
+  totalCount?: number;
+}
+
+export type WithPaginationContext<ContextT> = ContextT & {
+  pagination: Pagination;
+};
+
+export interface PaginationConfig {
+  defaultPageSize?: number;
+  maxPageSize?: number;
+}
+
+export default function koaPagination<StateT, ContextT, ResponseBodyT>({
+  defaultPageSize = 20,
+  maxPageSize = 100,
+}: PaginationConfig = {}): MiddlewareType<StateT, WithPaginationContext<ContextT>, ResponseBodyT> {
+  return async (ctx, next) => {
+    try {
+      const {
+        request: {
+          query: { page, page_size },
+        },
+      } = ctx;
+      // Query values are all string, need to convert to number first.
+      const pageNumber = page ? number().positive().parse(Number(page)) : 1;
+      const pageSize = page_size
+        ? number().positive().max(maxPageSize).parse(Number(page_size))
+        : defaultPageSize;
+
+      ctx.pagination = { offset: (pageNumber - 1) * pageSize, limit: pageSize };
+    } catch {
+      throw new RequestError({ code: 'guard.invalid_pagination', status: 400 });
+    }
+
+    await next();
+
+    // Only handle response when count value is set.
+    if (ctx.pagination.totalCount !== undefined) {
+      const { limit, offset, totalCount } = ctx.pagination;
+      const totalPage = Math.ceil(totalCount / limit) || 1; // Minimum page number is 1
+
+      // Our custom response header: Total-Number
+      ctx.set('Total-Number', String(totalCount));
+
+      // Response header's `Link`: https://datatracker.ietf.org/doc/html/rfc5988
+      const page = Math.floor(offset / limit) + 1; // Start from 1
+      ctx.append('Link', buildLink(ctx.request, 1, 'first'));
+      ctx.append('Link', buildLink(ctx.request, totalPage, 'last'));
+      if (page > 1) {
+        ctx.append('Link', buildLink(ctx.request, page - 1, 'prev'));
+      }
+
+      if (page < totalPage) {
+        ctx.append('Link', buildLink(ctx.request, page + 1, 'next'));
+      }
+    }
+  };
+}
