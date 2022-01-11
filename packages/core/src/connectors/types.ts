6a306729849f8d43a5d2393d8d2feaf7c3a6a9c5
wangsijie <wangsijie@silverhand.io>
2022-01-11 16:56:03 +0800

    feat: connector i18n

--- packages/core/src/connectors/types.ts
+++ packages/core/src/connectors/types.ts
@@ -3,9 +3,9 @@ import { ConnectorType } from '@logto/schemas';
 export interface ConnectorMetadata {
   id: string;
   type: ConnectorType;
-  name: string;
+  name: Record<string, string>;
   logo: string;
-  description: string;
+  description: Record<string, string>;
 }
 
 // The name `Connector` is used for database, use `ConnectorInstance` to avoid confusing.
