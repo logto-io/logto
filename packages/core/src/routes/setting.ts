dfaf79bc5faf6d489cebcf9371650cbf313e9141
Darcy Ye <darcyye@silverhand.io>
2022-01-13 20:05:14 +0800

    feat(settings): add settings related queries and apis (#174)
    * feat(settings): add settings related queries and APIs
    
    * feat: correct setting router type
    
    * chore: hide settingId from response
    
    * chore: reset default settingId for better readability
    
    * chore: fix input params check

---
+++ packages/core/src/routes/setting.ts
@@ -0,0 +1,28 @@
+import { Settings } from '@logto/schemas';
+
+import koaGuard from '@/middleware/koa-guard';
+import { getSetting, updateSetting } from '@/queries/setting';
+
+import { AuthedRouter } from './types';
+
+export default function settingRoutes<T extends AuthedRouter>(router: T) {
+  router.get('/settings', async (ctx, next) => {
+    const { id, ...rest } = await getSetting();
+    ctx.body = rest;
+
+    return next();
+  });
+
+  router.patch(
+    '/settings',
+    koaGuard({
+      body: Settings.guard.omit({ id: true }).partial(),
+    }),
+    async (ctx, next) => {
+      const { body: setting } = ctx.guard;
+      ctx.body = await updateSetting(setting);
+
+      return next();
+    }
+  );
+}
