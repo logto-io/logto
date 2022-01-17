a77149bb8b17a4e79b3e35c8a5fcaa8a0e36dfe2
Wang Sijie <wangsijie@silverhand.io>
2022-01-17 10:58:45 +0800

    feat: connector i18n (#171)
    * feat: connector i18n
    
    * fix: language key

--- packages/phrases/src/index.ts
+++ packages/phrases/src/index.ts
@@ -4,6 +4,7 @@ import { Normalize, Resource } from './types';
 
 export type LogtoErrorCode = Normalize<typeof en.errors>;
 export type LogtoErrorI18nKey = `errors:${LogtoErrorCode}`;
+export type Languages = keyof Resource;
 
 const resource: Resource = {
   en,
