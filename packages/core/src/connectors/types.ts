dd412ddf18805f4953ce25585abc0ff2fbeb2e51
wangsijie <wangsijie@silverhand.io>
2022-01-17 10:53:42 +0800

    fix: language key

--- packages/core/src/connectors/types.ts
+++ packages/core/src/connectors/types.ts
@@ -1,11 +1,12 @@
+import { Languages } from '@logto/phrases';
 import { ConnectorType } from '@logto/schemas';
 
 export interface ConnectorMetadata {
   id: string;
   type: ConnectorType;
-  name: Record<string, string>;
+  name: Record<Languages, string>;
   logo: string;
-  description: Record<string, string>;
+  description: Record<Languages, string>;
 }
 
 // The name `Connector` is used for database, use `ConnectorInstance` to avoid confusing.
