21e02faab1a892128bd4d58f6a25276e31631494
simeng-li <simeng@silverhand.io>
2022-01-18 09:30:39 +0800

    fix(middles): fix testcase typo
    fix testcase typo

--- packages/core/src/middleware/koa-camelcase-transform.test.ts
+++ packages/core/src/middleware/koa-camelcase-transform.test.ts
@@ -5,7 +5,7 @@ import koaCamelcaseTransform, { ResponseKeyCase } from './koa-camelcase-transfor
 const next = jest.fn();
 
 describe('koa-camelcase-transform', () => {
-  it('should transform body snakecase to camel case with keycase header camecase', async () => {
+  it('should transform body snakecase to camelcase with keycase header camecase', async () => {
     const ctx = createMockContext({
       headers: {
         'response-keycase': ResponseKeyCase.camelcase,
@@ -19,7 +19,7 @@ describe('koa-camelcase-transform', () => {
     expect(ctx.body).toHaveProperty('redirectUri');
   });
 
-  it('should not transform body to camel case without keycase header', async () => {
+  it('should not transform body to camelcase without keycase header', async () => {
     const ctx = createMockContext();
     ctx.body = { redirect_uri: 'foo' };
     await koaCamelcaseTransform()(ctx, next);
@@ -27,7 +27,7 @@ describe('koa-camelcase-transform', () => {
     expect(ctx.body).toHaveProperty('redirect_uri');
   });
 
-  it('should not transform body to camel case with keycase header snakecase', async () => {
+  it('should not transform body to camelcase with keycase header snakecase', async () => {
     const ctx = createMockContext({
       headers: {
         'response-keycase': ResponseKeyCase.snakecase,
