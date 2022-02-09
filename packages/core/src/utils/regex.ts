aea733580cba5b986c176c700ee92e8d02fb3507
Mingqing Ye <darcyye@silverhand.io>
2022-02-09 10:33:47 +0800

    feat(phone passwordless): rename phoneReg

--- packages/core/src/utils/regex.ts
+++ packages/core/src/utils/regex.ts
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export const emailRegEx = /^\S+@\S+\.\S+$/;
-=======
-export const emailReg = /^\S+@\S+\.\S+$/;
->>>>>>> 1ad2b52 (feat(phone passwordless): rename phoneReg)
 export const phoneRegEx = /^[1-9]\d{10}$/;
