4325007398c1ec1fa8ed630588109c87226c5454
simeng-li <simeng@silverhand.io>
2022-02-15 10:58:21 +0800

    refactor(core): throw error on pagination middleware (#228)
    throw internal error if no totalCount returned

--- packages/core/src/middleware/koa-pagination.ts
+++ packages/core/src/middleware/koa-pagination.ts
@@ -43,26 +43,28 @@ export default function koaPagination<StateT, ContextT, ResponseBodyT>({
 
     await next();
 
-    // Only handle response when count value is set.
-    if (ctx.pagination.totalCount !== undefined) {
-      const { limit, offset, totalCount } = ctx.pagination;
-      const totalPage = Math.ceil(totalCount / limit) || 1; // Minimum page number is 1
+    // Total count value should be returned, else return internal server-error.
+    if (ctx.pagination.totalCount === undefined) {
+      throw new Error('missing totalCount');
+    }
 
-      // Our custom response header: Total-Number
-      ctx.set('Total-Number', String(totalCount));
+    const { limit, offset, totalCount } = ctx.pagination;
+    const totalPage = Math.ceil(totalCount / limit) || 1; // Minimum page number is 1
 
-      // Response header's `Link`: https://datatracker.ietf.org/doc/html/rfc5988
-      const page = Math.floor(offset / limit) + 1; // Start from 1
-      ctx.append('Link', buildLink(ctx.request, 1, 'first'));
-      ctx.append('Link', buildLink(ctx.request, totalPage, 'last'));
+    // Our custom response header: Total-Number
+    ctx.set('Total-Number', String(totalCount));
 
-      if (page > 1) {
-        ctx.append('Link', buildLink(ctx.request, page - 1, 'prev'));
-      }
+    // Response header's `Link`: https://datatracker.ietf.org/doc/html/rfc5988
+    const page = Math.floor(offset / limit) + 1; // Start from 1
+    ctx.append('Link', buildLink(ctx.request, 1, 'first'));
+    ctx.append('Link', buildLink(ctx.request, totalPage, 'last'));
 
-      if (page < totalPage) {
-        ctx.append('Link', buildLink(ctx.request, page + 1, 'next'));
-      }
+    if (page > 1) {
+      ctx.append('Link', buildLink(ctx.request, page - 1, 'prev'));
+    }
+
+    if (page < totalPage) {
+      ctx.append('Link', buildLink(ctx.request, page + 1, 'next'));
     }
   };
 }
