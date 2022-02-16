5ce8c186525cfbb87534bc69381530d4c94db7ab
Gao Sun <gao@silverhand.io>
2022-02-16 15:04:34 +0800

    feat(console): init (#239)
    * feat(console): init
    
    * refactor: remove unused tsconfig props
    
    * fix: lint error

---
+++ packages/console/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import * as styles from './App.module.scss';
+
+export const App = () => {
+  return <h1 className={styles.app}>Hello world!</h1>;
+};
---
+++ packages/console/src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { App } from './App';
+
+const app = document.querySelector('#app');
+ReactDOM.render(<App />, app);
