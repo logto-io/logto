e6a17de65ddc3087e3df275213fcb783142aa994
simeng-li <simeng@silverhand.io>
2022-02-17 19:33:12 +0800

    fix(ut): revert chagnes
    revert changes

--- packages/core/src/middleware/koa-user-log.ts
+++ packages/core/src/middleware/koa-user-log.ts
@@ -50,11 +50,12 @@ export default function koaUserLog<StateT, ContextT, ResponseBodyT>(): Middlewar
 
     try {
       await next();
+      await insertLog(ctx, UserLogResult.Success);
+
+      return;
     } catch (error: unknown) {
       await insertLog(ctx, UserLogResult.Failed);
       throw error;
     }
-
-    await insertLog(ctx, UserLogResult.Success);
   };
 }
