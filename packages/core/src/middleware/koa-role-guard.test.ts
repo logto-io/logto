9cdf5e37257dd098b216ed4682ba07ed79e8eb24
simeng-li <simeng@silverhand.io>
2022-02-20 12:38:08 +0800

    fix(core): resolve cr comments
    remove getUserInfo

--- packages/core/src/middleware/koa-role-guard.test.ts
+++ packages/core/src/middleware/koa-role-guard.test.ts
@@ -1,15 +1,11 @@
 import { Context } from 'koa';
-import { IRouterParamContext } from 'koa-router';
-import { SlonikError } from 'slonik';
 
 import RequestError from '@/errors/RequestError';
-import * as userQuies from '@/queries/user';
 import { createContextWithRouteParameters } from '@/utils/test-utils';
 
 import { WithAuthContext } from './koa-auth';
 import koaRoleGuard from './koa-role-guard';
-
-const findUserByIdSpy = jest.spyOn(userQuies, 'findUserById');
+import { WithUserInfoContext } from './koa-user-info';
 
 // TODO: remove after log-1530 PR is merged and rebased
 const mockUser = {
@@ -18,39 +14,31 @@ const mockUser = {
   primaryEmail: 'foo@logto.io',
   primaryPhone: '111111',
   roleNames: ['admin'],
-  passwordEncrypted: null,
-  passwordEncryptionMethod: null,
-  passwordEncryptionSalt: null,
   name: null,
   avatar: null,
-  identities: {},
   customData: {},
 };
 
 describe('koaRoleGuard middleware', () => {
   const baseCtx = createContextWithRouteParameters();
 
-  const ctx: WithAuthContext<Context & IRouterParamContext> = {
+  const ctx: WithUserInfoContext<Context & WithAuthContext> = {
     ...baseCtx,
     auth: 'foo',
+    userInfo: mockUser,
   };
 
   const unauthorizedError = new RequestError({ code: 'auth.unauthorized', status: 401 });
 
   const next = jest.fn();
 
-  it('should throw if user not found', async () => {
-    findUserByIdSpy.mockRejectedValueOnce(new SlonikError());
-    await expect(koaRoleGuard()(ctx, next)).rejects.toMatchError(unauthorizedError);
-  });
-
   it('should throw if user dose not have admin role', async () => {
-    findUserByIdSpy.mockResolvedValueOnce({ ...mockUser, roleNames: ['guest'] });
+    ctx.userInfo.roleNames = ['guest'];
     await expect(koaRoleGuard()(ctx, next)).rejects.toMatchError(unauthorizedError);
   });
 
   it('should not throw for admin user', async () => {
-    findUserByIdSpy.mockResolvedValueOnce(mockUser);
+    ctx.userInfo.roleNames = ['admin'];
     await expect(koaRoleGuard()(ctx, next)).resolves.not.toThrow();
   });
 });
--- packages/core/src/middleware/koa-role-guard.ts
+++ packages/core/src/middleware/koa-role-guard.ts
@@ -2,24 +2,18 @@ import { UserRole } from '@logto/schemas';
 import { MiddlewareType } from 'koa';
 
 import RequestError from '@/errors/RequestError';
-import { findUserById } from '@/queries/user';
 import assertThat from '@/utils/assert-that';
 
-import { WithAuthContext } from './koa-auth';
+import { WithUserInfoContext } from './koa-user-info';
 
 export default function koaRoleGuard<
   StateT,
-  ContextT extends WithAuthContext,
+  ContextT extends WithUserInfoContext,
   ResponseBodyT
 >(): MiddlewareType<StateT, ContextT, ResponseBodyT> {
-  return async (ctx, next) => {
-    const userId = ctx.auth;
-    const userInfo = await findUserById(userId).catch(() => {
-      throw new RequestError({ code: 'auth.unauthorized', status: 401 });
-    });
-
+  return async ({ userInfo }, next) => {
     assertThat(
-      userInfo.roleNames.includes(UserRole.admin),
+      userInfo.roleNames?.includes(UserRole.admin),
       new RequestError({ code: 'auth.unauthorized', status: 401 })
     );
 
