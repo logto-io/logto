5005cabd5ade077266037ca25e9016377ffd8f10
Gao Sun <gao@silverhand.io>
2022-02-25 15:40:31 +0800

    feat(console): init layout

---
+++ packages/console/src/components/Topbar/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as styles from './index.module.scss';
+
+const Topbar = () => {
+  return <div className={styles.topbar}>Admin Console</div>;
+};
+
+export default Topbar;
