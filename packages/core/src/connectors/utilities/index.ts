c9400d0a4d0b03ac73f3235bc38885f459e58b35
Wang Sijie <wangsijie@silverhand.io>
2022-02-28 14:27:45 +0800

    test(core): github and connector utilities (#291)

--- packages/core/src/connectors/utilities/index.ts
+++ packages/core/src/connectors/utilities/index.ts
@@ -1,21 +1,9 @@
 import { ArbitraryObject } from '@logto/schemas';
 
-import RequestError from '@/errors/RequestError';
 import { findConnectorById, updateConnector } from '@/queries/connector';
 
 export const getConnectorConfig = async <T extends ArbitraryObject>(id: string): Promise<T> => {
   const connector = await findConnectorById(id);
-
-  // FIXME:
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (!connector) {
-    throw new RequestError({
-      code: 'entity.not_exists_with_id',
-      name: 'connector',
-      id,
-      status: 404,
-    });
-  }
 
   return connector.config as T;
 };
