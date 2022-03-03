0a56cbe41899255f61d988fc160f97e1b6d0d1c1
Gao Sun <gao@silverhand.io>
2022-03-03 14:21:22 +0800

    feat(console): topbar

--- packages/console/src/components/Topbar/index.tsx
+++ packages/console/src/components/Topbar/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 import * as styles from './index.module.scss';
+import logo from './logo.svg';
 
 const Topbar = () => {
-  return <div className={styles.topbar}>Admin Console</div>;
+  return (
+    <div className={styles.topbar}>
+      <img src={logo} />
+      <div className={styles.line} />
+      <div className={styles.text}>Admin Console</div>
+    </div>
+  );
 };
 
 export default Topbar;
