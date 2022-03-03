14d6c9e18a2c1c54e0f39e7af8eb1aef0dfac8cf
Gao Sun <gao@silverhand.io>
2022-03-03 15:00:20 +0800

    feat(console): topbar (#310)
    * feat(console): topbar
    
    * refactor(console): move logo to assets folder

--- packages/console/src/components/Topbar/index.tsx
+++ packages/console/src/components/Topbar/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import logo from '@/assets/images/logo.svg';
+
 import * as styles from './index.module.scss';
 
 const Topbar = () => {
-  return <div className={styles.topbar}>Admin Console</div>;
+  return (
+    <div className={styles.topbar}>
+      <img src={logo} />
+      <div className={styles.line} />
+      <div className={styles.text}>Admin Console</div>
+    </div>
+  );
 };
 
 export default Topbar;
