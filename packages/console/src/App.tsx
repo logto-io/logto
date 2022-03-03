cc8bb825fb1bd8713072dfe312eb2c4b2d1cae44
Gao Sun <gao@silverhand.io>
2022-03-03 15:02:30 +0800

    feat(console): active tab by route (#309)
    * feat(console): active tab by route
    
    * refactor(console): export sidebar utils and consts in index

--- packages/console/src/App.tsx
+++ packages/console/src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, useLocation, useNavigate } from 'react-router-dom';
 
 import './scss/normalized.scss';
 import * as styles from './App.module.scss';
 import Content from './components/Content';
-import Sidebar from './components/Sidebar';
+import Sidebar, { getPath, sections } from './components/Sidebar';
 import Topbar from './components/Topbar';
 import initI18n from './i18n/init';
 
 void initI18n();
 
-export const App = () => {
+const Main = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate(getPath(sections[0]?.items[0]?.title ?? ''));
+    }
+  }, [location.pathname, navigate]);
+
   return (
     <div className={styles.app}>
       <Topbar />
@@ -20,3 +30,11 @@ export const App = () => {
     </div>
   );
 };
+
+const App = () => (
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>
+);
+
+export default App;
--- packages/console/src/index.tsx
+++ packages/console/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { App } from './App';
+import App from './App';
 
 const app = document.querySelector('#app');
 ReactDOM.render(<App />, app);
