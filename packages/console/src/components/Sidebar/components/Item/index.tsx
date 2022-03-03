a265a3434ba157cca5fb24b11959f01e7aff9378
Gao Sun <gao@silverhand.io>
2022-03-03 17:42:47 +0800

    fix(console): tab route link (#314)

--- packages/console/src/components/Sidebar/components/Item/index.tsx
+++ packages/console/src/components/Sidebar/components/Item/index.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import React, { ReactChild } from 'react';
+import { TFuncKey, useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 import { getPath } from '../../utils';
@@ -7,15 +8,19 @@ import * as styles from './index.module.scss';
 
 type Props = {
   icon?: ReactChild;
-  title: string;
+  titleKey: TFuncKey<'translation', 'admin_console.tabs'>;
   isActive?: boolean;
 };
 
-const Item = ({ icon, title, isActive = false }: Props) => {
+const Item = ({ icon, titleKey, isActive = false }: Props) => {
+  const { t } = useTranslation(undefined, {
+    keyPrefix: 'admin_console.tabs',
+  });
+
   return (
-    <Link to={getPath(title)} className={classNames(styles.row, isActive && styles.active)}>
+    <Link to={getPath(titleKey)} className={classNames(styles.row, isActive && styles.active)}>
       {icon && <div className={styles.icon}>{icon}</div>}
-      <div className={styles.title}>{title}</div>
+      <div className={styles.title}>{t(titleKey)}</div>
     </Link>
   );
 };
