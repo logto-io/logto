c4dace9a515e1e60a7b755df7ba2e841f9a95412
Gao Sun <gao@silverhand.io>
2022-03-06 15:55:53 +0800

    fix(console): set modal app element

--- packages/console/src/index.tsx
+++ packages/console/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactModal from 'react-modal';
 
 import App from './App';
 
 const app = document.querySelector('#app');
+ReactModal.setAppElement('#app');
 ReactDOM.render(<App />, app);
