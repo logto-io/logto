680fad31a16089cc947968b48b00ce052c039c4e
Xiao Yijun <xiaoyijun@silverhand.io>
2022-03-08 22:12:07 +0800

    feat(console): create resource page

--- packages/console/src/pages/ApiResources/index.tsx
+++ packages/console/src/pages/ApiResources/index.tsx
@@ -1,6 +1,8 @@
 import { Resource } from '@logto/schemas';
-import React from 'react';
+import { conditional } from '@silverhand/essentials/lib/utilities/conditional.js';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import Modal from 'react-modal';
 import useSWR from 'swr';
 
 import Button from '@/components/Button';
@@ -9,20 +11,43 @@ import CardTitle from '@/components/CardTitle';
 import CopyToClipboard from '@/components/CopyToClipboard';
 import ImagePlaceholder from '@/components/ImagePlaceholder';
 import ItemPreview from '@/components/ItemPreview';
+import * as modalStyles from '@/scss/modal.module.scss';
 import { RequestError } from '@/swr';
 
+import CreateForm from './components/CreateForm';
 import * as styles from './index.module.scss';
 
 const ApiResources = () => {
+  const [isCreateFormOpen, setIsCreateFormOpen] = useState(false);
   const { t } = useTranslation(undefined, { keyPrefix: 'admin_console' });
-  const { data, error } = useSWR<Resource[], RequestError>('/api/resources');
+  const { data, error, mutate } = useSWR<Resource[], RequestError>('/api/resources');
   const isLoading = !data && !error;
 
   return (
     <Card>
       <div className={styles.headline}>
         <CardTitle title="api_resources.title" subtitle="api_resources.subtitle" />
-        <Button disabled title="admin_console.api_resources.create" />
+        <Button
+          title="admin_console.api_resources.create"
+          onClick={() => {
+            setIsCreateFormOpen(true);
+          }}
+        />
+        <Modal
+          isOpen={isCreateFormOpen}
+          className={modalStyles.content}
+          overlayClassName={modalStyles.overlay}
+        >
+          <CreateForm
+            onClose={(createdApiResource) => {
+              setIsCreateFormOpen(false);
+
+              if (createdApiResource) {
+                void mutate(conditional(data && [...data, createdApiResource]));
+              }
+            }}
+          />
+        </Modal>
       </div>
       <table className={styles.table}>
         <thead>
