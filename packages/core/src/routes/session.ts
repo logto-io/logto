9e677ca97ad97699d0dad527133e4dac9ee493e6
Darcy Ye <darcyye@silverhand.io>
2022-03-08 16:09:06 +0800

    feat(core): post /session/forgot-password/phone/verify-passcode-and-reset-password (#334)
    * feat(core): add post /session/forgot-password/phone/verify-passcode and UT
    
    * feat(core): reset password once passcode verification succeed

--- packages/core/src/routes/session.ts
+++ packages/core/src/routes/session.ts
@@ -504,6 +504,43 @@ export default function sessionRoutes<T extends AnonymousRouter>(router: T, prov
   );
 
   router.post(
+    '/session/forgot-password/phone/verify-passcode-and-reset-password',
+    koaGuard({
+      body: object({
+        phone: string().regex(phoneRegEx),
+        code: string(),
+        password: string().regex(passwordRegEx),
+      }),
+    }),
+    async (ctx, next) => {
+      const { jti } = await provider.interactionDetails(ctx.req, ctx.res);
+      const { phone, code, password } = ctx.guard.body;
+      ctx.userLog.phone = phone;
+      ctx.userLog.type = UserLogType.ForgotPasswordPhone;
+
+      assertThat(
+        await hasUserWithPhone(phone),
+        new RequestError({ code: 'user.phone_not_exists', status: 422 })
+      );
+
+      await verifyPasscode(jti, PasscodeType.ForgotPassword, code, { phone });
+      const { id } = await findUserByPhone(phone);
+      ctx.userLog.userId = id;
+
+      const { passwordEncryptionSalt, passwordEncrypted, passwordEncryptionMethod } =
+        encryptUserPassword(id, password);
+      await updateUserById(id, {
+        passwordEncryptionSalt,
+        passwordEncrypted,
+        passwordEncryptionMethod,
+      });
+      await assignInteractionResults(ctx, provider, { login: { accountId: id } });
+
+      return next();
+    }
+  );
+
+  router.post(
     '/session/bind-social',
     koaGuard({
       body: object({
