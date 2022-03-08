80f0f0efa7ed642b9dcf0e207ee13c17de42f3e5
IceHe.xyz <icehe@silverhand.io>
2022-03-08 22:28:23 +0800

    feat(core): find many data order by

--- packages/core/src/database/find-many.ts
+++ packages/core/src/database/find-many.ts
@@ -1,10 +1,10 @@
-import { SchemaLike, GeneratedSchema } from '@logto/schemas';
-import { notFalsy, Truthy } from '@silverhand/essentials';
+import { GeneratedSchema, SchemaLike } from '@logto/schemas';
+import { notFalsy } from '@silverhand/essentials';
 import { DatabasePoolType, sql } from 'slonik';
 
 import { isKeyOf } from '@/utils/schema';
 
-import { FindManyData } from './types';
+import { FindManyData, OrderBy } from './types';
 import { conditionalSql, convertToIdentifiers, convertToPrimitiveOrSql } from './utils';
 
 export const buildFindMany = <Schema extends SchemaLike, ReturnType extends SchemaLike>(
@@ -13,24 +13,40 @@ export const buildFindMany = <Schema extends SchemaLike, ReturnType extends Sche
 ) => {
   const { table, fields } = convertToIdentifiers(schema);
   const isKeyOfSchema = isKeyOf(schema);
-  const connectKeyValueWithEqualSign = (data: Partial<Schema>) =>
-    Object.entries(data)
+  const connectKeyValueWithEqualSign = (partialSchema: Partial<Schema>) =>
+    Object.entries(partialSchema)
       .map(
         ([key, value]) =>
           isKeyOfSchema(key) && sql`${fields[key]}=${convertToPrimitiveOrSql(key, value)}`
       )
-      .filter((value): value is Truthy<typeof value> => notFalsy(value));
+      .filter((entry) => notFalsy(entry));
 
-  return async ({ where, limit, offset }: FindManyData<Schema> = {}) => {
-    return pool.any<ReturnType>(sql`
+  const buildOrderBy = (orderBySchema: OrderBy<Schema>) =>
+    Object.entries(orderBySchema)
+      .map(([key, order]) => {
+        if (!isKeyOfSchema(key)) {
+          return false;
+        }
+
+        const orderDirection = order === 'asc' ? sql`asc` : sql`desc`;
+
+        return sql`${fields[key]} ${orderDirection}`;
+      })
+      .filter((entry) => notFalsy(entry));
+
+  return async ({ where, orderBy, limit, offset }: FindManyData<Schema> = {}) =>
+    pool.any<ReturnType>(sql`
       select ${sql.join(Object.values(fields), sql`, `)}
       from ${table}
       ${conditionalSql(
         where,
         (where) => sql`where ${sql.join(connectKeyValueWithEqualSign(where), sql` and `)}`
       )}
+      ${conditionalSql(
+        orderBy,
+        (orderBy) => sql`order by ${sql.join(buildOrderBy(orderBy), sql`, `)}`
+      )}
       ${conditionalSql(limit, (limit) => sql`limit ${limit}`)}
       ${conditionalSql(offset, (offset) => sql`offset ${offset}`)}
     `);
-  };
 };
