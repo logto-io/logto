676cb8efd53d648da142f95c963327d4ad45454c
wangsijie <wangsijie@silverhand.io>
2022-03-17 15:47:23 +0800

    feat(console): user details settings

--- packages/console/src/pages/UserDetails/index.tsx
+++ packages/console/src/pages/UserDetails/index.tsx
@@ -1,5 +1,7 @@
 import { User } from '@logto/schemas';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useController, useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 import ReactModal from 'react-modal';
 import { useParams } from 'react-router-dom';
@@ -7,19 +9,35 @@ import useSWR from 'swr';
 
 import ActionMenu, { ActionMenuItem } from '@/components/ActionMenu';
 import BackLink from '@/components/BackLink';
+import Button from '@/components/Button';
 import Card from '@/components/Card';
+import CodeEditor from '@/components/CodeEditor';
 import CopyToClipboard from '@/components/CopyToClipboard';
+import FormField from '@/components/FormField';
 import ImagePlaceholder from '@/components/ImagePlaceholder';
-import { RequestError } from '@/hooks/use-api';
+import TabNav, { TabNavLink } from '@/components/TabNav';
+import TextInput from '@/components/TextInput';
+import useApi, { RequestError } from '@/hooks/use-api';
 import Delete from '@/icons/Delete';
 import More from '@/icons/More';
 import Reset from '@/icons/Reset';
 import * as modalStyles from '@/scss/modal.module.scss';
+import { safeParseJson } from '@/utilities/json';
 
 import CreateSuccess from './components/CreateSuccess';
 import DeleteForm from './components/DeleteForm';
 import ResetPasswordForm from './components/ResetPasswordForm';
 import * as styles from './index.module.scss';
+
+type FormData = {
+  primaryEmail: string;
+  primaryPhone: string;
+  username: string;
+  name: string;
+  avatar: string;
+  roles: string;
+  customData: string;
+};
 
 const UserDetails = () => {
   const { id } = useParams();
@@ -27,8 +45,59 @@ const UserDetails = () => {
   const [isDeleteFormOpen, setIsDeleteFormOpen] = useState(false);
   const [isResetPasswordFormOpen, setIsResetPasswordFormOpen] = useState(false);
 
-  const { data, error } = useSWR<User, RequestError>(id && `/api/users/${id}`);
+  const { data, error, mutate } = useSWR<User, RequestError>(id && `/api/users/${id}`);
   const isLoading = !data && !error;
+
+  const { handleSubmit, register, control, reset } = useForm<FormData>();
+  const [submitting, setSubmitting] = useState(false);
+  const {
+    field: { onChange, value },
+  } = useController({ name: 'customData', control, rules: { required: true } });
+  const api = useApi();
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+    reset({
+      primaryEmail: data.primaryEmail ?? '',
+      primaryPhone: data.primaryPhone ?? '',
+      username: data.username ?? '',
+      name: data.name ?? '',
+      avatar: data.avatar ?? '',
+      roles: data.roleNames.join(','),
+      customData: JSON.stringify(data.customData, null, 2),
+    });
+  }, [data, reset]);
+
+  const onSubmit = handleSubmit(async (formData) => {
+    if (!data || submitting) {
+      return;
+    }
+
+    const customData = safeParseJson(formData.customData);
+
+    if (!customData) {
+      toast.error(t('user_details.custom_data_invalid'));
+
+      return;
+    }
+
+    const payload: Partial<User> = {
+      name: formData.name,
+      avatar: formData.avatar,
+      customData,
+    };
+    setSubmitting(true);
+
+    try {
+      const updatedUser = await api.patch(`/api/users/${data.id}`, { json: payload }).json<User>();
+      void mutate(updatedUser);
+      toast.success(t('user_details.saved'));
+    } finally {
+      setSubmitting(false);
+    }
+  });
 
   return (
     <div className={styles.container}>
@@ -36,7 +105,7 @@ const UserDetails = () => {
 
       {isLoading && <div>loading</div>}
       {error && <div>{`error occurred: ${error.metadata.code}`}</div>}
-      {data && (
+      {id && data && (
         <>
           <Card className={styles.header}>
             <ImagePlaceholder size={76} borderRadius={16} />
@@ -95,7 +164,68 @@ const UserDetails = () => {
               </ReactModal>
             </div>
           </Card>
-          <Card>TBD</Card>
+          <Card className={styles.body}>
+            <TabNav>
+              <TabNavLink href={`/users/${id}`}>{t('user_details.tab_settings')}</TabNavLink>
+              <TabNavLink href={`/users/${id}/logs`}>{t('user_details.tab_logs')}</TabNavLink>
+            </TabNav>
+            <form className={styles.form} onSubmit={onSubmit}>
+              <div className={styles.fields}>
+                <FormField
+                  title="admin_console.user_details.field_email"
+                  className={styles.textField}
+                >
+                  <TextInput readOnly {...register('primaryEmail')} />
+                </FormField>
+                <FormField
+                  title="admin_console.user_details.field_phone"
+                  className={styles.textField}
+                >
+                  <TextInput readOnly {...register('primaryPhone')} />
+                </FormField>
+                <FormField
+                  title="admin_console.user_details.field_username"
+                  className={styles.textField}
+                >
+                  <TextInput readOnly {...register('username')} />
+                </FormField>
+                <FormField
+                  title="admin_console.user_details.field_name"
+                  className={styles.textField}
+                >
+                  <TextInput {...register('name')} />
+                </FormField>
+                <FormField
+                  title="admin_console.user_details.field_avatar"
+                  className={styles.textField}
+                >
+                  <TextInput {...register('avatar')} />
+                </FormField>
+                <FormField
+                  title="admin_console.user_details.field_roles"
+                  className={styles.textField}
+                >
+                  <TextInput readOnly {...register('roles')} />
+                </FormField>
+                <FormField
+                  isRequired
+                  title="admin_console.user_details.field_custom_data"
+                  className={styles.textField}
+                >
+                  <CodeEditor height="200px" language="json" value={value} onChange={onChange} />
+                </FormField>
+              </div>
+              <div className={styles.submit}>
+                <Button
+                  disabled={submitting}
+                  htmlType="submit"
+                  type="primary"
+                  title="admin_console.user_details.save_changes"
+                  size="large"
+                />
+              </div>
+            </form>
+          </Card>
         </>
       )}
       {data && <CreateSuccess username={data.username ?? '-'} />}
