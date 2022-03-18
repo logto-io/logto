185e9047db96ce74d76d3461719d9cc9c28e1624
Xiao Yijun <xiaoyijun@silverhand.io>
2022-03-18 23:06:09 +0800

    refactor(console): delete application

--- packages/console/src/pages/ApplicationDetails/components/DeleteForm/index.tsx
+++ packages/console/src/pages/ApplicationDetails/components/DeleteForm/index.tsx
@@ -4,12 +4,9 @@ import { Trans, useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
 import Button from '@/components/Button';
-import Card from '@/components/Card';
-import CardTitle from '@/components/CardTitle';
-import IconButton from '@/components/IconButton';
+import ModalLayout from '@/components/ModalLayout';
 import TextInput from '@/components/TextInput';
 import useApi from '@/hooks/use-api';
-import Close from '@/icons/Close';
 
 import * as styles from './index.module.scss';
 
@@ -44,43 +41,45 @@ const DeleteForm = ({ id, name, onClose }: Props) => {
   };
 
   return (
-    // TODO LOG-1907: Modal
-    <Card className={styles.card}>
-      <div className={styles.header}>
-        <CardTitle title="application_details.reminder" />
-        <IconButton size="large" onClick={onClose}>
-          <Close />
-        </IconButton>
-      </div>
-      <div className={styles.body}>
-        <div className={styles.description}>
-          <Trans
-            t={t}
-            i18nKey="application_details.delete_description"
-            values={{ name }}
-            components={{ span: <span className={styles.hightlight} /> }}
+    <ModalLayout
+      title="application_details.reminder"
+      content={() => (
+        <div className={styles.content}>
+          <div className={styles.description}>
+            <Trans
+              t={t}
+              i18nKey="application_details.delete_description"
+              values={{ name }}
+              components={{ span: <span className={styles.hightlight} /> }}
+            />
+          </div>
+          <TextInput
+            value={inputName}
+            placeholder={t('application_details.enter_your_application_name')}
+            hasError={inputMismatched}
+            onChange={(event) => {
+              setInputName(event.currentTarget.value);
+            }}
           />
         </div>
-        <TextInput
-          value={inputName}
-          placeholder={t('application_details.enter_your_application_name')}
-          hasError={inputMismatched}
-          onChange={(event) => {
-            setInputName(event.currentTarget.value);
-          }}
-        />
-      </div>
-      <div className={styles.line} />
-      <div className={styles.footer}>
-        <Button type="outline" title="admin_console.application_details.cancel" onClick={onClose} />
-        <Button
-          disabled={inputMismatched || loading}
-          type="danger"
-          title="admin_console.application_details.delete"
-          onClick={handleDelete}
-        />
-      </div>
-    </Card>
+      )}
+      footer={() => (
+        <div className={styles.actions}>
+          <Button
+            type="outline"
+            title="admin_console.application_details.cancel"
+            onClick={onClose}
+          />
+          <Button
+            disabled={inputMismatched || loading}
+            type="danger"
+            title="admin_console.application_details.delete"
+            onClick={handleDelete}
+          />
+        </div>
+      )}
+      onClose={onClose}
+    />
   );
 };
 
