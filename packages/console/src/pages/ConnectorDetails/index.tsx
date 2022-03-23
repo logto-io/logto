42d58d3435b31b474cbded23c56a865999997e07
Xiao Yijun <xiaoyijun@silverhand.io>
2022-03-23 18:30:48 +0800

    refactor(console): remove redundant todo

--- packages/console/src/pages/ConnectorDetails/index.tsx
+++ packages/console/src/pages/ConnectorDetails/index.tsx
@@ -109,7 +109,6 @@ const ConnectorDetails = () => {
                 setIsReadMeOpen(false);
               }}
             >
-              {/* TODO - Implement the content when the documentation website is ready. */}
               <Markdown>{data.metadata.readme}</Markdown>
             </Drawer>
             <ActionMenu
