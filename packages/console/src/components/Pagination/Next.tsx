c288bccf002a37b260be0d451848c4c8208b6c84
wangsijie <wangsijie@silverhand.io>
2022-03-25 15:14:22 +0800

    feat(console): pagination

---
+++ packages/console/src/components/Pagination/Next.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Next = () => {
+  return (
+    <svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M2.05417 15.0708L9.125 7.99997L2.05417 0.929134L0.875001 2.10747L6.76833 7.99997L0.875001 13.8925L2.05417 15.0708Z"
+        fill="#747778"
+      />
+    </svg>
+  );
+};
+
+export default Next;
---
+++ packages/console/src/components/Pagination/Previous.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Previous = () => {
+  return (
+    <svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M7.94583 15.0708L0.875 7.99997L7.94583 0.929134L9.125 2.10747L3.23167 7.99997L9.125 13.8925L7.94583 15.0708Z"
+        fill="#747778"
+      />
+    </svg>
+  );
+};
+
+export default Previous;
