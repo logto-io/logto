0444efd8446e1fbc9ab114d64af3c100bf99f32b
simeng-li <simeng@silverhand.io>
2022-03-30 15:04:27 +0800

    feat(ui): add password less related api (#459)
    * feat(ui): add password less related api
    
    add password less related api
    
    * test(ui): add api ut
    
    add api ut

--- packages/ui/src/apis/register.ts
+++ packages/ui/src/apis/register.ts
@@ -14,3 +14,53 @@ export const register = async (username: string, password: string) => {
     })
     .json<Response>();
 };
+
+export const sendPhonePasscode = async (phone: string) => {
+  return ky
+    .post('/session/register/passwordless/phone/send-passcode', {
+      json: {
+        phone,
+      },
+    })
+    .json();
+};
+
+export const verifyPhonePasscode = async (phone: string, passcode: string) => {
+  type Response = {
+    redirectTo: string;
+  };
+
+  return ky
+    .post('/session/register/passwordless/phone/verify-passcode', {
+      json: {
+        phone,
+        passcode,
+      },
+    })
+    .json<Response>();
+};
+
+export const sendEmailPasscode = async (email: string) => {
+  return ky
+    .post('/session/register/passwordless/email/send-passcode', {
+      json: {
+        email,
+      },
+    })
+    .json();
+};
+
+export const verifyEmailPasscode = async (email: string, passcode: string) => {
+  type Response = {
+    redirectTo: string;
+  };
+
+  return ky
+    .post('/session/register/passwordless/email/verify-passcode', {
+      json: {
+        email,
+        passcode,
+      },
+    })
+    .json<Response>();
+};
--- packages/ui/src/apis/sign-in.ts
+++ packages/ui/src/apis/sign-in.ts
@@ -14,3 +14,53 @@ export const signInBasic = async (username: string, password: string) => {
     })
     .json<Response>();
 };
+
+export const sendPhonePasscode = async (phone: string) => {
+  return ky
+    .post('/session/sign-in/passwordless/phone/send-passcode', {
+      json: {
+        phone,
+      },
+    })
+    .json();
+};
+
+export const verifyPhonePasscode = async (phone: string, passcode: string) => {
+  type Response = {
+    redirectTo: string;
+  };
+
+  return ky
+    .post('/session/sign-in/passwordless/phone/verify-passcode', {
+      json: {
+        phone,
+        passcode,
+      },
+    })
+    .json<Response>();
+};
+
+export const sendEmailPasscode = async (email: string) => {
+  return ky
+    .post('/session/sign-in/passwordless/email/send-passcode', {
+      json: {
+        email,
+      },
+    })
+    .json();
+};
+
+export const verifyEmailPasscode = async (email: string, passcode: string) => {
+  type Response = {
+    redirectTo: string;
+  };
+
+  return ky
+    .post('/session/sign-in/passwordless/email/verify-passcode', {
+      json: {
+        email,
+        passcode,
+      },
+    })
+    .json<Response>();
+};
