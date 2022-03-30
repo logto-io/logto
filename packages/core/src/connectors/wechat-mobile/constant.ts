43e9a548328bc4e015193e33bacdb18e77aada91
Darcy Ye <darcyye@silverhand.io>
2022-03-30 17:16:49 +0800

    feat(core): add wechat-mobile connector

---
+++ packages/core/src/connectors/wechat-mobile/constant.ts
@@ -0,0 +1,2 @@
+export const authorizationEndpoint = 'https://help.wechat.com/app';
+export const scope = 'snsapi_userinfo';
---
+++ packages/core/src/connectors/wechat-mobile/index.ts
@@ -0,0 +1,30 @@
+/**
+ * The Implementation of OpenID Connect of WeChat Web Open Platform.
+ * https://developers.weixin.qq.com/doc/oplatform/Mobile_App/WeChat_Login/Development_Guide.html
+ */
+
+import { stringify } from 'query-string';
+
+import { metadata as weChatWebMetadata, WeChatConfig } from '@/connectors/wechat';
+
+import { ConnectorMetadata, GetAuthorizationUri } from '../types';
+import { getConnectorConfig } from '../utilities';
+import { authorizationEndpoint, scope } from './constant';
+
+export { validateConfig, getAccessToken, getUserInfo } from '@/connectors/wechat';
+
+export const metadata: ConnectorMetadata = {
+  ...weChatWebMetadata,
+  id: 'wechat-mobile',
+};
+
+export const getAuthorizationUri: GetAuthorizationUri = async (redirectUri, state) => {
+  const { appId } = await getConnectorConfig<WeChatConfig>(metadata.id);
+
+  return `${authorizationEndpoint}?${stringify({
+    appid: appId,
+    redirect_uri: encodeURI(redirectUri), // The variable `redirectUri` should match {appId, appSecret}
+    scope,
+    state,
+  })}`;
+};
