ac7a19f5166538283e9147edb3373785b30a9573
IceHe.xyz <icehe@silverhand.io>
2022-03-31 13:27:24 +0800

    feat(core): hex color regex

--- packages/core/src/utils/regex.ts
+++ packages/core/src/utils/regex.ts
@@ -3,3 +3,4 @@ export const phoneRegEx = /^[1-9]\d{10}$/;
 export const usernameRegEx = /^.{3,}$/;
 export const nameRegEx = /^.{3,}$/;
 export const passwordRegEx = /^.{6,}$/;
+export { hexColorRegEx } from '@logto/schemas';
