4ae58d0161c434954eefec4db06f191f7f66f71f
IceHe.xyz <icehe@silverhand.io>
2022-04-01 16:55:29 +0800

    feat(core): update social sign-in connector IDs only when social sign-in is enabled

--- packages/core/src/routes/sign-in-experience.test.ts
+++ packages/core/src/routes/sign-in-experience.test.ts
@@ -1,4 +1,9 @@
-import { SignInExperience, CreateSignInExperience, TermsOfUse } from '@logto/schemas';
+import {
+  SignInExperience,
+  CreateSignInExperience,
+  TermsOfUse,
+  SignInMethodState,
+} from '@logto/schemas';
 
 import * as signInExpLib from '@/lib/sign-in-experience';
 import {
@@ -39,18 +44,67 @@ jest.mock('@/queries/sign-in-experience', () => ({
   ),
 }));
 
-describe('signInExperiences routes', () => {
-  const signInExperienceRequester = createRequester({ authedRoutes: signInExperiencesRoutes });
+const signInExperienceRequester = createRequester({ authedRoutes: signInExperiencesRoutes });
 
-  it('GET /sign-in-exp', async () => {
-    const response = await signInExperienceRequester.get('/sign-in-exp');
+it('GET /sign-in-exp', async () => {
+  const response = await signInExperienceRequester.get('/sign-in-exp');
+  expect(response.status).toEqual(200);
+  expect(response.body).toEqual(mockSignInExperience);
+});
+
+describe('PATCH /sign-in-exp', () => {
+  it('should not update social connector ids when social sign-in is disabled', async () => {
+    const signInMethods = { ...mockSignInMethods, social: SignInMethodState.disabled };
+    const response = await signInExperienceRequester.patch('/sign-in-exp').send({
+      signInMethods,
+      socialSignInConnectorIds: ['facebook'],
+    });
     expect(response).toMatchObject({
       status: 200,
-      body: mockSignInExperience,
+      body: {
+        ...mockSignInExperience,
+        signInMethods,
+      },
     });
   });
 
-  it('PATCH /sign-in-exp', async () => {
+  it('should update enabled social connector IDs only when social sign-in is enabled', async () => {
+    const signInMethods = { ...mockSignInMethods, social: SignInMethodState.secondary };
+    const socialSignInConnectorIds = ['facebook'];
+    const signInExperience = {
+      signInMethods,
+      socialSignInConnectorIds,
+    };
+    const response = await signInExperienceRequester.patch('/sign-in-exp').send(signInExperience);
+    expect(response).toMatchObject({
+      status: 200,
+      body: {
+        ...mockSignInExperience,
+        signInMethods,
+        socialSignInConnectorIds,
+      },
+    });
+  });
+
+  it('should update social connector IDs in correct sorting order', async () => {
+    const signInMethods = { ...mockSignInMethods, social: SignInMethodState.secondary };
+    const socialSignInConnectorIds = ['github', 'facebook'];
+    const signInExperience = {
+      signInMethods,
+      socialSignInConnectorIds,
+    };
+    const response = await signInExperienceRequester.patch('/sign-in-exp').send(signInExperience);
+    expect(response).toMatchObject({
+      status: 200,
+      body: {
+        ...mockSignInExperience,
+        signInMethods,
+        socialSignInConnectorIds,
+      },
+    });
+  });
+
+  it('should succeed to update when the input is valid', async () => {
     const termsOfUse: TermsOfUse = { enabled: false };
     const socialSignInConnectorIds = ['github', 'facebook'];
 
@@ -86,7 +140,7 @@ describe('signInExperiences routes', () => {
     });
   });
 
-  it('PATCH /sign-in-exp should throw with invalid inputs', async () => {
+  it('should throw when the type of social connector IDs is wrong', async () => {
     const socialSignInConnectorIds = [123, 456];
 
     const response = await signInExperienceRequester.patch('/sign-in-exp').send({
@@ -94,4 +148,6 @@ describe('signInExperiences routes', () => {
     });
     expect(response.status).toEqual(400);
   });
+
+  // TODO: test other Zod guards of sign-in experiences
 });
