1dc615679b4197fce30852e39af99c8251942eb1
IceHe.xyz <icehe@silverhand.io>
2022-04-07 15:00:10 +0800

    feat(core): cors allowed origins

--- packages/core/src/oidc/utils.ts
+++ packages/core/src/oidc/utils.ts
@@ -1,4 +1,11 @@
-import { ApplicationType, OidcClientMetadata } from '@logto/schemas';
+import {
+  ApplicationType,
+  CustomClientMetadata,
+  customClientMetadataGuard,
+  CustomClientMetadataKey,
+  OidcClientMetadata,
+} from '@logto/schemas';
+import { errors } from 'oidc-provider';
 
 export const getApplicationTypeString = (type: ApplicationType) =>
   type === ApplicationType.Native ? 'native' : 'web';
@@ -8,3 +15,33 @@ export const buildOidcClientMetadata = (metadata?: OidcClientMetadata): OidcClie
   postLogoutRedirectUris: [],
   ...metadata,
 });
+
+const isOrigin = (value: string) => {
+  try {
+    const { origin } = new URL(value);
+
+    // Origin: <scheme> "://" <hostname> [ ":" <port> ]
+    return value === origin;
+  } catch {
+    return false;
+  }
+};
+
+export const validateCustomClientMetadata = (key: string, value: unknown) => {
+  const result = customClientMetadataGuard.pick({ [key]: true }).safeParse({ [key]: value });
+
+  if (!result.success) {
+    throw new errors.InvalidClientMetadata(key);
+  }
+
+  if (
+    key === CustomClientMetadataKey.CorsAllowedOrigins &&
+    Array.isArray(value) &&
+    value.some((origin) => !isOrigin(origin))
+  ) {
+    throw new errors.InvalidClientMetadata(CustomClientMetadataKey.CorsAllowedOrigins);
+  }
+};
+
+export const isOriginAllowed = (origin: string, customClientMetadata: CustomClientMetadata) =>
+  Boolean(customClientMetadata.corsAllowedOrigins?.includes(origin));
