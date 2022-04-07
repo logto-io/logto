11899731e7b0cee510ae73efc2e5e9bdd0950020
wangsijie <wangsijie@silverhand.io>
2022-04-07 12:08:25 +0800

    fix: set cn font size to 12 in sidebar section

--- packages/console/src/components/AppContent/components/Sidebar/components/Section/index.tsx
+++ packages/console/src/components/AppContent/components/Sidebar/components/Section/index.tsx
@@ -1,4 +1,6 @@
+import classNames from 'classnames';
 import React, { ReactNode } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import * as styles from './index.module.scss';
 
@@ -8,9 +10,17 @@ type Props = {
 };
 
 const Section = ({ children, title }: Props) => {
+  const {
+    i18n: { language },
+  } = useTranslation();
+
+  const languageClassname = language.split('-')[0];
+
   return (
     <div>
-      <div className={styles.title}>{title}</div>
+      <div className={classNames(styles.title, languageClassname && styles[languageClassname])}>
+        {title}
+      </div>
       {children}
     </div>
   );
