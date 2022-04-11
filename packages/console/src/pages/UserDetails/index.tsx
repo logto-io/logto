e361a359efc21e457be351a57d10bb07dcbd541a
Xiao Yijun <xiaoyijun@silverhand.io>
2022-04-11 14:27:08 +0800

    feat(console): link button component

--- packages/console/src/pages/UserDetails/index.tsx
+++ packages/console/src/pages/UserDetails/index.tsx
@@ -9,16 +9,17 @@ import { useParams } from 'react-router-dom';
 import useSWR from 'swr';
 
 import ActionMenu, { ActionMenuItem } from '@/components/ActionMenu';
-import BackLink from '@/components/BackLink';
 import Button from '@/components/Button';
 import Card from '@/components/Card';
 import CodeEditor from '@/components/CodeEditor';
 import CopyToClipboard from '@/components/CopyToClipboard';
 import FormField from '@/components/FormField';
 import ImagePlaceholder from '@/components/ImagePlaceholder';
+import LinkButton from '@/components/LinkButton';
 import TabNav, { TabNavLink } from '@/components/TabNav';
 import TextInput from '@/components/TextInput';
 import useApi, { RequestError } from '@/hooks/use-api';
+import Back from '@/icons/Back';
 import Delete from '@/icons/Delete';
 import More from '@/icons/More';
 import Reset from '@/icons/Reset';
@@ -101,8 +102,12 @@ const UserDetails = () => {
 
   return (
     <div className={detailsStyles.container}>
-      <BackLink to="/users">{t('user_details.back_to_users')}</BackLink>
-
+      <LinkButton
+        to="/users"
+        icon={<Back />}
+        title="admin_console.user_details.back_to_users"
+        className={styles.backLink}
+      />
       {isLoading && <div>loading</div>}
       {error && <div>{`error occurred: ${error.body.message}`}</div>}
       {id && data && (
