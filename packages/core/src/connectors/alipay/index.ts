6a15f159f04639a616359503510b6a974f082dca
Darcy Ye <darcyye@silverhand.io>
2022-04-12 17:38:38 +0800

    feat(core): fix

--- packages/core/src/connectors/alipay/index.ts
+++ packages/core/src/connectors/alipay/index.ts
@@ -38,8 +38,12 @@ import {
 } from './constant';
 
 // eslint-disable-next-line unicorn/prefer-module
-const pathToReadmeFile = path.join(__dirname, 'README.md');
+const currentPath = __dirname;
+const pathToReadmeFile = path.join(currentPath, 'README.md');
+const pathToConfigTemplate = path.join(currentPath, 'config-template.md');
 const readmeContentFallback = 'Please check README.md file directory.';
+const configTemplateFallback = 'Please check config-template.md file directory.';
+
 export const metadata: ConnectorMetadata = {
   id: 'alipay',
   type: ConnectorType.Social,
@@ -56,6 +60,9 @@ export const metadata: ConnectorMetadata = {
   readme: existsSync(pathToReadmeFile)
     ? readFileSync(pathToReadmeFile, 'utf8')
     : readmeContentFallback,
+  configTemplate: existsSync(pathToConfigTemplate)
+    ? readFileSync(pathToConfigTemplate, 'utf-8')
+    : configTemplateFallback,
 };
 
 const alipayConfigGuard = z.object({
