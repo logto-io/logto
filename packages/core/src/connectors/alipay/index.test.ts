b1a6f55796bf5d3f7b2542a124592882a21befc2
Darcy Ye <darcyye@silverhand.io>
2022-04-13 22:38:33 +0800

    feat(core): add UT

--- packages/core/src/connectors/alipay/index.test.ts
+++ packages/core/src/connectors/alipay/index.test.ts
@@ -65,6 +65,16 @@ describe('signingParameters', () => {
     );
   });
 
+  it('should return exact signature with the given parameters (with empty property in testingParameters)', () => {
+    const decamelizedParameters = AlipayMethods.signingPamameters({
+      ...testingParameters,
+      emptyProperty: '',
+    });
+    expect(decamelizedParameters.sign).toBe(
+      'td9+u0puul3HgbwLGL1X6z/vKKB/K25K5pjtLT/snQOp292RX3Y5j+FQUVuazTI2l65GpoSgA83LWNT9htQgtmdBmkCQ3bO6RWs38+2ZmBmH7MvpHx4ebUDhtebLUmHNuRFaNcpAZW92b0ZSuuJuahpLK8VNBgXljq+x0aD7WCRudPxc9fikR65NGxr5bwepl/9IqgMxwtajh1+PEJyhGGJhJxS1dCktGN0EiWXWNiogYT8NlFVCmw7epByKzCBWu4sPflU52gJMFHTdbav/0Tk/ZBs8RyP8Z8kcJA0jom2iT+dHqDpgkdzEmsR360UVNKCu5X7ltIiiObsAWmfluQ=='
+    );
+  });
+
   it('should not call JSON.parse() when biz_content is empty', () => {
     AlipayMethods.signingPamameters(testingParameters);
     expect(listenJSONParse).not.toHaveBeenCalled();
