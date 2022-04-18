b5163ac31f140af9601d4fdf7c8354e3266d0ebe
IceHe.xyz <icehe@silverhand.io>
2022-04-18 15:50:03 +0800

    fix(schemas): remove wrong default values from sign-in experience schema

--- packages/schemas/tables/sign_in_experiences.sql
+++ packages/schemas/tables/sign_in_experiences.sql
@@ -1,9 +1,9 @@
 create table sign_in_experiences (
   id varchar(21) not null,
-  branding jsonb /* @use Branding */ not null default '{}'::jsonb,
-  language_info jsonb /* @use LanguageInfo */ not null default '{}'::jsonb,
-  terms_of_use jsonb /* @use TermsOfUse */ not null default '{}'::jsonb,
-  sign_in_methods jsonb /* @use SignInMethods */ not null default '{}'::jsonb,
+  branding jsonb /* @use Branding */ not null,
+  language_info jsonb /* @use LanguageInfo */ not null,
+  terms_of_use jsonb /* @use TermsOfUse */ not null,
+  sign_in_methods jsonb /* @use SignInMethods */ not null,
   social_sign_in_connector_ids jsonb /* @use ConnectorIds */ not null default '[]'::jsonb,
   primary key (id)
 );
