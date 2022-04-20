f4a656300e5c7f2f3719126a5b810cf4cc00c958
Gao Sun <gao@silverhand.io>
2022-04-20 14:19:38 +0800

    fix: rebase error

--- packages/core/src/queries/log.ts
+++ packages/core/src/queries/log.ts
@@ -1,6 +1,5 @@
 import { CreateLog, Logs } from '@logto/schemas';
 
 import { buildInsertInto } from '@/database/insert-into';
-import pool from '@/database/pool';
 
-export const insertLog = buildInsertInto<CreateLog>(pool, Logs);
+export const insertLog = buildInsertInto<CreateLog>(Logs);
