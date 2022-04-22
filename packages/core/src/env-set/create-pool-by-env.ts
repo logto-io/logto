4481c2b351ad9673ef09cc84468c65cdabb3b8ef
Gao Sun <gao@silverhand.io>
2022-04-22 18:24:50 +0800

    refactor(core): add `--no-inquiry` option

--- packages/core/src/env-set/create-pool-by-env.ts
+++ packages/core/src/env-set/create-pool-by-env.ts
@@ -4,6 +4,7 @@ import { createPool } from 'slonik';
 import { createInterceptors } from 'slonik-interceptor-preset';
 
 import appendDotEnv from './append-dot-env';
+import { noInquiry } from './parameters';
 
 const createPoolByEnv = async (isTest: boolean) => {
   // Database connection is disabled in unit test environment
@@ -19,6 +20,10 @@ const createPoolByEnv = async (isTest: boolean) => {
 
     return createPool(databaseDsn, { interceptors });
   } catch (error: unknown) {
+    if (noInquiry) {
+      throw error;
+    }
+
     const answer = await inquirer.prompt({
       name: 'dsn',
       message: `No Postgres DSN (${key}) found in env variables. Please input the DSN which points to Logto database:`,
---
+++ packages/core/src/env-set/parameters.ts
@@ -0,0 +1,2 @@
+const parameters = process.argv.slice(2);
+export const noInquiry = parameters.includes('--no-inquiry');
