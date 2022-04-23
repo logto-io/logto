2b2a15492dcae04338169155a423392ae8083b01
Darcy Ye <darcyye@silverhand.io>
2022-04-23 12:58:55 +0800

    feat(core): fix

--- packages/connector-aliyun-dm/jest.config.ts
+++ packages/connector-aliyun-dm/jest.config.ts
@@ -1,17 +1,20 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^jose/(.*)$': '<rootDir>/node_modules/jose/dist/node/cjs/$1',
+  },
   setupFilesAfterEnv: ['jest-matcher-specific-error'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/build/'],
+  coverageReporters: ['text-summary', 'lcov'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
     },
   },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^jose/(.*)$': '<rootDir>/node_modules/jose/dist/node/cjs/$1',
-  },
-  coveragePathIgnorePatterns: ['/node_modules/', '/build/'],
-  coverageReporters: ['text-summary', 'lcov'],
 };
+
+export default config;
