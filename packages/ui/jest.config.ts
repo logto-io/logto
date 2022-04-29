b3bfd694b9327dfda40447edbc51523d0afbc0c3
simeng-li <simeng@silverhand.io>
2022-04-29 16:06:41 +0800

    fix(ui):  fix redundent code
    fix redundent code

--- packages/ui/jest.config.ts
+++ packages/ui/jest.config.ts
@@ -3,9 +3,6 @@ import { merge, Config } from '@logto/jest-config';
 const config: Config.InitialOptions = merge({
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
-  transformIgnorePatterns: [
-    '[/\\\\]node_modules[/\\\\]((?!ky[/\\\\]).)+\\.(js|jsx|mjs|cjs|ts|tsx)$',
-  ],
 });
 
 export default config;
