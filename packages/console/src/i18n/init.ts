048290b49f5f4c08882b1c51a289d31b7f18b590
Charles Zhao <charleszhao@silverhand.io>
2022-04-30 09:36:51 +0800

    feat(console): integrate admin console language settings

--- packages/console/src/i18n/init.ts
+++ packages/console/src/i18n/init.ts
@@ -1,9 +1,9 @@
-import resources from '@logto/phrases';
+import resources, { Language } from '@logto/phrases';
 import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-const initI18n = async () =>
+const initI18n = async (language?: Language) =>
   i18next
     .use(initReactI18next)
     .use(LanguageDetector)
@@ -13,6 +13,7 @@ const initI18n = async () =>
       interpolation: {
         escapeValue: false,
       },
+      lng: language,
     });
 
 export default initI18n;
