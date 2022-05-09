9568bc9269ea741b47d11aadcaf57d8e5c68a108
Gao Sun <gao@silverhand.io>
2022-05-09 15:35:53 +0800

    docs: use docs only mode

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -28,11 +28,13 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           breadcrumbs: false,
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
