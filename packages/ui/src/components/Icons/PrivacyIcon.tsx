d87ee24ed26876f437fe6a801fd0017a59311c77
simeng-li <simeng@silverhand.io>
2022-05-15 12:40:14 +0800

    fix(ui): fix svg render blinking issue
    fix svg render blinking issue

--- packages/ui/src/components/Icons/PrivacyIcon.tsx
+++ packages/ui/src/components/Icons/PrivacyIcon.tsx
@@ -1,6 +1,9 @@
+import classNames from 'classnames';
 import React, { SVGProps } from 'react';
 
 import Icon from '@/assets/icons/privacy-icon.svg';
+
+import styles from './PrivacyIcon.module.scss';
 
 type Props = {
   type?: 'show' | 'hide';
@@ -8,9 +11,14 @@ type Props = {
 
 const PrivacyIcon = ({ type = 'show', ...rest }: Props) => {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...rest}>
-      <use href={`${Icon}#${type}`} />
-    </svg>
+    <span className={classNames(styles[type], styles.privacyIcon)}>
+      <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...rest}>
+        <use href={`${Icon}#show`} />
+      </svg>
+      <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...rest}>
+        <use href={`${Icon}#hide`} />
+      </svg>
+    </span>
   );
 };
 
