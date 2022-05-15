cbe2497504322c81603317cbf2cc14f9ea45e103
Charles Zhao <charleszhao@silverhand.io>
2022-05-15 20:38:52 +0800

    fix(console): long text should wrap in code editor

--- packages/console/src/components/CodeEditor/index.tsx
+++ packages/console/src/components/CodeEditor/index.tsx
@@ -22,6 +22,8 @@ const CodeEditor = ({ language, isReadonly = false, value = '', onChange }: Prop
     <div className={styles.container}>
       <CopyToClipboard value={value} variant="icon" className={styles.copy} />
       <div className={styles.editor}>
+        {/* SyntaxHighlighter is a readonly component, so a transparent <textarea> layer is needed
+        in order to support user interactions, such as code editing, copy-pasting, etc. */}
         <textarea
           autoCapitalize="off"
           autoComplete="off"
@@ -32,13 +34,24 @@ const CodeEditor = ({ language, isReadonly = false, value = '', onChange }: Prop
           value={value}
           onChange={handleChange}
         />
+        {/* SyntaxHighlighter will generate a <pre> tag and a inner <code> tag. Both have
+        inline-styles by default. Therefore, We can only use inline styles to customize them.
+        Some styles have to be applied multiple times to each of them for the sake of consistency. */}
         <SyntaxHighlighter
+          wrapLongLines
+          codeTagProps={{
+            style: {
+              fontFamily: "'Roboto Mono', monospace", // Override default font-family of <code>
+            },
+          }}
           customStyle={{
             background: 'transparent',
             fontSize: '14px',
             margin: '0',
             padding: '0',
             borderRadius: '0',
+            wordBreak: 'break-all',
+            fontFamily: "'Roboto Mono', monospace", // Override default font-family of <pre>
           }}
           language={language}
           style={theme}
