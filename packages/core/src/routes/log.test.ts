4822644e284b86088416d79d6c28d12683ea3d27
IceHe.xyz <icehe@silverhand.io>
2022-05-16 19:09:50 +0800

    test(core): get /dashboard with DNU counts

--- packages/core/src/routes/log.test.ts
+++ packages/core/src/routes/log.test.ts
@@ -4,6 +4,11 @@ import { createRequester } from '@/utils/test-utils';
 
 const mockLogs = [{ id: 1 }, { id: 2 }];
 
+const mockDnuCounts = [
+  { date: '2022-05-01', count: 5 },
+  { date: '2022-05-02', count: 10 },
+];
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 const countLogs = jest.fn(async (condition: LogCondition) => ({
   count: mockLogs.length,
@@ -11,12 +16,17 @@ const countLogs = jest.fn(async (condition: LogCondition) => ({
 const findLogs = jest.fn(
   async (limit: number, offset: number, condition: LogCondition) => mockLogs
 );
+const getDnuCountsByTimeInterval = jest.fn(
+  async (startTimeInclusive: number, endTimeExclusive: number) => mockDnuCounts
+);
 /* eslint-enable @typescript-eslint/no-unused-vars */
 
 jest.mock('@/queries/log', () => ({
   countLogs: async (condition: LogCondition) => countLogs(condition),
   findLogs: async (limit: number, offset: number, condition: LogCondition) =>
     findLogs(limit, offset, condition),
+  getDnuCountsByTimeInterval: async (startTimeInclusive: number, endTimeExclusive: number) =>
+    getDnuCountsByTimeInterval(startTimeInclusive, endTimeExclusive),
 }));
 
 describe('logRoutes', () => {
@@ -46,6 +56,30 @@ describe('logRoutes', () => {
       expect(response.status).toEqual(200);
       expect(response.body).toEqual(mockLogs);
       expect(response.header).toHaveProperty('total-number', `${mockLogs.length}`);
+    });
+  });
+
+  describe('GET /dashboard', () => {
+    it('should fail when start date is earlier than end date', async () => {
+      const start = '2022-05-01';
+      const end = '2022-04-30';
+      const response = await logRequest.get(`/dashboard?start=${start}&end=${end}`);
+      expect(response.status).toEqual(400);
+    });
+
+    it('should call getDnuCountsByTimeInterval with correct parameters', async () => {
+      const start = '2022-05-01';
+      const end = '2022-05-02';
+      await logRequest.get(`/dashboard?start=${start}&end=${end}`);
+      expect(getDnuCountsByTimeInterval).toHaveBeenCalledWith(1_651_334_400_000, 1_651_507_200_000);
+    });
+
+    it('should return correct response', async () => {
+      const start = '2022-05-01';
+      const end = '2022-05-02';
+      const response = await logRequest.get(`/dashboard?start=${start}&end=${end}`);
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual({ dnuCounts: mockDnuCounts, dauCounts: {} });
     });
   });
 });
