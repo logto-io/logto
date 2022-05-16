e2217584a40098d6bfcd6a745e8e0d982e8936c0
simeng-li <simeng@silverhand.io>
2022-05-16 13:47:09 +0800

    fix(ui): fix create account page reload issue (#832)
    fix create account page reload issue

--- packages/ui/src/components/TextLink/index.tsx
+++ packages/ui/src/components/TextLink/index.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import React, { ReactNode, AnchorHTMLAttributes } from 'react';
 import { TFuncKey, useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 import * as styles from './index.module.scss';
 
@@ -9,10 +10,19 @@ export type Props = AnchorHTMLAttributes<HTMLAnchorElement> & {
   children?: ReactNode;
   text?: TFuncKey<'translation', 'main_flow'>;
   type?: 'primary' | 'secondary';
+  to?: string;
 };
 
-const TextLink = ({ className, children, text, type = 'primary', ...rest }: Props) => {
+const TextLink = ({ className, children, text, type = 'primary', to, ...rest }: Props) => {
   const { t } = useTranslation(undefined, { keyPrefix: 'main_flow' });
+
+  if (to) {
+    return (
+      <Link className={classNames(styles.link, styles[type], className)} to={to}>
+        {children ?? (text ? t(text) : '')}
+      </Link>
+    );
+  }
 
   return (
     <a className={classNames(styles.link, styles[type], className)} {...rest} rel="noreferrer">
