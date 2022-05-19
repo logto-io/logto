446abe4945db71a71c3c3fcfeabf01a4ab3c720c
Xiao Yijun <xiaoyijun@silverhand.io>
2022-05-19 11:08:30 +0800

    feat(core): disable introspection feature

--- packages/core/src/oidc/init.ts
+++ packages/core/src/oidc/init.ts
@@ -41,7 +41,6 @@ export default async function initOidc(app: Koa): Promise<Provider> {
     features: {
       userinfo: { enabled: true },
       revocation: { enabled: true },
-      introspection: { enabled: true },
       devInteractions: { enabled: false },
       resourceIndicators: {
         enabled: true,
