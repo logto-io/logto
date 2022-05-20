11d9488f4b5a2cbbc543809b01fbf03e10c63925
Gao Sun <gao@silverhand.io>
2022-05-20 10:23:13 +0800

    refactor(core): update management API indicator (#906)

--- packages/schemas/src/seeds/resource.ts
+++ packages/schemas/src/seeds/resource.ts
@@ -2,6 +2,6 @@ import { CreateResource } from '../db-entries';
 
 export const managementResource: Readonly<CreateResource> = Object.freeze({
   id: 'management-api',
-  indicator: 'https://logto.io/api',
+  indicator: 'https://api.logto.io',
   name: 'Logto Management API',
 });
