077ed120f09cdfdb81e95cbb434488569f87bfd1
Wang Sijie <wangsijie@silverhand.io>
2022-05-25 15:26:40 +0800

    fix(core): prevent session lost for bind social (#948)

--- packages/core/src/lib/session.ts
+++ packages/core/src/lib/session.ts
@@ -9,9 +9,24 @@ export const assignInteractionResults = async (
   result: InteractionResults,
   merge = false
 ) => {
-  const redirectTo = await provider.interactionResult(ctx.req, ctx.res, result, {
-    mergeWithLastSubmission: merge,
-  });
+  // The "mergeWithLastSubmission" will only merge current request's interfaction results,
+  // which is stored in ctx.oidc, we need to merge interaction results in two requests,
+  // have to do it manually
+  // refer to: https://github.com/panva/node-oidc-provider/blob/c243bf6b6663c41ff3e75c09b95fb978eba87381/lib/actions/authorization/interactions.js#L106
+  const details = merge ? await provider.interactionDetails(ctx.req, ctx.res) : undefined;
+
+  const redirectTo = await provider.interactionResult(
+    ctx.req,
+    ctx.res,
+    {
+      // Merge with current result
+      ...details?.result,
+      ...result,
+    },
+    {
+      mergeWithLastSubmission: merge,
+    }
+  );
   ctx.body = { redirectTo };
 };
 
