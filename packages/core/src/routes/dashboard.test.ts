65cff17658b12aaf2ec8855e56aa7b869b694990
IceHe.xyz <icehe@silverhand.io>
2022-05-25 15:02:34 +0800

    test(core): get /dashboard/users/total

--- packages/core/src/routes/dashboard.test.ts
+++ packages/core/src/routes/dashboard.test.ts
@@ -2,17 +2,29 @@ import dashboardRoutes from '@/routes/dashboard';
 import { createRequester } from '@/utils/test-utils';
 
 const totalUserCount = 1000;
+const countUsers = jest.fn(async () => ({ count: totalUserCount }));
 
 jest.mock('@/queries/user', () => ({
-  countUsers: async () => ({ count: totalUserCount }),
+  countUsers: async () => countUsers(),
 }));
 
 describe('dashboardRoutes', () => {
   const logRequest = createRequester({ authedRoutes: dashboardRoutes });
 
-  test('/dashboard/users/total should return correct response', async () => {
-    const response = await logRequest.get('/dashboard/users/total');
-    expect(response.status).toEqual(200);
-    expect(response.body).toEqual({ totalUserCount });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('GET /dashboard/users/total', () => {
+    it('should call countUsers with no parameters', async () => {
+      await logRequest.get('/dashboard/users/total');
+      expect(countUsers).toHaveBeenCalledWith();
+    });
+
+    it('/dashboard/users/total should return correct response', async () => {
+      const response = await logRequest.get('/dashboard/users/total');
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual({ totalUserCount });
+    });
   });
 });
