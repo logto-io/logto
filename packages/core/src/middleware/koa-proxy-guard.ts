95a9fd7622931a4e037d5746e2cfa1c34dd8a80e
simeng-li <simeng@silverhand.io>
2022-05-26 16:29:42 +0800

    fix(core): update proxy guard middleware
    update proxy guard middleware

--- packages/core/src/middleware/koa-proxy-guard.ts
+++ packages/core/src/middleware/koa-proxy-guard.ts
@@ -1,14 +1,12 @@
-import fs from 'fs/promises';
-import path from 'path';
-
 import { MiddlewareType } from 'koa';
 import { IRouterParamContext } from 'koa-router';
 import { Provider } from 'oidc-provider';
 
 import { MountedApps } from '@/env-set';
-import { fromRoot } from '@/env-set/parameters';
 
+// Need To Align With UI
 export const sessionNotFoundPath = '/unknown-session';
+export const guardedPath = ['/sign-in', '/register', '/social-register'];
 
 export default function koaSpaSessionGuard<
   StateT,
@@ -17,8 +15,6 @@ export default function koaSpaSessionGuard<
 >(provider: Provider): MiddlewareType<StateT, ContextT, ResponseBodyT> {
   return async (ctx, next) => {
     const requestPath = ctx.request.path;
-    const packagesPath = fromRoot ? 'packages/' : '..';
-    const clientPath = path.join(packagesPath, 'ui', 'dist');
 
     // Empty path Redirect
     if (requestPath === '/') {
@@ -27,22 +23,11 @@ export default function koaSpaSessionGuard<
       return next();
     }
 
-    // Check client routes session status only
-    if (Object.values(MountedApps).some((app) => requestPath.startsWith(`/${app}`))) {
-      return next();
-    }
-
-    // Client session guard
-    try {
-      await provider.interactionDetails(ctx.req, ctx.res);
-    } catch {
-      const spaDistFiles = await fs.readdir(clientPath);
-
-      if (
-        !spaDistFiles.some((file) => requestPath.startsWith('/' + file)) &&
-        !ctx.request.path.endsWith(sessionNotFoundPath) &&
-        !ctx.request.URL.searchParams.get('preview') // Should not check session on preview mode
-      ) {
+    // Session guard
+    if (guardedPath.some((path) => requestPath.startsWith(path))) {
+      try {
+        await provider.interactionDetails(ctx.req, ctx.res);
+      } catch {
         ctx.redirect(sessionNotFoundPath);
       }
     }
