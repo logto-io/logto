86b4a405e6dca50b0c4e70c8bc7c04420f003afc
Xiao Yijun <xiaoyijun@silverhand.io>
2022-05-26 12:30:58 +0800

    feat(console): update connector icons

--- packages/console/src/pages/Connectors/components/ConnectorName/index.tsx
+++ packages/console/src/pages/Connectors/components/ConnectorName/index.tsx
@@ -1,12 +1,11 @@
 import { ConnectorDTO, ConnectorType } from '@logto/schemas';
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-import emailConnectorIcon from '@/assets/images/connector-email.svg';
-import smsConnectorIcon from '@/assets/images/connector-sms.svg';
 import ItemPreview from '@/components/ItemPreview';
 import UnnamedTrans from '@/components/UnnamedTrans';
+import { connectorIconPlaceHolder, connectorTitlePlaceHolder } from '@/consts/connectors';
 
 import * as styles from './index.module.scss';
 
@@ -16,32 +15,15 @@ type Props = {
 };
 
 const ConnectorName = ({ type, connector }: Props) => {
-  const { t } = useTranslation(undefined, { keyPrefix: 'admin_console' });
-
-  const titlePlaceholder = useMemo(() => {
-    if (type === ConnectorType.Email) {
-      return t('connectors.type.email');
-    }
-
-    if (type === ConnectorType.SMS) {
-      return t('connectors.type.sms');
-    }
-
-    return t('connectors.type.social');
-  }, [type, t]);
-
-  const iconPlaceholder = useMemo(() => {
-    if (type === ConnectorType.Email) {
-      return emailConnectorIcon;
-    }
-
-    if (type === ConnectorType.SMS) {
-      return smsConnectorIcon;
-    }
-  }, [type]);
+  const { t } = useTranslation(undefined);
 
   if (!connector) {
-    return <ItemPreview title={titlePlaceholder} icon={<img src={iconPlaceholder} />} />;
+    return (
+      <ItemPreview
+        title={t(connectorTitlePlaceHolder[type])}
+        icon={<img src={connectorIconPlaceHolder[type]} />}
+      />
+    );
   }
 
   return (
