45a977790eca01b212f51047d5636ff882873dd8
IceHe.xyz <icehe@silverhand.io>
2022-05-26 11:24:52 +0800

    feat(core): get /dashboard/users/new (#940)

--- packages/core/src/routes/dashboard.test.ts
+++ packages/core/src/routes/dashboard.test.ts
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 import dashboardRoutes from '@/routes/dashboard';
 import { createRequester } from '@/utils/test-utils';
 
@@ -6,6 +8,31 @@ const countUsers = jest.fn(async () => ({ count: totalUserCount }));
 
 jest.mock('@/queries/user', () => ({
   countUsers: async () => countUsers(),
+}));
+
+const mockDailyNewUserCounts = [
+  { date: '2022-05-01', count: 1 },
+  { date: '2022-05-02', count: 2 },
+  { date: '2022-05-03', count: 3 },
+  { date: '2022-05-06', count: 6 },
+  { date: '2022-05-07', count: 7 },
+  { date: '2022-05-08', count: 8 },
+  { date: '2022-05-09', count: 9 },
+  { date: '2022-05-10', count: 10 },
+  { date: '2022-05-13', count: 13 },
+  { date: '2022-05-14', count: 14 },
+];
+
+const getDailyNewUserCountsByTimeInterval = jest.fn(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async (startTimeExclusive: number, endTimeInclusive: number) => mockDailyNewUserCounts
+);
+
+jest.mock('@/queries/log', () => ({
+  getDailyNewUserCountsByTimeInterval: async (
+    startTimeExclusive: number,
+    endTimeInclusive: number
+  ) => getDailyNewUserCountsByTimeInterval(startTimeExclusive, endTimeInclusive),
 }));
 
 describe('dashboardRoutes', () => {
@@ -25,6 +52,35 @@ describe('dashboardRoutes', () => {
       const response = await logRequest.get('/dashboard/users/total');
       expect(response.status).toEqual(200);
       expect(response.body).toEqual({ totalUserCount });
+    });
+  });
+
+  describe('GET /dashboard/users/new', () => {
+    beforeEach(() => {
+      jest.useFakeTimers().setSystemTime(new Date('2022-05-14'));
+    });
+
+    it('should call getDailyNewUserCountsByTimeInterval with the time interval (14 days ago 23:59:59.999, today 23:59:59.999]', async () => {
+      await logRequest.get('/dashboard/users/new');
+      expect(getDailyNewUserCountsByTimeInterval).toHaveBeenCalledWith(
+        dayjs().endOf('day').subtract(14, 'day').valueOf(),
+        dayjs().endOf('day').valueOf()
+      );
+    });
+
+    it('should return correct response', async () => {
+      const response = await logRequest.get('/dashboard/users/new');
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual({
+        today: {
+          count: 14,
+          delta: 1,
+        },
+        last7Days: {
+          count: 54,
+          delta: 35,
+        },
+      });
     });
   });
 });
--- packages/core/src/routes/dashboard.ts
+++ packages/core/src/routes/dashboard.ts
@@ -1,11 +1,58 @@
+import dayjs, { Dayjs } from 'dayjs';
+
+import { getDailyNewUserCountsByTimeInterval } from '@/queries/log';
 import { countUsers } from '@/queries/user';
 
 import { AuthedRouter } from './types';
+
+const getDateString = (day: Dayjs) => day.format('YYYY-MM-DD');
+
+const indicesFrom0To6 = [...Array.from({ length: 7 }).keys()];
 
 export default function dashboardRoutes<T extends AuthedRouter>(router: T) {
   router.get('/dashboard/users/total', async (ctx, next) => {
     const { count: totalUserCount } = await countUsers();
     ctx.body = { totalUserCount };
+
+    return next();
+  });
+
+  router.get('/dashboard/users/new', async (ctx, next) => {
+    const today = dayjs();
+    const fourteenDaysAgo = today.subtract(14, 'day');
+    const dailyNewUserCounts = await getDailyNewUserCountsByTimeInterval(
+      // Time interval: (14 days ago 23:59:59.999, today 23:59:59.999]
+      fourteenDaysAgo.endOf('day').valueOf(),
+      today.endOf('day').valueOf()
+    );
+
+    const last14DaysNewUserCounts = new Map(
+      dailyNewUserCounts.map(({ date, count }) => [date, count])
+    );
+
+    const todayNewUserCount = last14DaysNewUserCounts.get(getDateString(today)) ?? 0;
+    const yesterday = today.subtract(1, 'day');
+    const yesterdayNewUserCount = last14DaysNewUserCounts.get(getDateString(yesterday)) ?? 0;
+    const todayDelta = todayNewUserCount - yesterdayNewUserCount;
+
+    const last7DaysNewUserCount = indicesFrom0To6
+      .map((index) => getDateString(today.subtract(index, 'day')))
+      .reduce((sum, date) => sum + (last14DaysNewUserCounts.get(date) ?? 0), 0);
+    const newUserCountFrom13DaysAgoTo7DaysAgo = indicesFrom0To6
+      .map((index) => getDateString(today.subtract(7 + index, 'day')))
+      .reduce((sum, date) => sum + (last14DaysNewUserCounts.get(date) ?? 0), 0);
+    const last7DaysDelta = last7DaysNewUserCount - newUserCountFrom13DaysAgoTo7DaysAgo;
+
+    ctx.body = {
+      today: {
+        count: todayNewUserCount,
+        delta: todayDelta,
+      },
+      last7Days: {
+        count: last7DaysNewUserCount,
+        delta: last7DaysDelta,
+      },
+    };
 
     return next();
   });
