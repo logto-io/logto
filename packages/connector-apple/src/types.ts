52c745d41f8438b2a1f0ad6fb86f3a85b0c13fa3
Gao Sun <gao@silverhand.io>
2022-05-27 19:06:06 +0800

    refactor(connector): remove apple js sdk

--- packages/connector-apple/src/types.ts
+++ packages/connector-apple/src/types.ts
@@ -8,19 +8,7 @@ export type AppleConfig = z.infer<typeof appleConfigGuard>;
 
 // https://developer.apple.com/documentation/sign_in_with_apple/sign_in_with_apple_js/configuring_your_webpage_for_sign_in_with_apple#3331292
 export const appleDataGuard = z.object({
-  authorization: z.object({
-    id_token: z.string(),
-  }),
-  user: z
-    .object({
-      name: z
-        .object({
-          firstName: z.string().optional(),
-          lastName: z.string().optional(),
-        })
-        .optional(),
-    })
-    .optional(),
+  id_token: z.string(),
 });
 
 export type AppleData = z.infer<typeof appleDataGuard>;
