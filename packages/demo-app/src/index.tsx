ad0aa8e0c20a8d60f095b477e942b724fb53ca7d
Gao Sun <gao@silverhand.io>
2022-05-29 21:11:58 +0800

    feat(demo-app): init (#979)

---
+++ packages/demo-app/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// eslint-disable-next-line import/no-unassigned-import
+import '@logto/shared/declaration';
+
+import App from './App';
+
+const app = document.querySelector('#app');
+ReactDOM.render(<App />, app);
---
+++ packages/shared/declaration/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-unassigned-import */
+import './react-app.d';
+import './dom.d';
+/* eslint-enable import/no-unassigned-import */
