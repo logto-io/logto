1420bb28cec9c0e20b4d0645a58e436135f87c83
IceHe.xyz <icehe@silverhand.io>
2022-05-30 13:51:58 +0800

    feat(core,shared): get /dashboard/users/active (#953)
    * feat(core,shared): get /dashboard/users/active
    
    * refactor(core): get /dashboard/users/active
    
    * refactor(core): get /dashboard/users/active
    
    * refactor(core): simplify GET /dashboard/users/active
    
    * refactor(core): simplify dashboardRoutes

--- packages/core/src/routes/dashboard.ts
+++ packages/core/src/routes/dashboard.ts
@@ -1,11 +1,20 @@
+import { dateRegex } from '@logto/shared';
 import dayjs, { Dayjs } from 'dayjs';
+import { object, string } from 'zod';
 
-import { getDailyNewUserCountsByTimeInterval } from '@/queries/log';
+import koaGuard from '@/middleware/koa-guard';
+import {
+  countActiveUsersByTimeInterval,
+  getDailyActiveUserCountsByTimeInterval,
+  getDailyNewUserCountsByTimeInterval,
+} from '@/queries/log';
 import { countUsers } from '@/queries/user';
 
 import { AuthedRouter } from './types';
 
 const getDateString = (day: Dayjs) => day.format('YYYY-MM-DD');
+
+const lastTimestampOfDay = (day: Dayjs) => day.endOf('day').valueOf();
 
 const indicesFrom0To6 = [...Array.from({ length: 7 }).keys()];
 
@@ -19,11 +28,10 @@ export default function dashboardRoutes<T extends AuthedRouter>(router: T) {
 
   router.get('/dashboard/users/new', async (ctx, next) => {
     const today = dayjs();
-    const fourteenDaysAgo = today.subtract(14, 'day');
     const dailyNewUserCounts = await getDailyNewUserCountsByTimeInterval(
-      // Time interval: (14 days ago 23:59:59.999, today 23:59:59.999]
-      fourteenDaysAgo.endOf('day').valueOf(),
-      today.endOf('day').valueOf()
+      // (14 days ago 23:59:59.999, today 23:59:59.999]
+      lastTimestampOfDay(today.subtract(14, 'day')),
+      lastTimestampOfDay(today)
     );
 
     const last14DaysNewUserCounts = new Map(
@@ -56,4 +64,78 @@ export default function dashboardRoutes<T extends AuthedRouter>(router: T) {
 
     return next();
   });
+
+  router.get(
+    '/dashboard/users/active',
+    koaGuard({
+      query: object({ date: string().regex(dateRegex).optional() }),
+    }),
+    async (ctx, next) => {
+      const {
+        query: { date },
+      } = ctx.guard;
+
+      const targetDay = date ? dayjs(date) : dayjs(); // Defaults to today
+      const [
+        // DAU: Daily Active User
+        last30DauCounts,
+        // WAU: Weekly Active User
+        { count: previousWAU },
+        { count: wau },
+        // MAU: Monthly Active User
+        { count: previousMAU },
+        { count: mau },
+      ] = await Promise.all([
+        getDailyActiveUserCountsByTimeInterval(
+          // (30 days ago 23:59:59.999, target day 23:59:59.999]
+          lastTimestampOfDay(targetDay.subtract(30, 'day')),
+          lastTimestampOfDay(targetDay)
+        ),
+        countActiveUsersByTimeInterval(
+          // (14 days ago 23:59:59.999, 7 days ago 23:59:59.999]
+          lastTimestampOfDay(targetDay.subtract(14, 'day')),
+          lastTimestampOfDay(targetDay.subtract(7, 'day'))
+        ),
+        countActiveUsersByTimeInterval(
+          // (7 days ago 23:59:59.999, target day 23:59:59.999]
+          lastTimestampOfDay(targetDay.subtract(7, 'day')),
+          lastTimestampOfDay(targetDay)
+        ),
+        countActiveUsersByTimeInterval(
+          // (60 days ago 23:59:59.999, 30 days ago 23:59:59.999]
+          lastTimestampOfDay(targetDay.subtract(60, 'day')),
+          lastTimestampOfDay(targetDay.subtract(30, 'day'))
+        ),
+        countActiveUsersByTimeInterval(
+          // (30 days ago 23:59:59.999, target day 23:59:59.999]
+          lastTimestampOfDay(targetDay.subtract(30, 'day')),
+          lastTimestampOfDay(targetDay)
+        ),
+      ]);
+
+      const previousDate = getDateString(targetDay.subtract(1, 'day'));
+      const targetDate = getDateString(targetDay);
+
+      const previousDAU = last30DauCounts.find(({ date }) => date === previousDate)?.count ?? 0;
+      const dau = last30DauCounts.find(({ date }) => date === targetDate)?.count ?? 0;
+
+      ctx.body = {
+        dauCurve: last30DauCounts,
+        dau: {
+          count: dau,
+          delta: dau - previousDAU,
+        },
+        wau: {
+          count: wau,
+          delta: wau - previousWAU,
+        },
+        mau: {
+          count: mau,
+          delta: mau - previousMAU,
+        },
+      };
+
+      return next();
+    }
+  );
 }
