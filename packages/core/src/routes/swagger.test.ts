f0398af55e2601202f947052435141448ba28313
IceHe.xyz <icehe@silverhand.io>
2022-06-01 19:41:13 +0800

    test(core): swaggerRoutes

--- packages/core/src/routes/swagger.test.ts
+++ packages/core/src/routes/swagger.test.ts
@@ -7,22 +7,55 @@ import { AnonymousRouter, AuthedRouter } from '@/routes/types';
 import swaggerRoutes from './swagger';
 
 describe('swagger api', () => {
+  const authedRouter: AuthedRouter = new Router();
+  authedRouter.get('/mock', () => ({}));
+
   const anonymousRouter: AnonymousRouter = new Router();
-  const anotherRouter: AuthedRouter = new Router();
-  swaggerRoutes(anonymousRouter, [anotherRouter]);
+  swaggerRoutes(anonymousRouter, [authedRouter]);
 
   const app = new Koa();
   app.use(anonymousRouter.routes()).use(anonymousRouter.allowedMethods());
 
   const swaggerRequest = request(app.callback());
 
-  it('GET /swagger.json', async () => {
-    const response = await swaggerRequest.get('/swagger.json');
-    expect(response.status).toEqual(200);
-    expect(response.body).toHaveProperty('openapi');
-    expect(response.body).toHaveProperty('info');
+  describe('GET /swagger.json', () => {
+    it('should contain standard fields', async () => {
+      const response = await swaggerRequest.get('/swagger.json');
+      expect(response.status).toEqual(200);
+      expect(response.body).toMatchObject(
+        expect.objectContaining({
+          /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+          openapi: expect.any(String),
+          info: expect.objectContaining({
+            title: expect.any(String),
+            version: expect.any(String),
+          }),
+          paths: expect.any(Object),
+          /* eslint-enable @typescript-eslint/no-unsafe-assignment */
+        })
+      );
+    });
 
-    // TODO: find a better way to test the paths details should contain api list
-    expect(response.body).toHaveProperty('paths');
+    it('should contain specific paths', async () => {
+      const response = await swaggerRequest.get('/swagger.json');
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const { paths } = response.body;
+
+      expect(Object.entries(paths)).toHaveLength(2);
+      expect(paths).toMatchObject(
+        expect.objectContaining({
+          /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+          '/api/mock': expect.objectContaining({
+            head: expect.anything(),
+            get: expect.anything(),
+          }),
+          '/api/swagger.json': expect.objectContaining({
+            head: expect.anything(),
+            get: expect.anything(),
+          }),
+          /* eslint-enable @typescript-eslint/no-unsafe-assignment */
+        })
+      );
+    });
   });
 });
