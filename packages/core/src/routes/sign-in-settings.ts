00cfbab1d902b34ef93857944f0fa957adf3a1ea
Gao Sun <gao@silverhand.io>
2022-06-01 14:01:52 +0800

    feat(core): add etag for settings api

--- packages/core/src/routes/sign-in-settings.ts
+++ packages/core/src/routes/sign-in-settings.ts
@@ -1,4 +1,5 @@
 import { ConnectorMetadata } from '@logto/connector-types';
+import etag from 'etag';
 
 import { getConnectorInstances } from '@/connectors';
 import { findDefaultSignInExperience } from '@/queries/sign-in-experience';
@@ -6,25 +7,39 @@ import { findDefaultSignInExperience } from '@/queries/sign-in-experience';
 import { AnonymousRouter } from './types';
 
 export default function signInSettingsRoutes<T extends AnonymousRouter>(router: T) {
-  router.get('/sign-in-settings', async (ctx, next) => {
-    const [signInExperience, connectorInstances] = await Promise.all([
-      findDefaultSignInExperience(),
-      getConnectorInstances(),
-    ]);
-    const socialConnectors = signInExperience.socialSignInConnectorTargets.reduce<
-      Array<ConnectorMetadata & { id: string }>
-    >((previous, connectorTarget) => {
-      const connectors = connectorInstances.filter(
-        ({ metadata: { target }, connector: { enabled } }) => target === connectorTarget && enabled
-      );
+  router.get(
+    '/sign-in-settings',
+    async (ctx, next) => {
+      const [signInExperience, connectorInstances] = await Promise.all([
+        findDefaultSignInExperience(),
+        getConnectorInstances(),
+      ]);
+      const socialConnectors = signInExperience.socialSignInConnectorTargets.reduce<
+        Array<ConnectorMetadata & { id: string }>
+      >((previous, connectorTarget) => {
+        const connectors = connectorInstances.filter(
+          ({ metadata: { target }, connector: { enabled } }) =>
+            target === connectorTarget && enabled
+        );
 
-      return [
-        ...previous,
-        ...connectors.map(({ metadata, connector: { id } }) => ({ ...metadata, id })),
-      ];
-    }, []);
-    ctx.body = { ...signInExperience, socialConnectors };
+        return [
+          ...previous,
+          ...connectors.map(({ metadata, connector: { id } }) => ({ ...metadata, id })),
+        ];
+      }, []);
+      ctx.body = { ...signInExperience, socialConnectors };
 
-    return next();
-  });
+      return next();
+    },
+    async (ctx, next) => {
+      await next();
+
+      ctx.response.etag = etag(JSON.stringify(ctx.body));
+
+      if (ctx.fresh) {
+        ctx.status = 304;
+        ctx.body = null;
+      }
+    }
+  );
 }
