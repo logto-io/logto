e7e18bce1452071fe182c274d88cbe5776b94bd1
Gao Sun <gao@silverhand.io>
2022-06-02 12:56:14 +0800

    feat(core): throw 404 when demo app deleted

---
+++ packages/core/src/middleware/koa-check-demo-app.ts
@@ -0,0 +1,22 @@
+import { demoAppApplicationId } from '@logto/schemas/lib/seeds';
+import { MiddlewareType } from 'koa';
+
+import { findApplicationById } from '@/queries/application';
+
+export default function koaCheckDemoApp<StateT, ContextT, ResponseBodyT>(): MiddlewareType<
+  StateT,
+  ContextT,
+  ResponseBodyT
+> {
+  return async (ctx, next) => {
+    try {
+      await findApplicationById(demoAppApplicationId);
+
+      await next();
+
+      return;
+    } catch {
+      ctx.throw(404);
+    }
+  };
+}
