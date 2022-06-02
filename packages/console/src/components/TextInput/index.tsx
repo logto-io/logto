33f65394e59aa1f6d2acd93904586232692d3f16
simeng-li <simeng@silverhand.io>
2022-06-02 10:15:26 +0800

    fix(ac): fix ac text input
    fix ac text input

--- packages/console/src/components/TextInput/index.tsx
+++ packages/console/src/components/TextInput/index.tsx
@@ -1,35 +1,32 @@
 import classNames from 'classnames';
-import React, { forwardRef, HTMLProps, ReactNode } from 'react';
+import React, { forwardRef, HTMLProps, ReactNode, ForwardedRef } from 'react';
 
 import * as styles from './index.module.scss';
 
-// https://github.com/yannickcr/eslint-plugin-react/issues/2856
-/* eslint-disable react/require-default-props */
 type Props = HTMLProps<HTMLInputElement> & {
   hasError?: boolean;
   icon?: ReactNode;
-  disabled?: boolean;
-  readOnly?: boolean;
 };
-/* eslint-enable react/require-default-props */
 
-const TextInput = forwardRef<HTMLInputElement, Props>(
-  ({ hasError = false, icon, disabled, readOnly, ...rest }, reference) => {
-    return (
-      <div
-        className={classNames(
-          styles.container,
-          hasError && styles.error,
-          icon && styles.withIcon,
-          disabled && styles.disabled,
-          readOnly && styles.readOnly
-        )}
-      >
-        {icon && <span className={styles.icon}>{icon}</span>}
-        <input type="text" {...rest} ref={reference} disabled={disabled} readOnly={readOnly} />
-      </div>
-    );
-  }
-);
+const TextInput = (
+  { hasError = false, icon, disabled, className, readOnly, ...rest }: Props,
+  reference: ForwardedRef<HTMLInputElement>
+) => {
+  return (
+    <div
+      className={classNames(
+        styles.container,
+        hasError && styles.error,
+        icon && styles.withIcon,
+        disabled && styles.disabled,
+        readOnly && styles.readOnly,
+        className
+      )}
+    >
+      {icon && <span className={styles.icon}>{icon}</span>}
+      <input type="text" {...rest} ref={reference} disabled={disabled} readOnly={readOnly} />
+    </div>
+  );
+};
 
-export default TextInput;
+export default forwardRef(TextInput);
