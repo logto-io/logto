ff291dfd27b8338703b3e085b5e134dc007e0639
Darcy Ye <darcyye@silverhand.io>
2022-06-02 12:03:11 +0800

    chore(connectors): remove unused jest-matcher-specific-error in connector packages (#1024)

--- packages/connector-twilio-sms/jest.config.ts
+++ packages/connector-twilio-sms/jest.config.ts
@@ -2,7 +2,6 @@ import { Config, merge } from '@silverhand/jest-config';
 
 const config: Config.InitialOptions = merge({
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['jest-matcher-specific-error'],
 });
 
 export default config;
