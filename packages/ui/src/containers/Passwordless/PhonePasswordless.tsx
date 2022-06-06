1cf1c2b1a0e1dc3234d122e41265a211b4b282f0
simeng-li <simeng@silverhand.io>
2022-06-06 15:32:09 +0800

    fix(ui): directly throw account not exist in social bind flow
    directly throw account not exsit in social bind flow

--- packages/ui/src/containers/Passwordless/PhonePasswordless.tsx
+++ packages/ui/src/containers/Passwordless/PhonePasswordless.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { useCallback, useEffect, useState, useMemo } from 'react';
+import React, { useCallback, useEffect, useState, useMemo, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,9 +9,11 @@ import { PhoneInput } from '@/components/Input';
 import TermsOfUse from '@/containers/TermsOfUse';
 import useApi, { ErrorHandlers } from '@/hooks/use-api';
 import useForm from '@/hooks/use-form';
+import { PageContext } from '@/hooks/use-page-context';
 import usePhoneNumber, { countryList } from '@/hooks/use-phone-number';
 import useTerms from '@/hooks/use-terms';
-import { UserFlow } from '@/types';
+import { UserFlow, SearchParameters } from '@/types';
+import { getSearchParameters } from '@/utils';
 
 import PasswordlessConfirmModal from './PasswordlessConfirmModal';
 import * as styles from './index.module.scss';
@@ -28,6 +30,7 @@ type FieldState = {
 const defaultState: FieldState = { phone: '' };
 
 const PhonePasswordless = ({ type, className }: Props) => {
+  const { setToast } = useContext(PageContext);
   const [showPasswordlessConfirmModal, setShowPasswordlessConfirmModal] = useState(false);
   const { t } = useTranslation(undefined, { keyPrefix: 'main_flow' });
   const { phoneNumber, setPhoneNumber, isValidPhoneNumber } = usePhoneNumber();
@@ -38,7 +41,16 @@ const PhonePasswordless = ({ type, className }: Props) => {
 
   const errorHandlers: ErrorHandlers = useMemo(
     () => ({
-      'user.phone_not_exists': () => {
+      'user.phone_not_exists': (error) => {
+        const socialToBind = getSearchParameters(location.search, SearchParameters.bindWithSocial);
+
+        // Directly throw error if user is trying to bind with social
+        if (socialToBind) {
+          setToast(error.message);
+
+          return;
+        }
+
         setShowPasswordlessConfirmModal(true);
       },
       'user.phone_exists_register': () => {
@@ -48,7 +60,7 @@ const PhonePasswordless = ({ type, className }: Props) => {
         setFieldErrors({ phone: 'invalid_phone' });
       },
     }),
-    [setFieldErrors]
+    [setFieldErrors, setToast]
   );
 
   const sendPasscode = getSendPasscodeApi(type, 'sms');
