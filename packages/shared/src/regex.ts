a790248c091e444614652b08b05686e9934cb639
Wang Sijie <wangsijie@silverhand.io>
2022-06-14 11:36:03 +0800

    fix(core): remove name regex (#1109)

--- packages/shared/src/regex.ts
+++ packages/shared/src/regex.ts
@@ -1,7 +1,6 @@
 export const emailRegEx = /^\S+@\S+\.\S+$/;
 export const phoneRegEx = /^\d+$/;
 export const usernameRegEx = /^[A-Z_a-z-][\w-]*$/;
-export const nameRegEx = /^.+$/;
 export const passwordRegEx = /^.{6,}$/;
 export const redirectUriRegEx = /^https?:\/\//;
 export const hexColorRegEx = /^#[\da-f]{3}([\da-f]{3})?$/i;
