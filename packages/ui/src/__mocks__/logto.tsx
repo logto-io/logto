40e448b807b0ee21143eed9b8e6af90677bae7cd
Darcy Ye <darcyye@silverhand.io>
2022-06-22 10:50:09 +0800

    fix(ui): fix

--- packages/ui/src/__mocks__/logto.tsx
+++ packages/ui/src/__mocks__/logto.tsx
@@ -19,6 +19,7 @@ export const socialConnectors = [
     platform: ConnectorPlatform.Web,
     type: ConnectorType.Social,
     logo: 'https://user-images.githubusercontent.com/5717882/156983224-7ea0296b-38fa-419d-9515-67e8a9612e09.png',
+    logo_dark: null,
     name: {
       en: 'Sign in with GitHub',
       'zh-CN': '使用 GitHub 登录',
@@ -36,6 +37,7 @@ export const socialConnectors = [
     platform: ConnectorPlatform.Web,
     type: ConnectorType.Social,
     logo: 'https://user-images.githubusercontent.com/5717882/156983224-7ea0296b-38fa-419d-9515-67e8a9612e09.png',
+    logo_dark: null,
     name: {
       en: 'Sign in with Alipay',
       'zh-CN': '使用 Alipay 登录',
@@ -53,6 +55,7 @@ export const socialConnectors = [
     platform: ConnectorPlatform.Web,
     type: ConnectorType.Social,
     logo: 'https://user-images.githubusercontent.com/5717882/156983224-7ea0296b-38fa-419d-9515-67e8a9612e09.png',
+    logo_dark: null,
     name: {
       en: 'Sign in with WeChat',
       'zh-CN': '使用 WeChat 登录',
@@ -70,6 +73,7 @@ export const socialConnectors = [
     platform: ConnectorPlatform.Web,
     type: ConnectorType.Social,
     logo: 'https://user-images.githubusercontent.com/5717882/156983224-7ea0296b-38fa-419d-9515-67e8a9612e09.png',
+    logo_dark: null,
     name: {
       en: 'Sign in with Google',
       'zh-CN': '使用 Google 登录',
@@ -84,6 +88,7 @@ export const socialConnectors = [
     platform: ConnectorPlatform.Web,
     type: ConnectorType.Social,
     logo: 'https://user-images.githubusercontent.com/5717882/156983224-7ea0296b-38fa-419d-9515-67e8a9612e09.png',
+    logo_dark: null,
     name: {
       en: 'Sign in with Meta',
       'zh-CN': '使用 Meta 登录',
@@ -103,6 +108,7 @@ export const mockSocialConnectorData = {
   platform: ConnectorPlatform.Web,
   type: ConnectorType.Social,
   logo: 'http://logto.dev/logto.png',
+  logo_dark: null,
   name: {
     en: 'Sign in with Xxx',
     'zh-CN': '使用 Xxx 登录',
