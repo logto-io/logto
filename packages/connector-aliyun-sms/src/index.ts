4cd5d9476ba455f233b5ca45339dab50f5d23d68
Darcy Ye <darcyye@silverhand.io>
2022-06-24 13:23:26 +0800

    fix(connector): fix aliyun-sms error handling

--- packages/connector-aliyun-sms/src/index.ts
+++ packages/connector-aliyun-sms/src/index.ts
@@ -8,6 +8,7 @@ import {
   GetConnectorConfig,
 } from '@logto/connector-types';
 import { assert } from '@silverhand/essentials';
+import { HTTPError } from 'got';
 
 import { defaultMetadata } from './constant';
 import { sendSms } from './single-send-text';
@@ -38,32 +39,75 @@ export default class AliyunSmsConnector implements SmsConnector {
       new ConnectorError(ConnectorErrorCodes.TemplateNotFound, `Cannot find template!`)
     );
 
-    const httpResponse = await sendSms(
-      {
-        AccessKeyId: accessKeyId,
-        PhoneNumbers: phone,
-        SignName: signName,
-        TemplateCode: template.templateCode,
-        TemplateParam: JSON.stringify({ code }),
-      },
-      accessKeySecret
-    );
+    try {
+      const httpResponse = await sendSms(
+        {
+          AccessKeyId: accessKeyId,
+          PhoneNumbers: phone,
+          SignName: signName,
+          TemplateCode: template.code,
+          TemplateParam: JSON.stringify({ code }),
+        },
+        accessKeySecret
+      );
 
-    const { body: rawBody } = httpResponse;
-    const result = sendSmsResponseGuard.safeParse(JSON.parse(rawBody));
+      const { body: rawBody } = httpResponse;
+      const result = sendSmsResponseGuard.safeParse(JSON.parse(rawBody));
 
-    if (!result.success) {
-      throw new ConnectorError(ConnectorErrorCodes.InvalidResponse, result.error.message);
+      if (!result.success) {
+        throw new ConnectorError(ConnectorErrorCodes.InvalidResponse, result.error.message);
+      }
+
+      const { Code } = result.data;
+
+      /**
+       * You may wonder why the errorHandler is called twice here. We found that the SendSms API of
+       * the Aliyun SMS service may throw an error directly after receiving the request, or it may
+       * need developers to determine whether the request succeeded by `Code` getting from the response.
+       */
+      this.errorHandler(rawBody, Code);
+
+      return httpResponse;
+    } catch (error: unknown) {
+      if (error instanceof HTTPError) {
+        const {
+          response: { body: rawBody },
+        } = error;
+
+        assert(
+          typeof rawBody === 'string',
+          new ConnectorError(ConnectorErrorCodes.InvalidResponse)
+        );
+
+        this.errorHandler(rawBody);
+      }
+
+      throw error;
     }
-
-    const { Code } = result.data;
-
-    this.errorHandler(Code, rawBody);
-
-    return httpResponse;
   };
 
-  private readonly errorHandler = (code: string, message: string) => {
+  private readonly errorHandler = (message: string, code?: string) => {
+    if (!code) {
+      const result = sendSmsResponseGuard.safeParse(JSON.parse(message));
+
+      if (!result.success) {
+        throw new ConnectorError(ConnectorErrorCodes.InvalidResponse, result.error.message);
+      }
+
+      const { Code } = result.data;
+
+      assert(
+        !Code.includes('InvalidAccessKeyId'),
+        new ConnectorError(ConnectorErrorCodes.InvalidConfig, message)
+      );
+      assert(
+        Code !== 'SignatureDoesNotMatch' && Code !== 'IncompleteSignature',
+        new ConnectorError(ConnectorErrorCodes.InvalidConfig, message)
+      );
+
+      throw new ConnectorError(ConnectorErrorCodes.General, message);
+    }
+
     // See https://help.aliyun.com/document_detail/101346.htm?spm=a2c4g.11186623.0.0.29d710f5TUxolJ
     assert(
       !(code === 'isv.ACCOUNT_NOT_EXISTS' || code === 'isv.SMS_TEMPLATE_ILLEGAL'),
