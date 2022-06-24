f8210f29c4e4589ecd9c1c5cfffabeb6b61f5fb8
wangsijie <wangsijie@silverhand.io>
2022-06-24 17:24:07 +0800

    fix(console): change checkbox to controlled comp

--- packages/console/src/components/Checkbox/index.tsx
+++ packages/console/src/components/Checkbox/index.tsx
@@ -1,23 +1,36 @@
 import { nanoid } from 'nanoid';
-import React, { forwardRef, InputHTMLAttributes, ReactNode, Ref, useState } from 'react';
+import React, { ReactNode, useState } from 'react';
 
 import Icon from './Icon';
 import * as styles from './index.module.scss';
 
-type Props = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> & {
+type Props = {
+  // eslint-disable-next-line react/boolean-prop-naming
+  value: boolean;
+  onChange: (value: boolean) => void;
   label?: ReactNode;
+  // eslint-disable-next-line react/boolean-prop-naming
+  disabled: boolean;
 };
 
-const Checkbox = ({ label, disabled, ...rest }: Props, ref: Ref<HTMLInputElement>) => {
+const Checkbox = ({ value, onChange, label, disabled }: Props) => {
   const [id] = useState(nanoid());
 
   return (
     <div className={styles.checkbox}>
-      <input id={id} type="checkbox" disabled={disabled} {...rest} ref={ref} />
+      <input
+        id={id}
+        type="checkbox"
+        checked={value}
+        disabled={disabled}
+        onChange={(event) => {
+          onChange(event.target.checked);
+        }}
+      />
       <Icon className={styles.icon} />
       {label && <label htmlFor={id}>{label}</label>}
     </div>
   );
 };
 
-export default forwardRef<HTMLInputElement, Props>(Checkbox);
+export default Checkbox;
