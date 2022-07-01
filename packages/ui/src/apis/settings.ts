ee1ae3341f09ca9cec2b2c7c4aa270a6c1bd3765
simeng-li <simeng@silverhand.io>
2022-07-01 11:31:23 +0800

    refactor(core): update sign-in-settings api path
    update sign-in-settings api path

--- packages/ui/src/apis/settings.ts
+++ packages/ui/src/apis/settings.ts
@@ -7,5 +7,5 @@ import { SignInExperience } from '@logto/schemas';
 import ky from 'ky';
 
 export const getSignInExperience = async <T extends SignInExperience>(): Promise<T> => {
-  return ky.get('/api/sign-in-settings').json<T>();
+  return ky.get('/api/.well-known/sign-in-exp').json<T>();
 };
