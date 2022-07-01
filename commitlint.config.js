dcd7758d633ff98172b3c85edd4d105440073de2
Gao Sun <gao@silverhand.io>
2022-07-01 21:58:53 +0800

    fix: version script

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,6 @@ const { rules } = require('@commitlint/config-conventional');
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', [...rules['type-enum'][2], 'api']],
+    'type-enum': [2, 'always', [...rules['type-enum'][2], 'api', 'release']],
   },
 };
