3aac771f35bf2bda49c56f878f0823f3904028bb
Wang Sijie <wangsijie@silverhand.io>
2022-07-01 13:01:10 +0800

    fix(console): remove dashboard tip time range (#1323)

--- packages/phrases/src/locales/en.ts
+++ packages/phrases/src/locales/en.ts
@@ -453,20 +453,17 @@ const translation = {
       total_users: 'Total users',
       total_users_tip: 'Total users',
       new_users_today: 'New users today',
-      new_users_today_tip:
-        'The number of new users registered on your apps today (from {{today}} to {{now}})',
+      new_users_today_tip: 'The number of new users registered on your apps today',
       new_users_7_days: 'New users past 7 days',
-      new_users_7_days_tip:
-        'The number of new users registered on your apps in the past 7 days (from {{daysAgo}} to {{now}})',
+      new_users_7_days_tip: 'The number of new users registered on your apps in the past 7 days',
       daily_active_users: 'Daily active users',
-      daily_active_users_tip:
-        'The number of unique users exchanged tokens on your apps today (from {{today}} to {{now}})',
+      daily_active_users_tip: 'The number of unique users exchanged tokens on your apps today',
       weekly_active_users: 'Weekly active users',
       weekly_active_users_tip:
-        'The number of unique users exchanged tokens on your apps in the past 7 days (from {{daysAgo}} to {{now}})',
+        'The number of unique users exchanged tokens on your apps in the past 7 days',
       monthly_active_users: 'Monthly active users',
       monthly_active_users_tip:
-        'The number of unique users exchanged tokens on your apps in the past 30 days (from {{daysAgo}} to {{now}})',
+        'The number of unique users exchanged tokens on your apps in the past 30 days',
     },
     logs: {
       title: 'Audit Logs',
