3d52f9efe37ba9cedc1c3d82e17780911bd44126
Gao Sun <gao@silverhand.io>
2022-07-04 14:22:49 +0800

    refactor(phrases): sign out the demo app (#1400)

--- packages/phrases/src/locales/zh-cn.ts
+++ packages/phrases/src/locales/zh-cn.ts
@@ -491,7 +491,7 @@ const translation = {
     subtitle: '以下是本次登录的用户信息：',
     username: '用户名：',
     user_id: '用户 ID：',
-    sign_out: '退出登录',
+    sign_out: '退出登录示例应用',
     continue_explore: '或继续探索',
     customize_sign_in_experience: '自定义登录体验',
     enable_passwordless: '启用无密码登录',
