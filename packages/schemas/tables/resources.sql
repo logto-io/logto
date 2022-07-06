4d7a091d2416cd763370d817571319761825da77
simeng-li <simeng@silverhand.io>
2022-07-06 20:33:32 +0800

    refactor(core): adjust access token ttl (#1430)
    adjust access token ttl

--- packages/schemas/tables/resources.sql
+++ packages/schemas/tables/resources.sql
@@ -2,7 +2,7 @@ create table resources (
     id varchar(21) not null,
     name text not null,
     indicator text not null unique, /* resource indicator also used as audience */
-    access_token_ttl bigint not null default(86400), /* expiration value in seconds, default is 24h */
+    access_token_ttl bigint not null default(3600), /* expiration value in seconds, default is 1h */
     primary key (id)
 );
 
