e55cb75c9130aba8b0c91670583343492ce97222
IceHe.xyz <icehe@silverhand.io>
2022-07-07 20:01:43 +0800

    test(test): validate swagger.json

--- packages/integration-tests/tests/swagger-check.test.ts
+++ packages/integration-tests/tests/swagger-check.test.ts
@@ -1,7 +1,19 @@
+import OpenApiSchemaValidator from 'openapi-schema-validator';
+import { OpenAPI } from 'openapi-types';
+
 import api from '@/api';
 
 describe('Swagger check', () => {
-  it('should succeed to provide swagger.json', async () => {
-    expect(await api.get('swagger.json')).toHaveProperty('statusCode', 200);
+  it('should provide a valid swagger.json', async () => {
+    const response = await api.get('swagger.json');
+    expect(response).toHaveProperty('statusCode', 200);
+    expect(response.headers['content-type']).toContain('application/json');
+
+    expect(() => {
+      const object: unknown = JSON.parse(response.body);
+      const validator = new OpenApiSchemaValidator({ version: 3 });
+      const result = validator.validate(object as OpenAPI.Document);
+      expect(result.errors).toEqual([]);
+    }).not.toThrow();
   });
 });
