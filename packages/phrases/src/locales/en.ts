3f76c230dfddd57fb98be4d2ccf65046853127bf
Xiao Yijun <xiaoyijun@silverhand.io>
2022-07-13 19:47:15 +0800

    refactor(phrases): split I18n locale file

--- packages/phrases/src/locales/en.ts
+++ packages/phrases/src/locales/en.ts
@@ -1,3 +1,5 @@
-// Note: we need to re-export the locale under this `locales` folder,
-// so that the `useTranslation` function can infer the i18n key type.
-export { default } from '../language-pack/en';
+/**
+ * Note: we need to re-export the locale under this `locales` folder,
+ * so that the `useTranslation` function can infer the i18n key type.
+ */
+export { default } from '../language/en';
