4b2fde50a7d29aeba0309acba7884d23bf6370f8
Xiao Yijun <xiaoyijun@silverhand.io>
2022-07-13 18:02:06 +0800

    refactor(phrases): split I18n locale file

--- packages/phrases/src/locales/zh-cn.ts
+++ packages/phrases/src/locales/zh-cn.ts
@@ -1,1 +1,3 @@
+// Note: we need to re-export the locales under this `locales` folder,
+// so that the `useTranslation` function can infer the correct i18n key type.
 export { default } from '../language-pack/zh-cn';
