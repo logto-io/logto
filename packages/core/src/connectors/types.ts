baeb1405a6ec91613c003eb623018f79012f4854
Darcy Ye <darcyye@silverhand.io>
2022-07-14 14:57:19 +0800

    fix(connector): refactor ConnectorInstance as class

--- packages/core/src/connectors/types.ts
+++ packages/core/src/connectors/types.ts
@@ -1,12 +1,8 @@
-import { SmsConnector, EmailConnector, SocialConnector } from '@logto/connector-types';
 import { PasscodeType } from '@logto/schemas';
 import { z } from 'zod';
 
 export { ConnectorType } from '@logto/schemas';
 export type { ConnectorMetadata } from '@logto/schemas';
-
-// The name `Connector` is used for database, use `ConnectorInstance` to avoid confusing.
-export type IConnector = SmsConnector | EmailConnector | SocialConnector;
 
 export type TemplateType = PasscodeType | 'Test';
 
