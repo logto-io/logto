fdcd40f060e9afdfed01b7688e6ccc6e4f5ef96e
IceHe.xyz <icehe@silverhand.io>
2022-07-20 18:42:10 +0800

    test: integration tests for setting up a social connector

--- packages/integration-tests/src/connector-api.ts
+++ packages/integration-tests/src/connector-api.ts
@@ -3,3 +3,22 @@ import { ConnectorDto } from '@logto/schemas';
 import { authedAdminApi } from '@/api';
 
 export const listConnectors = async () => authedAdminApi.get('connectors').json<ConnectorDto[]>();
+
+export const getConnector = async (connectorId: string) =>
+  authedAdminApi.get(`connectors/${connectorId}`).json<ConnectorDto>();
+
+export const updateConnectorConfig = async (connectorId: string, config: Record<string, unknown>) =>
+  authedAdminApi
+    .patch({
+      url: `connectors/${connectorId}`,
+      json: { config },
+    })
+    .json<ConnectorDto>();
+
+export const enableConnector = async (connectorId: string) =>
+  authedAdminApi
+    .patch({
+      url: `connectors/${connectorId}/enabled`,
+      json: { enabled: true },
+    })
+    .json<ConnectorDto>();
--- packages/integration-tests/tests/connectors.test.ts
+++ packages/integration-tests/tests/connectors.test.ts
@@ -1,13 +1,43 @@
-import { listConnectors } from '@/connector-api';
+import {
+  enableConnector,
+  getConnector,
+  listConnectors,
+  updateConnectorConfig,
+} from '@/connector-api';
+
+const facebookConnectorId = 'facebook-universal';
+const facebookConnectorConfig = {
+  clientId: 'application_foo',
+  clientSecret: 'secret_bar',
+};
 
 test('connector flow', async () => {
   // List connectors after initializing a new Logto instance
-  const connectorDtos = await listConnectors();
+  const allConnectors = await listConnectors();
 
   // There should be no connectors, or all connectors should be disabled.
-  for (const connectorDto of connectorDtos) {
+  for (const connectorDto of allConnectors) {
     expect(connectorDto.enabled).toBeFalsy();
   }
 
-  // Next up: set up a social connector
+  // Set up a social connector
+  const updatedFacebookConnector = await updateConnectorConfig(
+    facebookConnectorId,
+    facebookConnectorConfig
+  );
+  expect(updatedFacebookConnector.config).toEqual(facebookConnectorConfig);
+  const enabledFacebookConnector = await enableConnector(facebookConnectorId);
+  expect(enabledFacebookConnector.enabled).toBeTruthy();
+
+  // The result of getting a connector should be same as the result of updating a connector above.
+  const facebookConnector = await getConnector(facebookConnectorId);
+  expect(facebookConnector.enabled).toBeTruthy();
+  expect(facebookConnector.config).toEqual(facebookConnectorConfig);
+
+  // Next up:
+  // - set up an SMS connector and then change to another SMS connector
+  // - set up an email connector and then change to another email connector
+  // - validate wrong connector config
+  // - send sms/email test message
+  // - list all connectors after manually setting up connectors
 });
