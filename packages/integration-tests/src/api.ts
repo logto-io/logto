5ef1464e39e7fb49898e22f266ca8aa2738f1c2a
Xiao Yijun <xiaoyijun@silverhand.io>
2022-07-20 17:24:41 +0800

    test(console): integration tests for applications (#1602)

--- packages/integration-tests/src/api.ts
+++ packages/integration-tests/src/api.ts
@@ -3,3 +3,10 @@ import got from 'got';
 import { logtoUrl } from '@/constants';
 
 export default got.extend({ prefixUrl: new URL('/api', logtoUrl) });
+
+export const authedAdminApi = got.extend({
+  prefixUrl: new URL('/api', logtoUrl),
+  headers: {
+    'development-user-id': 'integration-test-admin-user',
+  },
+});
