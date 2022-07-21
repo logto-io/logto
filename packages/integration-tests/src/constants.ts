7f1120a73095184b64ef83b42a57c70a252c9494
Xiao Yijun <xiaoyijun@silverhand.io>
2022-07-21 14:21:18 +0800

    fix(test): use demo app to test username-password flow in integration test

--- packages/integration-tests/src/constants.ts
+++ packages/integration-tests/src/constants.ts
@@ -1,7 +1,8 @@
+import { demoAppApplicationId } from '@logto/schemas/lib/seeds';
 import { getEnv } from '@silverhand/essentials';
 
 export const logtoUrl = getEnv('LOGTO_URL');
 
 export const discoveryUrl = `${logtoUrl}/oidc/.well-known/openid-configuration`;
 
-export const demoAppRedirectUri = `${logtoUrl}/demo-app`;
+export const demoAppRedirectUri = `${logtoUrl}/${demoAppApplicationId}`;
