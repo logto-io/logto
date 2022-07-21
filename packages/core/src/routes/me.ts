4bf6483ff4674052d4b5d00d647c0c408b3ecc7f
Gao Sun <gao@silverhand.io>
2022-07-21 21:35:39 +0800

    feat(core): api GET /me (#1650)

--- packages/core/src/routes/me.ts
+++ packages/core/src/routes/me.ts
@@ -1,5 +1,6 @@
-import { arbitraryObjectGuard } from '@logto/schemas';
+import { arbitraryObjectGuard, userInfoSelectFields } from '@logto/schemas';
 import { passwordRegEx } from '@logto/shared';
+import pick from 'lodash.pick';
 import { object, string } from 'zod';
 
 import { encryptUserPassword } from '@/lib/user';
@@ -9,6 +10,14 @@ import { findUserById, updateUserById } from '@/queries/user';
 import { AuthedRouter } from './types';
 
 export default function meRoutes<T extends AuthedRouter>(router: T) {
+  router.get('/me', async (ctx, next) => {
+    const user = await findUserById(ctx.auth);
+
+    ctx.body = pick(user, ...userInfoSelectFields);
+
+    return next();
+  });
+
   router.get('/me/custom-data', async (ctx, next) => {
     const { customData } = await findUserById(ctx.auth);
 
