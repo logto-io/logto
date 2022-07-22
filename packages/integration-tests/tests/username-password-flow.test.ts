6dd73db8f6e6c3e26878bbb73df33ca4629c8083
Xiao Yijun <xiaoyijun@silverhand.io>
2022-07-22 19:37:57 +0800

    refactor(test): user sign in

--- packages/integration-tests/tests/username-password-flow.test.ts
+++ packages/integration-tests/tests/username-password-flow.test.ts
@@ -12,33 +12,25 @@ import {
 import { generatePassword, generateUsername } from '@/utils';
 
 describe('username and password flow', () => {
-  const logtoConfig: LogtoConfig = {
-    endpoint: logtoUrl,
-    appId: demoAppApplicationId,
-    persistAccessToken: false,
-  };
+  it('should register and sign in with username and password successfully', async () => {
+    const logtoConfig: LogtoConfig = {
+      endpoint: logtoUrl,
+      appId: demoAppApplicationId,
+      persistAccessToken: false,
+    };
 
-  const username = generateUsername();
-  const password = generatePassword();
-
-  it('should register with username and password successfully', async () => {
     const logtoClient = new LogtoClient(logtoConfig);
+
     await logtoClient.signIn(demoAppRedirectUri);
 
     expect(logtoClient.navigateUrl).toBeTruthy();
 
     const interactionCookie = await visitSignInUri(logtoClient.navigateUrl);
+
+    const username = generateUsername();
+    const password = generatePassword();
 
     await registerUserWithUsernameAndPassword(username, password, interactionCookie);
-  });
-
-  it('should sign in with username and password successfully', async () => {
-    const logtoClient = new LogtoClient(logtoConfig);
-    await logtoClient.signIn(demoAppRedirectUri);
-
-    expect(logtoClient.navigateUrl).toBeTruthy();
-
-    const interactionCookie = await visitSignInUri(logtoClient.navigateUrl);
 
     const interactionCookieWithSession = await signInWithUsernameAndPassword(
       username,
