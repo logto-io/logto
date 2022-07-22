639c0bd6448b742d406581d59afb020b1a0feebf
Zhengqbbb <1074059947@qq.com>
2022-07-22 13:19:49 +0800

    chore: use `cz-git` as commitizen adapter

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,30 @@
 const { rules } = require('@commitlint/config-conventional');
+const { execSync } = require('child_process');
 
-/** @type {import('@commitlint/types').UserConfig} **/
+// precomputed scope
+const scopeComplete = execSync('git status --porcelain || true')
+  .toString()
+  .trim()
+  .split('\n')
+  .find((r) => ~r.indexOf('M  packages'))
+  ?.replace(/(\/)/g, '%%')
+  ?.match(/packages%%((\w|-)*)/)?.[1]
+  ?.replace(/^connector(\w|-)*/g, 'connector')
+
+/** @type {import('cz-git').UserConfig} */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', [...rules['type-enum'][2], 'api', 'release']],
     'scope-enum': [2, 'always', ['connector', 'console', 'core', 'demo-app', 'test', 'phrases', 'schemas', 'shared', 'ui', 'deps']]
   },
+  prompt: {
+    typesAppend: [
+      { value: 'api', name: 'api:      Api aspect modify' },
+    ],
+    customScopesAlign: !scopeComplete ? 'top' : 'bottom',
+    defaultScope: scopeComplete,
+    allowEmptyIssuePrefixs: false,
+    allowCustomIssuePrefixs: false,
+  }
 };
