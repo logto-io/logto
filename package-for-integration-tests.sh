7a212365172f3fed6d00e590fd757171065d978f
IceHe.xyz <icehe@silverhand.io>
2022-07-25 17:30:14 +0800

    fix(test): use package-for-integration-tests.sh

---
+++ package-for-integration-tests.sh
@@ -0,0 +1,25 @@
+set -eo pipefail
+
+echo Building packages
+pnpm -- lerna run build --stream
+
+cd packages/core
+pnpm link @logto/connector-mock-sms
+pnpm link @logto/connector-mock-email
+cd -
+
+echo Prune dependencies
+rm -rf node_modules packages/*/node_modules
+
+echo Install production dependencies
+NODE_ENV=production pnpm i
+
+echo Prune files
+rm -rf \
+.git .github .husky .vscode .parcel-cache pnpm-*.yaml *.js \
+packages/*/src \
+packages/*/*.config.js packages/*/*.config.ts packages/*/tsconfig*.json
+
+echo Tar
+cd ..
+tar -czf /tmp/logto.tar.gz logto
