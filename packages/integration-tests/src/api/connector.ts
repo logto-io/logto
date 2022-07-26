437ceb59a700c846d35c255eb4ed4f57267fea3e
simeng-li <simeng@silverhand.io>
2022-07-26 20:34:03 +0800

    chore(test): update path
    update path

--- packages/integration-tests/src/api/connector.ts
+++ packages/integration-tests/src/api/connector.ts
@@ -1,6 +1,6 @@
 import { ConnectorDto } from '@logto/schemas';
 
-import { authedAdminApi } from '@/api/api';
+import { authedAdminApi } from './api';
 
 export const listConnectors = async () => authedAdminApi.get('connectors').json<ConnectorDto[]>();
 
