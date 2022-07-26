9096c509071a41f3493c8f20d790f8268e5a58cb
Xiao Yijun <xiaoyijun@silverhand.io>
2022-07-26 14:29:38 +0800

    test: integration tests for logs (#1671)

---
+++ packages/integration-tests/tests/logs.test.ts
@@ -0,0 +1,22 @@
+import { LogDto } from '@logto/schemas';
+import { assert } from '@silverhand/essentials';
+
+import { authedAdminApi } from '@/api';
+import { registerUserAndSignIn } from '@/helper';
+
+describe('admin console logs', () => {
+  it('should get logs and visit log details successfully', async () => {
+    await registerUserAndSignIn();
+
+    const logs = await authedAdminApi.get('logs').json<LogDto[]>();
+
+    expect(logs.length).toBeGreaterThan(0);
+
+    const log = logs[0];
+    assert(log, new Error('Log is not valid'));
+
+    const logDetails = await authedAdminApi.get(`logs/${log.id}`).json<LogDto>();
+
+    expect(logDetails.id).toBe(log.id);
+  });
+});
