61177ca2a17dc8405a8cc54fb98859248a06cf73
simeng-li <simeng@silverhand.io>
2022-07-27 10:09:13 +0800

    chore(test): add dotenv (#1681)
    * chore(test): integration test update
    
    add dotenv
    
    * chore(core): update pnpm lock
    
    update pnpm lock

--- packages/integration-tests/jest.setup.js
+++ packages/integration-tests/jest.setup.js
@@ -1,10 +1,13 @@
 // Need to disable following rules to mock text-decode/text-encoder and crypto for jsdom
 // https://github.com/jsdom/jsdom/issues/1612
 import { Crypto } from '@peculiar/webcrypto';
+import dotenv from 'dotenv';
 import { TextDecoder, TextEncoder } from 'text-encoder';
 
 // eslint-disable-next-line unicorn/prefer-module
 const fetch = require('node-fetch');
+
+dotenv.config();
 
 /* eslint-disable @silverhand/fp/no-mutation */
 global.crypto = new Crypto();
