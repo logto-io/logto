80b5b7d9654a83c67ea101c59c87b994ae313a32
wangsijie <wangsijie@silverhand.io>
2022-08-02 10:54:12 +0800

    fix(shared): fix dark color generator

--- packages/shared/src/utilities/color.ts
+++ packages/shared/src/utilities/color.ts
@@ -4,13 +4,13 @@ import color from 'color';
 export const absoluteLighten = (baseColor: color, delta: number) => {
   const hslArray = baseColor.hsl().round().array();
 
-  return color([hslArray[0] ?? 0, hslArray[1] ?? 0, hslArray[2] ?? 0 + delta], 'hsl');
+  return color([hslArray[0] ?? 0, hslArray[1] ?? 0, (hslArray[2] ?? 0) + delta], 'hsl');
 };
 
 export const absoluteDarken = (baseColor: color, delta: number) => {
   const hslArray = baseColor.hsl().round().array();
 
-  return color([hslArray[0] ?? 0, hslArray[1] ?? 0, hslArray[2] ?? 0 - delta], 'hsl');
+  return color([hslArray[0] ?? 0, hslArray[1] ?? 0, (hslArray[2] ?? 0) - delta], 'hsl');
 };
 
 export const generateDarkColor = (lightColor: string) =>
