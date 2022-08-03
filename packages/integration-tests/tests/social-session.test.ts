ef3c3004ce61844188827ab9382805c0767e0f9c
simeng-li <simeng@silverhand.io>
2022-08-03 13:34:39 +0800

    fix(test): cr fix
    cr fix add user identities check

--- packages/integration-tests/tests/social-session.test.ts
+++ packages/integration-tests/tests/social-session.test.ts
@@ -1,13 +1,18 @@
 import { assert } from '@silverhand/essentials';
 import { HTTPError } from 'got';
 
-import { mockSocialConnectorId, mockSocialConnectorConfig } from '@/__mocks__/connectors-mock';
+import {
+  mockSocialConnectorId,
+  mockSocialConnectorTarget,
+  mockSocialConnectorConfig,
+} from '@/__mocks__/connectors-mock';
 import {
   signInWithSocial,
   getAuthWithSocial,
   registerWithSocial,
   bindWithSocial,
   signInWithUsernameAndPassword,
+  getUser,
 } from '@/api';
 import MockClient from '@/client';
 import { setUpConnector, createUserByAdmin } from '@/helpers';
@@ -18,7 +23,7 @@ const redirectUri = 'http://foo.dev/callback';
 const code = 'auth_code_foo';
 
 describe('social sign-in and register', () => {
-  const userId = crypto.randomUUID();
+  const socialUserId = crypto.randomUUID();
 
   beforeAll(async () => {
     await setUpConnector(mockSocialConnectorId, mockSocialConnectorConfig);
@@ -38,7 +43,10 @@ describe('social sign-in and register', () => {
     ).resolves.toBeTruthy();
 
     const response = await getAuthWithSocial(
-      { connectorId: mockSocialConnectorId, data: { state, redirectUri, code, userId } },
+      {
+        connectorId: mockSocialConnectorId,
+        data: { state, redirectUri, code, userId: socialUserId },
+      },
       client.interactionCookie
     ).catch((error: unknown) => error);
 
@@ -56,7 +64,8 @@ describe('social sign-in and register', () => {
     expect(client.isAuthenticated).toBeTruthy();
   });
 
-  /* Note: As currently we can not prepare a social identities through admin api.
+  /*
+   * Note: As currently we can not prepare a social identities through admin api.
    * The sign-in test case MUST run concurrently after the register test case
    */
   it('Sign-In with social', async () => {
@@ -73,7 +82,10 @@ describe('social sign-in and register', () => {
     ).resolves.toBeTruthy();
 
     const { redirectTo } = await getAuthWithSocial(
-      { connectorId: mockSocialConnectorId, data: { state, redirectUri, code, userId } },
+      {
+        connectorId: mockSocialConnectorId,
+        data: { state, redirectUri, code, userId: socialUserId },
+      },
       client.interactionCookie
     );
 
@@ -112,10 +124,22 @@ describe('social bind account', () => {
     // User with social does not exist
     expect(response instanceof HTTPError && response.response.statusCode === 422).toBe(true);
 
-    await signInWithUsernameAndPassword(username, password, client.interactionCookie);
+    const { redirectTo } = await signInWithUsernameAndPassword(
+      username,
+      password,
+      client.interactionCookie
+    );
 
     await expect(
       bindWithSocial(mockSocialConnectorId, client.interactionCookie)
     ).resolves.not.toThrow();
+
+    await client.processSession(redirectTo);
+
+    // User should bind with social identities
+    const { sub } = client.getIdTokenClaims();
+    const user = await getUser(sub);
+
+    expect(user.identities).toHaveProperty(mockSocialConnectorTarget);
   });
 });
