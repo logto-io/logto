85a4e90ff81116fb391be722400dfe086b1b0de2
IceHe.Life <icehe@silverhand.io>
2022-08-05 16:39:27 +0800

    refactor: simplify jest.config.ts

--- packages/connector-facebook/jest.config.ts
+++ packages/connector-facebook/jest.config.ts
@@ -1,7 +1,6 @@
 import { Config, merge } from '@silverhand/jest-config';
 
 const config: Config.InitialOptions = merge({
-  testEnvironment: 'node',
   setupFilesAfterEnv: ['jest-matcher-specific-error'],
 });
 
--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -1,7 +1,6 @@
 import { merge, Config } from '@silverhand/jest-config';
 
 const config: Config.InitialOptions = merge({
-  testEnvironment: 'node',
   setupFilesAfterEnv: ['jest-matcher-specific-error', './jest.setup.ts'],
   globalSetup: './jest.global-setup.ts',
 });
