11a11fc6c22c0112f3d5bc6c3ac08bce8f02c92b
IceHe.Life <icehe@silverhand.io>
2022-08-05 18:43:32 +0800

    refactor: simplify jest.config.ts (#1741)

--- packages/connector-sendgrid-mail/jest.config.ts
+++ packages/connector-sendgrid-mail/jest.config.ts
@@ -1,7 +1,1 @@
-import { Config, merge } from '@silverhand/jest-config';
-
-const config: Config.InitialOptions = merge({
-  testEnvironment: 'node',
-});
-
-export default config;
+export { default } from '@silverhand/jest-config';
