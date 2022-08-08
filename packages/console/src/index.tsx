2b81c1e42f871d3f73bafdb51950c8efc20ca867
simeng-li <simeng@silverhand.io>
2022-08-08 15:07:51 +0800

    fix(ui,console,demo-app): update react render method
    update react root render method

--- packages/console/src/index.tsx
+++ packages/console/src/index.tsx
@@ -1,8 +1,9 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import ReactModal from 'react-modal';
 
 import App from './App';
 
 const app = document.querySelector('#app');
+const root = app && createRoot(app);
 ReactModal.setAppElement('#app');
-ReactDOM.render(<App />, app);
+root?.render(<App />);
