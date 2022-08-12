03b6dffd38ffe65204e61426f310d3789633c37f
Gao Sun <gao@silverhand.io>
2022-08-12 17:14:47 +0800

    fix(test): logto url env key

--- packages/integration-tests/src/constants.ts
+++ packages/integration-tests/src/constants.ts
@@ -1,7 +1,7 @@
 import { demoAppApplicationId } from '@logto/schemas/lib/seeds';
 import { getEnv } from '@silverhand/essentials';
 
-export const logtoUrl = getEnv('LOGTO_URL');
+export const logtoUrl = getEnv('INTEGRATION_TESTS_LOGTO_URL');
 
 export const discoveryUrl = `${logtoUrl}/oidc/.well-known/openid-configuration`;
 
