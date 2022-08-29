ac20c95d9143a5d4db7000e245c9661462f53469
wangsijie <wangsijie@silverhand.io>
2022-08-29 17:29:44 +0800

    feat(core)!: load connectors by folder

---
+++ packages/core/src/cli/add-offical-connectors.ts
@@ -0,0 +1,16 @@
+import 'module-alias/register';
+import { getEnv } from '@silverhand/essentials';
+
+import { addOfficialConnectors } from '@/connectors/add-connectors';
+import { defaultConnectorDirectory } from '@/env-set';
+
+import { configDotEnv } from '../env-set/dot-env';
+
+configDotEnv();
+
+const addOfficialConnectorsCli = async () => {
+  const connectorDirectory = getEnv('CONNECTOR_DIRECTORY', defaultConnectorDirectory);
+  await addOfficialConnectors(connectorDirectory);
+};
+
+void addOfficialConnectorsCli();
