721a602c5a2b3d16e68f9d2aeb75193980e51724
simeng-li <simeng@silverhand.io>
2022-09-19 17:15:01 +0800

    fix(ui): fix ut
    fix ut

--- packages/ui/src/pages/ResetPassword/index.test.tsx
+++ packages/ui/src/pages/ResetPassword/index.test.tsx
@@ -1,10 +1,17 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 
 import ResetPassword from '.';
 
 describe('ForgotPassword', () => {
   it('render email forgot password properly', () => {
-    const { queryByText } = render(<ResetPassword />);
+    const { queryByText } = render(
+      <MemoryRouter initialEntries={['/reset-password']}>
+        <Routes>
+          <Route path="/reset-password" element={<ResetPassword />} />
+        </Routes>
+      </MemoryRouter>
+    );
 
     expect(queryByText('description.new_password')).not.toBeNull();
   });
