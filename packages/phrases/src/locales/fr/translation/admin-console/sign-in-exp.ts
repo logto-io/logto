a20e139b000f414c0e80076aad4db26eb4e8b177
Xiao Yijun <xiaoyijun@silverhand.io>
2022-09-26 16:35:27 +0800

    Revert "feat(console): auto detect language setting (#1941)"
    This reverts commit 49b430394dc961451a6abca26a95ebba8d22f68c.

--- packages/phrases/src/locales/fr/translation/admin-console/sign-in-exp.ts
+++ packages/phrases/src/locales/fr/translation/admin-console/sign-in-exp.ts
@@ -73,15 +73,13 @@ const sign_in_exp = {
     },
     languages: {
       title: 'LANGAGES',
-      enable_auto_detect: 'Enable auto detect', // UNTRANSLATED
-      description:
-        "Your software detects the user's location and switches to the local language. You can add new locales by translating UI from English to another language.", // UNTRANSLATED
-      manage_language: 'Manage language', // UNTRANSLATED
-      default_language: 'Default language', // UNTRANSLATED
-      default_language_description_auto:
-        'The default language will be used when a text segment is missing translation.', // UNTRANSLATED
-      default_language_description_fixed:
-        'When auto detect is off, the default language is the only language your software will show. Turn on auto detect for language customization.', // UNTRANSLATED
+      mode: 'Mode langue',
+      auto: 'Auto',
+      fixed: 'Fixé',
+      fallback_language: 'Langage par défaut',
+      fallback_language_tip:
+        'La langue de repli si Logto ne trouve pas de jeu de phrases dans la langue appropriée.',
+      fixed_language: 'Langue fixe',
     },
     authentication: {
       title: 'AUTHENTICATION',
