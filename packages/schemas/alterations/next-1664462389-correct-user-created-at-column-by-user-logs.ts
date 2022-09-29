012862ec8c96500eb981cef748c4483ee232f8a6
IceHe <icehe@silverhand.io>
2022-09-29 22:56:28 +0800

    chore(schemas): add db alteration script to repair user created-at column by user logs

---
+++ packages/schemas/alterations/next-1664462389-correct-user-created-at-column-by-user-logs.ts
@@ -0,0 +1,25 @@
+import { sql } from 'slonik';
+
+import { AlterationScript } from '../lib/types/alteration';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      update users
+      set created_at = user_logs.registered_at
+      from (
+        select logs.payload ->> 'userId' as user_id, max(logs.created_at) as registered_at
+        from logs
+        where logs.type in ('RegisterUsernamePassword', 'RegisterEmail', 'RegisterSms', 'RegisterSocial')
+        and logs.payload ->> 'result' = 'Success'
+        group by logs.payload ->> 'userId'
+      ) as user_logs
+      where users.id = user_logs.user_id;
+    `);
+  },
+  down: async (pool) => {
+    // It cannot be reverted automatically.
+  },
+};
+
+export default alteration;
