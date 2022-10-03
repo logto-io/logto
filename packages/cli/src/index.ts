a59ba6e3187b2bcb8c2ea59e7c51e5b80447d496
Gao Sun <gao@silverhand.io>
2022-10-03 17:52:28 +0800

    refactor(cli): experience improvements

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,89 +1,130 @@
 import { execSync } from 'child_process';
 import { existsSync } from 'fs';
-import { unlink } from 'fs/promises';
+import { mkdir } from 'fs/promises';
+import os from 'os';
 import path from 'path';
 
-import decompress from 'decompress';
+import chalk from 'chalk';
+import ora from 'ora';
 import * as prompts from 'prompts';
+import * as semver from 'semver';
+import tar from 'tar';
 
-import { downloadFile, isVersionGreaterThan, safeExecSync, trimVersion } from './functions';
+import { downloadFile, log, safeExecSync } from './utilities';
 
-const DIRECTORY = 'logto';
-const NODE_MAJOR_VERSION = 16;
-const POSTGRES_MAJOR_VERSION = 14;
+const pgRequired = new semver.SemVer('14.0.0');
 
-const getPromptResponse = async (pgVersion: string) =>
-  prompts.default(
+const validateNodeVersion = () => {
+  const required = new semver.SemVer('16.0.0');
+  const current = new semver.SemVer(execSync('node -v', { encoding: 'utf8', stdio: 'pipe' }));
+
+  if (required.compare(current) > 0) {
+    log.error(`Logto requires NodeJS >=${required.version}, but ${current.version} found.`);
+  }
+
+  if (current.major > required.major) {
+    log.warn(
+      `Logto is tested under NodeJS ^${required.version}, but version ${current.version} found.`
+    );
+  }
+};
+
+const getInstancePath = async () => {
+  const response = await prompts.default(
     [
       {
         name: 'instancePath',
         message: 'Where should we create your logto instance?',
         type: 'text',
-        initial: './' + DIRECTORY,
+        initial: './logto',
         format: (value: string) => path.resolve(value.trim()),
         validate: (value: string) =>
           existsSync(value) ? 'That path already exists, please try another.' : true,
       },
       {
         name: 'hasPostgresUrl',
-        message: `Logto requires PostgreSQL >= ${POSTGRES_MAJOR_VERSION}.0.0 but cannot find in the current environment. Do you have a remote PostgreSQL instance ready?`,
-        type: !isVersionGreaterThan(trimVersion(pgVersion), POSTGRES_MAJOR_VERSION) && 'confirm',
-        initial: true,
-      },
-      {
-        name: 'postgresUrl',
-        message: 'What is the URL of your PostgreSQL instance?',
-        type: (_, data) => (data.hasPostgresUrl ? 'text' : null),
-        format: (value: string) => value.trim(),
-        validate: (value: string) =>
-          (value &&
-            Boolean(
-              /^(?:([^\s#/:?]+):\/{2})?(?:([^\s#/?@]+)@)?([^\s#/?]+)?(?:\/([^\s#?]*))?(?:\?([^\s#]+))?\S*$/.test(
-                value
-              )
-            )) ||
-          'Please enter a valid connection URL.',
-      },
-      {
-        name: 'startInstance',
-        message: 'Would you like to start Logto now?',
-        type: 'confirm',
-        initial: true,
+        message: `Logto requires PostgreSQL >=${pgRequired.version} but cannot find in the current environment.\n  Do you have a remote PostgreSQL instance ready?`,
+        type: () => {
+          const pgOutput = safeExecSync('postgres --version') ?? '';
+          // Filter out all brackets in the output since Homebrew will append `(Homebrew)`.
+          const pgArray = pgOutput.split(' ').filter((value) => !value.startsWith('('));
+          const pgCurrent = semver.coerce(pgArray[pgArray.length - 1]);
+
+          return (!pgCurrent || pgCurrent.compare(pgRequired) < 0) && 'confirm';
+        },
+        format: (previous) => {
+          if (!previous) {
+            log.error('Logto requires a Postgres instance to run.');
+          }
+        },
       },
     ],
     {
       onCancel: () => {
-        throw new Error('Operation cancelled');
+        log.error('Operation cancelled');
       },
     }
   );
 
-async function main() {
-  const nodeVersion = execSync('node -v', { encoding: 'utf8', stdio: 'pipe' });
-  const pgOutput = safeExecSync('postgres --version') ?? '';
-  const pgArray = pgOutput.split(' ');
-  const pgVersion = pgArray[pgArray.length - 1] ?? '';
+  return String(response.instancePath);
+};
 
-  if (!isVersionGreaterThan(trimVersion(nodeVersion), NODE_MAJOR_VERSION)) {
-    throw new Error(`Logto requires NodeJS >= ${NODE_MAJOR_VERSION}.0.0.`);
-  }
+const tryStartInstance = async (instancePath: string) => {
+  const response = await prompts.default({
+    name: 'startInstance',
+    message: 'Would you like to start Logto now?',
+    type: 'confirm',
+    initial: true,
+  });
 
-  const response = await getPromptResponse(pgVersion);
-  const startCommand = `cd ${String(response.instancePath)} && npm start`;
-  const tarFileLocation = path.resolve('./logto.tar.gz');
+  const yes = Boolean(response.startInstance);
+  const startCommand = `cd ${instancePath} && npm start`;
 
-  await downloadFile(
-    'https://github.com/logto-io/logto/releases/latest/download/logto.tar.gz',
-    tarFileLocation
-  );
-  await decompress(tarFileLocation, response.instancePath);
-  await unlink(tarFileLocation);
-
-  if (response.startInstance) {
+  if (yes) {
     execSync(startCommand, { stdio: 'inherit' });
   } else {
-    console.log(`You can use ${startCommand} to start Logto. Happy hacking!`);
+    log.info(`You can use ${startCommand} to start Logto. Happy hacking!`);
   }
-}
+};
 
-await main();
+const downloadRelease = async () => {
+  const tarFilePath = path.resolve(os.tmpdir(), './logto.tar.gz');
+
+  log.info(`Download Logto to ${tarFilePath}`);
+  await downloadFile(
+    'https://github.com/logto-io/logto/releases/latest/download/logto.tar.gz',
+    tarFilePath
+  );
+
+  return tarFilePath;
+};
+
+const decompress = async (toPath: string, tarPath: string) => {
+  const decompressSpinner = ora({
+    text: `Decompress to ${toPath}`,
+    prefixText: chalk.blue('[info]'),
+  }).start();
+
+  try {
+    await mkdir(toPath);
+    await tar.extract({ file: tarPath, cwd: toPath, strip: 1 });
+  } catch {
+    decompressSpinner.fail();
+
+    return;
+  }
+
+  decompressSpinner.succeed();
+};
+
+const main = async () => {
+  validateNodeVersion();
+
+  const instancePath = await getInstancePath();
+  const tarPath = await downloadRelease();
+
+  await decompress(instancePath, tarPath);
+  await tryStartInstance(instancePath);
+};
+
+void main();
---
+++ packages/cli/src/utilities.ts
@@ -0,0 +1,65 @@
+import { execSync } from 'child_process';
+import { createWriteStream } from 'fs';
+
+import chalk from 'chalk';
+import got, { Progress } from 'got';
+import ora from 'ora';
+
+export const safeExecSync = (command: string) => {
+  try {
+    return execSync(command, { encoding: 'utf8', stdio: 'pipe' });
+  } catch {}
+};
+
+type Log = Readonly<{
+  info: typeof console.log;
+  warn: typeof console.log;
+  error: typeof console.log;
+}>;
+
+export const log: Log = Object.freeze({
+  info: (...args) => {
+    console.log(chalk.blue('[info]'), ...args);
+  },
+  warn: (...args) => {
+    console.log(chalk.yellow('[warn]'), ...args);
+  },
+  error: (...args) => {
+    console.log(chalk.red('[error]'), ...args);
+    // eslint-disable-next-line unicorn/no-process-exit
+    process.exit(1);
+  },
+});
+
+export const downloadFile = async (url: string, destination: string) => {
+  const file = createWriteStream(destination);
+  const stream = got.stream(url);
+  const spinner = ora({
+    text: 'Connecting',
+    prefixText: chalk.blue('[info]'),
+  }).start();
+
+  stream.pipe(file);
+
+  return new Promise((resolve, reject) => {
+    stream.on('downloadProgress', ({ total, percent }: Progress) => {
+      if (!total) {
+        return;
+      }
+
+      // eslint-disable-next-line @silverhand/fp/no-mutation
+      spinner.text = `${(percent * 100).toFixed(1)}%`;
+    });
+
+    file.on('error', (error) => {
+      spinner.fail();
+      reject(error.message);
+    });
+
+    file.on('finish', () => {
+      file.close();
+      spinner.succeed();
+      resolve(file);
+    });
+  });
+};
