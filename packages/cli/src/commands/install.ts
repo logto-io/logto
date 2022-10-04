f05691b4319279a49bf0bc87ba656b7990d52e53
Gao Sun <gao@silverhand.io>
2022-10-04 13:45:27 +0800

    feat(cli): command `init/i/install`

---
+++ packages/cli/src/commands/install.ts
@@ -0,0 +1,125 @@
+import { execSync } from 'child_process';
+import { existsSync } from 'fs';
+import { mkdir } from 'fs/promises';
+import os from 'os';
+import path from 'path';
+
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import ora from 'ora';
+import * as semver from 'semver';
+import tar from 'tar';
+
+import { downloadFile, log, safeExecSync } from '../utilities';
+
+export type InstallArgs = {
+  path?: string;
+  silent?: boolean;
+};
+
+const defaultPath = path.join(os.homedir(), 'logto');
+const pgRequired = new semver.SemVer('14.0.0');
+
+const validateNodeVersion = () => {
+  const required = new semver.SemVer('16.0.0');
+  const current = new semver.SemVer(execSync('node -v', { encoding: 'utf8', stdio: 'pipe' }));
+
+  if (required.compare(current) > 0) {
+    log.error(`Logto requires NodeJS >=${required.version}, but ${current.version} found.`);
+  }
+
+  if (current.major > required.major) {
+    log.warn(
+      `Logto is tested under NodeJS ^${required.version}, but version ${current.version} found.`
+    );
+  }
+};
+
+const validatePath = (value: string) =>
+  existsSync(path.resolve(value))
+    ? `The path ${chalk.green(value)} already exists, please try another.`
+    : true;
+
+const getInstancePath = async () => {
+  const { hasPostgresUrl } = await inquirer.prompt<{ hasPostgresUrl?: boolean }>({
+    name: 'hasPostgresUrl',
+    message: `Logto requires PostgreSQL >=${pgRequired.version} but cannot find in the current environment.\n  Do you have a remote PostgreSQL instance ready?`,
+    type: 'confirm',
+    when: () => {
+      const pgOutput = safeExecSync('postgres --version') ?? '';
+      // Filter out all brackets in the output since Homebrew will append `(Homebrew)`.
+      const pgArray = pgOutput.split(' ').filter((value) => !value.startsWith('('));
+      const pgCurrent = semver.coerce(pgArray[pgArray.length - 1]);
+
+      return !pgCurrent || pgCurrent.compare(pgRequired) < 0;
+    },
+  });
+
+  if (hasPostgresUrl === false) {
+    log.error('Logto requires a Postgres instance to run.');
+  }
+
+  const { instancePath } = await inquirer.prompt<{ instancePath: string }>({
+    name: 'instancePath',
+    message: 'Where should we create your Logto instance?',
+    type: 'input',
+    default: defaultPath,
+    filter: (value: string) => value.trim(),
+    validate: validatePath,
+  });
+
+  return instancePath;
+};
+
+const downloadRelease = async () => {
+  const tarFilePath = path.resolve(os.tmpdir(), './logto.tar.gz');
+
+  log.info(`Download Logto to ${tarFilePath}`);
+  await downloadFile(
+    'https://github.com/logto-io/logto/releases/latest/download/logto.tar.gz',
+    tarFilePath
+  );
+
+  return tarFilePath;
+};
+
+const decompress = async (toPath: string, tarPath: string) => {
+  const decompressSpinner = ora({
+    text: `Decompress to ${toPath}`,
+    prefixText: chalk.blue('[info]'),
+  }).start();
+
+  try {
+    await mkdir(toPath);
+    await tar.extract({ file: tarPath, cwd: toPath, strip: 1 });
+  } catch (error: unknown) {
+    decompressSpinner.fail();
+    log.error(error);
+
+    return;
+  }
+
+  decompressSpinner.succeed();
+};
+
+const install = async ({ path: pathArgument = defaultPath, silent = false }: InstallArgs) => {
+  validateNodeVersion();
+
+  const instancePath = (!silent && (await getInstancePath())) || pathArgument;
+  const isValidPath = validatePath(instancePath);
+
+  if (isValidPath !== true) {
+    log.error(isValidPath);
+  }
+
+  const tarPath = await downloadRelease();
+
+  await decompress(instancePath, tarPath);
+
+  const startCommand = `cd ${instancePath} && npm start`;
+  log.info(
+    `Use the command below to start Logto. Happy hacking!\n\n  ${chalk.green(startCommand)}`
+  );
+};
+
+export default install;
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,130 +1,32 @@
-import { execSync } from 'child_process';
-import { existsSync } from 'fs';
-import { mkdir } from 'fs/promises';
-import os from 'os';
-import path from 'path';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
-import chalk from 'chalk';
-import ora from 'ora';
-import * as prompts from 'prompts';
-import * as semver from 'semver';
-import tar from 'tar';
+import install from './commands/install';
 
-import { downloadFile, log, safeExecSync } from './utilities';
-
-const pgRequired = new semver.SemVer('14.0.0');
-
-const validateNodeVersion = () => {
-  const required = new semver.SemVer('16.0.0');
-  const current = new semver.SemVer(execSync('node -v', { encoding: 'utf8', stdio: 'pipe' }));
-
-  if (required.compare(current) > 0) {
-    log.error(`Logto requires NodeJS >=${required.version}, but ${current.version} found.`);
-  }
-
-  if (current.major > required.major) {
-    log.warn(
-      `Logto is tested under NodeJS ^${required.version}, but version ${current.version} found.`
-    );
-  }
-};
-
-const getInstancePath = async () => {
-  const response = await prompts.default(
-    [
-      {
-        name: 'instancePath',
-        message: 'Where should we create your logto instance?',
-        type: 'text',
-        initial: './logto',
-        format: (value: string) => path.resolve(value.trim()),
-        validate: (value: string) =>
-          existsSync(value) ? 'That path already exists, please try another.' : true,
-      },
-      {
-        name: 'hasPostgresUrl',
-        message: `Logto requires PostgreSQL >=${pgRequired.version} but cannot find in the current environment.\n  Do you have a remote PostgreSQL instance ready?`,
-        type: () => {
-          const pgOutput = safeExecSync('postgres --version') ?? '';
-          // Filter out all brackets in the output since Homebrew will append `(Homebrew)`.
-          const pgArray = pgOutput.split(' ').filter((value) => !value.startsWith('('));
-          const pgCurrent = semver.coerce(pgArray[pgArray.length - 1]);
-
-          return (!pgCurrent || pgCurrent.compare(pgRequired) < 0) && 'confirm';
-        },
-        format: (previous) => {
-          if (!previous) {
-            log.error('Logto requires a Postgres instance to run.');
-          }
-        },
-      },
-    ],
+void yargs(hideBin(process.argv))
+  .command(
+    ['init', 'i', 'install'],
+    'Download and run the latest Logto release',
     {
-      onCancel: () => {
-        log.error('Operation cancelled');
+      path: {
+        alias: 'p',
+        describe: 'Path of Logto, must be a non-existing path',
+        type: 'string',
       },
+      silent: {
+        alias: 's',
+        describe: 'Entering non-interactive mode',
+        type: 'boolean',
+      },
+    },
+    async ({ path, silent }) => {
+      await install({ path, silent });
     }
-  );
-
-  return String(response.instancePath);
-};
-
-const tryStartInstance = async (instancePath: string) => {
-  const response = await prompts.default({
-    name: 'startInstance',
-    message: 'Would you like to start Logto now?',
-    type: 'confirm',
-    initial: true,
-  });
-
-  const yes = Boolean(response.startInstance);
-  const startCommand = `cd ${instancePath} && npm start`;
-
-  if (yes) {
-    execSync(startCommand, { stdio: 'inherit' });
-  } else {
-    log.info(`You can use ${startCommand} to start Logto. Happy hacking!`);
-  }
-};
-
-const downloadRelease = async () => {
-  const tarFilePath = path.resolve(os.tmpdir(), './logto.tar.gz');
-
-  log.info(`Download Logto to ${tarFilePath}`);
-  await downloadFile(
-    'https://github.com/logto-io/logto/releases/latest/download/logto.tar.gz',
-    tarFilePath
-  );
-
-  return tarFilePath;
-};
-
-const decompress = async (toPath: string, tarPath: string) => {
-  const decompressSpinner = ora({
-    text: `Decompress to ${toPath}`,
-    prefixText: chalk.blue('[info]'),
-  }).start();
-
-  try {
-    await mkdir(toPath);
-    await tar.extract({ file: tarPath, cwd: toPath, strip: 1 });
-  } catch {
-    decompressSpinner.fail();
-
-    return;
-  }
-
-  decompressSpinner.succeed();
-};
-
-const main = async () => {
-  validateNodeVersion();
-
-  const instancePath = await getInstancePath();
-  const tarPath = await downloadRelease();
-
-  await decompress(instancePath, tarPath);
-  await tryStartInstance(instancePath);
-};
-
-void main();
+  )
+  .demandCommand(1)
+  .showHelpOnFail(true)
+  .strict()
+  .parserConfiguration({
+    'dot-notation': false,
+  })
+  .parse();
