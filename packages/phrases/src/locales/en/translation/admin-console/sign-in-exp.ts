03a49c0b43eec99bcd29a2e6c4c70e2996efca44
Xiao Yijun <xiaoyijun@silverhand.io>
2022-10-08 18:16:34 +0800

    feat(console): delete custom phrase

--- packages/phrases/src/locales/en/translation/admin-console/sign-in-exp.ts
+++ packages/phrases/src/locales/en/translation/admin-console/sign-in-exp.ts
@@ -92,6 +92,13 @@ const sign_in_exp = {
       custom_values: 'Custom values',
       clear_all: 'Clear all',
       unsaved_description: 'Changes won’t be saved if you leave this page without saving.',
+      deletion_title: 'Do you want to delete the added language?',
+      deletion_description:
+        'After deletion, your users won’t be able to browse in that language again.',
+      default_language_deletion_title: 'Default language can’t be deleted.',
+      default_language_deletion_description:
+        '{{language}} is set as your default language and can’t be deleted. ',
+      got_it: 'Got It',
     },
     authentication: {
       title: 'AUTHENTICATION',
