cc5c99beb02537820da980d07d2e142db73bf1d5
Gao Sun <gao@silverhand.io>
2022-10-08 18:18:49 +0800

    refactor(cli): support dotenv

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -5,9 +5,15 @@ import { hideBin } from 'yargs/helpers';
 import database from './commands/database';
 import install from './commands/install';
 
-dotenv.config();
-
 void yargs(hideBin(process.argv))
+  .option('env', {
+    alias: ['e', 'env-file'],
+    describe: 'The path to your `.env` file',
+    type: 'string',
+  })
+  .middleware(({ env }) => {
+    dotenv.config({ path: env });
+  })
   .command(install)
   .command(database)
   .demandCommand(1)
