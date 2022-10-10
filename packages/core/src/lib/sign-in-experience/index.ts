a17c0ae81f6ab9a52f2b5f5807fc07e466b1a39a
wangsijie <wangsijie@silverhand.io>
2022-10-10 15:27:25 +0800

    refactor(core): split sign in experience lib

---
+++ packages/core/src/lib/sign-in-experience/index.ts
@@ -0,0 +1,20 @@
+import { Branding, BrandingStyle, TermsOfUse } from '@logto/schemas';
+
+import assertThat from '@/utils/assert-that';
+
+export * from './sign-in-methods';
+
+export const validateBranding = (branding: Branding) => {
+  if (branding.style === BrandingStyle.Logo_Slogan) {
+    assertThat(branding.slogan?.trim(), 'sign_in_experiences.empty_slogan');
+  }
+
+  assertThat(branding.logoUrl.trim(), 'sign_in_experiences.empty_logo');
+};
+
+export const validateTermsOfUse = (termsOfUse: TermsOfUse) => {
+  assertThat(
+    !termsOfUse.enabled || termsOfUse.contentUrl,
+    'sign_in_experiences.empty_content_url_of_terms_of_use'
+  );
+};
