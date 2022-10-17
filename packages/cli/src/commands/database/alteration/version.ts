c492058a0c70d05347dd8dc4513a0d7cb033a3e8
Gao Sun <gao@silverhand.io>
2022-10-17 23:36:16 +0800

    refactor(cli): supports `latest` version for db alteration  (#2177)

--- packages/cli/src/commands/database/alteration/version.ts
+++ packages/cli/src/commands/database/alteration/version.ts
@@ -42,7 +42,9 @@ export const chooseAlterationsByVersion = async (
     .filter((version, index, self) => index === self.findIndex((another) => eq(version, another)))
     .slice()
     .sort((i, j) => compare(j, i));
-  const initialSemVersion = conditional(initialVersion && new SemVer(initialVersion));
+  const initialSemVersion = conditional(
+    initialVersion && initialVersion !== latestTag && new SemVer(initialVersion)
+  );
 
   if (!versions[0]) {
     return [];
