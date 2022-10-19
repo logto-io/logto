28b4bfbacfc2655a6f5c17cfd174ff53de3baf04
Gao Sun <gao@silverhand.io>
2022-10-19 15:54:36 +0800

    fix: handle versioning when no `next-*.ts` found

--- packages/schemas/update-next.sh
+++ packages/schemas/update-next.sh
@@ -3,5 +3,7 @@ CURRENT_VERSION=$(node -p "require('./package.json').version.replaceAll('-', '_'
 cd alterations
 
 for x in next-*.ts;
-  do mv $x $CURRENT_VERSION$(echo ${x#next});
+do
+  [ -e "$x" ] || continue; # Skip when no file found (will still loop $x with "next-*.ts")
+  mv $x $CURRENT_VERSION$(echo ${x#next});
 done
