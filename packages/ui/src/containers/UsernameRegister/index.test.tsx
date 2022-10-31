ed5a27315edb3ebf96da5cafe5f7781d59eb961f
simeng-li <simeng@silverhand.io>
2022-10-31 16:15:14 +0800

    feat(ui): add username container
    add username register container

---
+++ packages/ui/src/containers/UsernameRegister/index.test.tsx
@@ -0,0 +1,117 @@
+import { fireEvent, act, waitFor } from '@testing-library/react';
+
+import renderWithPageContext from '@/__mocks__/RenderWithPageContext';
+import SettingsProvider from '@/__mocks__/RenderWithPageContext/SettingsProvider';
+import { verifyUsernameExistence } from '@/apis/register';
+
+import UsernameRegister from '.';
+
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedNavigate,
+}));
+
+jest.mock('@/apis/register', () => ({
+  verifyUsernameExistence: jest.fn(async () => ({})),
+}));
+
+describe('<UsernameRegister />', () => {
+  test('default render', () => {
+    const { queryByText, container } = renderWithPageContext(<UsernameRegister />);
+    expect(container.querySelector('input[name="new-username"]')).not.toBeNull();
+    expect(queryByText('action.create')).not.toBeNull();
+  });
+
+  test('render with terms settings enabled', () => {
+    const { queryByText } = renderWithPageContext(
+      <SettingsProvider>
+        <UsernameRegister />
+      </SettingsProvider>
+    );
+    expect(queryByText('description.terms_of_use')).not.toBeNull();
+  });
+
+  test('username are required', () => {
+    const { queryByText, getByText } = renderWithPageContext(<UsernameRegister />);
+    const submitButton = getByText('action.create');
+    fireEvent.click(submitButton);
+
+    expect(queryByText('username_required')).not.toBeNull();
+
+    expect(verifyUsernameExistence).not.toBeCalled();
+  });
+
+  test('username with initial numeric char should throw', () => {
+    const { queryByText, getByText, container } = renderWithPageContext(<UsernameRegister />);
+    const submitButton = getByText('action.create');
+
+    const usernameInput = container.querySelector('input[name="new-username"]');
+
+    if (usernameInput) {
+      fireEvent.change(usernameInput, { target: { value: '1username' } });
+    }
+
+    fireEvent.click(submitButton);
+
+    expect(queryByText('username_should_not_start_with_number')).not.toBeNull();
+
+    expect(verifyUsernameExistence).not.toBeCalled();
+
+    // Clear error
+    if (usernameInput) {
+      fireEvent.change(usernameInput, { target: { value: 'username' } });
+    }
+
+    expect(queryByText('username_should_not_start_with_number')).toBeNull();
+  });
+
+  test('username with special character should throw', () => {
+    const { queryByText, getByText, container } = renderWithPageContext(<UsernameRegister />);
+    const submitButton = getByText('action.create');
+    const usernameInput = container.querySelector('input[name="new-username"]');
+
+    if (usernameInput) {
+      fireEvent.change(usernameInput, { target: { value: '@username' } });
+    }
+
+    fireEvent.click(submitButton);
+
+    expect(queryByText('username_valid_charset')).not.toBeNull();
+
+    expect(verifyUsernameExistence).not.toBeCalled();
+
+    // Clear error
+    if (usernameInput) {
+      fireEvent.change(usernameInput, { target: { value: 'username' } });
+    }
+
+    expect(queryByText('username_valid_charset')).toBeNull();
+  });
+
+  test('submit form properly with terms settings enabled', async () => {
+    const { getByText, container } = renderWithPageContext(
+      <SettingsProvider>
+        <UsernameRegister />
+      </SettingsProvider>
+    );
+    const submitButton = getByText('action.create');
+    const usernameInput = container.querySelector('input[name="new-username"]');
+
+    if (usernameInput) {
+      fireEvent.change(usernameInput, { target: { value: 'username' } });
+    }
+
+    const termsButton = getByText('description.agree_with_terms');
+    fireEvent.click(termsButton);
+
+    act(() => {
+      fireEvent.click(submitButton);
+    });
+
+    await waitFor(() => {
+      expect(verifyUsernameExistence).toBeCalledWith('username');
+    });
+  });
+});
--- packages/ui/src/containers/UsernameRegister/index.tsx
+++ packages/ui/src/containers/UsernameRegister/index.tsx
@@ -1,9 +1,103 @@
+import classNames from 'classnames';
+import { useEffect, useCallback, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
+import { verifyUsernameExistence } from '@/apis/register';
+import Button from '@/components/Button';
+import Input from '@/components/Input';
+import TermsOfUse from '@/containers/TermsOfUse';
+import type { ErrorHandlers } from '@/hooks/use-api';
+import useApi from '@/hooks/use-api';
+import useForm from '@/hooks/use-form';
+import useTerms from '@/hooks/use-terms';
+import { usernameValidation } from '@/utils/field-validations';
+
+import * as styles from './index.module.scss';
+
 type Props = {
   className?: string;
 };
 
-const UsernameRegister = ({ className }: Props) => (
-  <div className={className}>username register</div>
-);
+type FieldState = {
+  username: string;
+};
+
+const defaultState: FieldState = {
+  username: '',
+};
+
+const UsernameRegister = ({ className }: Props) => {
+  const { t } = useTranslation();
+  const { termsValidation } = useTerms();
+  const navigate = useNavigate();
+
+  const {
+    fieldValue,
+    setFieldValue,
+    setFieldErrors,
+    register: fieldRegister,
+    validateForm,
+  } = useForm(defaultState);
+
+  const errorHandlers: ErrorHandlers = useMemo(
+    () => ({
+      'user.username_exists_register': () => {
+        setFieldErrors((state) => ({
+          ...state,
+          username: 'username_exists',
+        }));
+      },
+    }),
+    [setFieldErrors]
+  );
+
+  const { result, run: asyncVerifyUsername } = useApi(verifyUsernameExistence, errorHandlers);
+
+  const onSubmitHandler = useCallback(
+    async (event?: React.FormEvent<HTMLFormElement>) => {
+      event?.preventDefault();
+
+      if (!validateForm()) {
+        return;
+      }
+
+      if (!(await termsValidation())) {
+        return;
+      }
+
+      void asyncVerifyUsername(fieldValue.username);
+    },
+    [validateForm, termsValidation, asyncVerifyUsername, fieldValue]
+  );
+
+  useEffect(() => {
+    if (result) {
+      navigate('/register/password');
+    }
+  }, [navigate, result]);
+
+  return (
+    <form className={classNames(styles.form, className)} onSubmit={onSubmitHandler}>
+      <div className={styles.formFields}>
+        <Input
+          className={styles.inputField}
+          name="new-username"
+          placeholder={t('input.username')}
+          {...fieldRegister('username', usernameValidation)}
+          onClear={() => {
+            setFieldValue((state) => ({ ...state, username: '' }));
+          }}
+        />
+      </div>
+
+      <TermsOfUse className={styles.terms} />
+
+      <Button title="action.create" onClick={async () => onSubmitHandler()} />
+
+      <input hidden type="submit" />
+    </form>
+  );
+};
 
 export default UsernameRegister;
