f79e79d07b019721d96f29152546ac55c0d90a96
simeng-li <simeng@silverhand.io>
2022-11-09 18:31:19 +0800

    feat(ui): add set email and phone passcode validation hooks
    add set email and phone passcode validation hooks

---
+++ packages/ui/src/containers/PasscodeValidation/use-continue-set-sms-passcode-validation.ts
@@ -0,0 +1,72 @@
+import { SignInIdentifier } from '@logto/schemas';
+import { useMemo, useCallback } from 'react';
+
+import { verifyContinueSetSmsPasscode, continueWithPhone } from '@/apis/continue';
+import type { ErrorHandlers } from '@/hooks/use-api';
+import useApi from '@/hooks/use-api';
+import useRequiredProfileErrorHandler from '@/hooks/use-required-profile-error-handler';
+import { UserFlow, SearchParameters } from '@/types';
+import { getSearchParameters } from '@/utils';
+
+import useIdentifierErrorAlert from './use-identifier-error-alert';
+import useSharedErrorHandler from './use-shared-error-handler';
+
+const useContinueSetSmsPasscodeValidation = (phone: string, errorCallback?: () => void) => {
+  const { sharedErrorHandlers, errorMessage, clearErrorMessage } = useSharedErrorHandler();
+
+  const requiredProfileErrorHandler = useRequiredProfileErrorHandler(true);
+
+  const identifierNotExistErrorHandler = useIdentifierErrorAlert(
+    UserFlow.continue,
+    SignInIdentifier.Sms,
+    phone
+  );
+
+  const verifyPasscodeErrorHandlers: ErrorHandlers = useMemo(
+    () => ({
+      ...sharedErrorHandlers,
+      callback: errorCallback,
+    }),
+    [errorCallback, sharedErrorHandlers]
+  );
+
+  const { run: verifyPasscode } = useApi(verifyContinueSetSmsPasscode, verifyPasscodeErrorHandlers);
+
+  const setPhoneErrorHandlers: ErrorHandlers = useMemo(
+    () => ({
+      'user.phone_not_exists': identifierNotExistErrorHandler,
+      ...requiredProfileErrorHandler,
+      callback: errorCallback,
+    }),
+    [errorCallback, identifierNotExistErrorHandler, requiredProfileErrorHandler]
+  );
+
+  const { run: setPhone } = useApi(continueWithPhone, setPhoneErrorHandlers);
+
+  const onSubmit = useCallback(
+    async (code: string) => {
+      const verified = await verifyPasscode(phone, code);
+
+      if (!verified) {
+        return;
+      }
+
+      const socialToBind = getSearchParameters(location.search, SearchParameters.bindWithSocial);
+
+      const result = await setPhone(phone, socialToBind);
+
+      if (result?.redirectTo) {
+        window.location.replace(result.redirectTo);
+      }
+    },
+    [phone, setPhone, verifyPasscode]
+  );
+
+  return {
+    errorMessage,
+    clearErrorMessage,
+    onSubmit,
+  };
+};
+
+export default useContinueSetSmsPasscodeValidation;
