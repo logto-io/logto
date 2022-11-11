9991f02596903f15bea2161961a703e8538d87c6
Gao Sun <gao@silverhand.io>
2022-11-11 20:39:08 +0800

    chore: add hooks alteration scripts

---
+++ packages/schemas/alterations/next-1668170095-hooks.ts
@@ -0,0 +1,26 @@
+import { sql } from 'slonik';
+
+import type { AlterationScript } from '../lib/types/alteration';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      create table hooks (
+        id varchar(128) not null,
+        event varchar(128) not null,
+        config jsonb /* @use HookConfig */ not null,
+        created_at timestamptz not null default(now()),
+        primary key (id)
+      );
+
+      create index hooks__event on hooks (event);
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      drop table hooks;
+    `);
+  },
+};
+
+export default alteration;
