7e0b4fc01c7aacda5a141d544e71cca4a8b77f9c
Gao Sun <gao@silverhand.io>
2022-11-11 14:29:35 +0800

    chore: add semver tag for core package

--- .scripts/publish.js
+++ .scripts/publish.js
@@ -1,6 +1,17 @@
+/**
+ * This script runs the following tasks:
+ * 
+ * 1. Tag main packages defined in `.changeset/config.json` if they are not tagged with the current version in `package.json`;
+ * 2. If no new git tag added, exit;
+ * 3. If at least one new git tag found, run `pnpm -r publish` and `git push --tags`.
+ * 
+ * The subsequential release tasks, such as create GitHub release and build Docker image, will be took over by GitHub workflows.
+ */
+
 const { execSync } = require('child_process');
 const changesetConfig = require('../.changeset/config.json');
 
+const corePackageName = '@logto/core';
 /** @type {Array<{ name: string; version?: string; path: string; private: boolean; }>} */
 const allPackages = JSON.parse(execSync('pnpm recursive list --depth=-1 --json', { encoding: 'utf8' }));
 const mainPackages = [...changesetConfig.fixed, ...changesetConfig.linked].map(([first]) => first);
@@ -28,6 +39,13 @@ const taggedPackages = mainPackages
 
     execSync(`git tag -a ${tag} -m'${tag}'`);
     console.log(`Tag ${tag} added`);
+
+    if (packageName === corePackageName) {
+      const semver = 'v' + version;
+      execSync(`git tag -a ${semver} -m'${semver}'`);
+      console.log(`Tag ${semver} added (SemVer of core package ${corePackageName})`);
+    }
+
     return packageName;
   })
   .filter((value) => !!value);
