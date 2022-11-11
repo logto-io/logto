9c45087a0f2893b29fc3f3572e3c05158503172b
Darcy Ye <darcyye@silverhand.io>
2022-11-11 19:16:34 +0800

    feat(schemas): update connector_instances table and add db alteration script (#2417)

--- packages/schemas/tables/connectors.sql
+++ packages/schemas/tables/connectors.sql
@@ -1,7 +1,10 @@
 create table connectors (
   id varchar(128) not null,
   enabled boolean not null default FALSE,
+  sync_profile boolean not null default FALSE,
+  connector_id varchar(128) not null,
   config jsonb /* @use ArbitraryObject */ not null default '{}'::jsonb,
+  metadata jsonb /* @use ConfigurableConnectorMetadata */ not null default '{}'::jsonb,
   created_at timestamptz not null default(now()),
   primary key (id)
 );
