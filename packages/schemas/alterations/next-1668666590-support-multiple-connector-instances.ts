76a3b82f8c2910bc97801915fa0e77728f1203a4
Darcy Ye <darcyye@silverhand.io>
2022-11-24 14:09:53 +0800

    feat: should not modify previous alteration scripts

--- packages/schemas/alterations/next-1668666590-support-multiple-connector-instances.ts
+++ packages/schemas/alterations/next-1668666590-support-multiple-connector-instances.ts
@@ -5,8 +5,6 @@ import type { AlterationScript } from '../lib/types/alteration.js';
 const alteration: AlterationScript = {
   up: async (pool) => {
     await pool.query(sql`
-      DELETE FROM connectors WHERE enabled = false;
-      ALTER TABLE connectors DROP COLUMN enabled;
       ALTER TABLE connectors ADD COLUMN sync_profile boolean NOT NULL DEFAULT false;
       ALTER TABLE connectors ADD COLUMN connector_id varchar(128);
       UPDATE connectors SET connector_id = id;
@@ -16,9 +14,7 @@ const alteration: AlterationScript = {
   },
   down: async (pool) => {
     await pool.query(sql`
-      ALTER TABLE connectors ADD COLUMN enabled boolean NOT NULL DEFAULT false;
       DELETE FROM connectors WHERE id <> connector_id;
-      UPDATE connectors SET enabled = true;
       ALTER TABLE connectors DROP COLUMN metadata;
       ALTER TABLE connectors DROP COLUMN connector_id;
       ALTER TABLE connectors DROP COLUMN sync_profile;
