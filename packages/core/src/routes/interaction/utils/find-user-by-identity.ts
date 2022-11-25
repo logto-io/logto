be75a6d311cac99b274277668e162819d2bf5f8e
simeng-li <simeng@silverhand.io>
2022-11-25 14:53:02 +0800

    fix(core): hide user not exsit error
    hide user not exsit error

--- packages/core/src/routes/interaction/utils/find-user-by-identity.ts
+++ packages/core/src/routes/interaction/utils/find-user-by-identity.ts
@@ -1,12 +1,8 @@
-import type { User } from '@logto/schemas';
-
-import RequestError from '#src/errors/RequestError/index.js';
 import { findUserByEmail, findUserByUsername, findUserByPhone } from '#src/queries/user.js';
-import assertThat from '#src/utils/assert-that.js';
 
 import type { UserIdentity } from '../types/guard.js';
 
-const findUser = (identity: UserIdentity) => {
+export default async function findUserByIdentity(identity: UserIdentity) {
   if ('username' in identity) {
     return findUserByUsername(identity.username);
   }
@@ -19,14 +15,5 @@ const findUser = (identity: UserIdentity) => {
     return findUserByPhone(identity.phone);
   }
 
-  // TODO: social identity
-
   return null;
-};
-
-export default async function findUserByIdentity(identity: UserIdentity): Promise<User> {
-  const user = await findUser(identity);
-  assertThat(user, new RequestError({ code: 'user.user_not_exist', status: 404 }));
-
-  return user;
 }
--- packages/core/src/routes/interaction/utils/verify-user.ts
+++ packages/core/src/routes/interaction/utils/verify-user.ts
@@ -7,6 +7,8 @@ import findUserByIdentity from './find-user-by-identity.js';
 
 export const verifyUserByVerifiedPasscodeIdentity = async (identifier: UserIdentity) => {
   const user = await findUserByIdentity(identifier);
+  assertThat(user, new RequestError({ code: 'user.user_not_exist', status: 404 }));
+
   const { id, isSuspended } = user;
   assertThat(!isSuspended, new RequestError({ code: 'user.suspended', status: 401 }));
 
@@ -19,6 +21,7 @@ export const verifyUserByIdentityAndPassword = async ({
 }: PasswordIdentifierPayload) => {
   const user = await findUserByIdentity(identity);
   const verifiedUser = await verifyUserPassword(user, password);
+
   const { isSuspended, id } = verifiedUser;
   assertThat(!isSuspended, new RequestError({ code: 'user.suspended', status: 401 }));
 
