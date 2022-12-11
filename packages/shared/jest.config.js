1d1e6e3244fd3a4459a911aa1827c27f38e4121e
Gao Sun <gao@silverhand.io>
2022-12-11 20:07:21 +0800

    test: update to native ESM (batch 1)

---
+++ packages/shared/jest.config.js
@@ -0,0 +1,10 @@
+const config = {
+  coveragePathIgnorePatterns: ['/node_modules/', '/src/__mocks__/'],
+  coverageReporters: ['text-summary', 'lcov'],
+  roots: ['./lib'],
+  moduleNameMapper: {
+    '^(chalk|inquirer)$': '<rootDir>/../shared/lib/esm/module-proxy.js',
+  },
+};
+
+export default config;
