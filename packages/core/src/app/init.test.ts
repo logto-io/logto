a862697f27af51e44266ceecf520541922df0260
Gao Sun <gao@silverhand.io>
2022-12-12 13:20:17 +0800

    refactor: code quality

--- packages/core/src/app/init.test.ts
+++ packages/core/src/app/init.test.ts
@@ -6,20 +6,18 @@ import { emptyMiddleware } from '#src/utils/test-utils.js';
 const { jest } = import.meta;
 
 const middlewareList = [
-  jest.fn(() => emptyMiddleware),
-  jest.fn(() => emptyMiddleware),
-  jest.fn(() => emptyMiddleware),
-  jest.fn(() => emptyMiddleware),
-  jest.fn(() => emptyMiddleware),
-  jest.fn(() => emptyMiddleware),
-] as const;
+  'error-handler',
+  'i18next',
+  'log',
+  'oidc-error-handler',
+  'slonik-error-handler',
+  'spa-proxy',
+].map((name) => {
+  const mock = jest.fn(() => emptyMiddleware);
+  mockEsmDefault(`#src/middleware/koa-${name}.js`, () => mock);
 
-mockEsmDefault('#src/middleware/koa-error-handler.js', () => middlewareList[0]);
-mockEsmDefault('#src/middleware/koa-i18next.js', () => middlewareList[1]);
-mockEsmDefault('#src/middleware/koa-log.js', () => middlewareList[2]);
-mockEsmDefault('#src/middleware/koa-oidc-error-handler.js', () => middlewareList[3]);
-mockEsmDefault('#src/middleware/koa-slonik-error-handler.js', () => middlewareList[4]);
-mockEsmDefault('#src/middleware/koa-spa-proxy.js', () => middlewareList[5]);
+  return mock;
+});
 
 const initI18n = await pickDefault(import('../i18n/init.js'));
 const initApp = await pickDefault(import('./init.js'));
