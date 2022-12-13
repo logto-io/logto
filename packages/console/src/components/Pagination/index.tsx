af9c69385be4102899f0f4599cb2b07c4b3b2791
Xiao Yijun <xiaoyijun@silverhand.io>
2022-12-13 17:22:39 +0800

    refactor(console): keep pagination when the table is loading

--- packages/console/src/components/Pagination/index.tsx
+++ packages/console/src/components/Pagination/index.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { useMemo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import ReactPaginate from 'react-paginate';
 
@@ -10,7 +11,7 @@ import * as styles from './index.module.scss';
 
 type Props = {
   pageIndex: number;
-  totalCount: number;
+  totalCount?: number;
   pageSize: number;
   className?: string;
   onChange?: (pageIndex: number) => void;
@@ -18,19 +19,41 @@ type Props = {
 
 const Pagination = ({ pageIndex, totalCount, pageSize, className, onChange }: Props) => {
   const { t } = useTranslation(undefined, { keyPrefix: 'admin_console' });
-  const pageCount = Math.ceil(totalCount / pageSize);
+
+  /**
+   * Note:
+   * The `totalCount` will become `undefined` temporarily when fetching data on page changes, and this causes the pagination to disappear.
+   * Cache `totalCount` to solve this problem.
+   */
+  const totalCountRef = useRef(totalCount);
+  const cachedTotalCount = useMemo(() => {
+    if (totalCount === undefined) {
+      if (totalCountRef.current === undefined) {
+        return 0;
+      }
+
+      return totalCountRef.current;
+    }
+
+    // eslint-disable-next-line @silverhand/fp/no-mutation
+    totalCountRef.current = totalCount;
+
+    return totalCount;
+  }, [totalCount]);
+
+  const pageCount = Math.ceil(cachedTotalCount / pageSize);
 
   if (pageCount <= 1) {
     return null;
   }
 
   const min = (pageIndex - 1) * pageSize + 1;
-  const max = Math.min(pageIndex * pageSize, totalCount);
+  const max = Math.min(pageIndex * pageSize, cachedTotalCount);
 
   return (
     <div className={classNames(styles.container, className)}>
       <div className={styles.positionInfo}>
-        {t('general.page_info', { min, max, total: totalCount })}
+        {t('general.page_info', { min, max, total: cachedTotalCount })}
       </div>
       <ReactPaginate
         className={styles.pagination}
