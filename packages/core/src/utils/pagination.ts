ac359e624410fe8c10b56544ae8808faac9c8416
Gao Sun <gao@silverhand.io>
2022-12-14 17:09:31 +0800

    refactor(core): remove `query-string`

--- packages/core/src/utils/pagination.ts
+++ packages/core/src/utils/pagination.ts
@@ -1,5 +1,4 @@
 import type { Request } from 'koa';
-import { stringify } from 'query-string';
 
 type LinkRelationType = 'first' | 'prev' | 'next' | 'last';
 
@@ -10,5 +9,8 @@ export const buildLink = (
 ): string => {
   const baseUrl = `${request.origin}${request.path}`;
 
-  return `<${baseUrl}?${stringify({ ...request.query, page })}>; rel="${type}"`;
+  return `<${baseUrl}?${new URLSearchParams({
+    ...request.query,
+    page: String(page),
+  }).toString()}>; rel="${type}"`;
 };
