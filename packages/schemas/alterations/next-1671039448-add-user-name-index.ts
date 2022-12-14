47db9a1c55d8d2fa9b40673699ff7960f6fbe1b3
Gao Sun <gao@silverhand.io>
2022-12-15 01:38:32 +0800

    refactor(schemas): add name index on users

---
+++ packages/schemas/alterations/next-1671039448-add-user-name-index.ts
@@ -0,0 +1,18 @@
+import { sql } from 'slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      create index users__name on users (name);
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      drop index users__name;
+    `);
+  },
+};
+
+export default alteration;
