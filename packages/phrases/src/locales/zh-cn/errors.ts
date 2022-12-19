5dd2eef34c2096ee25f713e2ebb274237893d96a
Charles Zhao <charleszhao@silverhand.io>
2022-12-20 00:01:05 +0800

    chore(phrases): improve Chinese translation (#2684)

--- packages/phrases/src/locales/zh-cn/errors.ts
+++ packages/phrases/src/locales/zh-cn/errors.ts
@@ -3,8 +3,8 @@ const errors = {
     invalid_input: 'Input is invalid. {{details}}', // UNTRANSLATED
   },
   auth: {
-    authorization_header_missing: '缺少权限标题',
-    authorization_token_type_not_supported: '权限类型不支持',
+    authorization_header_missing: 'Authorization 请求头缺失。',
+    authorization_token_type_not_supported: 'Authorization token 类型不支持',
     unauthorized: '未经授权。请检查凭据及其范围。',
     forbidden: '禁止访问。请检查用户 role 与权限。',
     expected_role_not_found: '未找到期望的 role。请检查用户 role 与权限。',
