2ab5e85e188a2ceb95acac8ffe9336a8a5704064
Gao Sun <gao@silverhand.io>
2022-12-25 15:32:54 +0800

    refactor: add license and comment

--- packages/shared/src/esm/mock-esm.ts
+++ packages/shared/src/esm/mock-esm.ts
@@ -28,6 +28,17 @@ export const pickDefault = async <T extends Record<'default', unknown>>(
 };
 
 // See https://github.com/sindresorhus/callsites
+/*
+MIT License
+
+Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (https://sindresorhus.com)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 /* eslint-disable @silverhand/fp/no-mutation */
 const callSites = (): NodeJS.CallSite[] => {
   const _prepareStackTrace = Error.prepareStackTrace;
@@ -58,6 +69,7 @@ export const createMockUtils = (jestInstance: WithEsmMock) => {
   };
 
   const mockEsmWithActual = async <T>(...[moduleName, factory]: MockParameters<T>): Promise<T> => {
+    // Remove this when we upgrade Node.js to v19+ since we can leverage `import.meta.resolve()` to manually resolve a module
     const resolved = moduleName.startsWith('.')
       ? path.join(path.dirname(callSites()[1]?.getFileName() ?? ''), moduleName)
       : moduleName;
