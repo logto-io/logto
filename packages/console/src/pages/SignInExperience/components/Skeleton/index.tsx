2d3f8404a6fee06b9c69a5f2989f8478a11a263c
Xiao Yijun <xiaoyijun@silverhand.io>
2022-12-28 12:01:56 +0800

    refactor(console): update loading skeletons

--- packages/console/src/pages/SignInExperience/components/Skeleton/index.tsx
+++ packages/console/src/pages/SignInExperience/components/Skeleton/index.tsx
@@ -1,48 +1,55 @@
+import classNames from 'classnames';
+
 import CardTitle from '@/components/CardTitle';
 import Spacer from '@/components/Spacer';
 
+import * as pageStyles from '../../index.module.scss';
 import * as styles from './index.module.scss';
 
 const Skeleton = () => {
   return (
-    <div className={styles.container}>
+    <div className={classNames(pageStyles.container, styles.container)}>
       <CardTitle
         title="sign_in_exp.title"
         subtitle="sign_in_exp.description"
-        className={styles.cardTitle}
+        className={pageStyles.cardTitle}
       />
-      <div className={styles.tabBar} />
-      <div className={styles.content}>
-        <div className={styles.setup}>
-          <div className={styles.card}>
-            <div className={styles.title} />
-            <div className={styles.field} />
-            <div className={styles.field} />
-          </div>
-          <div className={styles.card}>
-            <div className={styles.title} />
-            <div className={styles.field} />
-            <div className={styles.field} />
-          </div>
-        </div>
-        <div className={styles.preview}>
-          <div className={styles.header}>
-            <div className={styles.info}>
+      <div className={classNames(pageStyles.tabs, styles.tabBar)} />
+      <div className={classNames(pageStyles.content, styles.content)}>
+        <div className={pageStyles.contentTop}>
+          <div className={pageStyles.form}>
+            <div className={styles.card}>
               <div className={styles.title} />
-              <div className={styles.subtitle} />
+              <div className={styles.field} />
+              <div className={styles.field} />
             </div>
-            <Spacer />
-            <div className={styles.button} />
-            <div className={styles.button} />
+            <div className={styles.card}>
+              <div className={styles.title} />
+              <div className={styles.field} />
+              <div className={styles.field} />
+            </div>
           </div>
-          <div className={styles.previewContent}>
-            <div className={styles.mobile}>
-              <div className={styles.logo} />
-              <div className={styles.slogan} />
-              <div className={styles.field} />
-              <div className={styles.field} />
-              <div className={styles.button} />
-              <div className={styles.social} />
+          <div className={pageStyles.preview}>
+            <div className={styles.preview}>
+              <div className={styles.header}>
+                <div className={styles.info}>
+                  <div className={styles.title} />
+                  <div className={styles.subtitle} />
+                </div>
+                <Spacer />
+                <div className={styles.button} />
+                <div className={styles.button} />
+              </div>
+              <div className={styles.previewContent}>
+                <div className={styles.mobile}>
+                  <div className={styles.logo} />
+                  <div className={styles.slogan} />
+                  <div className={styles.field} />
+                  <div className={styles.field} />
+                  <div className={styles.button} />
+                  <div className={styles.social} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
