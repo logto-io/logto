2d9eaaece1e305757e441884214d2c77c5cddb80
Xiao Yijun <xiaoyijun@silverhand.io>
2022-12-28 11:47:17 +0800

    refactor(console): update loading skeletons

--- packages/console/src/components/DetailsSkeleton/index.tsx
+++ packages/console/src/components/DetailsSkeleton/index.tsx
@@ -1,3 +1,7 @@
+import classNames from 'classnames';
+
+import Card from '@/components/Card';
+import * as formCardStyles from '@/components/FormCard/index.module.scss';
 import Spacer from '@/components/Spacer';
 
 import * as styles from './index.module.scss';
@@ -14,8 +18,8 @@ const DetailsSkeleton = () => (
       <div className={styles.button} />
     </div>
     <div className={styles.tabBar} />
-    <div className={styles.content}>
-      <div className={styles.introduction}>
+    <Card className={classNames(formCardStyles.container, styles.content)}>
+      <div className={classNames(formCardStyles.introduction, styles.introduction)}>
         <div className={styles.title} />
         <div className={styles.description}>
           {Array.from({ length: 2 }).map((_, index) => (
@@ -24,13 +28,13 @@ const DetailsSkeleton = () => (
           ))}
         </div>
       </div>
-      <div className={styles.fields}>
+      <div className={classNames(formCardStyles.form, styles.form)}>
         {Array.from({ length: 4 }).map((_, index) => (
           // eslint-disable-next-line react/no-array-index-key
           <div key={index} className={styles.field} />
         ))}
       </div>
-    </div>
+    </Card>
   </div>
 );
 
