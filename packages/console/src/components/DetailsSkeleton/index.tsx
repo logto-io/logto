fd4bd8a2df53d3fa1c6658fffb4ebc68c040c6c2
Xiao Yijun <xiaoyijun@silverhand.io>
2022-12-28 14:22:35 +0800

    refactor(console): update loading skeletons (#2660)

--- packages/console/src/components/DetailsSkeleton/index.tsx
+++ packages/console/src/components/DetailsSkeleton/index.tsx
@@ -1,3 +1,7 @@
+import classNames from 'classnames';
+
+import Card from '@/components/Card';
+import * as formCardStyles from '@/components/FormCard/index.module.scss';
 import Spacer from '@/components/Spacer';
 
 import * as styles from './index.module.scss';
@@ -13,17 +17,24 @@ const DetailsSkeleton = () => (
       <Spacer />
       <div className={styles.button} />
     </div>
-    <div className={styles.content}>
-      <div className={styles.tabBar} />
-      {Array.from({ length: 4 }).map((_, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <div key={index} className={styles.field} />
-      ))}
-      <Spacer />
-      <div className={styles.footer}>
-        <div className={styles.button} />
+    <div className={styles.tabBar} />
+    <Card className={classNames(formCardStyles.container, styles.content)}>
+      <div className={classNames(formCardStyles.introduction, styles.introduction)}>
+        <div className={styles.title} />
+        <div className={styles.description}>
+          {Array.from({ length: 2 }).map((_, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={index} className={styles.text} />
+          ))}
+        </div>
       </div>
-    </div>
+      <div className={classNames(formCardStyles.form, styles.form)}>
+        {Array.from({ length: 4 }).map((_, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <div key={index} className={styles.field} />
+        ))}
+      </div>
+    </Card>
   </div>
 );
 
--- packages/console/src/pages/SignInExperience/components/Skeleton/index.tsx
+++ packages/console/src/pages/SignInExperience/components/Skeleton/index.tsx
@@ -1,41 +1,56 @@
+import classNames from 'classnames';
+
+import CardTitle from '@/components/CardTitle';
 import Spacer from '@/components/Spacer';
 
+import * as pageStyles from '../../index.module.scss';
 import * as styles from './index.module.scss';
 
 const Skeleton = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.setup}>
-        <div className={styles.title} />
-        <div className={styles.subtitle} />
-        <div className={styles.tabs} />
-        <div className={styles.field} />
-        <div className={styles.field} />
-        <div className={styles.field} />
-        <div className={styles.field} />
-        <Spacer />
-        <div className={styles.footer}>
-          <div className={styles.button} />
-        </div>
-      </div>
-      <div className={styles.preview}>
-        <div className={styles.header}>
-          <div className={styles.info}>
-            <div className={styles.title} />
-            <div className={styles.subtitle} />
+    <div className={classNames(pageStyles.container, styles.container)}>
+      <CardTitle
+        title="sign_in_exp.title"
+        subtitle="sign_in_exp.description"
+        className={pageStyles.cardTitle}
+      />
+      <div className={classNames(pageStyles.tabs, styles.tabBar)} />
+      <div className={classNames(pageStyles.content, styles.content)}>
+        <div className={pageStyles.contentTop}>
+          <div className={pageStyles.form}>
+            <div className={styles.card}>
+              <div className={styles.title} />
+              <div className={styles.field} />
+              <div className={styles.field} />
+            </div>
+            <div className={styles.card}>
+              <div className={styles.title} />
+              <div className={styles.field} />
+              <div className={styles.field} />
+            </div>
           </div>
-          <Spacer />
-          <div className={styles.button} />
-          <div className={styles.button} />
-        </div>
-        <div className={styles.content}>
-          <div className={styles.mobile}>
-            <div className={styles.logo} />
-            <div className={styles.slogan} />
-            <div className={styles.field} />
-            <div className={styles.field} />
-            <div className={styles.button} />
-            <div className={styles.social} />
+          <div className={pageStyles.preview}>
+            <div className={styles.preview}>
+              <div className={styles.header}>
+                <div className={styles.info}>
+                  <div className={styles.title} />
+                  <div className={styles.subtitle} />
+                </div>
+                <Spacer />
+                <div className={styles.button} />
+                <div className={styles.button} />
+              </div>
+              <div className={styles.previewContent}>
+                <div className={styles.mobile}>
+                  <div className={styles.logo} />
+                  <div className={styles.slogan} />
+                  <div className={styles.field} />
+                  <div className={styles.field} />
+                  <div className={styles.button} />
+                  <div className={styles.social} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
