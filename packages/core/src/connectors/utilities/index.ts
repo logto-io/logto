912cc70fe549b2059447a90f935abbefb8f72701
Darcy Ye <darcyye@silverhand.io>
2022-12-29 14:44:49 +0800

    feat: support png and jpeg for connector logo

--- packages/core/src/connectors/utilities/index.ts
+++ packages/core/src/connectors/utilities/index.ts
@@ -34,16 +34,48 @@ export function validateConnectorModule(
   }
 }
 
-export const readUrl = async (
-  url: string,
-  baseUrl: string,
-  type: 'text' | 'svg'
-): Promise<string> => {
+type UrlType = 'text' | 'svg' | 'png' | 'jpeg' | undefined;
+
+const getUrlType = (url: string): UrlType => {
+  const splitUrl = url.split('/').filter(Boolean);
+
+  assertThat(
+    splitUrl.length > 1,
+    new ConnectorError(ConnectorErrorCodes.InvalidMetadata, { message: `Invalid url: ${url}.` })
+  );
+
+  const splitBaseUrl = splitUrl.slice(-1)[0]?.split('.').filter(Boolean);
+  const extension = splitBaseUrl && splitBaseUrl.length > 1 ? splitBaseUrl.slice(-1)[0] : undefined;
+
+  if (!extension) {
+    return undefined; // Short circuit for parsed URL.
+  }
+
+  if (extension === 'png' || extension === 'svg') {
+    return extension;
+  }
+
+  if (extension === 'jpg' || extension === 'jpeg') {
+    return 'jpeg';
+  }
+
+  if (extension === 'md' || extension === 'json' || extension === 'txt') {
+    return 'text';
+  }
+
+  throw new ConnectorError(ConnectorErrorCodes.InvalidMetadata, {
+    message: `Invalid url: ${url} with unsupported file type.`,
+  });
+};
+
+export const readUrl = async (url: string, baseUrl: string, type?: UrlType): Promise<string> => {
   if (!url) {
     return url;
   }
 
-  if (type !== 'text' && url.startsWith('http')) {
+  const urlType = type ?? getUrlType(url);
+
+  if (!urlType || (urlType !== 'text' && url.startsWith('http'))) {
     return url;
   }
 
@@ -51,10 +83,12 @@ export const readUrl = async (
     return url;
   }
 
-  if (type === 'svg') {
+  if (urlType === 'svg' || urlType === 'jpeg' || urlType === 'png') {
     const data = await readFile(path.join(baseUrl, url));
 
-    return `data:image/svg+xml;base64,${data.toString('base64')}`;
+    const imageTypes = urlType === 'svg' ? 'svg+xml' : urlType;
+
+    return `data:image/${imageTypes};base64,${data.toString('base64')}`;
   }
 
   return readFile(path.join(baseUrl, url), 'utf8');
@@ -63,8 +97,8 @@ export const readUrl = async (
 export const parseMetadata = async (metadata: AllConnector['metadata'], packagePath: string) => {
   return {
     ...metadata,
-    logo: await readUrl(metadata.logo, packagePath, 'svg'),
-    logoDark: metadata.logoDark && (await readUrl(metadata.logoDark, packagePath, 'svg')),
+    logo: await readUrl(metadata.logo, packagePath),
+    logoDark: metadata.logoDark && (await readUrl(metadata.logoDark, packagePath)),
     readme: await readUrl(metadata.readme, packagePath, 'text'),
     configTemplate: await readUrl(metadata.configTemplate, packagePath, 'text'),
   };
