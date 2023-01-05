1c30b8b31abe3538600c520e9c7976b534f4430b
Xiao Yijun <xiaoyijun@silverhand.io>
2023-01-05 16:47:42 +0800

    feat(console): delete role

--- packages/phrases/src/locales/en/translation/admin-console/role-details.ts
+++ packages/phrases/src/locales/en/translation/admin-console/role-details.ts
@@ -1,7 +1,8 @@
 const role_details = {
   back_to_roles: 'Back to Roles', // UNTRANSLATED
   identifier: 'Identifier', // UNTRANSLATED
-  delete_description: 'This action cannot be undone. It will permanently delete the role.', // UNTRANSLATED
+  delete_description:
+    'Doing so will remove the permissions associated with this role from the affected users and delete the mapping between roles, users, and permissions.',
   role_deleted: 'The role {{name}} has been successfully deleted', // UNTRANSLATED
   settings_tab: 'Settings', // UNTRANSLATED
   users_tab: 'Users', // UNTRANSLATED
