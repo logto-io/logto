0958b7305da3615b9be507a5a4e4f54d83d6752f
Xiao Yijun <xiaoyijun@silverhand.io>
2023-01-06 11:24:56 +0800

    feat(console): create permission

--- packages/console/src/pages/ApiResourceDetails/ApiResourcePermissions/index.tsx
+++ packages/console/src/pages/ApiResourceDetails/ApiResourcePermissions/index.tsx
@@ -1,15 +1,20 @@
 import type { Scope } from '@logto/schemas';
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 import { useTranslation } from 'react-i18next';
 import { useOutletContext } from 'react-router-dom';
 import useSWR from 'swr';
 
 import Delete from '@/assets/images/delete.svg';
+import Plus from '@/assets/images/plus.svg';
 import Button from '@/components/Button';
 import IconButton from '@/components/IconButton';
+import Search from '@/components/Search';
 import Table from '@/components/Table';
 import type { RequestError } from '@/hooks/use-api';
 
 import type { ApiResourceDetailsOutletContext } from '../types';
+import CreatePermissionModal from './components/CreatePermissionModal';
 import * as styles from './index.module.scss';
 
 const ApiResourcePermissions = () => {
@@ -25,50 +30,82 @@ const ApiResourcePermissions = () => {
 
   const isLoading = !scopes && !error;
 
+  const [isCreateFormOpen, setIsCreateFormOpen] = useState(false);
+
   return (
-    <Table
-      className={styles.permissionTable}
-      rowIndexKey="id"
-      rowGroups={[{ key: 'scopes', data: scopes }]}
-      columns={[
-        {
-          title: t('api_resource_details.permission.name_column'),
-          dataIndex: 'name',
-          colSpan: 6,
-          render: ({ name }) => <div className={styles.name}>{name}</div>,
-        },
-        {
-          title: t('api_resource_details.permission.description_column'),
-          dataIndex: 'description',
-          colSpan: 9,
-          render: ({ description }) => <div className={styles.description}>{description}</div>,
-        },
-        {
-          title: null,
-          dataIndex: 'delete',
-          colSpan: 1,
-          render: () => (
-            <IconButton>
-              <Delete />
-            </IconButton>
+    <>
+      <Table
+        className={styles.permissionTable}
+        rowIndexKey="id"
+        rowGroups={[{ key: 'scopes', data: scopes }]}
+        columns={[
+          {
+            title: t('api_resource_details.permission.name_column'),
+            dataIndex: 'name',
+            colSpan: 6,
+            render: ({ name }) => <div className={styles.name}>{name}</div>,
+          },
+          {
+            title: t('api_resource_details.permission.description_column'),
+            dataIndex: 'description',
+            colSpan: 9,
+            render: ({ description }) => <div className={styles.description}>{description}</div>,
+          },
+          {
+            title: null,
+            dataIndex: 'delete',
+            colSpan: 1,
+            render: () => (
+              <IconButton>
+                <Delete />
+              </IconButton>
+            ),
+          },
+        ]}
+        filter={
+          <div className={styles.filter}>
+            <Search />
+            <Button
+              title="api_resource_details.permission.create_button"
+              type="primary"
+              size="large"
+              icon={<Plus />}
+              onClick={() => {
+                setIsCreateFormOpen(true);
+              }}
+            />
+          </div>
+        }
+        isLoading={isLoading}
+        placeholder={{
+          content: (
+            <Button
+              title="api_resource_details.permission.create_button"
+              type="outline"
+              onClick={() => {
+                setIsCreateFormOpen(true);
+              }}
+            />
           ),
-        },
-      ]}
-      isLoading={isLoading}
-      placeholder={{
-        content: (
-          <Button
-            title="api_resource_details.permission.create_button"
-            type="outline"
-            onClick={() => {
-              // TODO @xiaoyijun Create Permission
-            }}
-          />
-        ),
-      }}
-      errorMessage={error?.body?.message ?? error?.message}
-      onRetry={async () => mutate(undefined, true)}
-    />
+        }}
+        errorMessage={error?.body?.message ?? error?.message}
+        onRetry={async () => mutate(undefined, true)}
+      />
+      {isCreateFormOpen && (
+        <CreatePermissionModal
+          resourceId={resourceId}
+          onClose={(scope) => {
+            if (scope) {
+              toast.success(
+                t('api_resource_details.permission.permission_created', { name: scope.name })
+              );
+              void mutate();
+            }
+            setIsCreateFormOpen(false);
+          }}
+        />
+      )}
+    </>
   );
 };
 
