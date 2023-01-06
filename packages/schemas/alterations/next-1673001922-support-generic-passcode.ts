0decdb796dc352d0c1de582d6c62518b4752cd1d
Charles Zhao <charleszhao@silverhand.io>
2023-01-06 18:41:23 +0800

    refactor(schemas): alter passcodes table and set jti to nullable

---
+++ packages/schemas/alterations/next-1673001922-support-generic-passcode.ts
@@ -0,0 +1,23 @@
+import { sql } from 'slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      alter table passcodes alter column interaction_jti drop not null;
+      create index passcodes__email_type on passcodes (email, type);
+      create index passcodes__phone_type on passcodes (phone, type);
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      delete from passcodes where interaction_jti is null;
+      drop index passcodes__email_type;
+      drop index passcodes__phone_type;
+      alter table passcodes alter column interaction_jti set not null;
+    `);
+  },
+};
+
+export default alteration;
--- packages/schemas/tables/passcodes.sql
+++ packages/schemas/tables/passcodes.sql
@@ -1,6 +1,6 @@
 create table passcodes (
   id varchar(21) not null,
-  interaction_jti varchar(128) not null,
+  interaction_jti varchar(128),
   phone varchar(32),
   email varchar(128),
   type varchar(32) not null,
@@ -14,5 +14,17 @@ create table passcodes (
 create index passcodes__interaction_jti_type
 on passcodes (
   interaction_jti,
+  type
+);
+
+create index passcodes__email_type
+on passcodes (
+  email,
+  type
+);
+
+create index passcodes__phone_type
+on passcodes (
+  phone,
   type
 );
