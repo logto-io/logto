5ffcbab83d2f16933e6792abfdd863173c9180b7
Gao Sun <gao@silverhand.io>
2023-01-07 20:44:10 +0800

    refactor(core,cli): application query factory

--- packages/core/src/database/update-where.ts
+++ packages/core/src/database/update-where.ts
@@ -3,6 +3,7 @@ import { convertToIdentifiers, convertToPrimitiveOrSql, conditionalSql } from '@
 import type { UpdateWhereData } from '@logto/shared';
 import type { Truthy } from '@silverhand/essentials';
 import { notFalsy } from '@silverhand/essentials';
+import type { CommonQueryMethods } from 'slonik';
 import { sql } from 'slonik';
 
 import envSet from '#src/env-set/index.js';
@@ -20,50 +21,56 @@ type BuildUpdateWhere = {
   ) => Promise<void>;
 };
 
-export const buildUpdateWhere: BuildUpdateWhere = <
-  Schema extends SchemaLike,
-  ReturnType extends SchemaLike
->(
-  schema: GeneratedSchema<Schema>,
-  returning = false
-) => {
-  const { table, fields } = convertToIdentifiers(schema);
-  const isKeyOfSchema = isKeyOf(schema);
-  const connectKeyValueWithEqualSign = (data: Partial<Schema>, jsonbMode: 'replace' | 'merge') =>
-    Object.entries(data)
-      .map(([key, value]) => {
-        if (!isKeyOfSchema(key)) {
-          return;
-        }
+export const buildUpdateWhereWithPool =
+  (pool: CommonQueryMethods): BuildUpdateWhere =>
+  <Schema extends SchemaLike, ReturnType extends SchemaLike>(
+    schema: GeneratedSchema<Schema>,
+    returning = false
+  ) => {
+    const { table, fields } = convertToIdentifiers(schema);
+    const isKeyOfSchema = isKeyOf(schema);
+    const connectKeyValueWithEqualSign = (data: Partial<Schema>, jsonbMode: 'replace' | 'merge') =>
+      Object.entries(data)
+        .map(([key, value]) => {
+          if (!isKeyOfSchema(key)) {
+            return;
+          }
 
-        if (jsonbMode === 'merge' && value && typeof value === 'object' && !Array.isArray(value)) {
-          /**
-           * Jsonb || operator is used to shallow merge two jsonb types of data
-           * all jsonb data field must be non-nullable
-           * https://www.postgresql.org/docs/current/functions-json.html
-           */
-          return sql`
-            ${fields[key]}=
-              coalesce(${fields[key]},'{}'::jsonb)|| ${convertToPrimitiveOrSql(key, value)}
-          `;
-        }
+          if (
+            jsonbMode === 'merge' &&
+            value &&
+            typeof value === 'object' &&
+            !Array.isArray(value)
+          ) {
+            /**
+             * Jsonb || operator is used to shallow merge two jsonb types of data
+             * all jsonb data field must be non-nullable
+             * https://www.postgresql.org/docs/current/functions-json.html
+             */
+            return sql`
+              ${fields[key]}=
+                coalesce(${fields[key]},'{}'::jsonb)|| ${convertToPrimitiveOrSql(key, value)}
+            `;
+          }
 
-        return sql`${fields[key]}=${convertToPrimitiveOrSql(key, value)}`;
-      })
-      .filter((value): value is Truthy<typeof value> => notFalsy(value));
+          return sql`${fields[key]}=${convertToPrimitiveOrSql(key, value)}`;
+        })
+        .filter((value): value is Truthy<typeof value> => notFalsy(value));
 
-  return async ({ set, where, jsonbMode }: UpdateWhereData<Schema>) => {
-    const {
-      rows: [data],
-    } = await envSet.pool.query<ReturnType>(sql`
-      update ${table}
-      set ${sql.join(connectKeyValueWithEqualSign(set, jsonbMode), sql`, `)}
-      where ${sql.join(connectKeyValueWithEqualSign(where, jsonbMode), sql` and `)}
-      ${conditionalSql(returning, () => sql`returning *`)}
-    `);
+    return async ({ set, where, jsonbMode }: UpdateWhereData<Schema>) => {
+      const {
+        rows: [data],
+      } = await pool.query<ReturnType>(sql`
+        update ${table}
+        set ${sql.join(connectKeyValueWithEqualSign(set, jsonbMode), sql`, `)}
+        where ${sql.join(connectKeyValueWithEqualSign(where, jsonbMode), sql` and `)}
+        ${conditionalSql(returning, () => sql`returning *`)}
+      `);
 
-    assertThat(!returning || data, new UpdateError(schema, { set, where, jsonbMode }));
+      assertThat(!returning || data, new UpdateError(schema, { set, where, jsonbMode }));
 
-    return data;
+      return data;
+    };
   };
-};
+
+export const buildUpdateWhere = buildUpdateWhereWithPool(envSet.pool);
--- packages/core/src/queries/application.ts
+++ packages/core/src/queries/application.ts
@@ -2,52 +2,77 @@ import type { Application, CreateApplication } from '@logto/schemas';
 import { Applications } from '@logto/schemas';
 import type { OmitAutoSetFields } from '@logto/shared';
 import { convertToIdentifiers, conditionalSql, manyRows } from '@logto/shared';
+import type { CommonQueryMethods } from 'slonik';
 import { sql } from 'slonik';
 
-import { buildFindEntityById } from '#src/database/find-entity-by-id.js';
-import { buildInsertInto } from '#src/database/insert-into.js';
-import { getTotalRowCount } from '#src/database/row-count.js';
-import { buildUpdateWhere } from '#src/database/update-where.js';
+import { buildFindEntityByIdWithPool } from '#src/database/find-entity-by-id.js';
+import { buildInsertIntoWithPool } from '#src/database/insert-into.js';
+import { getTotalRowCountWithPool } from '#src/database/row-count.js';
+import { buildUpdateWhereWithPool } from '#src/database/update-where.js';
 import envSet from '#src/env-set/index.js';
 import { DeletionError } from '#src/errors/SlonikError/index.js';
 
 const { table, fields } = convertToIdentifiers(Applications);
 
-export const findTotalNumberOfApplications = async () => getTotalRowCount(table);
-
-export const findAllApplications = async (limit: number, offset: number) =>
-  manyRows(
-    envSet.pool.query<Application>(sql`
-      select ${sql.join(Object.values(fields), sql`, `)}
-      from ${table}
-      order by ${fields.createdAt} desc
-      ${conditionalSql(limit, (limit) => sql`limit ${limit}`)}
-      ${conditionalSql(offset, (offset) => sql`offset ${offset}`)}
-    `)
+export const createApplicationQueries = (pool: CommonQueryMethods) => {
+  const findTotalNumberOfApplications = async () => getTotalRowCountWithPool(pool)(table);
+  const findAllApplications = async (limit: number, offset: number) =>
+    manyRows(
+      pool.query<Application>(sql`
+        select ${sql.join(Object.values(fields), sql`, `)}
+        from ${table}
+        order by ${fields.createdAt} desc
+        ${conditionalSql(limit, (limit) => sql`limit ${limit}`)}
+        ${conditionalSql(offset, (offset) => sql`offset ${offset}`)}
+      `)
+    );
+  const findApplicationById = buildFindEntityByIdWithPool(pool)<CreateApplication, Application>(
+    Applications
   );
+  const insertApplication = buildInsertIntoWithPool(pool)<CreateApplication, Application>(
+    Applications,
+    {
+      returning: true,
+    }
+  );
+  const updateApplication = buildUpdateWhereWithPool(pool)<CreateApplication, Application>(
+    Applications,
+    true
+  );
+  const updateApplicationById = async (
+    id: string,
+    set: Partial<OmitAutoSetFields<CreateApplication>>
+  ) => updateApplication({ set, where: { id }, jsonbMode: 'merge' });
 
-export const findApplicationById = buildFindEntityById<CreateApplication, Application>(
-  Applications
-);
+  const deleteApplicationById = async (id: string) => {
+    const { rowCount } = await envSet.pool.query(sql`
+      delete from ${table}
+      where ${fields.id}=${id}
+    `);
 
-export const insertApplication = buildInsertInto<CreateApplication, Application>(Applications, {
-  returning: true,
-});
+    if (rowCount < 1) {
+      throw new DeletionError(Applications.table, id);
+    }
+  };
 
-const updateApplication = buildUpdateWhere<CreateApplication, Application>(Applications, true);
-
-export const updateApplicationById = async (
-  id: string,
-  set: Partial<OmitAutoSetFields<CreateApplication>>
-) => updateApplication({ set, where: { id }, jsonbMode: 'merge' });
-
-export const deleteApplicationById = async (id: string) => {
-  const { rowCount } = await envSet.pool.query(sql`
-    delete from ${table}
-    where ${fields.id}=${id}
-  `);
-
-  if (rowCount < 1) {
-    throw new DeletionError(Applications.table, id);
-  }
+  return {
+    findTotalNumberOfApplications,
+    findAllApplications,
+    findApplicationById,
+    insertApplication,
+    updateApplication,
+    updateApplicationById,
+    deleteApplicationById,
+  };
 };
+
+/** @deprecated Will be removed soon. Use createApplicationQueries() factory instead. */
+export const {
+  findTotalNumberOfApplications,
+  findAllApplications,
+  findApplicationById,
+  insertApplication,
+  updateApplication,
+  updateApplicationById,
+  deleteApplicationById,
+} = createApplicationQueries(envSet.pool);
