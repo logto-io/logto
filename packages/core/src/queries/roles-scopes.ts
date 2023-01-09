4b21b7d16e283cfeff1aff42641c6ac57003f65d
Gao Sun <gao@silverhand.io>
2023-01-09 16:32:35 +0800

    refactor(core): multiple query factories (#2857)

--- packages/core/src/queries/roles-scopes.ts
+++ packages/core/src/queries/roles-scopes.ts
@@ -1,6 +1,7 @@
 import type { RolesScope } from '@logto/schemas';
 import { RolesScopes } from '@logto/schemas';
 import { convertToIdentifiers } from '@logto/shared';
+import type { CommonQueryMethods } from 'slonik';
 import { sql } from 'slonik';
 
 import envSet from '#src/env-set/index.js';
@@ -8,29 +9,37 @@ import { DeletionError } from '#src/errors/SlonikError/index.js';
 
 const { table, fields } = convertToIdentifiers(RolesScopes);
 
-export const insertRolesScopes = async (rolesScopes: RolesScope[]) =>
-  envSet.pool.query(sql`
-    insert into ${table} (${fields.scopeId}, ${fields.roleId}) values
-    ${sql.join(
-      rolesScopes.map(({ scopeId, roleId }) => sql`(${scopeId}, ${roleId})`),
-      sql`, `
-    )}
-  `);
+export const createRolesScopesQueries = (pool: CommonQueryMethods) => {
+  const insertRolesScopes = async (rolesScopes: RolesScope[]) =>
+    pool.query(sql`
+      insert into ${table} (${fields.scopeId}, ${fields.roleId}) values
+      ${sql.join(
+        rolesScopes.map(({ scopeId, roleId }) => sql`(${scopeId}, ${roleId})`),
+        sql`, `
+      )}
+    `);
 
-export const findRolesScopesByRoleId = async (roleId: string) =>
-  envSet.pool.any<RolesScope>(sql`
-    select ${sql.join(Object.values(fields), sql`,`)}
-    from ${table}
-    where ${fields.roleId}=${roleId}
-  `);
+  const findRolesScopesByRoleId = async (roleId: string) =>
+    pool.any<RolesScope>(sql`
+      select ${sql.join(Object.values(fields), sql`,`)}
+      from ${table}
+      where ${fields.roleId}=${roleId}
+    `);
 
-export const deleteRolesScope = async (roleId: string, scopeId: string) => {
-  const { rowCount } = await envSet.pool.query(sql`
-    delete from ${table}
-    where ${fields.scopeId} = ${scopeId} and ${fields.roleId} = ${roleId}
-  `);
+  const deleteRolesScope = async (roleId: string, scopeId: string) => {
+    const { rowCount } = await pool.query(sql`
+      delete from ${table}
+      where ${fields.scopeId} = ${scopeId} and ${fields.roleId} = ${roleId}
+    `);
 
-  if (rowCount < 1) {
-    throw new DeletionError(RolesScopes.table);
-  }
+    if (rowCount < 1) {
+      throw new DeletionError(RolesScopes.table);
+    }
+  };
+
+  return { insertRolesScopes, findRolesScopesByRoleId, deleteRolesScope };
 };
+
+/** @deprecated Will be removed soon. Use createRolesScopesQueries() factory instead. */
+export const { insertRolesScopes, findRolesScopesByRoleId, deleteRolesScope } =
+  createRolesScopesQueries(envSet.pool);
