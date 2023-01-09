49a28bf589eb14ac21ef11dc3132f9c957c8ca55
simeng-li <simeng@silverhand.io>
2023-01-09 19:12:31 +0800

    refactor(core): remove session api
    remove session api

--- packages/core/src/routes/init.ts
+++ packages/core/src/routes/init.ts
@@ -2,11 +2,9 @@ import { UserRole } from '@logto/schemas';
 import Koa from 'koa';
 import Router from 'koa-router';
 
-import koaAuditLogLegacy from '#src/middleware/koa-audit-log-legacy.js';
 import type TenantContext from '#src/tenants/TenantContext.js';
 
 import koaAuth from '../middleware/koa-auth.js';
-import koaLogSessionLegacy from '../middleware/koa-log-session-legacy.js';
 import adminUserRoleRoutes from './admin-user-role.js';
 import adminUserRoutes from './admin-user.js';
 import applicationRoutes from './application.js';
@@ -21,19 +19,14 @@ import phraseRoutes from './phrase.js';
 import profileRoutes from './profile.js';
 import resourceRoutes from './resource.js';
 import roleRoutes from './role.js';
-import sessionRoutes from './session/index.js';
 import settingRoutes from './setting.js';
 import signInExperiencesRoutes from './sign-in-experience/index.js';
 import statusRoutes from './status.js';
 import swaggerRoutes from './swagger.js';
-import type { AnonymousRouter, AnonymousRouterLegacy, AuthedRouter } from './types.js';
+import type { AnonymousRouter, AuthedRouter } from './types.js';
 import wellKnownRoutes from './well-known.js';
 
 const createRouters = (tenant: TenantContext) => {
-  const sessionRouter: AnonymousRouterLegacy = new Router();
-  sessionRouter.use(koaAuditLogLegacy(), koaLogSessionLegacy(tenant.provider));
-  sessionRoutes(sessionRouter, tenant);
-
   const interactionRouter: AnonymousRouter = new Router();
   interactionRoutes(interactionRouter, tenant);
 
@@ -62,21 +55,19 @@ const createRouters = (tenant: TenantContext) => {
   authnRoutes(anonymousRouter, tenant);
   // The swagger.json should contain all API routers.
   swaggerRoutes(anonymousRouter, [
-    sessionRouter,
     interactionRouter,
     profileRouter,
     managementRouter,
     anonymousRouter,
   ]);
 
-  return [sessionRouter, interactionRouter, profileRouter, managementRouter, anonymousRouter];
+  return [interactionRouter, profileRouter, managementRouter, anonymousRouter];
 };
 
 export default function initRouter(tenant: TenantContext): Koa {
   const apisApp = new Koa();
 
   for (const router of createRouters(tenant)) {
-    // @ts-expect-error will remove once interaction refactor finished
     apisApp.use(router.routes()).use(router.allowedMethods());
   }
 
