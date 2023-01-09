40cc5df487816c8f8249790b0974413e5b2aecf6
wangsijie <wangsijie94@gmail.com>
2023-01-09 20:02:37 +0800

    test: add it for admin-user roles

---
+++ packages/integration-tests/src/tests/api/admin-user.roles.test.ts
@@ -0,0 +1,41 @@
+import { adminConsoleAdminRoleId } from '@logto/schemas';
+import { HTTPError } from 'got';
+
+import { assignRolesToUser, getUserRoles, deleteRoleFromUser } from '#src/api/index.js';
+import { createUserByAdmin } from '#src/helpers.js';
+
+describe('admin console user management (roles)', () => {
+  it('should get empty list successfully', async () => {
+    const user = await createUserByAdmin();
+
+    const roles = await getUserRoles(user.id);
+    expect(roles.length).toBe(0);
+  });
+
+  it('should assign role to user and get list successfully', async () => {
+    const user = await createUserByAdmin();
+
+    await assignRolesToUser(user.id, [adminConsoleAdminRoleId]);
+    const roles = await getUserRoles(user.id);
+    expect(roles[0]).toHaveProperty('id', adminConsoleAdminRoleId);
+  });
+
+  it('should delete role from user successfully', async () => {
+    const user = await createUserByAdmin();
+
+    await assignRolesToUser(user.id, [adminConsoleAdminRoleId]);
+    await deleteRoleFromUser(user.id, adminConsoleAdminRoleId);
+
+    const roles = await getUserRoles(user.id);
+    expect(roles.length).toBe(0);
+  });
+
+  it('should delete non-exist-role from user failed', async () => {
+    const user = await createUserByAdmin();
+
+    const response = await deleteRoleFromUser(user.id, adminConsoleAdminRoleId).catch(
+      (error: unknown) => error
+    );
+    expect(response instanceof HTTPError && response.response.statusCode === 404).toBe(true);
+  });
+});
