39f15acb404cd6d394745c57fce40deece71ccfd
simeng-li <simeng@silverhand.io>
2023-01-10 13:49:00 +0800

    refactor(core): remove session api (#2878)

--- packages/core/src/routes/types.ts
+++ packages/core/src/routes/types.ts
@@ -1,16 +1,12 @@
 import type { ExtendableContext } from 'koa';
 import type Router from 'koa-router';
 
-import type { WithLogContextLegacy } from '#src/middleware/koa-audit-log-legacy.js';
 import type { WithLogContext } from '#src/middleware/koa-audit-log.js';
 import type { WithAuthContext } from '#src/middleware/koa-auth.js';
 import type { WithI18nContext } from '#src/middleware/koa-i18next.js';
 import type TenantContext from '#src/tenants/TenantContext.js';
 
 export type AnonymousRouter = Router<unknown, WithLogContext & WithI18nContext>;
-
-/** @deprecated This will be removed soon. Use `kua-log-session.js` instead. */
-export type AnonymousRouterLegacy = Router<unknown, WithLogContextLegacy & WithI18nContext>;
 
 export type AuthedRouter = Router<
   unknown,
