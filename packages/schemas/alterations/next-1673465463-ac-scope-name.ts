9ec3a8fcb7498a320feff10d2c31e96778b59e47
wangsijie <wangsijie@silverhand.io>
2023-01-12 16:33:14 +0800

    fix(schemas): modify management api default scope name (#2920)

---
+++ packages/schemas/alterations/next-1673465463-ac-scope-name.ts
@@ -0,0 +1,22 @@
+import { sql } from 'slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const newScopeName = 'management-api:default';
+const oldScopeName = 'default';
+const recordId = 'management-api-scope';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      update scopes set name = ${newScopeName} where id = ${recordId}
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      update scopes set name = ${oldScopeName} where id = ${recordId}
+    `);
+  },
+};
+
+export default alteration;
