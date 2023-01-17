aef690778db573c738040ed536931f974e745f8d
wangsijie <wangsijie@silverhand.io>
2023-01-17 12:54:14 +0800

    fix(schemas): fix ac admin id and scope (#2965)

--- packages/schemas/alterations/next-1673863835-ac-scope-role.ts
+++ packages/schemas/alterations/next-1673863835-ac-scope-role.ts
@@ -2,11 +2,27 @@ import { sql } from 'slonik';
 
 import type { AlterationScript } from '../lib/types/alteration.js';
 
-const managementResourceScopeId = 'management-api-scope';
 const adminConsoleAdminRoleId = 'ac-admin-id';
+const adminRoleName = 'admin';
+const managementResourceScopeId = 'management-api-scope';
 
 const alteration: AlterationScript = {
   up: async (pool) => {
+    // Fix role id
+    const newRole = await pool.maybeOne(sql`
+      select * from roles
+      where id = ${adminConsoleAdminRoleId}
+    `);
+
+    if (!newRole) {
+      await pool.query(sql`
+        update roles
+        set id = ${adminConsoleAdminRoleId}
+        where name = ${adminRoleName}
+      `);
+    }
+
+    // Fix scope role
     const relation = await pool.maybeOne(sql`
       select * from roles_scopes
       where scope_id = ${managementResourceScopeId}
