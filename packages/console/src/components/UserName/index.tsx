dbeca44833de0c6934f31e2f1830aa57a33381a8
wangsijie <wangsijie94@gmail.com>
2023-01-18 15:45:03 +0800

    fix(console): remove username admin tag

--- packages/console/src/components/UserName/index.tsx
+++ packages/console/src/components/UserName/index.tsx
@@ -1,5 +1,4 @@
-import type { UserWithRoleNames } from '@logto/schemas';
-import { UserRole } from '@logto/schemas';
+import type { User } from '@logto/schemas';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import useSWR from 'swr';
@@ -14,13 +13,11 @@ type Props = {
 };
 
 const UserName = ({ userId, isLink = false }: Props) => {
-  const { data, error } = useSWR<UserWithRoleNames, RequestError>(`/api/users/${userId}`);
+  const { data, error } = useSWR<User, RequestError>(`/api/users/${userId}`);
   const { t } = useTranslation(undefined, { keyPrefix: 'admin_console' });
 
   const isLoading = !data && !error;
   const name = data?.name ?? t('users.unnamed');
-
-  const isAdmin = data?.roleNames.includes(UserRole.Admin);
 
   if (isLoading) {
     return null;
@@ -28,15 +25,12 @@ const UserName = ({ userId, isLink = false }: Props) => {
 
   return (
     <div className={styles.userName}>
-      {isLink && !isAdmin ? (
+      {isLink ? (
         <Link to={`/users/${userId}`} target="_blank" className={styles.link}>
           {name}
         </Link>
       ) : (
-        <span>
-          {name}
-          {isAdmin && <> ({t('admin_user')})</>}
-        </span>
+        <span>{name}</span>
       )}
       <span className={styles.userId}>{userId}</span>
     </div>
