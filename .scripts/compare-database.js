5027203e29601e75ec047bc06618e6206085409b
Gao Sun <gao@silverhand.io>
2023-01-28 20:03:07 +0800

    chore: fix script indent

--- .scripts/compare-database.js
+++ .scripts/compare-database.js
@@ -51,27 +51,26 @@ const queryDatabaseManifest = async (database) => {
 
   const { rows: funcs } = await pool.query(/* sql */`
     select n.nspname as schema_name,
-       p.proname as specific_name,
-       case p.prokind 
-            when 'f' then 'FUNCTION'
-            when 'p' then 'PROCEDURE'
-            when 'a' then 'AGGREGATE'
-            when 'w' then 'WINDOW'
-            end as kind,
-       l.lanname as language,
-       case when l.lanname = 'internal' then p.prosrc
-            else pg_get_functiondef(p.oid)
-            end as definition,
-       pg_get_function_arguments(p.oid) as arguments,
-       t.typname as return_type
+      p.proname as specific_name,
+      case p.prokind 
+        when 'f' then 'FUNCTION'
+        when 'p' then 'PROCEDURE'
+        when 'a' then 'AGGREGATE'
+        when 'w' then 'WINDOW'
+        end as kind,
+      l.lanname as language,
+      case when l.lanname = 'internal' then p.prosrc
+        else pg_get_functiondef(p.oid)
+        end as definition,
+      pg_get_function_arguments(p.oid) as arguments,
+      t.typname as return_type
     from pg_proc p
     left join pg_namespace n on p.pronamespace = n.oid
     left join pg_language l on p.prolang = l.oid
     left join pg_type t on t.oid = p.prorettype 
     where n.nspname not in ('pg_catalog', 'information_schema')
     and l.lanname != 'c' -- Filter out c functions since we don't use them
-    order by schema_name,
-            specific_name;
+    order by schema_name, specific_name;
   `);
 
   const { rows: triggers } = await pool.query(/* sql */`select * from information_schema.triggers;`);
