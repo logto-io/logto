fb5d5ed297a3ff159d36551450099aa22b2d1859
simeng-li <simeng@silverhand.io>
2023-01-30 16:15:50 +0800

    fix(ui): remove social callback page from history (#3002)

--- packages/ui/src/hooks/use-social-register.ts
+++ packages/ui/src/hooks/use-social-register.ts
@@ -5,8 +5,11 @@ import { registerWithVerifiedSocial } from '@/apis/interaction';
 import useApi from './use-api';
 import useRequiredProfileErrorHandler from './use-required-profile-error-handler';
 
-const useSocialRegister = (connectorId?: string) => {
-  const requiredProfileErrorHandlers = useRequiredProfileErrorHandler({ linkSocial: connectorId });
+const useSocialRegister = (connectorId?: string, replace?: boolean) => {
+  const requiredProfileErrorHandlers = useRequiredProfileErrorHandler({
+    linkSocial: connectorId,
+    replace,
+  });
 
   const { result: registerResult, run: asyncRegisterWithSocial } = useApi(
     registerWithVerifiedSocial,
