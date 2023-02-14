57549c734323f01ce8d858842b9882cc6d240eb0
Xiao Yijun <xiaoyijun@silverhand.io>
2023-02-14 15:34:57 +0800

    feat(console): cloud preview welcome page

--- packages/console/src/pages/CloudPreview/components/ActionBar/index.tsx
+++ packages/console/src/pages/CloudPreview/components/ActionBar/index.tsx
@@ -1,10 +1,16 @@
+import { useContext } from 'react';
+
+import { CloudPreviewPageContext } from '../../containers/CloudPreviewPageProvider';
+import { CloudPreviewPage } from '../../types';
 import WelcomePageAction from './actions/WelcomePageAction';
 import * as styles from './index.module.scss';
 
 const ActionBar = () => {
+  const { currentPage } = useContext(CloudPreviewPageContext);
+
   return (
     <div className={styles.container}>
-      <WelcomePageAction />
+      {currentPage === CloudPreviewPage.Welcome && <WelcomePageAction />}
     </div>
   );
 };
