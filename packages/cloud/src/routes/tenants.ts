9a3b3f4bae120814d2ef52bcfbbfb73663b4883e
Gao Sun <gao@silverhand.io>
2023-02-18 18:54:07 +0800

    refactor(cloud): guard api response

--- packages/cloud/src/routes/tenants.ts
+++ packages/cloud/src/routes/tenants.ts
@@ -1,4 +1,5 @@
 import { Router } from '@withtyped/server';
+import { z } from 'zod';
 
 import { client } from '#src/queries/index.js';
 import { createTenantsQueries } from '#src/queries/tenants.js';
@@ -6,15 +7,19 @@ import { getTenantIdFromManagementApiIndicator } from '#src/utils/tenant.js';
 
 const { getManagementApiLikeIndicatorsForUser } = createTenantsQueries(client);
 
-export const tenants = new Router('/tenants').get('/', {}, async (context, next) => {
-  const { rows } = await getManagementApiLikeIndicatorsForUser('some_user_id');
+export const tenants = new Router('/tenants').get(
+  '/',
+  { response: z.object({ id: z.string(), indicator: z.string() }).array() },
+  async (context, next) => {
+    const { rows } = await getManagementApiLikeIndicatorsForUser('some_user_id');
 
-  const tenants = rows
-    .map(({ indicator }) => ({
-      id: getTenantIdFromManagementApiIndicator(indicator),
-      indicator,
-    }))
-    .filter((tenant): tenant is { id: string; indicator: string } => Boolean(tenant.id));
+    const tenants = rows
+      .map(({ indicator }) => ({
+        id: getTenantIdFromManagementApiIndicator(indicator),
+        indicator,
+      }))
+      .filter((tenant): tenant is { id: string; indicator: string } => Boolean(tenant.id));
 
-  return next({ ...context, json: tenants });
-});
+    return next({ ...context, json: tenants });
+  }
+);
