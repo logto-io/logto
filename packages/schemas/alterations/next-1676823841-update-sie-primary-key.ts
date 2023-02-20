735796059c35dc662c73c899754a9f64422bacbe
Gao Sun <gao@silverhand.io>
2023-02-20 11:33:20 +0800

    refactor(schemas): add alteration script

---
+++ packages/schemas/alterations/next-1676823841-update-sie-primary-key.ts
@@ -0,0 +1,26 @@
+import { sql } from 'slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      drop index sign_in_experiences__id;
+      alter table sign_in_experiences
+        drop constraint sign_in_experiences_pkey,
+        add primary key (tenant_id, id);
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      alter table sign_in_experiences
+        drop constraint sign_in_experiences_pkey,
+        add primary key (id);
+
+      create index sign_in_experiences__id
+        on sign_in_experiences (tenant_id, id);
+    `);
+  },
+};
+
+export default alteration;
