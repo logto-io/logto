b215b8dc1749cf5589f885bd5457d7e1ec5e6fef
Gao Sun <gao@silverhand.io>
2023-02-22 22:27:08 +0800

    refactor(schemas): add comments for constants

--- packages/schemas/src/consts/system.ts
+++ packages/schemas/src/consts/system.ts
@@ -1,2 +1,14 @@
+/** The API Resource Indicator for Logto Cloud. It's only useful when domain-based multi-tenancy is enabled. */
 export const cloudApiIndicator = 'https://cloud.logto.io/api';
+
+/**
+ * In OSS:
+ *
+ * - Only one single user tenant (`default`) is available.
+ * - Admin tenant and Admin Console share one endpoint (`ADMIN_ENDPOINT`).
+ *
+ * There's no need to parse tenant ID from the first path segment in OSS, and the segment should be a fixed value.
+ *
+ * If we use `/default`, the URL will look ugly; thus we keep the old fashion `/console`.
+ */
 export const ossConsolePath = '/console';
