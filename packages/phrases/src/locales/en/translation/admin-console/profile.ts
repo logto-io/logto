a4347d1dd1b153f09e63a16fc40c7caa7aa7b069
Charles Zhao <charleszhao@silverhand.io>
2023-02-23 22:44:53 +0800

    feat(console): new profile settings page

---
+++ packages/phrases/src/locales/en/translation/admin-console/profile.ts
@@ -0,0 +1,43 @@
+const profile = {
+  title: 'Account Settings',
+  description:
+    'Change your account settings and manage your personal information here to ensure your account security.',
+  settings: {
+    title: 'PROFILE SETTINGS',
+    profile_information: 'Profile information',
+    avatar: 'Avatar',
+    name: 'Name',
+    username: 'Username',
+  },
+  link_account: {
+    title: 'LINK ACCOUNT',
+    email_sign_in: 'Email sign-In',
+    email: 'Email',
+    social_sign_in: 'Social sign-In',
+  },
+  password: {
+    title: 'PASSWORD & SECURITY',
+    password: 'Password',
+    reset_password: 'Reset Password',
+  },
+  delete_account: {
+    title: 'DELETE ACCOUNT',
+    label: 'Delete account',
+    description:
+      'Deleting your account will remove all of your personal information, user data, and configuration. This action cannot be undone.',
+    button: 'Delete account',
+    dialog_paragraph_1:
+      "We're sorry to hear that you want to delete your account. Deleting your account will permanently remove all data, including user information, logs, and settings, and this action cannot be undone. So please make sure to backup any important data before proceeding.",
+    dialog_paragraph_2:
+      'To proceed with the account deletion process, please email our support team at <a>mail</a> with the subject “Account Deletion Request”. We will assist you and ensure that all of your data is properly deleted from our system.',
+    dialog_paragraph_3:
+      'Thank you for choosing Logto Cloud. If you have any further questions or concerns, please do not hesitate to reach out to us.',
+  },
+  edit: 'Edit',
+  change: 'Change',
+  link: 'Link',
+  unlink: 'Unlink',
+  not_set: 'Not set',
+};
+
+export default profile;
---
+++ packages/phrases/src/locales/zh-cn/translation/admin-console/profile.ts
@@ -0,0 +1,41 @@
+const profile = {
+  title: '账户管理',
+  description: '在这里，你可以修改账户设置和管理个人信息，以确保账户安全。',
+  settings: {
+    title: '账户设置',
+    profile_information: '个人信息',
+    avatar: '头像',
+    name: '姓名',
+    username: '用户名',
+  },
+  link_account: {
+    title: '关联账户',
+    email_sign_in: '邮件登录',
+    email: '邮件',
+    social_sign_in: '社交账号登录',
+  },
+  password: {
+    title: '密码和安全',
+    password: '密码',
+    reset_password: '重置密码',
+  },
+  delete_account: {
+    title: '删除账户',
+    label: '删除账户',
+    description: '删除账户将会删除所有个人信息、用户数据和配置。此操作无法撤销。',
+    button: '删除账户',
+    dialog_paragraph_1:
+      '很抱歉听到您想要删除您的帐户。删除您的帐户将永久删除所有数据，包括用户信息、日志和设置，此操作无法撤销。因此，请确保在继续之前备份任何重要数据。',
+    dialog_paragraph_2:
+      '要继续删除帐户的删除过程，请通过 <a>mail</a> 向我们的支持团队发送主题为“帐户删除请求”的邮件。我们将协助您并确保所有数据都已从我们的系统中正确删除。',
+    dialog_paragraph_3:
+      '感谢您选择 Logto Cloud。如果您有任何进一步的问题或疑虑，请随时与我们联系。',
+  },
+  edit: '编辑',
+  change: '变更',
+  link: '关联',
+  unlink: '取消关联',
+  not_set: '未设置',
+};
+
+export default profile;
