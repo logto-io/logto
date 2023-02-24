3965e32daf46a5a6b8be7bea287db4a141d8cf5d
simeng-li <simeng@silverhand.io>
2023-02-24 10:13:27 +0800

    chore(phrases): update content
    update content

--- packages/phrases/src/locales/en/translation/oidc.ts
+++ packages/phrases/src/locales/en/translation/oidc.ts
@@ -1,5 +1,5 @@
 const oidc = {
-  logout_success: 'You have been successfully signed out.',
+  logout_success: 'You have successfully signed out.',
 };
 
 export default oidc;
--- packages/phrases/src/locales/ko/translation/oidc.ts
+++ packages/phrases/src/locales/ko/translation/oidc.ts
@@ -1,5 +1,5 @@
 const oidc = {
-  logout_success: 'You have been successfully signed out.', // UNTRANSLATED
+  logout_success: 'You have successfully signed out.', // UNTRANSLATED
 };
 
 export default oidc;
