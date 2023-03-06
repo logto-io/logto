6aab01e56c9408216511bbcd68bf416fef768406
simeng-li <simeng@silverhand.io>
2023-03-06 10:08:57 +0800

    feat(ui): add interactionMode OIDC param (#3280)

--- packages/core/src/routes/consts.ts
+++ packages/core/src/routes/consts.ts
@@ -1,10 +1,12 @@
 const signIn = '/sign-in';
+const signUp = '/register';
 
 export const routes = Object.freeze({
   signIn: {
     credentials: signIn,
     consent: `${signIn}/consent`,
   },
+  signUp,
 } as const);
 
 export const verificationTimeout = 10 * 60; // 10 mins.
