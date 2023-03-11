533504ab4d942af9960534394005a778db905441
Gao Sun <gao@silverhand.io>
2023-03-11 18:08:19 +0800

    refactor(core): add etag to all GET APIs

--- packages/core/src/routes/well-known.ts
+++ packages/core/src/routes/well-known.ts
@@ -4,7 +4,6 @@ import { adminTenantId } from '@logto/schemas';
 
 import { EnvSet, getTenantEndpoint } from '#src/env-set/index.js';
 import RequestError from '#src/errors/RequestError/index.js';
-import koaBodyEtag from '#src/middleware/koa-body-etag.js';
 
 import type { AnonymousRouter, RouterInitArgs } from './types.js';
 
@@ -17,57 +16,49 @@ export default function wellKnownRoutes<T extends AnonymousRouter>(
   } = libraries;
 
   if (id === adminTenantId) {
-    router.get(
-      '/.well-known/endpoints/:tenantId',
-      async (ctx, next) => {
-        if (!ctx.params.tenantId) {
-          throw new RequestError('request.invalid_input');
-        }
-
-        ctx.body = {
-          user: getTenantEndpoint(ctx.params.tenantId, EnvSet.values),
-        };
-
-        return next();
-      },
-      koaBodyEtag()
-    );
-  }
-
-  router.get(
-    '/.well-known/sign-in-exp',
-    async (ctx, next) => {
-      const [signInExperience, logtoConnectors] = await Promise.all([
-        getSignInExperience(),
-        getLogtoConnectors(),
-      ]);
-
-      const forgotPassword = {
-        phone: logtoConnectors.some(({ type }) => type === ConnectorType.Sms),
-        email: logtoConnectors.some(({ type }) => type === ConnectorType.Email),
-      };
-
-      const socialConnectors = signInExperience.socialSignInConnectorTargets.reduce<
-        Array<ConnectorMetadata & { id: string }>
-      >((previous, connectorTarget) => {
-        const connectors = logtoConnectors.filter(
-          ({ metadata: { target } }) => target === connectorTarget
-        );
-
-        return [
-          ...previous,
-          ...connectors.map(({ metadata, dbEntry: { id } }) => ({ ...metadata, id })),
-        ];
-      }, []);
+    router.get('/.well-known/endpoints/:tenantId', async (ctx, next) => {
+      if (!ctx.params.tenantId) {
+        throw new RequestError('request.invalid_input');
+      }
 
       ctx.body = {
-        ...signInExperience,
-        socialConnectors,
-        forgotPassword,
+        user: getTenantEndpoint(ctx.params.tenantId, EnvSet.values),
       };
 
       return next();
-    },
-    koaBodyEtag()
-  );
+    });
+  }
+
+  router.get('/.well-known/sign-in-exp', async (ctx, next) => {
+    const [signInExperience, logtoConnectors] = await Promise.all([
+      getSignInExperience(),
+      getLogtoConnectors(),
+    ]);
+
+    const forgotPassword = {
+      phone: logtoConnectors.some(({ type }) => type === ConnectorType.Sms),
+      email: logtoConnectors.some(({ type }) => type === ConnectorType.Email),
+    };
+
+    const socialConnectors = signInExperience.socialSignInConnectorTargets.reduce<
+      Array<ConnectorMetadata & { id: string }>
+    >((previous, connectorTarget) => {
+      const connectors = logtoConnectors.filter(
+        ({ metadata: { target } }) => target === connectorTarget
+      );
+
+      return [
+        ...previous,
+        ...connectors.map(({ metadata, dbEntry: { id } }) => ({ ...metadata, id })),
+      ];
+    }, []);
+
+    ctx.body = {
+      ...signInExperience,
+      socialConnectors,
+      forgotPassword,
+    };
+
+    return next();
+  });
 }
