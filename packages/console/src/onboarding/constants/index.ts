90877bf7a19e5080ae32a2ec58fa01a4dd1dfb26
Gao Sun <gao@silverhand.io>
2023-03-13 17:52:13 +0800

    chore: update blog url and title

--- packages/console/src/onboarding/constants/index.ts
+++ packages/console/src/onboarding/constants/index.ts
@@ -1,3 +1,2 @@
 export const reservationLink = 'https://calendly.com/logto/30min';
-export const logtoBlogLink =
-  'https://www.notion.so/silverhand/About-Logto-Cloud-Preview-ca316bc05f2a4b9188047da014124434?pvs=4';
+export const logtoBlogLink = 'https://docs.logto.io/blog?utm_source=console';
