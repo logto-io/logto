e671a4d1d5345fedf713da58acdcffa01569b70e
Xiao Yijun <xiaoyijun@silverhand.io>
2023-03-16 14:12:22 +0800

    refactor(demo-app): update phrases

--- packages/phrases/src/locales/de/translation/demo-app.ts
+++ packages/phrases/src/locales/de/translation/demo-app.ts
@@ -1,9 +1,9 @@
 const demo_app = {
-  title: 'Du hast dich erfolgreich in der Demo App angemeldet!',
+  title: "You've successfully signed in the live preview!", // UNTRANSLATED
   subtitle: 'Here is your log in information:',
   username: 'Benutzername: ',
   user_id: 'Benutzer ID: ',
-  sign_out: 'Aus der Demo App ausloggen',
+  sign_out: 'Sign Out the Live Preview', // UNTRANSLATED
   continue_explore: 'Oder weiter zum Entdecken',
   customize_sign_in_experience: 'Anmeldeoberfläche anpassen',
   enable_passwordless: 'Passwordless einschalten',
--- packages/phrases/src/locales/zh-cn/translation/demo-app.ts
+++ packages/phrases/src/locales/zh-cn/translation/demo-app.ts
@@ -1,9 +1,9 @@
 const demo_app = {
-  title: '恭喜！你已成功登录到示例应用！',
+  title: "You've successfully signed in the live preview!", // UNTRANSLATED
   subtitle: '以下是本次登录的用户信息：',
   username: '用户名：',
   user_id: '用户 ID：',
-  sign_out: '退出登录示例应用',
+  sign_out: 'Sign Out the Live Preview', // UNTRANSLATED
   continue_explore: '或继续探索',
   customize_sign_in_experience: '自定义登录体验',
   enable_passwordless: '启用无密码登录',
