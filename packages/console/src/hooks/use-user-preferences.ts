c368c10dd89c40103289c0aa62f472fee9254110
Charles Zhao <charleszhao@silverhand.io>
2023-03-20 12:10:58 +0800

    fix(console): language and theme should have default value

--- packages/console/src/hooks/use-user-preferences.ts
+++ packages/console/src/hooks/use-user-preferences.ts
@@ -1,8 +1,10 @@
 import { builtInLanguages as builtInConsoleLanguages } from '@logto/phrases';
+import type { Theme } from '@logto/schemas';
 import { useContext, useEffect, useMemo } from 'react';
 import { z } from 'zod';
 
 import { AppThemeContext, buildDefaultAppearanceMode } from '@/contexts/AppThemeProvider';
+import type { DynamicAppearanceMode } from '@/types/appearance-mode';
 import { appearanceModeGuard } from '@/types/appearance-mode';
 
 import useMeCustomData from './use-me-custom-data';
@@ -11,13 +13,23 @@ const adminConsolePreferencesKey = 'adminConsolePreferences';
 
 const userPreferencesGuard = z.object({
   language: z.enum(builtInConsoleLanguages).optional(),
-  appearanceMode: appearanceModeGuard,
+  appearanceMode: appearanceModeGuard.optional(),
   experienceNoticeConfirmed: z.boolean().optional(),
   getStartedHidden: z.boolean().optional(),
   connectorSieNoticeConfirmed: z.boolean().optional(),
 });
 
 export type UserPreferences = z.infer<typeof userPreferencesGuard>;
+
+type DefaultUserPreference = {
+  language: (typeof builtInConsoleLanguages)[number];
+  appearanceMode: Theme | DynamicAppearanceMode.System;
+} & Omit<UserPreferences, 'language' | 'appearanceMode'>;
+
+const defaultUserPreferences: DefaultUserPreference = {
+  appearanceMode: buildDefaultAppearanceMode(),
+  language: 'en',
+};
 
 const useUserPreferences = () => {
   const { data, error, isLoading, isLoaded, update: updateMeCustomData } = useMeCustomData();
@@ -27,10 +39,11 @@ const useUserPreferences = () => {
     const parsed = z.object({ [adminConsolePreferencesKey]: userPreferencesGuard }).safeParse(data);
 
     return parsed.success
-      ? parsed.data[adminConsolePreferencesKey]
-      : {
-          appearanceMode: buildDefaultAppearanceMode(),
-        };
+      ? {
+          ...defaultUserPreferences,
+          ...parsed.data[adminConsolePreferencesKey],
+        }
+      : defaultUserPreferences;
   }, [data]);
 
   const update = async (data: Partial<UserPreferences>) => {
