97cd683b68ebaa3573843f099d007268ec661590
Xiao Yijun <xiaoyijun@silverhand.io>
2023-03-20 10:55:21 +0800

    refactor(console): display loading when user assets service is loading

--- packages/console/src/containers/AppBoundary/index.tsx
+++ packages/console/src/containers/AppBoundary/index.tsx
@@ -1,6 +1,8 @@
 import type { ReactNode } from 'react';
 import { useEffect } from 'react';
 
+import AppLoading from '@/components/AppLoading';
+import useUserAssetsService from '@/hooks/use-user-assets-service';
 import useUserPreferences from '@/hooks/use-user-preferences';
 import initI18n from '@/i18n/init';
 
@@ -13,11 +15,17 @@ const AppBoundary = ({ children }: Props) => {
     data: { language },
   } = useUserPreferences();
 
+  const { isLoading: isUserAssetsServiceLoading } = useUserAssetsService();
+
   useEffect(() => {
     (async () => {
       void initI18n(language);
     })();
   }, [language]);
+
+  if (isUserAssetsServiceLoading) {
+    return <AppLoading />;
+  }
 
   // eslint-disable-next-line react/jsx-no-useless-fragment
   return <>{children}</>;
--- packages/console/src/hooks/use-user-assets-service.ts
+++ packages/console/src/hooks/use-user-assets-service.ts
@@ -1,11 +1,16 @@
 import type { UserAssetsServiceStatus } from '@logto/schemas';
 import useSWRImmutable from 'swr/immutable';
 
+import type { RequestError } from './use-api';
+
 const useUserAssetsService = () => {
-  const { data } = useSWRImmutable<UserAssetsServiceStatus>('api/user-assets/service-status');
+  const { data, error } = useSWRImmutable<UserAssetsServiceStatus, RequestError>(
+    'api/user-assets/service-status'
+  );
 
   return {
     isReady: data?.status === 'ready',
+    isLoading: !error && !data,
   };
 };
 
