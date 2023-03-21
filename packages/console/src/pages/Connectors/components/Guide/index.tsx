38b94b8c7cb53e802454567ec08ab3cd7163ef3f
Xiao Yijun <xiaoyijun@silverhand.io>
2023-03-21 15:57:28 +0800

    refactor(console): connector guide readme content scrollbar

--- packages/console/src/pages/Connectors/components/Guide/index.tsx
+++ packages/console/src/pages/Connectors/components/Guide/index.tsx
@@ -3,8 +3,10 @@ import { isLanguageTag } from '@logto/language-kit';
 import type { ConnectorFactoryResponse, ConnectorResponse, RequestErrorBody } from '@logto/schemas';
 import { ConnectorType } from '@logto/schemas';
 import { conditional } from '@silverhand/essentials';
+import classNames from 'classnames';
 import i18next from 'i18next';
 import { HTTPError } from 'ky';
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 import { useEffect, useRef, useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
@@ -48,8 +50,12 @@ const Guide = ({ connector, onClose }: Props) => {
   const [conflictConnectorName, setConflictConnectorName] = useState<Record<string, string>>();
   const { t } = useTranslation(undefined, { keyPrefix: 'admin_console' });
   const { type: connectorType, formItems, target, isStandard, configTemplate } = connector ?? {};
-
   const { language } = i18next;
+
+  const readmeTitleRef = useRef<HTMLDivElement>(null);
+  const [contentScrollTop, setContentScrollTop] = useState(0);
+  const isReadmeTitleInVisible =
+    readmeTitleRef.current && contentScrollTop > Number(readmeTitleRef.current.clientHeight);
 
   const isSocialConnector =
     connectorType !== ConnectorType.Sms && connectorType !== ConnectorType.Email;
@@ -182,8 +188,26 @@ const Guide = ({ connector, onClose }: Props) => {
         </div>
         <div className={styles.content}>
           <div className={styles.readme}>
-            <div className={styles.readmeTitle}>README: {title}</div>
-            <Markdown className={styles.readmeContent}>{content}</Markdown>
+            <div
+              ref={readmeTitleRef}
+              className={classNames(styles.readmeTitle, isReadmeTitleInVisible && styles.invisible)}
+            >
+              README: {title}
+            </div>
+            <OverlayScrollbarsComponent
+              options={{ scrollbars: { autoHide: 'leave', autoHideDelay: 0 } }}
+              className={styles.readmeContent}
+              events={{
+                scroll: (instance) => {
+                  const {
+                    viewport: { scrollTop },
+                  } = instance.elements();
+                  setContentScrollTop(scrollTop);
+                },
+              }}
+            >
+              <Markdown>{content}</Markdown>
+            </OverlayScrollbarsComponent>
           </div>
           <div className={styles.setup}>
             <FormProvider {...methods}>
