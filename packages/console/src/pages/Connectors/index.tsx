6cd15d06986ece68f79e306c6dcfdcc35790a625
Darcy Ye <darcyye@silverhand.io>
2023-03-22 17:57:31 +0800

    fix(console): should not mutate when closing create connector modal (#3577)

--- packages/console/src/pages/Connectors/index.tsx
+++ packages/console/src/pages/Connectors/index.tsx
@@ -204,9 +204,7 @@ function Connectors() {
       <CreateForm
         isOpen={Boolean(createConnectorType)}
         type={createConnectorType}
-        onClose={async (id) => {
-          await mutate();
-
+        onClose={(id) => {
           if (createConnectorType && id) {
             navigate(buildGuidePathname(createConnectorType, id), { replace: true });
 
