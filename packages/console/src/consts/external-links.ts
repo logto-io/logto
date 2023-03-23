d9d77ae1adace1cfef57da91df354a29561b0261
Gao Sun <gao@silverhand.io>
2023-03-23 16:50:24 +0800

    chore(console): fix contact email

--- packages/console/src/consts/external-links.ts
+++ packages/console/src/consts/external-links.ts
@@ -1,4 +1,4 @@
 export const discordLink = 'https://discord.gg/UEPaF3j5e6';
 export const githubLink = 'https://github.com/logto-io/logto';
 export const githubIssuesLink = 'https://github.com/logto-io/logto/issues';
-export const contactEmailLink = 'mailto:feedback@logto.io';
+export const contactEmailLink = 'mailto:contact@logto.io';
