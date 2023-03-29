3fd9292a601889732d3966f33b4442ea875359a6
Xiao Yijun <xiaoyijun@silverhand.io>
2023-03-29 12:58:19 +0800

    fix(console): generate user password

--- packages/console/src/pages/Users/components/CreateForm/utils.ts
+++ packages/console/src/pages/Users/components/CreateForm/utils.ts
@@ -1,12 +1,12 @@
-import { customAlphabet, nanoid } from 'nanoid';
+import { customAlphabet } from 'nanoid';
 
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 const digits = '0123456789';
 
 // Note: password requires a minimum of 8 characters and contains a mix of letters, numbers, and symbols.
 export const createInitialPassword = () => {
-  const randomAlphabet = customAlphabet(alphabet, 1)();
-  const randomDigit = customAlphabet(digits, 1)();
+  const randomAlphabet = customAlphabet(alphabet, 1);
+  const randomDigit = customAlphabet(digits, 1);
 
-  return `${randomAlphabet}${randomDigit}${nanoid(6)}`;
+  return `${randomAlphabet(1)}${randomDigit(1)}${randomAlphabet(6)}`;
 };
