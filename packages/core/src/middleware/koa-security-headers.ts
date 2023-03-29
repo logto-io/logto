0f017063a85e838be45737df4b52fe21d11500a5
simeng-li <simeng@silverhand.io>
2023-03-29 15:07:02 +0800

    chore(core): update helmet middleware
    update helmet middleware

--- packages/core/src/middleware/koa-security-headers.ts
+++ packages/core/src/middleware/koa-security-headers.ts
@@ -1,52 +1,40 @@
 import type { MiddlewareType } from 'koa';
-import compose from 'koa-compose';
 import helmet from 'koa-helmet';
+
+/**
+ * Apply security headers to the response using koa-helmet
+ * @see https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html for recommended headers
+ * @see https://helmetjs.github.io/ for more details
+ * @returns koa middleware
+ */
 
 export default function koaSecurityHeaders<StateT, ContextT, ResponseBodyT>(): MiddlewareType<
   StateT,
   ContextT,
   ResponseBodyT
 > {
-  return compose([
-    // Expect-ct: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#expect-ct  not recommended
-    // helmet.expectCt(),
+  /**
+   * Applied rules:
+   * - crossOriginOpenerPolicy: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#cross-origin-opener-policy-coop
+   * - hidePoweredBy: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-powered-by
+   * - hsts: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#strict-transport-security-hsts
+   * - ieNoOpen: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-download-options
+   * - noSniff: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-content-type-options
+   * - permittedCrossDomainPolicies: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-permitted-cross-domain-policies
+   * - referrerPolicy: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#referrer-policy
+   * - xssFilter: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-xss-protection
+   * - originAgentCluster: https://whatpr.org/html/6214/origin.html#origin-keyed-agent-clusters
+   */
 
-    // X-Powered-By: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-powered-by
-    helmet.hidePoweredBy(),
-
-    // Strict-Transport-Security: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#strict-transport-security-hsts
-    helmet.hsts(),
-
-    // X-Download-Options
-    helmet.ieNoOpen(),
-
-    // X-Content-Type-Options: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-content-type-options
-    helmet.noSniff(),
-
-    // X-Permitted-Cross-Domain-Policies
-    helmet.permittedCrossDomainPolicies(),
-
-    // Referrer-Policy https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#referrer-policy
-    // @ts-expect-error broken type definition, koaHelmet types definitions remain 6.* for now broken
-    helmet.referrerPolicy({
+  return helmet({
+    contentSecurityPolicy: false, // Will be set exclusively by the app
+    expectCt: false, // Not recommended, will be deprecated by modern browsers
+    dnsPrefetchControl: false,
+    frameguard: false, // TODO  Will be set exclusively by the app
+    crossOriginResourcePolicy: false, // TODO  Will be set exclusively by the app
+    crossOriginEmbedderPolicy: false, // TODO Will be set exclusively by the app
+    referrerPolicy: {
       policy: 'strict-origin-when-cross-origin',
-    }),
-
-    // X-XSS-Protection https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-xss-protection
-    helmet.xssFilter(),
-
-    // TODO X-Frame-Options: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-frame-options
-    // Need to load preview and terms of uses in iframe. Use a Content Security Policy (CSP) header to guard per app instead.
-    // helmet.frameguard(),
-
-    /* eslint-disable @typescript-eslint/no-unsafe-call */
-    // cross-Origin-Opener-Policy: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#cross-origin-opener-policy-coop
-    // @ts-expect-error koaHelmet types definitions remain 6.* for now
-    helmet.crossOriginOpenerPolicy(),
-
-    // Cross-Origin-Resource-Policy: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#cross-origin-resource-policy-corp
-    // @ts-expect-error koaHelmet types definitions remain 6.* for now
-    helmet.crossOriginResourcePolicy(),
-    /* eslint-enable @typescript-eslint/no-unsafe-call */
-  ]);
+    },
+  });
 }
