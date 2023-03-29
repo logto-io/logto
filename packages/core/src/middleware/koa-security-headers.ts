2b101efd2f7e873a0da4db69d3cf2ca229499a66
simeng-li <simeng@silverhand.io>
2023-03-29 11:09:07 +0800

    chore(core): add some comments
    add some comments

--- packages/core/src/middleware/koa-security-headers.ts
+++ packages/core/src/middleware/koa-security-headers.ts
@@ -2,32 +2,48 @@ import type { MiddlewareType } from 'koa';
 import compose from 'koa-compose';
 import helmet from 'koa-helmet';
 
-import { EnvSet } from '#src/env-set/index.js';
-
 export default function koaSecurityHeaders<StateT, ContextT, ResponseBodyT>(): MiddlewareType<
   StateT,
   ContextT,
   ResponseBodyT
 > {
-  const { isProduction, endpoint, adminUrlSet } = EnvSet.values;
-
-  const adminUrls = adminUrlSet.deduplicated().map((url) => url.origin);
-
   return compose([
-    helmet.expectCt(), // Expect-CT
-    helmet.hidePoweredBy(), // X-Powered-By
-    helmet.hsts(), // Strict-Transport-Security
-    helmet.ieNoOpen(), // X-Download-Options
-    helmet.noSniff(), // X-Content-Type-Options
-    helmet.permittedCrossDomainPolicies(), // X-Permitted-Cross-Domain-Policies
-    helmet.referrerPolicy(), // Referrer-Policy
-    helmet.xssFilter(), // X-XSS-Protection
+    // Expect-ct: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#expect-ct  not recommended
+    // helmet.expectCt(),
+
+    // X-Powered-By: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-powered-by
+    helmet.hidePoweredBy(),
+
+    // Strict-Transport-Security: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#strict-transport-security-hsts
+    helmet.hsts(),
+
+    // X-Download-Options
+    helmet.ieNoOpen(),
+
+    // X-Content-Type-Options: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-content-type-options
+    helmet.noSniff(),
+
+    // X-Permitted-Cross-Domain-Policies
+    helmet.permittedCrossDomainPolicies(),
+
+    // Referrer-Policy https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#referrer-policy
+    helmet.referrerPolicy(),
+
+    // X-XSS-Protection https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-xss-protection
+    helmet.xssFilter(),
+
+    // TODO X-Frame-Options: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#x-frame-options
+    // Need to load preview and terms of uses in iframe. Use a Content Security Policy (CSP) header to guard per app instead.
+    // helmet.frameguard(),
 
     /* eslint-disable @typescript-eslint/no-unsafe-call */
+    // cross-Origin-Opener-Policy: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#cross-origin-opener-policy-coop
     // @ts-expect-error koaHelmet types definitions remain 6.* for now
-    helmet.crossOriginOpenerPolicy(), // Sets "Cross-Origin-Opener-Policy: same-origin"
+    helmet.crossOriginOpenerPolicy(),
+
+    // Cross-Origin-Resource-Policy: https://cheatsheetseries.owasp.org/cheatsheets/HTTP_Headers_Cheat_Sheet.html#cross-origin-resource-policy-corp
     // @ts-expect-error koaHelmet types definitions remain 6.* for now
-    helmet.crossOriginResourcePolicy(), // Sets "Cross-Origin-Resource-Policy: same-site"
+    helmet.crossOriginResourcePolicy(),
     /* eslint-enable @typescript-eslint/no-unsafe-call */
   ]);
 }
