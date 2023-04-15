457cb2822426094195f82ad4c6094a0e039b91b9
Charles Zhao <charleszhao@silverhand.io>
2023-04-15 08:10:42 +0800

    fix(console): adding social connector should mark related get-started action item as completed (#3693)
    * fix(console): adding social connector should mark related get-started action item as completed
    
    * chore: add changeset

---
+++ packages/schemas/alterations/next-1681267285-fix-get-started-passwordless-status.ts
@@ -0,0 +1,32 @@
+import { sql } from 'slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const adminConsoleConfigKey = 'adminConsole';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    const tenantIds = await pool.many<{ id: string }>(sql`select id from tenants`);
+
+    await Promise.all(
+      tenantIds.map(async ({ id }) => {
+        const { count } = await pool.one<{ count: number }>(sql`
+          select count(*) from connectors
+            where tenant_id = ${id} and connector_id <> 'logto-sms' and connector_id <> 'logto-email' and connector_id <> 'logto-social-demo';
+        `);
+
+        if (count > 0) {
+          await pool.query(sql`
+            update logto_configs set value = jsonb_set(value, '{passwordlessConfigured}', 'true')
+              where tenant_id = ${id} and key = ${adminConsoleConfigKey};
+          `);
+        }
+      })
+    );
+  },
+  down: async () => {
+    // Do nothing as there is no alteration made to the DB schemas, only fixing data.
+  },
+};
+
+export default alteration;
