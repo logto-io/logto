8565ab3dba636701bcf9c49c21e3a05b941a7067
Xiao Yijun <xiaoyijun@silverhand.io>
2023-04-17 17:55:04 +0800

    refactor(console): add user title display rule (#3701)

---
+++ packages/console/src/utils/user.ts
@@ -0,0 +1,11 @@
+import { type User } from '@logto/schemas';
+import { conditional } from '@silverhand/essentials';
+
+const getUserIdentity = (user: User) => {
+  const { primaryEmail, primaryPhone, username } = user;
+  return primaryEmail ?? primaryPhone ?? username;
+};
+
+export const getUserTitle = (user: User) => user.name ?? getUserIdentity(user);
+
+export const getUserSubtitle = (user: User) => conditional(user.name && getUserIdentity(user));
