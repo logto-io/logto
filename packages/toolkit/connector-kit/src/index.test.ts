6a463f6a2ad63e57744b4433e8b28685e10d54aa
Darcy Ye <darcyye@silverhand.io>
2023-04-28 10:50:05 +0800

    chore(toolkit): add UTs for connector-kit (#3760)

---
+++ packages/toolkit/connector-kit/src/index.test.ts
@@ -0,0 +1,50 @@
+import { z } from 'zod';
+
+import { parseJson, parseJsonObject, validateConfig } from './index.js';
+
+describe('connector-kit', () => {
+  describe('validateConfig', () => {
+    it('valid config', () => {
+      const testingTypeGuard = z.unknown();
+      type TestingType = z.infer<typeof testingTypeGuard>;
+      const testingConfig = { foo: 'foo', bar: 1, baz: true };
+      expect(() => {
+        validateConfig<TestingType>(testingConfig, testingTypeGuard);
+      }).not.toThrow();
+    });
+
+    it('invalid config', () => {
+      const testingTypeGuard = z.record(z.string());
+      type TestingType = z.infer<typeof testingTypeGuard>;
+      const testingConfig = { foo: 'foo', bar: 1 };
+      expect(() => {
+        validateConfig<TestingType>(testingConfig, testingTypeGuard);
+      }).toThrow();
+    });
+  });
+
+  describe('parseJson', () => {
+    it('should return parsed result', () => {
+      const literalContent = 'foo';
+      expect(parseJson(JSON.stringify(literalContent))).toEqual(literalContent);
+
+      const objectContent = { foo: 'foo', bar: 1, baz: true, qux: [1, '2', null] };
+      expect(parseJson(JSON.stringify(objectContent))).toEqual(objectContent);
+    });
+
+    it('throw error when parsing invalid Json string', () => {
+      expect(() => parseJson('[1,2,3,"4",]')).toThrow();
+    });
+  });
+
+  describe('parseJsonObject', () => {
+    it('should return parsed object', () => {
+      const objectContent = { foo: 'foo', bar: 1, baz: true, qux: [1, '2', null] };
+      expect(parseJsonObject(JSON.stringify(objectContent))).toEqual(objectContent);
+    });
+
+    it('throw error when parsing non-object result', () => {
+      expect(() => parseJsonObject(JSON.stringify('foo'))).toThrow();
+    });
+  });
+});
