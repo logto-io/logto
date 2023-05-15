ccbfd7ea065e59499174273f9a519e2379ff6226
Charles Zhao <charleszhao@silverhand.io>
2023-05-15 19:01:15 +0800

    chore: add code annotation

--- packages/core/src/routes/connector/authorization-uri.ts
+++ packages/core/src/routes/connector/authorization-uri.ts
@@ -12,6 +12,20 @@ export default function connectorAuthorizationUriRoutes<T extends AuthedRouter>(
 ) {
   const { getLogtoConnectorById } = tenant.connectors;
 
+  /**
+   * Returns the authorization uri of the social platform that bundled with the given social connector.
+   * With the returned authorization uri, you can redirect users to the social platform to authenticate.
+   * The usage of this API is usually coupled with `POST /users/:userId/identities` to link authenticated
+   * social identity to a Logto user.
+   *
+   * Note: Currently due to technical limitations, this API does not support the following connectors that
+   * rely on Logto interaction session: `@logto/apple`, `@logto/connector-saml`, `@logto/connector-oidc`
+   * and `@logto/connector-oauth`.
+   *
+   * @param {string} connectorId - The id of the connector
+   * @param {string} state - A random string generated on the client side to prevent CSRF attack
+   * @param {string} redirectUri - The uri to navigate back to after authenticated by the social platform
+   */
   router.post(
     '/connectors/:connectorId/authorization-uri',
     koaGuard({
