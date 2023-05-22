85fb03651c2923b591a05c8fb86ed012013502ec
Xiao Yijun <xiaoyijun@silverhand.io>
2023-05-22 10:49:49 +0800

    feat(console): regenerate signing key (#3866)

---
+++ packages/console/src/pages/WebhookDetails/WebhookSettings/components/SigningKeyField/index.tsx
@@ -0,0 +1,91 @@
+import { type Hook } from '@logto/schemas';
+import { useCallback, useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { useTranslation } from 'react-i18next';
+
+import Redo from '@/assets/images/redo.svg';
+import Button from '@/components/Button';
+import ConfirmModal from '@/components/ConfirmModal';
+import CopyToClipboard from '@/components/CopyToClipboard';
+import DynamicT from '@/components/DynamicT';
+import FormField from '@/components/FormField';
+import useApi from '@/hooks/use-api';
+
+import * as styles from './index.module.scss';
+
+type Props = {
+  hookId: string;
+  signingKey: string;
+  onSigningKeyUpdated: (signingKey: string) => void;
+};
+
+function SigningKeyField({ hookId, signingKey, onSigningKeyUpdated }: Props) {
+  const { t } = useTranslation(undefined);
+  const api = useApi();
+  const [isRegenerateFormOpen, setIsRegenerateFormOpen] = useState(false);
+  const [isRegenerating, setIsRegenerating] = useState(false);
+
+  const regenerateSigningKey = useCallback(
+    async (silent = false) => {
+      if (isRegenerating) {
+        return;
+      }
+      setIsRegenerating(true);
+      try {
+        const { signingKey } = await api.patch(`api/hooks/${hookId}/signing-key`).json<Hook>();
+        if (!silent) {
+          toast.success(t('admin_console.webhook_details.settings.regenerated'));
+        }
+        setIsRegenerateFormOpen(false);
+        onSigningKeyUpdated(signingKey);
+      } finally {
+        setIsRegenerating(false);
+      }
+    },
+    [api, hookId, isRegenerating, onSigningKeyUpdated, t]
+  );
+
+  useEffect(() => {
+    if (!signingKey) {
+      void regenerateSigningKey(true);
+    }
+  }, [regenerateSigningKey, signingKey]);
+
+  return (
+    <FormField
+      title="webhook_details.settings.signing_key"
+      tip={<DynamicT forKey="webhook_details.settings.signing_key_tip" />}
+    >
+      <CopyToClipboard
+        hasVisibilityToggle
+        value={signingKey}
+        variant="border"
+        className={styles.signingKeyField}
+      />
+      <Button
+        type="text"
+        size="small"
+        icon={<Redo />}
+        title="webhook_details.settings.regenerate"
+        className={styles.regenerateButton}
+        onClick={() => {
+          setIsRegenerateFormOpen(true);
+        }}
+      />
+      <ConfirmModal
+        isOpen={isRegenerateFormOpen}
+        isLoading={isRegenerating}
+        confirmButtonText="webhook_details.settings.regenerate"
+        title="webhook_details.settings.regenerate_key_title"
+        onCancel={async () => {
+          setIsRegenerateFormOpen(false);
+        }}
+        onConfirm={async () => regenerateSigningKey()}
+      >
+        <DynamicT forKey="webhook_details.settings.regenerate_key_reminder" />
+      </ConfirmModal>
+    </FormField>
+  );
+}
+
+export default SigningKeyField;
