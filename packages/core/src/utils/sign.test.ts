324c8818995f43be87c832a002a95cc94adbe909
Xiao Yijun <xiaoyijun@silverhand.io>
2023-05-23 16:15:18 +0800

    refactor(core): update sign method

--- packages/core/src/utils/sign.test.ts
+++ packages/core/src/utils/sign.test.ts
@@ -8,7 +8,7 @@ describe('sign', () => {
       foo: 'foo',
     };
 
-    const signature = await sign(signingKey, payload);
+    const signature = sign(signingKey, payload);
 
     const expectedResult =
       'sha256=436958f1dbfefab37712fb3927760490fbf7757da8c0b2306ee7b485f0360eee';
@@ -19,7 +19,7 @@ describe('sign', () => {
   it('should generate correct signature if payload is empty', async () => {
     const signingKey = 'foo';
     const payload = {};
-    const signature = await sign(signingKey, payload);
+    const signature = sign(signingKey, payload);
 
     const expectedResult =
       'sha256=c76356efa19d219d1d7e08ccb20b1d26db53b143156f406c99dcb8e0876d6c55';
--- packages/core/src/utils/sign.ts
+++ packages/core/src/utils/sign.ts
@@ -1,18 +1,8 @@
-import { subtle } from 'node:crypto';
+import { createHmac } from 'node:crypto';
 
-export const sign = async (signingKey: string, payload: Record<string, unknown>) => {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(JSON.stringify(payload));
-  const key = await subtle.importKey(
-    'raw',
-    encoder.encode(signingKey),
-    { name: 'HMAC', hash: 'SHA-256' },
-    false,
-    ['sign']
-  );
-  const signature = await subtle.sign('HMAC', key, data);
-  const signatureHex = Array.from(new Uint8Array(signature))
-    .map((byte) => byte.toString(16).padStart(2, '0'))
-    .join('');
-  return `sha256=${signatureHex}`;
+export const sign = (signingKey: string, payload: Record<string, unknown>) => {
+  const hmac = createHmac('sha256', signingKey);
+  const payloadString = JSON.stringify(payload);
+  hmac.update(payloadString);
+  return `sha256=${hmac.digest('hex')}`;
 };
