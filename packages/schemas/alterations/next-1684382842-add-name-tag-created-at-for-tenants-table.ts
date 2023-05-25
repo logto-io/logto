39e395d496d91daa33636ea76805c6be0196ee28
Darcy Ye <darcyye@silverhand.io>
2023-05-25 15:05:39 +0800

    chore(schemas): update tenants db alteration script

--- packages/schemas/alterations/next-1684382842-add-name-tag-created-at-for-tenants-table.ts
+++ packages/schemas/alterations/next-1684382842-add-name-tag-created-at-for-tenants-table.ts
@@ -6,36 +6,13 @@ const alteration: AlterationScript = {
   up: async (pool) => {
     // Add new tenant columns for name, tag, and created_at.
     await pool.query(sql`
-      alter table tenants add column name varchar(128);
+      alter table tenants add column name varchar(128) not null default 'My Project';
       alter table tenants add column tag varchar(64) not null default 'development';
       alter table tenants add column created_at timestamptz not null default(now());
-    `);
-    // Manually set the name for existing tenants since the trigger is for new tenants only.
-    await pool.query(sql`
-      update tenants set name = concat('tenant_', id);
-    `);
-    await pool.query(sql`
-      alter table tenants alter column name set not null;
-    `);
-    // Create a trigger to set the tenant name since column reference is not available as default value.
-    await pool.query(sql`
-      create function set_tenant_name() returns trigger as
-      $$ begin
-        new.name := concat('tenant_', new.id);
-        return new;
-      end; $$ language plpgsql;
-    `);
-    await pool.query(sql`
-      create trigger set_tenant_name_trigger
-      before insert on tenants
-      for each row when (new.name is null)
-      execute procedure set_tenant_name();
     `);
   },
   down: async (pool) => {
     await pool.query(sql`
-      drop trigger set_tenant_name_trigger on tenants;
-      drop function set_tenant_name;
       alter table tenants drop column name;
       alter table tenants drop column tag;
       alter table tenants drop column created_at;
--- packages/schemas/src/models/tenants.ts
+++ packages/schemas/src/models/tenants.ts
@@ -2,15 +2,13 @@ import { createModel } from '@withtyped/server';
 
 import { TenantTag } from '../index.js';
 
-export const defaultTenantName = 'My Project';
-
 export const Tenants = createModel(/* sql */ `
   /* init_order = 0 */
   create table tenants (
     id varchar(21) not null,
     db_user varchar(128),
     db_user_password varchar(128),
-    name varchar(128) not null default '${defaultTenantName}',
+    name varchar(128) not null default 'My Project',
     tag varchar(64) not null default '${TenantTag.Development}',
     created_at timestamptz not null default(now()),
     primary key (id),
