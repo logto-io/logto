7fb5374963c412cf8d818b5d59e01dcd9a1009c4
wangsijie <wangsijie@silverhand.io>
2023-05-26 11:45:26 +0800

    feat(schemas): add domains table (#3884)

---
+++ packages/schemas/tables/domains.sql
@@ -0,0 +1,17 @@
+create table domains (
+  tenant_id varchar(21) not null
+    references tenants (id) on update cascade on delete cascade,
+  id varchar(21) not null,
+  domain varchar(256) not null,
+  status varchar(32) not null default('PendingVerification'),
+  error_message varchar(1024),
+  dns_records jsonb /* @use DomainDnsRecords */ not null default '[]'::jsonb,
+  cloudflare_data jsonb /* @use CloudflareData */,
+  updated_at timestamptz not null default(now()),
+  created_at timestamptz not null default(now()),
+  primary key (id),
+  constraint domains__domain
+    unique (domain)
+);
+
+create index domains__id on domains (tenant_id, id);
