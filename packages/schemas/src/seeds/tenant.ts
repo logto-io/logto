3808798101ef2960db2d442f38ee873221bf7295
Darcy Ye <darcyye@silverhand.io>
2023-06-01 16:38:27 +0800

    chore(schemas): remove useless comment

--- packages/schemas/src/seeds/tenant.ts
+++ packages/schemas/src/seeds/tenant.ts
@@ -5,8 +5,4 @@ import type { Tenants } from '../models/tenants.js';
 export const defaultTenantId = 'default';
 export const adminTenantId = 'admin';
 
-/**
- * `createModel` from @withtyped/server can not properly infer the model
- * type, manually define it here for now.
- */
 export type TenantModel = InferModelType<typeof Tenants>;
