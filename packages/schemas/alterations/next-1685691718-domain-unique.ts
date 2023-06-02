3533a6d6b62602c9cda609dbb7cc8c7fcd657e07
wangsijie <wangsijie94@gmail.com>
2023-06-02 15:50:33 +0800

    fix(schemas): modify domain table unique index

---
+++ packages/schemas/alterations/next-1685691718-domain-unique.ts
@@ -0,0 +1,20 @@
+import { sql } from 'slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      alter table domains drop constraint domains__domain;
+      alter table domains add constraint domains__domain unique (tenant_id, domain);
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      alter table domains drop constraint domains__domain;
+      alter table domains add constraint domains__domain unique (domain);
+    `);
+  },
+};
+
+export default alteration;
