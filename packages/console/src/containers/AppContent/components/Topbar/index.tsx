76cd7b7abcb761a7631ae0e7f6c41fce37aeab9c
Darcy Ye <darcyye@silverhand.io>
2023-06-05 11:33:18 +0800

    feat(console): add tenant list dropdown

--- packages/console/src/containers/AppContent/components/Topbar/index.tsx
+++ packages/console/src/containers/AppContent/components/Topbar/index.tsx
@@ -4,11 +4,12 @@ import { useTranslation } from 'react-i18next';
 import CloudLogo from '@/assets/images/cloud-logo.svg';
 import Logo from '@/assets/images/logo.svg';
 import Spacer from '@/components/Spacer';
-import { isCloud } from '@/consts/env';
+import { isProduction, isCloud } from '@/consts/env';
 import EarlyBirdGift from '@/onboarding/components/EarlyBirdGift';
 
 import Contact from './Contact';
 import DocumentNavButton from './DocumentNavButton';
+import TenantSelector from './TenantSelector';
 import UserInfo from './UserInfo';
 import * as styles from './index.module.scss';
 
@@ -23,6 +24,7 @@ function Topbar({ className }: Props) {
   return (
     <div className={classNames(styles.topbar, className)}>
       <LogtoLogo className={styles.logo} />
+      {isCloud && !isProduction && <TenantSelector />}
       {!isCloud && (
         <>
           <div className={styles.line} />
