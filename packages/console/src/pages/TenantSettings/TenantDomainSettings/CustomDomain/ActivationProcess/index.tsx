cd679359e1a9672817800a235794d2c1fbc672e3
Xiao Yijun <xiaoyijun@silverhand.io>
2023-06-09 10:50:58 +0800

    refactor(console): remove redundant ssl dns config for custom domain (#4012)

--- packages/console/src/pages/TenantSettings/TenantDomainSettings/CustomDomain/ActivationProcess/index.tsx
+++ packages/console/src/pages/TenantSettings/TenantDomainSettings/CustomDomain/ActivationProcess/index.tsx
@@ -1,4 +1,4 @@
-import { type Domain, type DomainDnsRecords, type DomainDnsRecord } from '@logto/schemas';
+import { type Domain } from '@logto/schemas';
 
 import DynamicT from '@/components/DynamicT';
 import InlineNotification from '@/components/InlineNotification';
@@ -12,31 +12,8 @@ type Props = {
   customDomain: Domain;
 };
 
-const isSetupSslDnsRecord = ({ type, name }: DomainDnsRecord) =>
-  type.toUpperCase() === 'TXT' && name.includes('_acme-challenge');
-
 function ActivationProcess({ customDomain }: Props) {
   const { dnsRecords, status: domainStatus, errorMessage } = customDomain;
-
-  const { verifyDomainDnsRecord, setupSslDnsRecord } = dnsRecords.reduce<{
-    verifyDomainDnsRecord: DomainDnsRecords;
-    setupSslDnsRecord: DomainDnsRecords;
-  }>(
-    (result, record) =>
-      isSetupSslDnsRecord(record)
-        ? {
-            ...result,
-            setupSslDnsRecord: [...result.setupSslDnsRecord, record],
-          }
-        : {
-            ...result,
-            verifyDomainDnsRecord: [...result.verifyDomainDnsRecord, record],
-          },
-    {
-      verifyDomainDnsRecord: [],
-      setupSslDnsRecord: [],
-    }
-  );
 
   return (
     <div className={styles.container}>
@@ -57,16 +34,14 @@ function ActivationProcess({ customDomain }: Props) {
         tip="domain.custom.checking_dns_tip"
         domainStatus={domainStatus}
       >
-        <DnsRecordsTable records={verifyDomainDnsRecord} />
+        <DnsRecordsTable records={dnsRecords} />
       </Step>
       <Step
         step={2}
         title="domain.custom.enable_ssl"
         tip="domain.custom.checking_dns_tip"
         domainStatus={domainStatus}
-      >
-        <DnsRecordsTable records={setupSslDnsRecord} />
-      </Step>
+      />
     </div>
   );
 }
