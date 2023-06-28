c1fa47f2804df6e1f6dc99f47418b96a7bc0dd2a
Gao Sun <gao@silverhand.io>
2023-06-28 15:51:45 +0800

    refactor(console): redirect to current tenant when needed

--- packages/console/src/utils/url.ts
+++ packages/console/src/utils/url.ts
@@ -2,3 +2,11 @@ export const buildUrl = (path: string, searchParameters: Record<string, string>)
   `${path}?${new URLSearchParams(searchParameters).toString()}`;
 
 export const formatSearchKeyword = (keyword: string) => `%${keyword}%`;
+
+/** If the current pathname is `/callback` or ends with `-callback`, we consider it as a callback page. */
+export const isInCallback = () =>
+  ['/callback', '-callback'].some((path) => window.location.pathname.endsWith(path));
+
+/** If the current pathname is a callback page and the pathname only has one level. */
+export const isInFirstLevelCallback = () =>
+  window.location.pathname.split('/').length === 1 && isInCallback();
