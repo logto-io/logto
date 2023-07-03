e89ccd4d4cf0f25eb0e3381e35dda2a0b7500219
Gao Sun <gao@silverhand.io>
2023-07-03 14:33:19 +0800

    refactor: cloud cleanup (#4103)
    * refactor: cleanup cloud
    
    * chore: fix cloud dependencies
    
    * chore: update LICENSE

--- packages/cloud/src/utils/tenant.ts
+++
@@ -1,9 +0,0 @@
-import { getManagementApiResourceIndicator } from '@logto/schemas';
-import { trySafe } from '@silverhand/essentials';
-
-export const getTenantIdFromManagementApiIndicator = (indicator: string) => {
-  const toMatch = '^' + getManagementApiResourceIndicator('([^.]*)') + '$';
-  const url = trySafe(() => new URL(indicator));
-
-  return url && new RegExp(toMatch).exec(url.href)?.[1];
-};
