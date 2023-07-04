61c49845a6874c5f5645ca3930413718b610e86f
Darcy Ye <darcyye@silverhand.io>
2023-07-04 18:14:35 +0800

    refactor(core,schemas,cli): save cloud service m2m app credentials (#4109)

--- packages/core/src/libraries/logto-config.ts
+++ packages/core/src/libraries/logto-config.ts
@@ -1,12 +1,19 @@
 import type { LogtoOidcConfigType } from '@logto/schemas';
-import { logtoOidcConfigGuard, LogtoOidcConfigKey } from '@logto/schemas';
+import {
+  cloudApiIndicator,
+  cloudConnectionDataGuard,
+  logtoOidcConfigGuard,
+  LogtoOidcConfigKey,
+} from '@logto/schemas';
 import chalk from 'chalk';
 import { z, ZodError } from 'zod';
 
 import type Queries from '#src/tenants/Queries.js';
 import { consoleLog } from '#src/utils/console.js';
 
-export const createLogtoConfigLibrary = ({ getRowsByKeys }: Queries['logtoConfigs']) => {
+export const createLogtoConfigLibrary = ({
+  logtoConfigs: { getRowsByKeys, getCloudConnectionData: queryCloudConnectionData },
+}: Pick<Queries, 'logtoConfigs'>) => {
   const getOidcConfigs = async (): Promise<LogtoOidcConfigType> => {
     try {
       const { rows } = await getRowsByKeys(Object.values(LogtoOidcConfigKey));
@@ -35,5 +42,20 @@ export const createLogtoConfigLibrary = ({ getRowsByKeys }: Queries['logtoConfig
     }
   };
 
-  return { getOidcConfigs };
+  const getCloudConnectionData = async () => {
+    const { value } = await queryCloudConnectionData();
+    const result = cloudConnectionDataGuard.safeParse(value);
+
+    if (!result.success) {
+      return;
+    }
+
+    return {
+      appId: result.data.appId,
+      appSecret: result.data.appSecret,
+      resource: cloudApiIndicator,
+    };
+  };
+
+  return { getOidcConfigs, getCloudConnectionData };
 };
---
+++ packages/schemas/alterations/next-1688375200-sync-cloud-m2m-to-logto-config.ts
@@ -0,0 +1,85 @@
+import { sql } from 'slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const adminTenantId = 'admin';
+
+const cloudServiceApplicationName = 'Cloud Service';
+
+const cloudConnectionResourceIndicator = 'https://cloud.logto.io/api';
+
+enum ApplicationType {
+  Native = 'Native',
+  SPA = 'SPA',
+  Traditional = 'Traditional',
+  MachineToMachine = 'MachineToMachine',
+}
+
+const cloudConnectionConfigKey = 'cloudConnection';
+
+type Application = {
+  tenantId: string;
+  id: string;
+  name: string;
+  secret: string;
+  description: string;
+  type: ApplicationType;
+  oidcClientMetadata: unknown;
+  customClientMetadata: {
+    tenantId: string;
+  };
+  createdAt: number;
+};
+
+type CloudConnectionConfig = {
+  tenantId: string;
+  key: string;
+  value: unknown;
+};
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    const rows = await pool.many<Application>(
+      sql`select * from applications where type = ${ApplicationType.MachineToMachine} and tenant_id = ${adminTenantId} and name = ${cloudServiceApplicationName}`
+    );
+
+    const { rows: existingCloudConnections } = await pool.query<CloudConnectionConfig>(sql`
+      select * from logto_configs where key = ${cloudConnectionConfigKey}
+    `);
+    const tenantIdsWithExistingRecords = new Set(
+      existingCloudConnections.map(({ tenantId }) => tenantId)
+    );
+    const filteredRows = rows.filter(
+      ({ customClientMetadata: { tenantId } }) => !tenantIdsWithExistingRecords.has(tenantId)
+    );
+
+    if (filteredRows.length === 0) {
+      return;
+    }
+
+    await pool.query(sql`
+      insert into logto_configs (tenant_id, key, value) values ${sql.join(
+        filteredRows.map(({ id, secret, customClientMetadata }) => {
+          const { tenantId } = customClientMetadata;
+          const cloudConnectionValue = {
+            appId: id,
+            appSecret: secret,
+            resource: cloudConnectionResourceIndicator,
+          };
+
+          return sql`(${tenantId}, ${cloudConnectionConfigKey}, ${JSON.stringify(
+            cloudConnectionValue
+          )})`;
+        }),
+        sql`,`
+      )}
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      delete from logto_configs where key = ${cloudConnectionConfigKey}
+    `);
+  },
+};
+
+export default alteration;
