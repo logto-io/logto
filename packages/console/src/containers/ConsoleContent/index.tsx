02ccc24d72c2e081498dcb07ef416aa79cd2c45c
Xiao Yijun <xiaoyijun@silverhand.io>
2023-07-08 12:02:49 +0800

    chore(console): block subscritpion route (#4139)

--- packages/console/src/containers/ConsoleContent/index.tsx
+++ packages/console/src/containers/ConsoleContent/index.tsx
@@ -9,7 +9,7 @@ import {
   WebhookDetailsTabs,
   TenantSettingsTabs,
 } from '@/consts';
-import { isCloud } from '@/consts/env';
+import { isCloud, isProduction } from '@/consts/env';
 import OverlayScrollbar from '@/ds-components/OverlayScrollbar';
 import ApiResourceDetails from '@/pages/ApiResourceDetails';
 import ApiResourcePermissions from '@/pages/ApiResourceDetails/ApiResourcePermissions';
@@ -148,7 +148,9 @@ function ConsoleContent() {
                 <Route index element={<Navigate replace to={TenantSettingsTabs.Settings} />} />
                 <Route path={TenantSettingsTabs.Settings} element={<TenantBasicSettings />} />
                 <Route path={TenantSettingsTabs.Domains} element={<TenantDomainSettings />} />
-                <Route path={TenantSettingsTabs.Subscription} element={<Subscription />} />
+                {!isProduction && (
+                  <Route path={TenantSettingsTabs.Subscription} element={<Subscription />} />
+                )}
               </Route>
             )}
           </Routes>
