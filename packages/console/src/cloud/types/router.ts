fa193b21bc06f7a4ff08ae5ba12ae4e67c7cd2a7
Xiao Yijun <xiaoyijun@silverhand.io>
2023-07-10 17:22:04 +0800

    feat(console): add switch plan action bar

--- packages/console/src/cloud/types/router.ts
+++ packages/console/src/cloud/types/router.ts
@@ -1,8 +1,10 @@
 import type router from '@logto/cloud/routes';
 import { type GuardedResponse, type RouterRoutes } from '@withtyped/client';
 
-export type GetRoutes = RouterRoutes<typeof router>['get'];
+type GetRoutes = RouterRoutes<typeof router>['get'];
 
 export type SubscriptionPlanResponse = GuardedResponse<
   GetRoutes['/api/subscription-plans']
 >[number];
+
+export type Subscription = GuardedResponse<GetRoutes['/api/tenants/:tenantId/subscription']>;
