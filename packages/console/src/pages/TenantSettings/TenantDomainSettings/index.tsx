5d3cd7a16b71a378189270ebb57a91f75d065888
Xiao Yijun <xiaoyijun@silverhand.io>
2023-07-18 23:48:57 +0800

    feat(console): apply quota limit for custom domain

--- packages/console/src/pages/TenantSettings/TenantDomainSettings/index.tsx
+++ packages/console/src/pages/TenantSettings/TenantDomainSettings/index.tsx
@@ -1,8 +1,16 @@
 import { withAppInsights } from '@logto/app-insights/react';
+import { Trans, useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 import FormCard from '@/components/FormCard';
 import PageMeta from '@/components/PageMeta';
+import ProTag from '@/components/ProTag';
+import { contactEmailLink } from '@/consts';
+import { isProduction } from '@/consts/env';
 import FormField from '@/ds-components/FormField';
+import InlineNotification from '@/ds-components/InlineNotification';
+import TextLink from '@/ds-components/TextLink';
+import useCurrentSubscriptionPlan from '@/hooks/use-current-subscription-plan';
 import useCustomDomain from '@/hooks/use-custom-domain';
 import useDocumentationUrl from '@/hooks/use-documentation-url';
 
@@ -14,18 +22,34 @@ import DefaultDomain from './DefaultDomain';
 import * as styles from './index.module.scss';
 
 function TenantDomainSettings() {
-  const { data: customDomain, isLoading, mutate } = useCustomDomain(true);
+  const { data: customDomain, isLoading: isLoadingCustomDomain, mutate } = useCustomDomain(true);
+  const { data: currentPlan, error: fetchCurrentPlanError } = useCurrentSubscriptionPlan();
+  const isLoadingCurrentPlan = !currentPlan && !fetchCurrentPlanError;
   const { getDocumentationUrl } = useDocumentationUrl();
+  const { t } = useTranslation(undefined, { keyPrefix: 'admin_console' });
+  const navigate = useNavigate();
 
-  if (isLoading) {
+  if (isLoadingCustomDomain || isLoadingCurrentPlan) {
     return <Skeleton />;
   }
+
+  const customDomainEnabled =
+    /**
+     * Todo: @xiaoyijun remove this condition on subscription features ready.
+     */
+    isProduction ||
+    Boolean(currentPlan?.quota.customDomainEnabled) ||
+    /**
+     * Note: this is for tenants which already have a custom domain before we have subscription features.
+     */
+    Boolean(customDomain);
 
   return (
     <div className={styles.container}>
       <PageMeta titleKey={['tenants.tabs.domains', 'tenants.title']} />
       <FormCard
         title="domain.custom.custom_domain"
+        tag={!isProduction && <ProTag />}
         description="domain.custom.custom_domain_description"
         learnMoreLink={getDocumentationUrl('docs/recipes/custom-domain')}
       >
@@ -33,7 +57,29 @@ function TenantDomainSettings() {
           {customDomain ? (
             <CustomDomain customDomain={customDomain} onDeleteCustomDomain={mutate} />
           ) : (
-            <AddDomainForm onCustomDomainAdded={mutate} />
+            <AddDomainForm
+              isCustomDomainEnabled={customDomainEnabled}
+              onCustomDomainAdded={mutate}
+            />
+          )}
+          {!isProduction && !customDomain && !customDomainEnabled && (
+            <InlineNotification
+              hasIcon={false}
+              severity="info"
+              action="upsell.compare_plans"
+              className={styles.upsellNotification}
+              onClick={() => {
+                navigate('/tenant-settings/subscription');
+              }}
+            >
+              <Trans
+                components={{
+                  a: <TextLink href={contactEmailLink} target="_blank" />,
+                }}
+              >
+                {t('upsell.paywall.custom_domain')}
+              </Trans>
+            </InlineNotification>
           )}
         </FormField>
       </FormCard>
