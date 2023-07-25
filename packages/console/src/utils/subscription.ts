9d02e1300c434bf05803644142546ac80376c531
Xiao Yijun <xiaoyijun@silverhand.io>
2023-07-25 14:24:56 +0800

    fix(console): avoid reading response error body more than once (#4223)

--- packages/console/src/utils/subscription.ts
+++ packages/console/src/utils/subscription.ts
@@ -1,7 +1,7 @@
 import { ResponseError } from '@withtyped/client';
 import dayjs from 'dayjs';
 
-import { responseErrorBodyGuard } from '@/cloud/hooks/use-cloud-api';
+import { tryReadResponseErrorBody } from '@/cloud/hooks/use-cloud-api';
 import { type SubscriptionPlanResponse } from '@/cloud/types/router';
 import {
   communitySupportEnabledMap,
@@ -64,11 +64,7 @@ export const isExceededQuotaLimitError = async (error: unknown) => {
     return false;
   }
 
-  try {
-    const responseBody = await error.response.json();
-    const { message } = responseErrorBodyGuard.parse(responseBody);
-    return message.includes('Exceeded quota limit');
-  } catch {
-    return false;
-  }
+  const { message } = (await tryReadResponseErrorBody(error)) ?? {};
+
+  return Boolean(message?.includes('Exceeded quota limit'));
 };
