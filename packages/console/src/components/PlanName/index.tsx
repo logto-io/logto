7ce014d0337f95b73285cb52ee0e723697cebf79
Xiao Yijun <xiaoyijun@silverhand.io>
2023-08-02 20:51:25 +0800

    chore(phrases): apply title-cased plan name for tenant plan selection button (#4276)

--- packages/console/src/components/PlanName/index.tsx
+++ packages/console/src/components/PlanName/index.tsx
@@ -1,6 +1,7 @@
 import { type TFuncKey } from 'i18next';
+import { useTranslation } from 'react-i18next';
+import titleize from 'titleize';
 
-import DynamicT from '@/ds-components/DynamicT';
 import { ReservedPlanName } from '@/types/subscriptions';
 
 const registeredPlanNamePhraseMap: Record<
@@ -16,19 +17,19 @@ const registeredPlanNamePhraseMap: Record<
 
 type Props = {
   name: string;
+  isTitleCase?: boolean;
 };
 
-function PlanName({ name }: Props) {
+function PlanName({ name, isTitleCase = false }: Props) {
+  const { t } = useTranslation(undefined, { keyPrefix: 'admin_console.subscription' });
   const planNamePhrase = registeredPlanNamePhraseMap[name];
 
   /**
    * Note: fallback to the plan name if the phrase is not registered.
    */
-  if (!planNamePhrase) {
-    return <span>{name}</span>;
-  }
+  const planName = planNamePhrase ? String(t(planNamePhrase)) : name;
 
-  return <DynamicT forKey={`subscription.${planNamePhrase}`} />;
+  return <span>{isTitleCase ? titleize(planName) : planName}</span>;
 }
 
 export default PlanName;
