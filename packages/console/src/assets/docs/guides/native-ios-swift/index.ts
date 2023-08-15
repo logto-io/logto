7b47db55a5a5c208acfd271cf6149d25e4ec2950
Gao Sun <gao@silverhand.io>
2023-08-15 16:09:24 +0800

    refactor(console): migrate existing guides to v2

---
+++ packages/console/src/assets/docs/guides/native-ios-swift/index.ts
@@ -0,0 +1,15 @@
+import { ApplicationType } from '@logto/schemas';
+
+import { type GuideMetadata } from '../types';
+
+const metadata: Readonly<GuideMetadata> = Object.freeze({
+  name: 'iOS (Swift)',
+  description: 'iOS (Swift) application integration guide.',
+  target: ApplicationType.Native,
+  sample: {
+    repo: 'swift',
+    path: 'Demos/SwiftUI%20Demo',
+  },
+});
+
+export default metadata;
---
+++ packages/console/src/assets/docs/guides/web-next/index.ts
@@ -0,0 +1,16 @@
+import { ApplicationType } from '@logto/schemas';
+
+import { type GuideMetadata } from '../types';
+
+const metadata: Readonly<GuideMetadata> = Object.freeze({
+  name: 'Next.js',
+  description:
+    'Next.js is a React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
+  target: ApplicationType.Traditional,
+  sample: {
+    repo: 'js',
+    path: 'packages/next-sample',
+  },
+});
+
+export default metadata;
