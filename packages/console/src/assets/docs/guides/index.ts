3ac73356aaa8af60532e0fdf5158e178e0fb237b
Gao Sun <gao@silverhand.io>
2023-08-15 17:10:05 +0800

    refactor(console): update content

--- packages/console/src/assets/docs/guides/index.ts
+++ packages/console/src/assets/docs/guides/index.ts
@@ -2,11 +2,39 @@
 
 import { lazy } from 'react';
 
+import nativeAndroidJava from './native-android-java/index';
+import nativeAndroidKt from './native-android-kt/index';
+import nativeIosSwift from './native-ios-swift/index';
 import spaReact from './spa-react/index';
+import spaVanilla from './spa-vanilla/index';
+import spaVue from './spa-vue/index';
 import { type Guide } from './types';
 import webExpress from './web-express/index';
+import webGo from './web-go/index';
+import webNext from './web-next/index';
 
 const guides: Readonly<Guide[]> = Object.freeze([
+  {
+    id: 'native-android-java',
+    Logo: lazy(async () => import('./native-android-java/logo.svg')),
+    Component: lazy(async () => import('./native-android-java/README.mdx')),
+    metadata: nativeAndroidJava,
+  },
+
+  {
+    id: 'native-android-kt',
+    Logo: lazy(async () => import('./native-android-kt/logo.svg')),
+    Component: lazy(async () => import('./native-android-kt/README.mdx')),
+    metadata: nativeAndroidKt,
+  },
+
+  {
+    id: 'native-ios-swift',
+    Logo: lazy(async () => import('./native-ios-swift/logo.svg')),
+    Component: lazy(async () => import('./native-ios-swift/README.mdx')),
+    metadata: nativeIosSwift,
+  },
+
   {
     id: 'spa-react',
     Logo: lazy(async () => import('./spa-react/logo.svg')),
@@ -15,10 +43,38 @@ const guides: Readonly<Guide[]> = Object.freeze([
   },
 
   {
+    id: 'spa-vanilla',
+    Logo: lazy(async () => import('./spa-vanilla/logo.svg')),
+    Component: lazy(async () => import('./spa-vanilla/README.mdx')),
+    metadata: spaVanilla,
+  },
+
+  {
+    id: 'spa-vue',
+    Logo: lazy(async () => import('./spa-vue/logo.svg')),
+    Component: lazy(async () => import('./spa-vue/README.mdx')),
+    metadata: spaVue,
+  },
+
+  {
     id: 'web-express',
     Logo: lazy(async () => import('./web-express/logo.svg')),
     Component: lazy(async () => import('./web-express/README.mdx')),
     metadata: webExpress,
+  },
+
+  {
+    id: 'web-go',
+    Logo: lazy(async () => import('./web-go/logo.svg')),
+    Component: lazy(async () => import('./web-go/README.mdx')),
+    metadata: webGo,
+  },
+
+  {
+    id: 'web-next',
+    Logo: lazy(async () => import('./web-next/logo.svg')),
+    Component: lazy(async () => import('./web-next/README.mdx')),
+    metadata: webNext,
   },
 ]);
 
