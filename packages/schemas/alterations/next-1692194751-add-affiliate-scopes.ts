386d32177c3e786069db813903a79d5b40e1d359
Gao Sun <gao@silverhand.io>
2023-08-16 22:08:43 +0800

    refactor: fix alteration

--- packages/schemas/alterations/next-1692194751-add-affiliate-scopes.ts
+++ packages/schemas/alterations/next-1692194751-add-affiliate-scopes.ts
@@ -14,27 +14,42 @@ const alteration: AlterationScript = {
       and indicator = 'https://cloud.logto.io/api'
     `);
 
+    const { id: roleId } = await pool.one<{ id: string }>(sql`
+      select id from roles
+      where tenant_id = ${adminTenantId}
+      and name = 'admin:admin'
+    `);
+
+    const createAffiliateId = generateStandardId();
+    const manageAffiliateId = generateStandardId();
+
     await pool.query(sql`
       insert into scopes (tenant_id, id, name, description, resource_id)
         values (
           ${adminTenantId},
-          ${generateStandardId()},
+          ${createAffiliateId},
           'create:affiliate',
           'Allow creating new affiliates and logs.',
           ${resourceId}
         ), (
           ${adminTenantId},
-          ${generateStandardId()},
+          ${manageAffiliateId},
           'manage:affiliate',
           'Allow managing affiliates, including create, update, and delete.',
           ${resourceId}
         );
     `);
+
+    await pool.query(sql`
+      insert into roles_scopes (tenant_id, id, role_id, scope_id) values
+        (${adminTenantId}, ${generateStandardId()}, ${roleId}, ${createAffiliateId}),
+        (${adminTenantId}, ${generateStandardId()}, ${roleId}, ${manageAffiliateId});
+    `);
   },
   down: async (pool) => {
     await pool.query(sql`
       delete from scopes
-      where tenant_id = ${adminTenantId} and name = any('create:affiliate', 'manage:affiliate');
+      where tenant_id = ${adminTenantId} and name = any(array['create:affiliate', 'manage:affiliate']);
     `);
   },
 };
