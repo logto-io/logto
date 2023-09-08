9a44a914cf3fe9b5eeab7739afc880f414a76ef7
Darcy Ye <darcyye@silverhand.io>
2023-09-08 16:22:39 +0800

    chore(core): add search for GET role-application

--- packages/core/src/queries/application.ts
+++ packages/core/src/queries/application.ts
@@ -1,7 +1,7 @@
 import type { Application, CreateApplication } from '@logto/schemas';
 import { ApplicationType, Applications } from '@logto/schemas';
 import type { OmitAutoSetFields } from '@logto/shared';
-import { convertToIdentifiers } from '@logto/shared';
+import { convertToIdentifiers, conditionalSql } from '@logto/shared';
 import type { CommonQueryMethods } from 'slonik';
 import { sql } from 'slonik';
 
@@ -11,8 +11,24 @@ import { buildInsertIntoWithPool } from '#src/database/insert-into.js';
 import { getTotalRowCountWithPool } from '#src/database/row-count.js';
 import { buildUpdateWhereWithPool } from '#src/database/update-where.js';
 import { DeletionError } from '#src/errors/SlonikError/index.js';
+import { buildConditionsFromSearch } from '#src/utils/search.js';
+import type { Search } from '#src/utils/search.js';
 
 const { table, fields } = convertToIdentifiers(Applications);
+
+const buildApplicationConditions = (search: Search) => {
+  const hasSearch = search.matches.length > 0;
+  const searchFields = [
+    Applications.fields.id,
+    Applications.fields.name,
+    Applications.fields.description,
+  ];
+
+  return conditionalSql(
+    hasSearch,
+    () => sql`and ${buildConditionsFromSearch(search, searchFields)}`
+  );
+};
 
 export const createApplicationQueries = (pool: CommonQueryMethods) => {
   const findTotalNumberOfApplications = async () => getTotalRowCountWithPool(pool)(table);
@@ -54,7 +70,7 @@ export const createApplicationQueries = (pool: CommonQueryMethods) => {
     return { count: Number(count) };
   };
 
-  const countM2mApplicationsByIds = async (applicationIds: string[]) => {
+  const countM2mApplicationsByIds = async (search: Search, applicationIds: string[]) => {
     if (applicationIds.length === 0) {
       return { count: 0 };
     }
@@ -64,12 +80,14 @@ export const createApplicationQueries = (pool: CommonQueryMethods) => {
       from ${table}
       where ${fields.type} = ${ApplicationType.MachineToMachine}
       and ${fields.id} in (${sql.join(applicationIds, sql`, `)})
+      ${buildApplicationConditions(search)}
     `);
 
     return { count: Number(count) };
   };
 
   const findM2mApplicationsByIds = async (
+    search: Search,
     limit: number,
     offset: number,
     applicationIds: string[]
@@ -83,6 +101,7 @@ export const createApplicationQueries = (pool: CommonQueryMethods) => {
       from ${table}
       where ${fields.type} = ${ApplicationType.MachineToMachine}
       and ${fields.id} in (${sql.join(applicationIds, sql`, `)})
+      ${buildApplicationConditions(search)}
       limit ${limit}
       offset ${offset}
     `);
--- packages/core/src/routes/admin-user-role.test.ts
+++ packages/core/src/routes/admin-user-role.test.ts
@@ -104,40 +104,41 @@ describe('user role routes', () => {
       expect(insertUsersRoles).toHaveBeenCalledWith([]);
     });
 
-  it('POST /users/:id/roles', async () => {
-    findUsersRolesByUserId.mockResolvedValueOnce([]);
-    const response = await roleRequester.post(`/users/${mockUser.id}/roles`).send({
-      roleIds: [mockAdminUserRole.id],
+    it('POST /users/:id/roles', async () => {
+      findUsersRolesByUserId.mockResolvedValueOnce([]);
+      const response = await roleRequester.post(`/users/${mockUser.id}/roles`).send({
+        roleIds: [mockAdminUserRole.id],
+      });
+      expect(response.status).toEqual(201);
+      expect(insertUsersRoles).toHaveBeenCalledWith([
+        { id: mockId, userId: mockUser.id, roleId: mockAdminUserRole.id },
+      ]);
     });
-    expect(response.status).toEqual(201);
-    expect(insertUsersRoles).toHaveBeenCalledWith([
-      { id: mockId, userId: mockUser.id, roleId: mockAdminUserRole.id },
-    ]);
-  });
 
-  it('PUT /users/:id/roles', async () => {
-    findUsersRolesByUserId.mockResolvedValueOnce([mockUserRole]);
-    const response = await roleRequester.put(`/users/${mockUser.id}/roles`).send({
-      roleIds: [mockAdminUserRole2.id],
+    it('PUT /users/:id/roles', async () => {
+      findUsersRolesByUserId.mockResolvedValueOnce([mockUserRole]);
+      const response = await roleRequester.put(`/users/${mockUser.id}/roles`).send({
+        roleIds: [mockAdminUserRole2.id],
+      });
+      expect(response.status).toEqual(200);
+      expect(deleteUsersRolesByUserIdAndRoleId).toHaveBeenCalledWith(
+        mockUser.id,
+        mockAdminUserRole.id
+      );
+      expect(insertUsersRoles).toHaveBeenCalledWith([
+        { id: mockId, userId: mockUser.id, roleId: mockAdminUserRole2.id },
+      ]);
     });
-    expect(response.status).toEqual(200);
-    expect(deleteUsersRolesByUserIdAndRoleId).toHaveBeenCalledWith(
-      mockUser.id,
-      mockAdminUserRole.id
-    );
-    expect(insertUsersRoles).toHaveBeenCalledWith([
-      { id: mockId, userId: mockUser.id, roleId: mockAdminUserRole2.id },
-    ]);
-  });
 
-  it('DELETE /users/:id/roles/:roleId', async () => {
-    const response = await roleRequester.delete(
-      `/users/${mockUser.id}/roles/${mockAdminUserRole.id}`
-    );
-    expect(response.status).toEqual(204);
-    expect(deleteUsersRolesByUserIdAndRoleId).toHaveBeenCalledWith(
-      mockUser.id,
-      mockAdminUserRole.id
-    );
+    it('DELETE /users/:id/roles/:roleId', async () => {
+      const response = await roleRequester.delete(
+        `/users/${mockUser.id}/roles/${mockAdminUserRole.id}`
+      );
+      expect(response.status).toEqual(204);
+      expect(deleteUsersRolesByUserIdAndRoleId).toHaveBeenCalledWith(
+        mockUser.id,
+        mockAdminUserRole.id
+      );
+    });
   });
 });
