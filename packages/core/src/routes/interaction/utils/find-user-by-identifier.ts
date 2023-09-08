a89ce6eeed8b7be46eed46ba1be65e56a550eefb
Gao Sun <gao@silverhand.io>
2023-09-08 13:42:54 +0800

    refactor(core): check personal info when setting password

--- packages/core/src/routes/interaction/utils/find-user-by-identifier.ts
+++ packages/core/src/routes/interaction/utils/find-user-by-identifier.ts
@@ -2,6 +2,16 @@ import type TenantContext from '#src/tenants/TenantContext.js';
 
 import type { UserIdentity } from '../types/index.js';
 
+/**
+ * Find user by the given identifier in the following order:
+ *
+ * 1. Find user by username
+ * 2. Find user by email
+ * 3. Find user by phone
+ * 4. Find user by social identity
+ *
+ * @returns The found user or `null` if no user is found.
+ */
 export default async function findUserByIdentifier(
   { queries, connectors }: TenantContext,
   identity: UserIdentity
--- packages/core/src/routes/interaction/utils/validate-password.ts
+++ packages/core/src/routes/interaction/utils/validate-password.ts
@@ -1,21 +1,78 @@
-import { type PasswordPolicyChecker } from '@logto/core-kit';
+import { type PasswordPolicyChecker, type UserInfo } from '@logto/core-kit';
 import { type Optional } from '@silverhand/essentials';
 
 import RequestError from '#src/errors/RequestError/index.js';
+import type TenantContext from '#src/tenants/TenantContext.js';
+
+import { type AnonymousInteractionResult } from '../types/index.js';
+
+import findUserByIdentifier from './find-user-by-identifier.js';
+
+/**
+ * Fetch current user information from the interaction storage by checking `identifiers`
+ * and `profile` properties.
+ *
+ * Data in `profile` will override data in `identifiers`, if any. Because `profile` will
+ * overwrite the data in `identifiers` if this interaction is validated.
+ */
+const fetchUserInfo = async (
+  tenant: TenantContext,
+  { identifiers, profile }: Pick<AnonymousInteractionResult, 'identifiers' | 'profile'>
+): Promise<UserInfo> => {
+  const users = await Promise.all(
+    identifiers?.map(async (identifier) => {
+      switch (identifier.key) {
+        case 'emailVerified': {
+          return findUserByIdentifier(tenant, { email: identifier.value });
+        }
+        case 'phoneVerified': {
+          return findUserByIdentifier(tenant, { phone: identifier.value });
+        }
+        case 'social': {
+          return findUserByIdentifier(tenant, identifier);
+        }
+        case 'accountId': {
+          return tenant.queries.users.findUserById(identifier.value);
+        }
+        default: {
+          return null;
+        }
+      }
+    }) ?? []
+  );
+
+  // Use the first non-null user as the current user. Users with different account IDs
+  // should not be mixed in the same interaction, and it should be validated in some other
+  // places.
+  const user = users.find((user) => user !== null);
+
+  return {
+    username: user?.username ?? profile?.username,
+    email: user?.primaryEmail ?? profile?.email,
+    phoneNumber: user?.primaryPhone ?? profile?.phone,
+    name: user?.name ?? undefined,
+  };
+};
 
 /**
  * Validate password against the given password policy if the password is not undefined,
  * throw a {@link RequestError} if the password is invalid; otherwise, do nothing.
  */
 export const validatePassword = async (
+  tenant: TenantContext,
   password: Optional<string>,
-  checker: PasswordPolicyChecker
+  checker: PasswordPolicyChecker,
+  { identifiers, profile }: Pick<AnonymousInteractionResult, 'identifiers' | 'profile'>
 ) => {
   if (password === undefined) {
     return;
   }
 
-  const issues = await checker.check(password, {});
+  const issues = await checker.check(
+    password,
+    checker.policy.rejects.userInfo ? await fetchUserInfo(tenant, { identifiers, profile }) : {}
+  );
+
   if (issues.length > 0) {
     throw new RequestError('password.rejected', issues);
   }
