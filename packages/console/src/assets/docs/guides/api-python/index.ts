d1c25b9c26ead44566d80573abeceebb59c93915
Charles Zhao <charleszhao@silverhand.io>
2023-09-15 18:56:05 +0800

    feat(console): add api resource guide for python (#4518)

---
+++ packages/console/src/assets/docs/guides/api-python/index.ts
@@ -0,0 +1,9 @@
+import { type GuideMetadata } from '../types';
+
+const metadata: Readonly<GuideMetadata> = Object.freeze({
+  name: 'Python',
+  description: 'Integrate Logto into your Python web app, such as Django and Flask.',
+  target: 'API',
+});
+
+export default metadata;
