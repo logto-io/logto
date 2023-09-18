ae33bacc20f610cf1cc68d3e4421fcc237b447bf
Charles Zhao <charleszhao@silverhand.io>
2023-09-18 14:28:02 +0800

    fix(console): should show user initial letter avatar in roles

--- packages/schemas/src/types/role.ts
+++ packages/schemas/src/types/role.ts
@@ -2,7 +2,7 @@ import type { Application, Role, User } from '../db-entries/index.js';
 
 export type RoleResponse = Role & {
   usersCount: number;
-  featuredUsers: Array<Pick<User, 'avatar' | 'id' | 'name'>>;
+  featuredUsers: Array<Pick<User, 'avatar' | 'id' | 'name' | 'username' | 'primaryEmail'>>;
   applicationsCount: number;
   featuredApplications: Array<Pick<Application, 'id' | 'name'>>;
 };
