a9f06903738ec7955e11d1c26effe5482c40e02d
simeng-li <simeng@silverhand.io>
2023-09-21 14:08:40 +0800

    refactor(console): replace invoice url link with API call
    replace invoice url link with API call

--- packages/console/src/pages/TenantSettings/BillingHistory/index.tsx
+++ packages/console/src/pages/TenantSettings/BillingHistory/index.tsx
@@ -1,8 +1,9 @@
 import { withAppInsights } from '@logto/app-insights/react';
 import { conditional } from '@silverhand/essentials';
 import dayjs from 'dayjs';
-import { useContext, useMemo } from 'react';
+import { useCallback, useContext, useMemo } from 'react';
 
+import { useCloudApi } from '@/cloud/hooks/use-cloud-api';
 import EmptyDataPlaceholder from '@/components/EmptyDataPlaceholder';
 import ItemPreview from '@/components/ItemPreview';
 import PageMeta from '@/components/PageMeta';
@@ -16,6 +17,7 @@ import { formatPeriod } from '@/utils/subscription';
 import InvoiceStatusTag from './InvoiceStatusTag';
 
 function BillingHistory() {
+  const cloudApi = useCloudApi();
   const { currentTenantId } = useContext(TenantsContext);
   const { data: invoices, error } = useInvoices(currentTenantId);
   const isLoadingInvoices = !invoices && !error;
@@ -23,6 +25,20 @@ function BillingHistory() {
     // Don't show draft invoices
     () => invoices?.filter(({ status }) => status !== 'draft'),
     [invoices]
+  );
+
+  const openStripeHostedInvoicePage = useCallback(
+    async (invoiceId: string) => {
+      const { hostedInvoiceUrl } = await cloudApi.get(
+        '/api/tenants/:tenantId/invoices/:invoiceId/hosted-invoice-url',
+        {
+          params: { tenantId: currentTenantId, invoiceId },
+        }
+      );
+
+      window.open(hostedInvoiceUrl, '_blank');
+    },
+    [cloudApi, currentTenantId]
   );
 
   return (
@@ -35,13 +51,11 @@ function BillingHistory() {
           {
             title: <DynamicT forKey="subscription.billing_history.invoice_column" />,
             dataIndex: 'planName',
-            render: ({ planName, hostedInvoiceUrl, periodStart, periodEnd }) => {
+            render: ({ planName, periodStart, periodEnd }) => {
               return (
                 <ItemPreview
                   title={formatPeriod({ periodStart, periodEnd, displayYear: true })}
                   subtitle={conditional(planName && <PlanName name={planName} />)}
-                  to={conditional(hostedInvoiceUrl)}
-                  toTarget="_blank"
                 />
               );
             },
@@ -70,11 +84,8 @@ function BillingHistory() {
         ]}
         isLoading={isLoadingInvoices}
         placeholder={<EmptyDataPlaceholder />}
-        rowClickHandler={({ hostedInvoiceUrl }) => {
-          if (!hostedInvoiceUrl) {
-            return;
-          }
-          window.open(hostedInvoiceUrl, '_blank');
+        rowClickHandler={({ id }) => {
+          void openStripeHostedInvoicePage(id);
         }}
       />
     </div>
