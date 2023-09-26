ea86aff2b20e022f2c16d11c4aa165ca103a5a03
Xiao Yijun <xiaoyijun@silverhand.io>
2023-09-26 14:04:44 +0800

    chore: rename `ui` to `experience` in commit message scope enums

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', [...rules['type-enum'][2], 'api', 'release']],
-    'scope-enum': [2, 'always', ['connector', 'console', 'core', 'demo-app', 'test', 'phrases', 'schemas', 'shared', 'ui', 'deps','cli', 'toolkit', 'cloud', 'app-insights']],
+    'scope-enum': [2, 'always', ['connector', 'console', 'core', 'demo-app', 'test', 'phrases', 'schemas', 'shared', 'experience', 'deps','cli', 'toolkit', 'cloud', 'app-insights']],
     // Slightly increase the tolerance to allow the appending PR number
     ...(isCi && { 'header-max-length': [2, 'always', 110] }),
     'body-max-line-length': [2, 'always', 110],
