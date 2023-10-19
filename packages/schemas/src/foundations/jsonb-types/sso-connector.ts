294b0dd893e4a16c8ec97438ac4f3b2ecf87cb4f
simeng-li <simeng@silverhand.io>
2023-10-19 16:17:59 +0800

    fix(schemas): fix schema typo
    fix schema typo

--- packages/schemas/src/foundations/jsonb-types/sso-connector.ts
+++ packages/schemas/src/foundations/jsonb-types/sso-connector.ts
@@ -5,7 +5,7 @@ export const ssoDomainsGuard = z.array(z.string());
 export type SsoDomains = z.infer<typeof ssoDomainsGuard>;
 
 export const ssoBrandingGuard = z.object({
-  logto: z.string().optional(),
+  logo: z.string().optional(),
   darkLogo: z.string().optional(),
 });
 
