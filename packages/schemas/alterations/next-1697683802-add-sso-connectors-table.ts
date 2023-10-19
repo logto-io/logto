9f6c2f25215520e778f4313f4e9001551d0c1b37
simeng-li <simeng@silverhand.io>
2023-10-19 11:23:38 +0800

    fix(schemas): remove dummy comments
    remove dummy comments

--- packages/schemas/alterations/next-1697683802-add-sso-connectors-table.ts
+++ packages/schemas/alterations/next-1697683802-add-sso-connectors-table.ts
@@ -12,10 +12,6 @@ const getDatabaseName = async (pool: CommonQueryMethods) => {
   return currentDatabase.replaceAll('-', '_');
 };
 
-/**
- * Hight light fields
- */
-
 const alteration: AlterationScript = {
   up: async (pool) => {
     const database = await getDatabaseName(pool);
