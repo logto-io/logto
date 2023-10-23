f98b43c57fb99b8e084bf47bfce58c726260ac4d
Gao Sun <gao@silverhand.io>
2023-10-23 16:34:41 +0800

    refactor(console): remove role transfer component

--- packages/console/src/components/RoleEntitiesTransfer/utils.ts
+++
@@ -1,4 +0,0 @@
-import type { User, Application } from '@logto/schemas';
-
-export const isUser = (entity: User | Application): entity is User =>
-  'customData' in entity || 'identities' in entity;
