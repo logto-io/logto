d41b71a54a1353e85089c2160f87c8da6e3ecb3b
Hussain Shaikh <zakirroll@gmail.com>
2023-10-30 11:54:45 +0530

    feat(core): google cloud storage provider (#4660)

---
+++ packages/core/src/utils/storage/google-storage.ts
@@ -0,0 +1,33 @@
+import { Storage } from '@google-cloud/storage';
+
+import type { UploadFile } from './types.js';
+
+const defaultPublicDomain = 'storage.googleapis.com';
+
+export const buildGoogleStorage = (projectId: string, keyFilename: string, bucketName: string) => {
+  const storage = new Storage({ projectId, keyFilename });
+  const bucket = storage.bucket(bucketName);
+
+  const uploadFile: UploadFile = async (
+    data: Buffer,
+    objectKey: string,
+    { contentType, publicUrl } = {}
+  ) => {
+    const file = bucket.file(objectKey);
+
+    await file.save(data, {
+      contentType,
+      public: Boolean(publicUrl),
+    });
+
+    if (publicUrl) {
+      return { url: `${publicUrl}/${objectKey}` };
+    }
+
+    return {
+      url: `https://${defaultPublicDomain}/${bucketName}/${objectKey}`,
+    };
+  };
+
+  return { uploadFile };
+};
