b7fcaa9b8e97597cd57e5a065cbd535a1bfa27e3
simeng-li <simeng@silverhand.io>
2023-11-01 16:54:18 +0800

    fix(schemas): fix alterations
    fix alterations

--- packages/schemas/tables/user_sso_identities.sql
+++ packages/schemas/tables/user_sso_identities.sql
@@ -1,12 +1,18 @@
-/* init_order = 1 */
+/* init_order = 2 */
 
 create table user_sso_identities (
   tenant_id varchar(21) not null
     references tenants (id) on update cascade on delete cascade,
+  id varchar(21) not null,
   user_id varchar(12) not null references users (id) on update cascade on delete cascade,
   /** Unique provider identifier. Issuer of the OIDC connectors, entityId of the SAML providers */
   issuer varchar(256) not null,
+  /** Provider user identity id*/
   identity_id varchar(128) not null,
   detail jsonb /* @use JsonObject */ not null default '{}'::jsonb,
-  primary key (tenant_id, issuer, identity_id)
+  created_at timestamp not null default(now()),
+  primary key (id)
 );
+
+create index user_sso_identities__issuer__identity_id 
+  on user_sso_identities (tenant_id, issuer, identity_id);
