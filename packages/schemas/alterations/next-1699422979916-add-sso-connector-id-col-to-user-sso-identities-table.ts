fac21c56beee864d5e495411270553bc02f3350f
simeng-li <simeng@silverhand.io>
2023-11-08 14:38:28 +0800

    fix(schemas): fix alteration script
    fix alteration script

--- packages/schemas/alterations/next-1699422979916-add-sso-connector-id-col-to-user-sso-identities-table.ts
+++ packages/schemas/alterations/next-1699422979916-add-sso-connector-id-col-to-user-sso-identities-table.ts
@@ -5,12 +5,12 @@ import type { AlterationScript } from '../lib/types/alteration.js';
 const alteration: AlterationScript = {
   up: async (pool) => {
     await pool.query(sql`
-      alter table users_sso_identities add column sso_connector_id varchar(128) not null references sso_connectors (id) on update cascade on delete cascade;
+      alter table user_sso_identities add column sso_connector_id varchar(128) not null references sso_connectors (id) on update cascade on delete cascade;
     `);
   },
   down: async (pool) => {
     await pool.query(sql`
-      alter table users_sso_identities drop column sso_connector_id;
+      alter table user_sso_identities drop column sso_connector_id;
     `);
   },
 };
