9b9044482fb85747b0a45ba031cd3bd65070644c
simeng-li <simeng@silverhand.io>
2023-11-08 14:55:11 +0800

    fix(schemas): fix alteration script
    fix alteration script

--- packages/schemas/tables/user_sso_identities.sql
+++ packages/schemas/tables/user_sso_identities.sql
@@ -11,10 +11,9 @@ create table user_sso_identities (
   identity_id varchar(128) not null,
   detail jsonb /* @use JsonObject */ not null default '{}'::jsonb,
   created_at timestamp not null default(now()),
-  sso_connector_id 
+  sso_connector_id
     varchar(128) not null
     references sso_connectors (id) on update cascade on delete cascade,
-
   primary key (id),
   constraint user_sso_identities__issuer__identity_id
     unique (tenant_id, issuer, identity_id)
