1ed494a6478dd9ddb5e0fe51c211a64280676ef2
simeng-li <simeng@silverhand.io>
2023-11-08 14:03:18 +0800

    refactor(schemas): add new sso_connector_id row to the users_sso_identities table
    add new sso_connector_id row to the users_sso_identities table

---
+++ packages/schemas/alterations/next-1699422979916-add-sso-connector-id-col-to-users-sso-identities-table.ts
@@ -0,0 +1,18 @@
+import { sql } from 'slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      alter table users_sso_identities add column sso_connector_id varchar(128) not null references sso_connectors (id) on update cascade on delete cascade;
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      alter table users_sso_identities drop column sso_connector_id;
+    `);
+  },
+};
+
+export default alteration;
