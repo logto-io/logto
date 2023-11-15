bfea0b0fdd51cb7480647332a4178f44f00c7610
Gao Sun <gao@silverhand.io>
2023-11-15 17:20:31 +0800

    refactor(core): fix org apis (#4890)

--- packages/core/src/routes/organization/roles.ts
+++ packages/core/src/routes/organization/roles.ts
@@ -1,5 +1,13 @@
-import { OrganizationRoles } from '@logto/schemas';
+import {
+  type CreateOrganizationRole,
+  OrganizationRoles,
+  organizationRoleWithScopesGuard,
+} from '@logto/schemas';
+import { generateStandardId } from '@logto/shared';
+import { z } from 'zod';
 
+import koaGuard from '#src/middleware/koa-guard.js';
+import koaPagination from '#src/middleware/koa-pagination.js';
 import SchemaRouter from '#src/utils/SchemaRouter.js';
 
 import { type AuthedRouter, type RouterInitArgs } from '../types.js';
@@ -20,9 +28,63 @@ export default function organizationRoleRoutes<T extends AuthedRouter>(
   ]: RouterInitArgs<T>
 ) {
   const router = new SchemaRouter(OrganizationRoles, roles, {
+    disabled: { get: true, post: true },
     errorHandler,
     searchFields: ['name'],
   });
+
+  router.get(
+    '/',
+    koaPagination(),
+    koaGuard({
+      response: organizationRoleWithScopesGuard.array(),
+      status: [200],
+    }),
+    async (ctx, next) => {
+      const { limit, offset } = ctx.pagination;
+      const [count, entities] = await roles.findAll(limit, offset);
+
+      ctx.pagination.totalCount = count;
+      ctx.body = entities;
+      return next();
+    }
+  );
+
+  /** Allows to carry an initial set of scopes for creating a new organization role. */
+  type CreateOrganizationRolePayload = Omit<CreateOrganizationRole, 'id'> & {
+    organizationScopeIds: string[];
+  };
+
+  const createGuard: z.ZodType<CreateOrganizationRolePayload, z.ZodTypeDef, unknown> =
+    OrganizationRoles.createGuard
+      .omit({
+        id: true,
+      })
+      .extend({
+        organizationScopeIds: z.array(z.string()).default([]),
+      });
+
+  router.post(
+    '/',
+    koaGuard({
+      body: createGuard,
+      response: OrganizationRoles.guard,
+      status: [201, 422],
+    }),
+    async (ctx, next) => {
+      const { organizationScopeIds: scopeIds, ...data } = ctx.guard.body;
+      const role = await roles.insert({ id: generateStandardId(), ...data });
+
+      if (scopeIds.length > 0) {
+        await rolesScopes.insert(...scopeIds.map<[string, string]>((id) => [role.id, id]));
+      }
+
+      ctx.body = role;
+      ctx.status = 201;
+      return next();
+    }
+  );
+
   router.addRelationRoutes(rolesScopes, 'scopes');
 
   originalRouter.use(router.routes());
