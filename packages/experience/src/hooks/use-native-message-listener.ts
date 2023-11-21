0a4ceaaaf8fe094ed85674978de67a9d0e3cfec6
simeng-li <simeng@silverhand.io>
2023-11-21 14:48:17 +0800

    feat(experience): add native listener to SSO callback page
    add native listenter to SSO callback page

--- packages/experience/src/hooks/use-native-message-listener.ts
+++ packages/experience/src/hooks/use-native-message-listener.ts
@@ -4,12 +4,21 @@ import { isNativeWebview } from '@/utils/native-sdk';
 
 import useToast from './use-toast';
 
-const useNativeMessageListener = () => {
+/**
+ * UseNativeMessageListener
+ *
+ * Used to monitor native error message.
+ * If native error message is sent, it will be displayed as toast.
+ * Add a bypass parameter to bypass the native error message listener.
+ *
+ * @param {boolean} bypass (default: false)
+ */
+const useNativeMessageListener = (bypass = false) => {
   const { setToast } = useToast();
 
   // Monitor Native Error Message
   useEffect(() => {
-    if (!isNativeWebview()) {
+    if (!isNativeWebview() || bypass) {
       return;
     }
 
