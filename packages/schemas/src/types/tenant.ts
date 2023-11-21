651902c97dd7e413ef0f56ad32176611fc2fb80d
wangsijie <wangsijie@silverhand.io>
2023-11-21 11:35:07 +0800

    refactor(schemas): add js-doc to tenant tag enum (#4909)
    * refactor(schemas): add jsdoc to tenant tag enum
    
    * chore: code review fix
    
    Co-authored-by: Charles Zhao <charleszhao@silverhand.io>
    
    * chore: code review fix
    
    Co-authored-by: Charles Zhao <charleszhao@silverhand.io>
    
    ---------
    
    Co-authored-by: Charles Zhao <charleszhao@silverhand.io>

--- packages/schemas/src/types/tenant.ts
+++ packages/schemas/src/types/tenant.ts
@@ -1,5 +1,8 @@
 export enum TenantTag {
+  /* Development tenants are free to use but are not meant to be used as production environment */
   Development = 'development',
+  /* @deprecated */
   Staging = 'staging',
+  /* A production tenant must have an associated subscription plan, even if it's a free plan */
   Production = 'production',
 }
