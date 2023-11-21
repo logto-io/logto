fbf545ecd404b68dbfe08085eb645476083c48a9
simeng-li <simeng@silverhand.io>
2023-11-21 16:09:07 +0800

    feat(experience): add struct guard to session storage hook  (#4933)
    * feat(experience): add struct guard to session storage hook
    
    add struct guard to the session storage hook
    
    * test(experience): add unit test for the hook
    
    add unit test for the session storages hook

--- packages/experience/src/types/guard.ts
+++ packages/experience/src/types/guard.ts
@@ -1,4 +1,9 @@
-import { SignInIdentifier, MissingProfile, MfaFactor } from '@logto/schemas';
+import {
+  SignInIdentifier,
+  MissingProfile,
+  MfaFactor,
+  type SsoConnectorMetadata,
+} from '@logto/schemas';
 import * as s from 'superstruct';
 
 import { UserFlow } from '.';
@@ -106,3 +111,11 @@ export const webAuthnStateGuard = s.assign(
 );
 
 export type WebAuthnState = s.Infer<typeof webAuthnStateGuard>;
+
+/* Single Sign On */
+export const ssoConnectorMetadataGuard: s.Describe<SsoConnectorMetadata> = s.object({
+  id: s.string(),
+  logo: s.string(),
+  darkLogo: s.optional(s.string()),
+  connectorName: s.string(),
+});
