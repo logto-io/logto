0e334ecdb1aab53c5aa3b65a7d12b39f2186124c
Charles Zhao <charleszhao@silverhand.io>
2023-11-22 14:38:00 +0800

    fix(console): remove row hover effects from organization inline tables (#4941)

--- packages/console/src/pages/Organizations/TemplateTable/index.tsx
+++ packages/console/src/pages/Organizations/TemplateTable/index.tsx
@@ -69,6 +69,7 @@ function TemplateTable<
       {!noData && (
         <Table
           hasBorder
+          isRowHoverEffectDisabled
           isLoading={isLoading}
           rowGroups={[
             {
