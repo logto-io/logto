edc147edb28c19a0334d8ccf19eedc3585d55ac5
simeng-li <simeng@silverhand.io>
2023-11-23 18:44:58 +0800

    refactor(core): refactor SAML SSO idpConfig type
    refactor SAML SSO idepConfig type

--- packages/core/src/sso/SamlSsoConnector/index.test.ts
+++ packages/core/src/sso/SamlSsoConnector/index.test.ts
@@ -3,16 +3,13 @@ import { SsoProviderName } from '@logto/schemas';
 
 import { mockSsoConnector as _mockSsoConnector } from '#src/__mocks__/sso.js';
 
+import { type SamlConnectorConfig } from '../types/saml.js';
+
 import { samlSsoConnectorFactory } from './index.js';
 
 const mockSsoConnector = { ..._mockSsoConnector, providerName: SsoProviderName.SAML };
 
 describe('SamlSsoConnector', () => {
-  it('SamlSsoConnector should contains static properties', () => {
-    expect(samlSsoConnectorFactory.providerName).toEqual(SsoProviderName.SAML);
-    expect(samlSsoConnectorFactory.configGuard).toBeDefined();
-  });
-
   it('constructor should work properly', () => {
     // eslint-disable-next-line unicorn/consistent-function-scoping
     const createSamlSsoConnector = () =>
@@ -21,20 +18,43 @@ describe('SamlSsoConnector', () => {
     expect(createSamlSsoConnector).not.toThrow();
   });
 
-  it('constructor should throw error if config is invalid', () => {
+  it('should get SP config properly without proper IdP config', async () => {
     const temporaryMockSsoConnector = { ...mockSsoConnector, config: { metadata: 123 } };
-    const result = samlSsoConnectorFactory.configGuard.safeParse(temporaryMockSsoConnector.config);
-
-    if (result.success) {
-      throw new Error('Invalid config');
-    }
-
-    const createSamlSsoConnector = () => {
-      return new samlSsoConnectorFactory.constructor(temporaryMockSsoConnector, 'default_tenant');
-    };
-
-    expect(createSamlSsoConnector).toThrow(
-      new ConnectorError(ConnectorErrorCodes.InvalidConfig, result.error)
+    const connector = new samlSsoConnectorFactory.constructor(
+      temporaryMockSsoConnector,
+      'default_tenant'
     );
+
+    const { serviceProvider, identityProvider } = await connector.getConfig();
+    expect(serviceProvider.entityId).not.toBeUndefined();
+    expect(serviceProvider.assertionConsumerServiceUrl).not.toBeUndefined();
+    expect(identityProvider).toBeUndefined();
+  });
+
+  it('should throw error on calling getIdpMetadata, if the config is invalid', async () => {
+    const connector = new samlSsoConnectorFactory.constructor(mockSsoConnector, 'default_tenant');
+
+    await expect(async () => connector.getSamlIdpMetadata()).rejects.toThrow(
+      new ConnectorError(ConnectorErrorCodes.InvalidConfig, 'config not found')
+    );
+  });
+
+  it.each([
+    { metadata: 'metadata' },
+    { metadataUrl: 'https://example.com' },
+    {
+      entityId: '123',
+      signInEndpoint: 'https://example.com',
+      x509Certificate: 'mockCert',
+    },
+  ])('should parse config with %p successfully', (config: SamlConnectorConfig) => {
+    const temporaryMockSsoConnector = { ...mockSsoConnector, config };
+
+    const connector = new samlSsoConnectorFactory.constructor(
+      temporaryMockSsoConnector,
+      'default_tenant'
+    );
+
+    expect(connector.idpConfig).toEqual(config);
   });
 });
