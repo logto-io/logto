c47e0192ff304878ba58ba25669009906c27e053
simeng-li <simeng@silverhand.io>
2023-11-28 13:55:47 +0800

    feat(schemas): add unique constrain to the SSO connectorName field (#4974)
    add unique constrain to the SSO connectorName field

---
+++ packages/schemas/alterations/next-1701054133-add-unique-constraint-to-the-sso-connector-name.ts
@@ -0,0 +1,21 @@
+import { sql } from 'slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      alter table sso_connectors
+        add constraint sso_connectors__connector_name__unique 
+          unique (tenant_id, connector_name);
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      alter table sso_connectors
+        drop constraint sso_connectors__connector_name__unique;
+    `);
+  },
+};
+
+export default alteration;
