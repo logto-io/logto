28ae477088580b6c4402a3e78248f57c8d7d9a59
simeng-li <simeng@silverhand.io>
2023-11-29 16:08:02 +0800

    fix(console): fix lint error
    fix lint error

--- packages/console/src/assets/docs/single-sign-on/index.ts
+++ packages/console/src/assets/docs/single-sign-on/index.ts
@@ -2,7 +2,7 @@ import { SsoProviderName } from '@logto/schemas';
 import { type MDXProps } from 'mdx/types';
 import { lazy, type LazyExoticComponent, type FunctionComponent } from 'react';
 
-export type GuideComponentType = LazyExoticComponent<FunctionComponent<MDXProps>>;
+type GuideComponentType = LazyExoticComponent<FunctionComponent<MDXProps>>;
 
 const ssoConnectorGuides: Readonly<{ [key in SsoProviderName]?: GuideComponentType }> = {
   [SsoProviderName.SAML]: lazy(async () => import('./SAML/README.mdx')),
