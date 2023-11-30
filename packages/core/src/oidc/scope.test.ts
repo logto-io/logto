68a2bad47e8a370f7ee816f8b1e2718a7478d07e
Gao Sun <gao@silverhand.io>
2023-11-30 13:05:15 +0800

    chore: fix tests

--- packages/core/src/oidc/scope.test.ts
+++ packages/core/src/oidc/scope.test.ts
@@ -1,19 +1,19 @@
-import { getUserClaims } from './scope.js';
+import { getAcceptedUserClaims } from './scope.js';
 
 const use = {
   idToken: 'id_token',
   userinfo: 'userinfo',
-};
+} as const;
 
 describe('OIDC getUserClaims()', () => {
   it('should return proper ID Token claims', () => {
-    expect(getUserClaims(use.idToken, 'openid profile', {}, [])).toEqual([
+    expect(getAcceptedUserClaims(use.idToken, 'openid profile', {}, [])).toEqual([
       'name',
       'picture',
       'username',
     ]);
 
-    expect(getUserClaims(use.idToken, 'openid profile email phone', {}, [])).toEqual([
+    expect(getAcceptedUserClaims(use.idToken, 'openid profile email phone', {}, [])).toEqual([
       'name',
       'picture',
       'username',
@@ -23,36 +23,25 @@ describe('OIDC getUserClaims()', () => {
       'phone_number_verified',
     ]);
 
-    expect(getUserClaims(use.idToken, 'openid profile custom_data identities', {}, [])).toEqual([
-      'name',
-      'picture',
-      'username',
-    ]);
+    expect(
+      getAcceptedUserClaims(use.idToken, 'openid profile custom_data identities', {}, [])
+    ).toEqual(['name', 'picture', 'username']);
 
-    expect(getUserClaims(use.idToken, 'openid profile email', {}, ['email_verified'])).toEqual([
-      'name',
-      'picture',
-      'username',
-      'email',
-    ]);
+    expect(
+      getAcceptedUserClaims(use.idToken, 'openid profile email', {}, ['email_verified'])
+    ).toEqual(['name', 'picture', 'username', 'email']);
   });
 
   it('should return proper Userinfo claims', () => {
-    expect(getUserClaims(use.userinfo, 'openid profile custom_data identities', {}, [])).toEqual([
-      'name',
-      'picture',
-      'username',
-      'custom_data',
-      'identities',
-    ]);
+    expect(
+      getAcceptedUserClaims(use.userinfo, 'openid profile custom_data identities', {}, [])
+    ).toEqual(['name', 'picture', 'username', 'custom_data', 'identities']);
   });
 
   // Ignore `_claims` since [Claims Parameter](https://github.com/panva/node-oidc-provider/tree/main/docs#featuresclaimsparameter) is not enabled
   it('should ignore claims parameter', () => {
-    expect(getUserClaims(use.idToken, 'openid profile custom_data', { email: null }, [])).toEqual([
-      'name',
-      'picture',
-      'username',
-    ]);
+    expect(
+      getAcceptedUserClaims(use.idToken, 'openid profile custom_data', { email: null }, [])
+    ).toEqual(['name', 'picture', 'username']);
   });
 });
