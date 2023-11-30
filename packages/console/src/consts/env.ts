86f0a9ab61f1e5f413c0bc68dd9750adae7e8c80
simeng-li <simeng@silverhand.io>
2023-11-30 21:21:24 +0800

    fix(console): fix lint
    allow isDev tag always exist

--- packages/console/src/consts/env.ts
+++ packages/console/src/consts/env.ts
@@ -4,6 +4,6 @@ import { yes } from '@silverhand/essentials';
 export const isProduction = process.env.NODE_ENV === 'production';
 export const isCloud = yes(process.env.IS_CLOUD);
 export const adminEndpoint = process.env.ADMIN_ENDPOINT;
-// eslint-disable-next-line unicorn/prevent-abbreviations -- we love dev
+// eslint-disable-next-line unicorn/prevent-abbreviations, import/no-unused-modules -- we love dev
 export const isDevFeaturesEnabled =
   !isProduction || yes(process.env.DEV_FEATURES_ENABLED) || yes(process.env.INTEGRATION_TEST);
