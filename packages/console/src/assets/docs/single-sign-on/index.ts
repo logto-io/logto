3559c000a3b6257082e842908625ff569c76dd74
simeng-li <simeng@silverhand.io>
2023-12-04 18:54:02 +0800

    refactor(console): rename the sso guide
    rename the sso guide

--- packages/console/src/assets/docs/single-sign-on/index.ts
+++ packages/console/src/assets/docs/single-sign-on/index.ts
@@ -5,11 +5,11 @@ import { lazy, type LazyExoticComponent, type FunctionComponent } from 'react';
 type GuideComponentType = LazyExoticComponent<FunctionComponent<MDXProps>>;
 
 const ssoConnectorGuides: Readonly<{ [key in SsoProviderName]?: GuideComponentType }> = {
-  [SsoProviderName.SAML]: lazy(async () => import('./saml-provider/README.mdx')),
-  [SsoProviderName.OIDC]: lazy(async () => import('./oidc-provider/README.mdx')),
+  [SsoProviderName.SAML]: lazy(async () => import('./saml/README.mdx')),
+  [SsoProviderName.OIDC]: lazy(async () => import('./oidc/README.mdx')),
   [SsoProviderName.AZURE_AD]: lazy(async () => import('./azure-ad/README.mdx')),
   [SsoProviderName.GOOGLE_WORKSPACE]: lazy(async () => import('./google-workspace/README.mdx')),
-  [SsoProviderName.OKTA]: lazy(async () => import('./okta-provider/README.mdx')),
+  [SsoProviderName.OKTA]: lazy(async () => import('./okta/README.mdx')),
 };
 
 export default ssoConnectorGuides;
