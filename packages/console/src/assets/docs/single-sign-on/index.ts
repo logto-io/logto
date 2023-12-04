903b0eeaf8da00e8c8137ded08c618396f6d6d7c
simeng-li <simeng@silverhand.io>
2023-12-04 18:48:59 +0800

    refactor(console): rename the sso connector guide path
    rename the sso connector path

--- packages/console/src/assets/docs/single-sign-on/index.ts
+++ packages/console/src/assets/docs/single-sign-on/index.ts
@@ -5,11 +5,11 @@ import { lazy, type LazyExoticComponent, type FunctionComponent } from 'react';
 type GuideComponentType = LazyExoticComponent<FunctionComponent<MDXProps>>;
 
 const ssoConnectorGuides: Readonly<{ [key in SsoProviderName]?: GuideComponentType }> = {
-  [SsoProviderName.SAML]: lazy(async () => import('./SAML/README.mdx')),
-  [SsoProviderName.OIDC]: lazy(async () => import('./OIDC/README.mdx')),
-  [SsoProviderName.AZURE_AD]: lazy(async () => import('./AzureAD/README.mdx')),
-  [SsoProviderName.GOOGLE_WORKSPACE]: lazy(async () => import('./GoogleWorkspace/README.mdx')),
-  [SsoProviderName.OKTA]: lazy(async () => import('./OKTA/README.mdx')),
+  [SsoProviderName.SAML]: lazy(async () => import('./saml-provider/README.mdx')),
+  [SsoProviderName.OIDC]: lazy(async () => import('./oidc-provider/README.mdx')),
+  [SsoProviderName.AZURE_AD]: lazy(async () => import('./azure-ad/README.mdx')),
+  [SsoProviderName.GOOGLE_WORKSPACE]: lazy(async () => import('./google-workspace/README.mdx')),
+  [SsoProviderName.OKTA]: lazy(async () => import('./okta-provider/README.mdx')),
 };
 
 export default ssoConnectorGuides;
