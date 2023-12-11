13efa187e8846640ba084f225ce3ef94846fc3dd
Darcy Ye <darcyye@silverhand.io>
2023-12-11 12:49:19 +0800

    refactor(test): refactor code using cls and dcls methods if possible

--- packages/integration-tests/src/tests/console/sso-connectors/sso-connectors.test.ts
+++ packages/integration-tests/src/tests/console/sso-connectors/sso-connectors.test.ts
@@ -7,12 +7,20 @@ import {
   expectToSaveChanges,
   waitForToast,
 } from '#src/ui-helpers/index.js';
-import { expectNavigation, appendPathname } from '#src/utils.js';
+import { expectNavigation, appendPathname, dcls, cls } from '#src/utils.js';
 
 import { findModalFooterButton, fillSsoConnectorCreationModal } from './helpers.js';
 import { ssoConnectorTestCases } from './sso-connectors-test-cases.js';
 
 await page.setViewport({ width: 1920, height: 1080 });
+
+const emailDomainInputFieldSelector = [
+  'form',
+  `${dcls('input')}${cls('multiple')}[role=button]`,
+  'input',
+].join(' ');
+
+const emailDomainErrorMessageSelector = ['form', dcls('field'), dcls('errorMessage')].join(' ');
 
 describe('create SSO connectors', () => {
   const logtoConsoleUrl = new URL(logtoConsoleUrlString);
@@ -28,7 +36,7 @@ describe('create SSO connectors', () => {
     );
 
     await expect(page).toMatchElement(
-      'div[class$=main] div[class$=headline] div[class$=titleEllipsis]',
+      [dcls('main'), dcls('headline'), dcls('titleEllipsis')].join(' '),
       {
         text: 'Enterprise SSO',
       }
@@ -39,7 +47,7 @@ describe('create SSO connectors', () => {
 
   it('can open create SSO connector modal from table placeholder and create the first SSO connector', async () => {
     // When no SSO connector is created, use the create button in placeholder.
-    await expect(page).toClick('table div[class$=placeholder] button span', {
+    await expect(page).toClick(['table', dcls('placeholder'), 'button', 'span'].join(' '), {
       text: 'Add enterprise connector',
     });
 
@@ -59,9 +67,12 @@ describe('create SSO connectors', () => {
       await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
       // When there are existing SSO connector(s), use the create button in page header.
-      await expect(page).toClick('div[class$=main] div[class$=headline] button[type=button] span', {
-        text: 'Add enterprise connector',
-      });
+      await expect(page).toClick(
+        [dcls('main'), dcls('headline'), 'button[type=button]', 'span'].join(' '),
+        {
+          text: 'Add enterprise connector',
+        }
+      );
 
       await expectModalWithTitle(page, 'Add enterprise connector');
 
@@ -78,9 +89,12 @@ describe('create SSO connectors', () => {
     await page.waitForNavigation({ waitUntil: 'networkidle0' });
 
     // When there are existing SSO connector(s), use the create button in page header.
-    await expect(page).toClick('div[class$=main] div[class$=headline] button[type=button] span', {
-      text: 'Add enterprise connector',
-    });
+    await expect(page).toClick(
+      [dcls('main'), dcls('headline'), 'button[type=button]', 'span'].join(' '),
+      {
+        text: 'Add enterprise connector',
+      }
+    );
 
     await expectModalWithTitle(page, 'Add enterprise connector');
 
@@ -97,7 +111,7 @@ describe('create SSO connectors', () => {
 
     // Error message should be shown.
     await expect(page).toMatchElement(
-      '.ReactModalPortal div[class$=field] div[class$=errorMessage]',
+      ['.ReactModalPortal', dcls('field'), dcls('errorMessage')].join(' '),
       {
         text: 'Connector name already exists. Please choose a different name.',
       }
@@ -106,7 +120,7 @@ describe('create SSO connectors', () => {
     await expect(findModalFooterButton(true)).resolves.toBeTruthy();
 
     await expect(page).toFill(
-      '.ReactModalPortal input[type=text][name=connectorName]',
+      ['.ReactModalPortal', 'input[type=text][name=connectorName]'].join(' '),
       `${connectorName} (1)`
     );
 
@@ -129,14 +143,14 @@ describe('create SSO connectors', () => {
 
   it("can go to SSO connector's 'SSO Experience' tab", async () => {
     // Navigate to "SSO Experience" tab
-    await expect(page).toClick('nav div[class$=item] div[class$=link] a', {
+    await expect(page).toClick(['nav', dcls('item'), dcls('link'), 'a'].join(' '), {
       text: 'SSO Experience',
     });
 
     // Confirm the current path is for "SSO Experience".
     expect(page.url().endsWith('/experience')).toBeTruthy();
     // Confirm the current tab is "SSO Experience".
-    await expect(page).toMatchElement('nav div[class$=item] div[class$=selected] a', {
+    await expect(page).toMatchElement(['nav', dcls('item'), dcls('selected'), 'a'].join(' '), {
       text: 'SSO Experience',
     });
   });
@@ -144,55 +158,55 @@ describe('create SSO connectors', () => {
   it("can configure SSO connectors's 'SSO Experience' GENERAL setup", async () => {
     // Expect to see inline notification alert to configure email domain.
     await expect(page).toMatchElement(
-      'form div[class*=alert][class$=inlineNotification] div[class$=content]',
+      ['form', `${dcls('alert')}${cls('inlineNotification')}`, dcls('content')].join(' '),
       {
         text: 'Add email domain to guide enterprise users to their identity provider for Single Sign-on.',
       }
     );
 
     // Configure email domain
-    await expect(page).toFill(
-      'form div[class*=input][class*=multiple][role=button] input',
-      'svhd.io'
-    );
+    await expect(page).toFill(emailDomainInputFieldSelector, 'svhd.io');
 
     // Press enter to add email domain
     await page.keyboard.press('Enter');
 
     // Input email domain with invalid format
-    await expect(page).toFill('form div[class*=input][class*=multiple][role=button] input', 'abc');
+    await expect(page).toFill(emailDomainInputFieldSelector, 'abc');
 
     // Press space to add email domain
     await page.keyboard.press('Space');
-    await expect(page).toMatchElement('form div[class$=field] div[class$=errorMessage]', {
+    await expect(page).toMatchElement(emailDomainErrorMessageSelector, {
       text: 'Invalid domain format.',
     });
 
     // Input public email domain (e.g., 'gmail.com', 'yahoo.com' etc)
-    await expect(page).toFill(
-      'form div[class*=input][class*=multiple][role=button] input',
-      'gmail.com'
-    );
+    await expect(page).toFill(emailDomainInputFieldSelector, 'gmail.com');
 
     // Press tab to add email domain
     await page.keyboard.press('Tab');
-    await expect(page).toMatchElement('form div[class$=field] div[class$=errorMessage]', {
+    await expect(page).toMatchElement(emailDomainErrorMessageSelector, {
       text: 'Public email domains are not allowed. Invalid domain format.',
     });
 
     // Remove last added email domain (which is `gmail.com` in this case). CSS selector can not specify the text content of the component.
     await expect(page).toClick(
-      'form div[class*=input][class*=multiple][role=button] div[class*=info][class*=tag]:last-of-type button'
+      [
+        'form',
+        `${dcls('input')}${cls('multiple')}[role=button]`,
+        `${dcls('info')}${cls('tag')}:last-of-type`,
+        'button',
+      ].join(' ')
     );
 
     // Error message got updated, since forbidden email domain is removed.
-    await expect(page).toMatchElement('form div[class$=field] div[class$=errorMessage]', {
+    await expect(page).toMatchElement(emailDomainErrorMessageSelector, {
       text: 'Invalid domain format.',
     });
-    await expect(page).toFill('form div[class*=input][class*=multiple][role=button] input', 'abc');
+    await expect(page).toFill(emailDomainInputFieldSelector, 'abc');
 
     // Input field blurred to input email domain.
     await page.$eval(
+      // Can not use `emailDomainInputFieldSelector` here since it could break the type inference.
       'form div[class*=input][class*=multiple][role=button] input',
       (element: HTMLInputElement) => {
         element.blur();
@@ -200,24 +214,29 @@ describe('create SSO connectors', () => {
     );
 
     // Does not allow duplicate email domain.
-    await expect(page).toMatchElement('form div[class$=field] div[class$=errorMessage]', {
+    await expect(page).toMatchElement(emailDomainErrorMessageSelector, {
       text: 'There are duplicate domains. Invalid domain format.',
     });
 
     // Remove last two added email domains (which are two `abc` in this case).
     await expect(page).toClick(
-      'form div[class*=input][class*=multiple][role=button] div[class*=info][class*=tag]:last-of-type button'
+      [
+        'form',
+        `${dcls('input')}${cls('multiple')}[role=button]`,
+        `${dcls('info')}${cls('tag')}:last-of-type`,
+        'button',
+      ].join(' ')
     );
 
     // Focus on email domain input field component (at this time, the input field is empty).
-    const inputField = await page.$('form div[class*=input][class*=multiple][role=button] input');
+    const inputField = await page.$(emailDomainInputFieldSelector);
     await inputField?.focus();
     // Should remove the last input email domain with double backspace when the input box is empty.
     await inputField?.press('Backspace');
     await inputField?.press('Backspace');
 
     // Since incorrect email domains are removed, error message no longer exists.
-    const errorMessage = await page.$('form div[class$=field] div[class$=errorMessage]');
+    const errorMessage = await page.$(emailDomainErrorMessageSelector);
     expect(errorMessage).toBeNull();
 
     await expectToSaveChanges(page);
