dcde26af70ebcc754c472f7adf42ae8e97e08785
simeng-li <simeng@silverhand.io>
2023-12-13 15:16:48 +0800

    fix(schemas): fix dirty char
    fix dirty char

--- packages/schemas/tables/application_user_consent_resource_scopes.sql
+++ packages/schemas/tables/application_user_consent_resource_scopes.sql
@@ -1,6 +1,6 @@
 /* init_order = 3 */
 
-/** The resource scopes (permissions) assigned to an application's consent request. */å
+/** The resource scopes (permissions) assigned to an application's consent request. */
 create table application_user_consent_resource_scopes (
   tenant_id varchar(21) not null
     references tenants (id) on update cascade on delete cascade,
