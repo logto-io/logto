2b273b4a6f115bf3fe5b86167644373e1f58102b
Xiao Yijun <xiaoyijun@silverhand.io>
2024-01-03 11:20:08 +0800

    fix(experience): update text content in the preview on custom phrases updated (#5192)

--- packages/experience/src/i18n/init.ts
+++ packages/experience/src/i18n/init.ts
@@ -4,8 +4,11 @@ import { initReactI18next } from 'react-i18next';
 
 import { getI18nResource } from '@/i18n/utils';
 
-const initI18n = async () => {
-  const { resources, lng } = await getI18nResource();
+// Call once globally
+i18next.use(initReactI18next);
+
+const initI18n = async (initialLanguage?: string) => {
+  const { resources, lng } = await getI18nResource(initialLanguage);
 
   const options: InitOptions = {
     resources,
@@ -15,12 +18,14 @@ const initI18n = async () => {
     },
   };
 
-  i18next.use(initReactI18next);
-
   const i18n = i18next.init(options);
 
-  // @ts-expect-error - i18next doesn't have a type definition for this. Must called after i18next is initialized
-  i18next.services.formatter.add('zhOrSpaces', (value: string, lng) => {
+  /**
+   * Note
+   * - Must call after i18next is initialized
+   * - Don't worry to call this multiple times, i18next will replace the formatter if it's already added.
+   */
+  i18next.services.formatter?.add('zhOrSpaces', (value: string, lng) => {
     if (lng !== 'zh-CN') {
       return value;
     }
