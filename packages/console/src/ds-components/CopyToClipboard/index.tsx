9901f58df349709a3ccd08635ffd707c30f3912c
Xiao Yijun <xiaoyijun@silverhand.io>
2024-01-08 11:30:39 +0800

    fix(console): remove hover state from tip button after tip is closed (#5197)

--- packages/console/src/ds-components/CopyToClipboard/index.tsx
+++ packages/console/src/ds-components/CopyToClipboard/index.tsx
@@ -70,6 +70,10 @@ function CopyToClipboard(
   }, []);
 
   const copy: MouseEventHandler<HTMLButtonElement> = async () => {
+    /**
+     * Note: should blur the copy icon button before the tooltip is shown, or it will remain focused after the tooltip is closed.
+     */
+    copyIconReference.current?.blur();
     setCopyState('copying');
     await navigator.clipboard.writeText(value);
     setCopyState('copied');
