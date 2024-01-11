67be81e6dfa404d20e0e4c9c7ab0a9568e8c83cd
wangsijie <wangsijie@silverhand.io>
2024-01-11 15:30:42 +0800

    feat(core): delete protected app (#5191)

--- packages/core/src/libraries/protected-app.ts
+++ packages/core/src/libraries/protected-app.ts
@@ -2,9 +2,29 @@ import { EnvSet } from '#src/env-set/index.js';
 import type Queries from '#src/tenants/Queries.js';
 import SystemContext from '#src/tenants/SystemContext.js';
 import assertThat from '#src/utils/assert-that.js';
-import { updateProtectedAppSiteConfigs } from '#src/utils/cloudflare/index.js';
+import {
+  deleteProtectedAppSiteConfigs,
+  updateProtectedAppSiteConfigs,
+} from '#src/utils/cloudflare/index.js';
 
 export type ProtectedAppLibrary = ReturnType<typeof createProtectedAppLibrary>;
+
+const getProviderConfig = async () => {
+  const { protectedAppConfigProviderConfig } = SystemContext.shared;
+  assertThat(protectedAppConfigProviderConfig, 'application.protected_app_not_configured');
+
+  return protectedAppConfigProviderConfig;
+};
+
+const deleteRemoteAppConfigs = async (host: string): Promise<void> => {
+  if (EnvSet.values.isIntegrationTest) {
+    return;
+  }
+
+  const protectedAppConfigProviderConfig = await getProviderConfig();
+
+  await deleteProtectedAppSiteConfigs(protectedAppConfigProviderConfig, host);
+};
 
 export const createProtectedAppLibrary = (queries: Queries) => {
   const {
@@ -17,8 +37,7 @@ export const createProtectedAppLibrary = (queries: Queries) => {
       return;
     }
 
-    const { protectedAppConfigProviderConfig } = SystemContext.shared;
-    assertThat(protectedAppConfigProviderConfig, 'application.protected_app_not_configured');
+    const protectedAppConfigProviderConfig = await getProviderConfig();
 
     const { protectedAppMetadata, id, secret } = await findApplicationById(applicationId);
     if (!protectedAppMetadata) {
@@ -41,5 +60,6 @@ export const createProtectedAppLibrary = (queries: Queries) => {
 
   return {
     syncAppConfigsToRemote,
+    deleteRemoteAppConfigs,
   };
 };
