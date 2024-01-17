b5724675c27c0cfeadc78fb7f29c41ce6b334d4b
wangsijie <wangsijie@silverhand.io>
2024-01-17 14:40:27 +0800

    feat(core,schemas): prepare schemas for protected app custom domain

--- packages/schemas/src/foundations/jsonb-types/applications.ts
+++ packages/schemas/src/foundations/jsonb-types/applications.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
+import { cloudflareDataGuard, domainDnsRecordsGuard, domainStatusGuard } from './custom-domain.js';
+
 export const protectedAppMetadataGuard = z.object({
   /* The host of the site */
   host: z.string(),
@@ -13,6 +15,22 @@ export const protectedAppMetadataGuard = z.object({
       path: z.string(),
     })
   ),
+  /* Custom domain */
+  customDomains: z
+    .object({
+      /* The domain name, e.g app.example.com */
+      domain: z.string(),
+      /* The status of the domain in Cloudflare */
+      status: domainStatusGuard,
+      /* The error message if any */
+      error: z.string().nullable(),
+      /* The DNS records of the domain */
+      dnsRecords: domainDnsRecordsGuard,
+      /* The remote Cloudflare data */
+      cloudflareData: cloudflareDataGuard.nullable(),
+    })
+    .array()
+    .optional(),
 });
 
 export type ProtectedAppMetadata = z.infer<typeof protectedAppMetadataGuard>;
