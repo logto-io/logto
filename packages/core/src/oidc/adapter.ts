f1d07a3db06d74f2889728031a3f41d4b96454a7
simeng-li <simeng@silverhand.io>
2024-01-18 11:38:30 +0800

    fix(core): remove the resource scopes from client metadata
    remove the resource scopes from client metadata

--- packages/core/src/oidc/adapter.ts
+++ packages/core/src/oidc/adapter.ts
@@ -1,12 +1,6 @@
 import type { CreateApplication } from '@logto/schemas';
-import {
-  ApplicationType,
-  OrganizationScopes,
-  Scopes,
-  adminConsoleApplicationId,
-  demoAppApplicationId,
-} from '@logto/schemas';
-import { deduplicate, appendPath, tryThat, conditional } from '@silverhand/essentials';
+import { ApplicationType, adminConsoleApplicationId, demoAppApplicationId } from '@logto/schemas';
+import { appendPath, tryThat, conditional } from '@silverhand/essentials';
 import { addSeconds } from 'date-fns';
 import type { AdapterFactory, AllClientMetadata } from 'oidc-provider';
 import { errors } from 'oidc-provider';
@@ -59,46 +53,34 @@ const buildDemoAppClientMetadata = (envSet: EnvSet): AllClientMetadata => {
 };
 
 /**
- * Restrict third-party client scopes to the app-level enabled user consent scopes
+ * Restrict third-party client OP scopes to the app-level enabled user claims scopes
  *
- * - userScopes: app-level enabled user claim scopes
- * - resourceScopes: app-level enabled resource scopes
- * - organizationScopes app-level enabled organization scopes
+ * client OP scopes include:
+ * - OIDC scopes: openid, offline_access
+ * - custom scopes: @see {@link https://github.com/panva/node-oidc-provider/blob/main/docs/README.md#scopes}
+ * - scopes defined in user claims: @see {@link https://github.com/panva/node-oidc-provider/blob/main/docs/README.md#claims}
  *
  * @remark
- * We use the client scope metadata to restrict the third-party client scopes, @see {@link https://github.com/panva/node-oidc-provider/blob/main/docs/README.md#clients}
+ * We use the client metadata scope metadata to restrict the third-party client scopes,
+ *
+ * - @see {@link https://github.com/panva/node-oidc-provider/blob/main/docs/README.md#clients}
+ * - client metadata scope must be a valid OP scope, otherwise a invalid metadata error will be thrown. @see{@link https://github.com/panva/node-oidc-provider/blob/main/lib/helpers/client_schema.js#L626}
+ * - resource scopes (including Logto organization scopes) are not include in the OP scope, it won't be validate by the client metadata scope as well. @see {@link https://github.com/panva/node-oidc-provider/blob/main/lib/actions/authorization/check_scope.js#L47}
+ * - resource scopes (including Logto organization scopes) will be filtered in the resource server's scopes fetching method. @see {@link https://github.com/panva/node-oidc-provider/blob/main/docs/README.md#getresourceserverinfo}
+ *
  * Auth request will be rejected if the requested scopes are not included in the client scope metadata.
  */
 const getThirdPartyClientScopes = async (
-  {
-    userConsentUserScopes,
-    userConsentResourceScopes,
-    userConsentOrganizationScopes,
-  }: Queries['applications'],
+  { userConsentUserScopes }: Queries['applications'],
   applicationId: string
 ) => {
-  const [availableUserScopes, [, resourceScopes], [, organizationScopes]] = await Promise.all([
-    userConsentUserScopes.findAllByApplicationId(applicationId),
-    userConsentResourceScopes.getEntities(Scopes, {
-      applicationId,
-    }),
-    userConsentOrganizationScopes.getEntities(OrganizationScopes, {
-      applicationId,
-    }),
-  ]);
-
-  const clientScopes = [
-    'openid',
-    'offline_access',
-    ...availableUserScopes,
-    ...resourceScopes.map(({ name }) => name),
-    ...organizationScopes.map(({ name }) => name),
-  ];
+  const availableUserScopes = await userConsentUserScopes.findAllByApplicationId(applicationId);
+  const clientScopes = ['openid', 'offline_access', ...availableUserScopes];
 
   // ClientScopes does not support prefix matching, so we need to include all the scopes.
   // Resource scopes name are not unique, we need to deduplicate them.
   // Requested resource scopes and organization scopes will be validated in resource server fetching method exclusively.
-  return deduplicate(clientScopes);
+  return clientScopes;
 };
 
 export default function postgresAdapter(
@@ -141,7 +123,7 @@ export default function postgresAdapter(
       ...transpileMetadata(client_id, snakecaseKeys(oidcClientMetadata)),
       // `node-oidc-provider` won't camelCase custom parameter keys, so we need to keep the keys camelCased
       ...customClientMetadata,
-      /* Third-party client scopes are restricted to the app-level enabled user consent scopes. @see {@link https://github.com/panva/node-oidc-provider/blob/main/docs/README.md#clients} */
+      /* Third-party client scopes are restricted to the app-level enabled user scopes. */
       ...conditional(clientScopes && { scope: clientScopes.join(' ') }),
     });
 
@@ -157,7 +139,7 @@ export default function postgresAdapter(
           new errors.InvalidClient(`invalid client ${id}`)
         );
 
-        // FIXME: @simeng-li Remove this check after the third-party client scope feature is released
+        // FIXME: @simeng-li Remove this check after the third-party feature is released
         if (EnvSet.values.isDevFeaturesEnabled && application.isThirdParty) {
           const clientScopes = await getThirdPartyClientScopes(applications, id);
           return transpileClient(application, clientScopes);
