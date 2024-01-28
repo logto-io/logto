81152100ab43fb50438eeb2db74625008a8b0be5
Gao Sun <gao@silverhand.io>
2024-01-28 22:20:51 +0800

    refactor: update alteration

--- packages/schemas/alterations/next-1706449174-update-organization-invitation-column.ts
+++ packages/schemas/alterations/next-1706449174-update-organization-invitation-column.ts
@@ -7,12 +7,16 @@ const alteration: AlterationScript = {
     await pool.query(sql`
       alter table organization_invitation_role_relations
         rename column invitation_id to organization_invitation_id;
+      alter table organization_invitation_role_relations
+        rename constraint organization_invitation_role_relations_invitation_id_fkey to organization_invitation_role_re_organization_invitation_id_fkey;
     `);
   },
   down: async (pool) => {
     await pool.query(sql`
       alter table organization_invitation_role_relations
         rename column organization_invitation_id to invitation_id;
+        alter table organization_invitation_role_relations
+        rename constraint organization_invitation_role_re_organization_invitation_id_fkey to organization_invitation_role_relations_invitation_id_fkey;
     `);
   },
 };
