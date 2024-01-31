0875fc0b19834886c0e914ea4d1018f8f6aeb1ae
Xiao Yijun <xiaoyijun@silverhand.io>
2024-01-31 14:50:43 +0800

    refactor: remove pro tag for m2m app in dev tenant

--- packages/console/src/pages/Applications/components/TypeDescription/index.tsx
+++ packages/console/src/pages/Applications/components/TypeDescription/index.tsx
@@ -1,11 +1,7 @@
-import { ApplicationType, ReservedPlanId } from '@logto/schemas';
+import { type ApplicationType } from '@logto/schemas';
 import classNames from 'classnames';
-import { useContext } from 'react';
 
 import ApplicationIcon from '@/components/ApplicationIcon';
-import FeatureTag from '@/components/FeatureTag';
-import { isCloud } from '@/consts/env';
-import { SubscriptionDataContext } from '@/contexts/SubscriptionDataProvider';
 
 import * as styles from './index.module.scss';
 
@@ -18,23 +14,12 @@ type Props = {
 };
 
 function TypeDescription({ title, subtitle, description, type, size = 'large' }: Props) {
-  const { currentPlan } = useContext(SubscriptionDataContext);
-  const hasPaywall = isCloud && type === ApplicationType.MachineToMachine;
-
   return (
     <div className={classNames(styles.container, styles[size])}>
       <ApplicationIcon type={type} />
       <div className={styles.title}>{title}</div>
       <div className={styles.subtitle}>{subtitle}</div>
       <div className={styles.description}>{description}</div>
-      {hasPaywall && (
-        <div className={styles.proTag}>
-          <FeatureTag
-            isVisible={!currentPlan.quota.machineToMachineLimit}
-            plan={ReservedPlanId.Pro}
-          />
-        </div>
-      )}
     </div>
   );
 }
