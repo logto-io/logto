5307a28c38e7bd51a45f4c0c70377876efa93c30
simeng-li <simeng@silverhand.io>
2024-02-20 10:32:39 +0800

    fix(demo-app): fix i18next type declaration
    fix i18next type declaration

--- packages/demo-app/src/include.d/react-i18next.d.ts
+++ packages/demo-app/src/include.d/react-i18next.d.ts
@@ -1,10 +1,8 @@
 // https://react.i18next.com/latest/typescript#create-a-declaration-file
 
 import type { LocalePhrase } from '@logto/phrases';
-// eslint-disable-next-line unused-imports/no-unused-imports
-import { CustomTypeOptions } from 'react-i18next';
 
-declare module 'react-i18next' {
+declare module 'i18next' {
   interface CustomTypeOptions {
     resources: LocalePhrase;
   }
