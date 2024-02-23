ee036bd77dce7429905764bccc610b85bf0c17cb
Darcy Ye <darcyye@silverhand.io>
2024-02-23 10:57:38 +0800

    chore(console): align PHP/Python guide structure with docs (#5420)

--- packages/console/src/assets/docs/guides/web-php/index.ts
+++ packages/console/src/assets/docs/guides/web-php/index.ts
@@ -6,6 +6,10 @@ const metadata: Readonly<GuideMetadata> = Object.freeze({
   name: 'PHP',
   description: 'Integrate Logto into your PHP web app, such as Lavarel.',
   target: ApplicationType.Traditional,
+  sample: {
+    repo: 'php',
+    path: 'samples',
+  },
   fullTutorial: {
     title: 'Full PHP SDK tutorial',
     url: 'https://docs.logto.io/sdk/php',
