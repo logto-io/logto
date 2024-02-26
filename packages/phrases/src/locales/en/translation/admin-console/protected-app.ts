2f782a3ab5969c7d5b8e29468cd9e5f5c4cdbf7e
Gao Sun <gao@silverhand.io>
2024-02-26 16:25:31 +0800

    refactor(console): update content

--- packages/phrases/src/locales/en/translation/admin-console/protected-app.ts
+++ packages/phrases/src/locales/en/translation/admin-console/protected-app.ts
@@ -1,21 +1,20 @@
 const protected_app = {
-  name: 'Protected app',
-  title: 'Create a Protected App: Epic speed, simplicity, non-SDK integration',
+  name: 'Protected App',
+  title: 'Create a Protected App: Add authentication with simplicity and epic speed',
   description:
-    'Serverless deployment with Logto Workers, powered by Cloudflare for top-tier performance and 0ms cold starts worldwide. <a>Learn more</a>',
+    'Protected App securely maintains user sessions and proxies your app requests. Powered by Cloudflare Workers, enjoy the top-tier performance and 0ms cold start worldwide. <a>Learn more</a>',
   fast_create: 'Fast create',
-  modal_title: 'Create protected app',
+  modal_title: 'Create Protected App',
   modal_subtitle:
-    'No more integrating Logto SDK. Add authentication to your existing web app with ease.',
+    'Enable secure and fast protection with clicks. Add authentication to your existing web app with ease.',
   form: {
     url_field_label: 'Your origin URL',
-    url_field_placeholder: 'https://',
-    url_field_description:
-      'Provide the address of your application requiring authentication protection.',
+    url_field_placeholder: 'https://domain.com/',
+    url_field_description: 'Provide the address of your app requiring authentication protection.',
     url_field_modification_notice:
-      'Modifications to the Origin URL may take up to 1-2 minutes to become effective across global network locations.',
+      'Modifications to the origin URL may take up to 1-2 minutes to become effective across global network locations.',
     url_field_tooltip:
-      "Provide the address of your application, excluding any '/pathname'. After creation, you can customize route authentication rules.\n\nNote: The Origin URL itself doesn't necessitate authentication; protection is applied exclusively to accesses via the designated app domain.",
+      "Provide the address of your application, excluding any '/pathname'. After creation, you can customize route authentication rules.\n\nNote: The origin URL itself doesn't necessitate authentication; protection is applied exclusively to accesses via the designated app domain.",
     domain_field_label: 'App domain',
     domain_field_placeholder: 'your-domain',
     domain_field_description:
@@ -33,13 +32,13 @@ const protected_app = {
         "Invalid subdomain format: use only lowercase letters, numbers, and hyphens '-'.",
       url_required: 'Origin URL is required.',
       invalid_url:
-        "Invalid Origin URL format: Use http:// or https://. Note: '/pathname' is not currently supported.",
+        "Invalid origin URL format: Use http:// or https://. Note: '/pathname' is not currently supported.",
       localhost:
         'Please expose your local server to the internet first. Learn more about <a>local development</a>.',
     },
   },
   success_message:
-    '🎉  App authentication successfully enabled! Explore the new protection of your website.',
+    '🎉 App authentication successfully enabled! Explore the new experience of your website.',
 };
 
 export default Object.freeze(protected_app);
--- packages/phrases/src/locales/pt-br/translation/admin-console/protected-app.ts
+++ packages/phrases/src/locales/pt-br/translation/admin-console/protected-app.ts
@@ -1,32 +1,31 @@
 const protected_app = {
   /** UNTRANSLATED */
-  name: 'Protected app',
+  name: 'Protected App',
   /** UNTRANSLATED */
-  title: 'Create a Protected App: Epic speed, simplicity, non-SDK integration',
+  title: 'Create a Protected App: Add authentication with simplicity and epic speed',
   /** UNTRANSLATED */
   description:
-    'Serverless deployment with Logto Workers, powered by Cloudflare for top-tier performance and 0ms cold starts worldwide. <a>Learn more</a>',
+    'Protected App securely maintains user sessions and proxies your app requests. Powered by Cloudflare Workers, enjoy the top-tier performance and 0ms cold start worldwide. <a>Learn more</a>',
   /** UNTRANSLATED */
   fast_create: 'Fast create',
   /** UNTRANSLATED */
-  modal_title: 'Create protected app',
+  modal_title: 'Create Protected App',
   /** UNTRANSLATED */
   modal_subtitle:
-    'No more integrating Logto SDK. Add authentication to your existing web app with ease.',
+    'Enable secure and fast protection with clicks. Add authentication to your existing web app with ease.',
   form: {
     /** UNTRANSLATED */
     url_field_label: 'Your origin URL',
     /** UNTRANSLATED */
-    url_field_placeholder: 'https://',
+    url_field_placeholder: 'https://domain.com/',
     /** UNTRANSLATED */
-    url_field_description:
-      'Provide the address of your application requiring authentication protection.',
+    url_field_description: 'Provide the address of your app requiring authentication protection.',
     /** UNTRANSLATED */
     url_field_modification_notice:
-      'Modifications to the Origin URL may take up to 1-2 minutes to become effective across global network locations.',
+      'Modifications to the origin URL may take up to 1-2 minutes to become effective across global network locations.',
     /** UNTRANSLATED */
     url_field_tooltip:
-      "Provide the address of your application, excluding any '/pathname'. After creation, you can customize route authentication rules.\n\nNote: The Origin URL itself doesn't necessitate authentication; protection is applied exclusively to accesses via the designated app domain.",
+      "Provide the address of your application, excluding any '/pathname'. After creation, you can customize route authentication rules.\n\nNote: The origin URL itself doesn't necessitate authentication; protection is applied exclusively to accesses via the designated app domain.",
     /** UNTRANSLATED */
     domain_field_label: 'App domain',
     /** UNTRANSLATED */
@@ -56,7 +55,7 @@ const protected_app = {
       url_required: 'Origin URL is required.',
       /** UNTRANSLATED */
       invalid_url:
-        "Invalid Origin URL format: Use http:// or https://. Note: '/pathname' is not currently supported.",
+        "Invalid origin URL format: Use http:// or https://. Note: '/pathname' is not currently supported.",
       /** UNTRANSLATED */
       localhost:
         'Please expose your local server to the internet first. Learn more about <a>local development</a>.',
@@ -64,7 +63,7 @@ const protected_app = {
   },
   /** UNTRANSLATED */
   success_message:
-    '🎉  App authentication successfully enabled! Explore the new protection of your website.',
+    '🎉 App authentication successfully enabled! Explore the new experience of your website.',
 };
 
 export default Object.freeze(protected_app);
