998cf2b8398d62629447316e548d709e96316573
simeng-li <simeng@silverhand.io>
2024-02-26 17:59:49 +0800

    fix(test): remove api timeout
    remove api timeout

--- packages/integration-tests/src/api/api.ts
+++ packages/integration-tests/src/api/api.ts
@@ -4,7 +4,6 @@ import { logtoConsoleUrl, logtoUrl, logtoCloudUrl } from '#src/constants.js';
 
 const api = got.extend({
   prefixUrl: new URL('/api', logtoUrl),
-  timeout: { response: 5000 }, // The default is 60s which is way too long for tests.
 });
 
 export default api;
