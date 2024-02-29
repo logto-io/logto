9f240234b18389721f604d57d0344d0fed5adbab
Gao Sun <gao@silverhand.io>
2024-02-29 13:07:01 +0800

    refactor(console): update loading component

--- packages/console/src/components/AppLoading/index.tsx
+++ packages/console/src/components/AppLoading/index.tsx
@@ -1,18 +1,12 @@
-import { Theme } from '@logto/schemas';
-
-import IllustrationDark from '@/assets/images/loading-illustration-dark.svg';
-import Illustration from '@/assets/images/loading-illustration.svg';
+import Logo from '@/assets/images/logo.svg';
 import { Daisy as Spinner } from '@/ds-components/Spinner';
-import useTheme from '@/hooks/use-theme';
 
 import * as styles from './index.module.scss';
 
 function AppLoading() {
-  const theme = useTheme();
-
   return (
     <div className={styles.container}>
-      {theme === Theme.Light ? <Illustration /> : <IllustrationDark />}
+      <Logo />
       <Spinner />
     </div>
   );
