c65730bcd22380f82d91c3b64d2a6deb80d86c90
Xiao Yijun <xiaoyijun@silverhand.io>
2024-03-01 12:43:55 +0800

    refactor(core, schemas): enhance the time accuracy of dau data (#5451)

---
+++ packages/schemas/alterations/next-1709190131-enhance-dau-data-accuracy.ts
@@ -0,0 +1,18 @@
+import { sql } from 'slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      alter table daily_active_users alter column date set default now();
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      alter table daily_active_users alter column date drop default;
+    `);
+  },
+};
+
+export default alteration;
