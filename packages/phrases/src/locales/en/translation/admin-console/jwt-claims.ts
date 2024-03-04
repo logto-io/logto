76b41034d179b6ba3cb2a14ab9b6f3dc3ced3331
simeng-li <simeng@silverhand.io>
2024-03-04 18:54:45 +0800

    feat(console, phrases): add data source guides
    add data source guides

--- packages/phrases/src/locales/en/translation/admin-console/jwt-claims.ts
+++ packages/phrases/src/locales/en/translation/admin-console/jwt-claims.ts
@@ -7,6 +7,37 @@ const jwt_claims = {
   user_jwt: 'user JWT',
   machine_to_machine_jwt: 'machine-to-machine JWT',
   code_editor_title: 'Customize the {{token}} claims',
+  data_source_tab: 'Data source',
+  test_tab: 'Test claim',
+  jwt_claims_description:
+    'Handler that will be called during the access token generation process to add custom claims to the token. The function should return an object with the custom claims.',
+  user_data: {
+    title: 'User data',
+    subtitle:
+      'Input parameter `data.user`, providing essential user information linked to the present access token.',
+  },
+  token_data: {
+    title: 'Token data',
+    subtitle:
+      'Input parameter `token`, providing the payload of the current access token for contextual reference.',
+  },
+  fetch_external_data: {
+    title: 'Fetch external data',
+    subtitle: 'Incorporate data sources from your external APIs directly into your custom claims.',
+    description:
+      'Use the `fetch` function to call your external APIs and include the data in your custom claims. Example: ',
+  },
+  environment_variables: {
+    title: 'Set environment variables',
+    subtitle:
+      'Use environment variables to store sensitive information and access them in your custom claims handler.',
+  },
+  jwt_claims_hint:
+    'Limit custom claims to under 50KB. Default JWT claims are automatically included in the token and can not be overridden.',
+  test_data: {
+    title: 'Test',
+    subtitle: "Edit the context to adjust the token's request states and test your custom claims.",
+  },
 };
 
 export default Object.freeze(jwt_claims);
--- packages/phrases/src/locales/ja/translation/admin-console/jwt-claims.ts
+++ packages/phrases/src/locales/ja/translation/admin-console/jwt-claims.ts
@@ -14,6 +14,52 @@ const jwt_claims = {
   machine_to_machine_jwt: 'machine-to-machine JWT',
   /** UNTRANSLATED */
   code_editor_title: 'Customize the {{token}} claims',
+  /** UNTRANSLATED */
+  data_source_tab: 'Data source',
+  /** UNTRANSLATED */
+  test_tab: 'Test claim',
+  /** UNTRANSLATED */
+  jwt_claims_description:
+    'Handler that will be called during the access token generation process to add custom claims to the token. The function should return an object with the custom claims.',
+  user_data: {
+    /** UNTRANSLATED */
+    title: 'User data',
+    /** UNTRANSLATED */
+    subtitle:
+      'Input parameter `data.user`, providing essential user information linked to the present access token.',
+  },
+  token_data: {
+    /** UNTRANSLATED */
+    title: 'Token data',
+    /** UNTRANSLATED */
+    subtitle:
+      'Input parameter `token`, providing the payload of the current access token for contextual reference.',
+  },
+  fetch_external_data: {
+    /** UNTRANSLATED */
+    title: 'Fetch external data',
+    /** UNTRANSLATED */
+    subtitle: 'Incorporate data sources from your external APIs directly into your custom claims.',
+    /** UNTRANSLATED */
+    description:
+      'Use the `fetch` function to call your external APIs and include the data in your custom claims. Example: ',
+  },
+  environment_variables: {
+    /** UNTRANSLATED */
+    title: 'Set environment variables',
+    /** UNTRANSLATED */
+    subtitle:
+      'Use environment variables to store sensitive information and access them in your custom claims handler.',
+  },
+  /** UNTRANSLATED */
+  jwt_claims_hint:
+    'Limit custom claims to under 50KB. Default JWT claims are automatically included in the token and can not be overridden.',
+  test_data: {
+    /** UNTRANSLATED */
+    title: 'Test',
+    /** UNTRANSLATED */
+    subtitle: "Edit the context to adjust the token's request states and test your custom claims.",
+  },
 };
 
 export default Object.freeze(jwt_claims);
