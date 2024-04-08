4022c5ae1f7aa1c2187d52497cef16a34c2a4609
wangsijie <wangsijie@silverhand.io>
2024-04-08 14:57:32 +0800

    chore(schemas): fix alteration down

--- packages/schemas/alterations/next-1712559358-fix-down-organization-resource-scope.ts
+++ packages/schemas/alterations/next-1712559358-fix-down-organization-resource-scope.ts
@@ -4,8 +4,24 @@ import type { AlterationScript } from '../lib/types/alteration.js';
 
 import { applyTableRls, dropTableRls } from './utils/1704934999-tables.js';
 
+/**
+ * The script `next-1711955211-organization-resource-scope` was merged after `next-1712041436-rename-organization-member-role-to-collaborator`,
+ * so the table `organization_role_resource_scope_relations` may not be created in the database.
+ * This script is to fix the issue, try to create the table if it is not exists.
+ */
+
 const alteration: AlterationScript = {
   up: async (pool) => {
+    const result = await pool.maybeOne(sql`
+      select * from information_schema.tables 
+      where table_name = 'organization_role_resource_scope_relations'
+    `);
+
+    if (result) {
+      // The table already exists, do nothing
+      return;
+    }
+
     await pool.query(sql`
       create table organization_role_resource_scope_relations (
         tenant_id varchar(21) not null
@@ -20,15 +36,6 @@ const alteration: AlterationScript = {
     await applyTableRls(pool, 'organization_role_resource_scope_relations');
   },
   down: async (pool) => {
-    const result = await pool.maybeOne(sql`
-      select * from information_schema.tables 
-      where table_name = 'organization_role_resource_scope_relations'
-    `);
-
-    if (!result) {
-      return;
-    }
-
     await dropTableRls(pool, 'organization_role_resource_scope_relations');
     await pool.query(sql`
       drop table organization_role_resource_scope_relations
