196ac9d06d5f610c17e7dc8340539419a32b394e
Gao Sun <gao@silverhand.io>
2024-04-11 17:20:58 +0800

    refactor(console): update reservation link

--- packages/console/src/consts/external-links.ts
+++ packages/console/src/consts/external-links.ts
@@ -1,15 +1,10 @@
-import { buildUrl } from '@/utils/url';
-
 export const discordLink = 'https://discord.gg/UEPaF3j5e6';
 export const githubOrgLink = 'https://github.com/logto-io';
 export const githubLink = 'https://github.com/logto-io/logto';
 export const githubIssuesLink = 'https://github.com/logto-io/logto/issues';
 export const contactEmail = 'contact@logto.io';
 export const contactEmailLink = `mailto:${contactEmail}`;
-export const reservationLink = buildUrl('https://calendly.com/logto/30min', {
-  // Note: month format is YYYY-MM
-  month: new Date().toISOString().slice(0, 7),
-});
+export const reservationLink = 'https://cal.com/logto/30min';
 export const trustAndSecurityLink = 'https://logto.io/trust-and-security';
 export const pricingLink = 'https://logto.io/pricing';
 export const newPlansBlogLink = 'https://blog.logto.io/logto-pricing-v2';
