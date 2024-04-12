a8b6777cfd014493ed36d97255a7fc630063a01d
simeng-li <simeng@silverhand.io>
2024-04-12 17:49:01 +0800

    fix(schemas): fix the alteration script
    fix the alteration script

--- packages/schemas/alterations/next-1712912361-delete-jwt-customier-with-empty-script.ts
+++ packages/schemas/alterations/next-1712912361-delete-jwt-customier-with-empty-script.ts
@@ -2,11 +2,6 @@ import { sql } from '@silverhand/slonik';
 
 import type { AlterationScript } from '../lib/types/alteration.js';
 
-const enum LogtoJwtTokenKey {
-  AccessToken = 'jwt.accessToken',
-  ClientCredentials = 'jwt.clientCredentials',
-}
-
 const alteration: AlterationScript = {
   // We are making the jwt-customizer script field mandatory
   // Delete the records in logto_configs where key is jwt.accessToken or jwt.clientCredentials and value jsonb's script field is undefined
@@ -14,7 +9,7 @@ const alteration: AlterationScript = {
     await pool.query(
       sql`
         delete from logto_configs 
-        where key in ('${LogtoJwtTokenKey.AccessToken}', '${LogtoJwtTokenKey.ClientCredentials}')
+        where key in ('jwt.accessToken', 'jwt.clientCredentials')
         and value->>'script' is null
       `
     );
