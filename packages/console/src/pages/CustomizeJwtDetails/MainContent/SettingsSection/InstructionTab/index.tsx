94b62fb0b462094c8709d0aedc8b0601fa0ffdb5
simeng-li <simeng@silverhand.io>
2024-04-16 10:08:32 +0800

    style(console): minor custom jwt page style iteration (#5711)
    minor custom jwt page styles iteration

--- packages/console/src/pages/CustomizeJwtDetails/MainContent/SettingsSection/InstructionTab/index.tsx
+++ packages/console/src/pages/CustomizeJwtDetails/MainContent/SettingsSection/InstructionTab/index.tsx
@@ -53,7 +53,8 @@ function InstructionTab({ isActive }: Props) {
               ? accessTokenPayloadTypeDefinition
               : clientCredentialsPayloadTypeDefinition
           }
-          height="320px"
+          // ClientCredentials token payload has only a few fields, so it doesn't need to be as tall as the AccessToken payload.
+          height={tokenType === LogtoJwtTokenKeyType.AccessToken ? '320px' : '200px'}
           theme="logto-dark"
           options={typeDefinitionCodeEditorOptions}
         />
