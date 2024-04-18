dcd71df02d236f31228e6849c0a1d7034c7a0ae5
simeng-li <simeng@silverhand.io>
2024-04-18 17:41:42 +0800

    fix(console): enable dev feature for integration tests
    enable dev feature for integration tests

--- packages/console/src/consts/env.ts
+++ packages/console/src/consts/env.ts
@@ -4,5 +4,4 @@ export const isProduction = process.env.NODE_ENV === 'production';
 export const isCloud = yes(process.env.IS_CLOUD);
 export const adminEndpoint = process.env.ADMIN_ENDPOINT;
 
-// eslint-disable-next-line @typescript-eslint/no-inferrable-types
-export const isDevFeaturesEnabled: boolean = false;
+export const isDevFeaturesEnabled: boolean = yes(process.env.INTEGRATION_TEST);
