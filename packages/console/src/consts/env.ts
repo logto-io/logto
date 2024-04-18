895c62b9f4ee6ee361f41d4ec6d26ba062cdf171
simeng-li <simeng@silverhand.io>
2024-04-18 17:04:05 +0800

    fix(console): force dev feature enabled false
    force dev feature enabled false

--- packages/console/src/consts/env.ts
+++ packages/console/src/consts/env.ts
@@ -4,5 +4,4 @@ export const isProduction = process.env.NODE_ENV === 'production';
 export const isCloud = yes(process.env.IS_CLOUD);
 export const adminEndpoint = process.env.ADMIN_ENDPOINT;
 
-export const isDevFeaturesEnabled =
-  !isProduction || yes(process.env.DEV_FEATURES_ENABLED) || yes(process.env.INTEGRATION_TEST);
+export const isDevFeaturesEnabled = false;
