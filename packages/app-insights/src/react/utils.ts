c1c746bca4be538e43d1138daa237013fdc96cbb
Gao Sun <gao@silverhand.io>
2024-04-18 15:20:24 +0800

    refactor: remove AppInsights for React (#5742)

--- packages/app-insights/src/react/utils.ts
+++
@@ -1,5 +0,0 @@
-/**
- * **CAUTION:** This function takes the last two parts of the hostname which may cause issues for
- * some second-level domains, e.g. `.co.uk`.
- */
-export const getPrimaryDomain = () => window.location.hostname.split('.').slice(-2).join('.');
