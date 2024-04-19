45fd3b22ff1d44ebdd2ca3ab0b3425c2c8eb35b1
simeng-li <simeng@silverhand.io>
2024-04-19 11:47:07 +0800

    fix(console): revert the isDevFeaturesEnabled changes in console (#5755)
    revert the isDevFeatureEnabled hot fix

--- packages/console/src/consts/env.ts
+++ packages/console/src/consts/env.ts
@@ -4,4 +4,5 @@ export const isProduction = process.env.NODE_ENV === 'production';
 export const isCloud = yes(process.env.IS_CLOUD);
 export const adminEndpoint = process.env.ADMIN_ENDPOINT;
 
-export const isDevFeaturesEnabled: boolean = yes(process.env.INTEGRATION_TEST);
+export const isDevFeaturesEnabled =
+  !isProduction || yes(process.env.DEV_FEATURES_ENABLED) || yes(process.env.INTEGRATION_TEST);
