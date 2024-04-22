711c51c0fd1c4b1a2d77b3fe03ea69a5efb23865
Charles Zhao <charleszhao@silverhand.io>
2024-04-22 11:10:33 +0800

    refactor(console): update organization guide and tenant member routers (#5766)

--- packages/console/src/hooks/use-console-routes/index.tsx
+++ packages/console/src/hooks/use-console-routes/index.tsx
@@ -1,6 +1,6 @@
 import { condArray } from '@silverhand/essentials';
 import { useMemo } from 'react';
-import { type RouteObject } from 'react-router-dom';
+import { Navigate, type RouteObject } from 'react-router-dom';
 
 import { isCloud, isDevFeaturesEnabled } from '@/consts/env';
 import Dashboard from '@/pages/Dashboard';
@@ -8,6 +8,11 @@ import GetStarted from '@/pages/GetStarted';
 import Mfa from '@/pages/Mfa';
 import NotFound from '@/pages/NotFound';
 import OrganizationGuide from '@/pages/Organizations/Guide';
+import Introduction from '@/pages/Organizations/Guide/Introduction';
+import OrganizationInfo from '@/pages/Organizations/Guide/OrganizationInfo';
+import OrganizationPermissions from '@/pages/Organizations/Guide/OrganizationPermissions';
+import OrganizationRoles from '@/pages/Organizations/Guide/OrganizationRoles';
+import { steps } from '@/pages/Organizations/Guide/const';
 import SigningKeys from '@/pages/SigningKeys';
 
 import { apiResources } from './routes/api-resources';
@@ -46,7 +51,17 @@ export const useConsoleRoutes = () => {
         roles,
         isDevFeaturesEnabled && organizationTemplate,
         organizations,
-        !isDevFeaturesEnabled && { path: 'organization-guide/*', element: <OrganizationGuide /> },
+        !isDevFeaturesEnabled && {
+          path: 'organization-guide/*',
+          element: <OrganizationGuide />,
+          children: [
+            { index: true, element: <Navigate replace to={steps.introduction} /> },
+            { path: steps.introduction, element: <Introduction /> },
+            { path: steps.permissions, element: <OrganizationPermissions /> },
+            { path: steps.roles, element: <OrganizationRoles /> },
+            { path: steps.organizationInfo, element: <OrganizationInfo /> },
+          ],
+        },
         profile,
         { path: 'signing-keys', element: <SigningKeys /> },
         isCloud && tenantSettings,
--- packages/console/src/pages/Organizations/Guide/index.tsx
+++ packages/console/src/pages/Organizations/Guide/index.tsx
@@ -1,18 +1,11 @@
 import { useCallback } from 'react';
 import Modal from 'react-modal';
-// FIXME: @charles
-// eslint-disable-next-line no-restricted-imports
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import DsModalHeader from '@/ds-components/ModalHeader';
 import useTenantPathname from '@/hooks/use-tenant-pathname';
 import * as modalStyles from '@/scss/modal.module.scss';
 
-import Introduction from './Introduction';
-import OrganizationInfo from './OrganizationInfo';
-import OrganizationPermissions from './OrganizationPermissions';
-import OrganizationRoles from './OrganizationRoles';
-import { steps } from './const';
 import * as styles from './index.module.scss';
 
 function Guide() {
@@ -30,13 +23,7 @@ function Guide() {
           subtitle="organizations.guide.subtitle"
           onClose={onClose}
         />
-        <Routes>
-          <Route index element={<Navigate replace to={steps.introduction} />} />
-          <Route path={steps.introduction} element={<Introduction />} />
-          <Route path={steps.permissions} element={<OrganizationPermissions />} />
-          <Route path={steps.roles} element={<OrganizationRoles />} />
-          <Route path={steps.organizationInfo} element={<OrganizationInfo />} />
-        </Routes>
+        <Outlet />
       </div>
     </Modal>
   );
