88ee906b756f9e08583b6896016c2106b3aaabd6
Gao Sun <gao@silverhand.io>
2024-06-23 22:33:06 +0800

    refactor(core): fork client credentials grant

--- packages/core/src/oidc/grants/index.ts
+++ packages/core/src/oidc/grants/index.ts
@@ -5,6 +5,7 @@ import instance from 'oidc-provider/lib/helpers/weak_cache.js';
 import { type EnvSet } from '#src/env-set/index.js';
 import type Queries from '#src/tenants/Queries.js';
 
+import * as clientCredentials from './client-credentials.js';
 import * as refreshToken from './refresh-token.js';
 
 export const registerGrants = (oidc: Provider, envSet: EnvSet, queries: Queries) => {
@@ -14,14 +15,22 @@ export const registerGrants = (oidc: Provider, envSet: EnvSet, queries: Queries)
 
   // If resource indicators are enabled, append `resource` to the parameters and allow it to
   // be duplicated
-  const parameterConfig: [parameters: string[], duplicates: string[]] = resourceIndicators.enabled
-    ? [[...refreshToken.parameters, 'resource'], ['resource']]
-    : [[...refreshToken.parameters], []];
+  const getParameterConfig = (
+    parameters: readonly string[]
+  ): [parameters: string[], duplicates: string[]] =>
+    resourceIndicators.enabled
+      ? [[...parameters, 'resource'], ['resource']]
+      : [[...parameters], []];
 
-  // Override the default `refresh_token` grant
+  // Override the default grants
   oidc.registerGrantType(
     GrantType.RefreshToken,
     refreshToken.buildHandler(envSet, queries),
-    ...parameterConfig
+    ...getParameterConfig(refreshToken.parameters)
+  );
+  oidc.registerGrantType(
+    GrantType.ClientCredentials,
+    clientCredentials.buildHandler(envSet, queries),
+    ...getParameterConfig(clientCredentials.parameters)
   );
 };
