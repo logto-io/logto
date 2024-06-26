f22ed8955db33a7db6aa2ac64eba73c9f4bd0caf
Gao Sun <gao@silverhand.io>
2024-06-26 16:39:23 +0800

    refactor(console): remove unused config

--- packages/console/src/mdx-components/Mermaid/index.tsx
+++ packages/console/src/mdx-components/Mermaid/index.tsx
@@ -8,51 +8,6 @@ mermaid.initialize({
   startOnLoad: true,
   theme: 'default',
   securityLevel: 'loose',
-  themeCSS: `
-    g.classGroup rect {
-      fill: #282a36;
-      stroke: #6272a4;
-    } 
-    g.classGroup text {
-      fill: #f8f8f2;
-    }
-    g.classGroup line {
-      stroke: #f8f8f2;
-      stroke-width: 0.5;
-    }
-    .classLabel .box {
-      stroke: #21222c;
-      stroke-width: 3;
-      fill: #21222c;
-      opacity: 1;
-    }
-    .classLabel .label {
-      fill: #f1fa8c;
-    }
-    .relation {
-      stroke: #ff79c6;
-      stroke-width: 1;
-    }
-    #compositionStart, #compositionEnd {
-      fill: #bd93f9;
-      stroke: #bd93f9;
-      stroke-width: 1;
-    }
-    #aggregationEnd, #aggregationStart {
-      fill: #21222c;
-      stroke: #50fa7b;
-      stroke-width: 1;
-    }
-    #dependencyStart, #dependencyEnd {
-      fill: #00bcd4;
-      stroke: #00bcd4;
-      stroke-width: 1;
-    } 
-    #extensionStart, #extensionEnd {
-      fill: #f8f8f2;
-      stroke: #f8f8f2;
-      stroke-width: 1;
-    }`,
   fontFamily: 'Fira Code',
 });
 
