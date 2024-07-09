2334196c41b74c06e8fe482d3dc9b3a525f468aa
wangsijie <wangsijie@silverhand.io>
2024-07-09 15:04:31 +0800

    refactor(core): refactor organizations in grants

--- packages/core/src/oidc/grants/utils.ts
+++ packages/core/src/oidc/grants/utils.ts
@@ -1,13 +1,24 @@
+import { buildOrganizationUrn } from '@logto/core-kit';
+import { cond } from '@silverhand/essentials';
 import type Provider from 'oidc-provider';
-import { errors, type KoaContextWithOIDC } from 'oidc-provider';
+import { type Account, errors, type KoaContextWithOIDC } from 'oidc-provider';
 import certificateThumbprint from 'oidc-provider/lib/helpers/certificate_thumbprint.js';
 import epochTime from 'oidc-provider/lib/helpers/epoch_time.js';
 import dpopValidate from 'oidc-provider/lib/helpers/validate_dpop.js';
 import instance from 'oidc-provider/lib/helpers/weak_cache.js';
 
+import { type EnvSet } from '#src/env-set/index.js';
+import type Queries from '#src/tenants/Queries.js';
 import assertThat from '#src/utils/assert-that.js';
 
-const { InvalidGrant, InvalidClient } = errors;
+import {
+  getSharedResourceServerData,
+  isOrganizationConsentedToApplication,
+  isThirdPartyApplication,
+  reversedResourceAccessTokenTtl,
+} from '../resource.js';
+
+const { InvalidGrant, InvalidClient, AccessDenied } = errors;
 
 /**
  * Handle DPoP bound access tokens.
@@ -78,4 +89,103 @@ export const handleClientCertificate = async (
       throw new InvalidGrant('failed x5t#S256 verification');
     }
   }
+};
+
+/**
+ * Implement access check for RFC 0001
+ */
+export const checkOrganizationAccess = async (
+  ctx: KoaContextWithOIDC,
+  queries: Queries,
+  account: Account
+): Promise<{ organizationId?: string }> => {
+  const { client, params } = ctx.oidc;
+
+  assertThat(params, new InvalidGrant('parameters must be available'));
+  assertThat(client, new InvalidClient('client must be available'));
+
+  const organizationId = cond(Boolean(params.organization_id) && String(params.organization_id));
+
+  if (organizationId) {
+    // Check membership
+    if (
+      !(await queries.organizations.relations.users.exists({
+        organizationId,
+        userId: account.accountId,
+      }))
+    ) {
+      const error = new AccessDenied('user is not a member of the organization');
+      // eslint-disable-next-line @silverhand/fp/no-mutation
+      error.statusCode = 403;
+      throw error;
+    }
+
+    // Check if the organization is granted (third-party application only) by the user
+    if (
+      (await isThirdPartyApplication(queries, client.clientId)) &&
+      !(await isOrganizationConsentedToApplication(
+        queries,
+        client.clientId,
+        account.accountId,
+        organizationId
+      ))
+    ) {
+      const error = new AccessDenied('organization access is not granted to the application');
+      // eslint-disable-next-line @silverhand/fp/no-mutation
+      error.statusCode = 403;
+      throw error;
+    }
+
+    // Check if the organization requires MFA and the user has MFA enabled
+    const { isMfaRequired, hasMfaConfigured } = await queries.organizations.getMfaStatus(
+      organizationId,
+      account.accountId
+    );
+    if (isMfaRequired && !hasMfaConfigured) {
+      const error = new AccessDenied('organization requires MFA but user has no MFA configured');
+      // eslint-disable-next-line @silverhand/fp/no-mutation
+      error.statusCode = 403;
+      throw error;
+    }
+  }
+
+  return { organizationId };
+};
+
+/**
+ * Implement organization token for RFC 0001
+ */
+export const handleOrganizationToken = async ({
+  envSet,
+  availableScopes,
+  accessToken: at,
+  organizationId,
+  scope,
+}: {
+  envSet: EnvSet;
+  availableScopes: string[];
+  accessToken: InstanceType<Provider['AccessToken']> | InstanceType<Provider['ClientCredentials']>;
+  organizationId: string;
+  scope: Set<string>;
+}): Promise<void> => {
+  /* eslint-disable @silverhand/fp/no-mutation */
+  const audience = buildOrganizationUrn(organizationId);
+
+  /** The intersection of the available scopes and the requested scopes. */
+  const issuedScopes = availableScopes.filter((name) => scope.has(name)).join(' ');
+
+  at.aud = audience;
+  // Note: the original implementation uses `new provider.ResourceServer` to create the resource
+  // server. But it's not available in the typings. The class is actually very simple and holds
+  // no provider-specific context. So we just create the object manually.
+  // See https://github.com/panva/node-oidc-provider/blob/cf2069cbb31a6a855876e95157372d25dde2511c/lib/helpers/resource_server.js
+  at.resourceServer = {
+    ...getSharedResourceServerData(envSet),
+    accessTokenTTL: reversedResourceAccessTokenTtl,
+    audience,
+    scope: availableScopes.join(' '),
+  };
+  at.scope = issuedScopes;
+
+  /* eslint-enable @silverhand/fp/no-mutation */
 };
