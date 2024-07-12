6bf3bebb4066e47a939c65b7e95476cc08e2075d
Xiao Yijun <xiaoyijun@silverhand.io>
2024-07-12 21:51:10 +0800

    feat(experience): support loading state for buttons (#6232)

---
+++ packages/experience/src/components/Button/RotatingRingIcon.tsx
@@ -0,0 +1,7 @@
+import Ring from '@/assets/icons/loading-ring.svg';
+
+import * as RotatingRingIconStyles from './RotatingRingIcon.module.scss';
+
+const RotatingRingIcon = () => <Ring className={RotatingRingIconStyles.icon} />;
+
+export default RotatingRingIcon;
