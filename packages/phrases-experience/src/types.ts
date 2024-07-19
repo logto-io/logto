9024dd5cc269aacfc97e7b996160b11527be64c2
Charles Zhao <charleszhao@silverhand.io>
2024-07-19 16:11:37 +0800

    fix: fix typescript type error in experience phrases

--- packages/phrases-experience/src/types.ts
+++ packages/phrases-experience/src/types.ts
@@ -1,10 +1,17 @@
 import type en from './locales/en/index.js';
 
-type FlattenKeys<T, Prefix extends string = ''> = {
-  [K in keyof T]: T[K] extends Record<string, unknown>
-    ? `${Prefix}${string & K}.${FlattenKeys<T[K]>}`
-    : `${Prefix}${string & K}`;
-}[keyof T];
+type FlattenKeys<
+  T,
+  Prefix extends string = '',
+  D extends number = 11, // Depth limit is actually 10, since the initial value of A is [0]
+  A extends unknown[] = [0],
+> = A['length'] extends D
+  ? never
+  : {
+      [K in keyof T]: T[K] extends Record<string, unknown>
+        ? `${Prefix}${string & K}.${FlattenKeys<T[K], '', D, [0, ...A]>}`
+        : `${Prefix}${string & K}`;
+    }[keyof T];
 
 export type LocalePhrase = typeof en;
 
