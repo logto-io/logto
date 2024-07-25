3b9f9405e806d00db9b56dfb89f8782ab525f128
simeng-li <simeng@silverhand.io>
2024-07-25 17:34:00 +0800

    test(core): add update application with new custom data test
    add update application with new custom data test

--- packages/integration-tests/src/tests/api/application/application-custom-data.test.ts
+++ packages/integration-tests/src/tests/api/application/application-custom-data.test.ts
@@ -2,8 +2,10 @@ import { ApplicationType } from '@logto/schemas';
 
 import {
   createApplication,
+  deleteApplication,
   getApplication,
   patchApplicationCustomData,
+  updateApplication,
 } from '#src/api/application.js';
 
 describe('application custom data API', () => {
@@ -20,5 +22,25 @@ describe('application custom data API', () => {
     const fetchedApplication = await getApplication(application.id);
 
     expect(fetchedApplication.customData.key).toEqual(customData.key);
+
+    await deleteApplication(application.id);
+  });
+
+  it('should put application custom data successfully', async () => {
+    const applicationName = 'test-create-app';
+    const applicationType = ApplicationType.SPA;
+    const application = await createApplication(applicationName, applicationType);
+    const customData = { key: 'foo' };
+
+    const result = await patchApplicationCustomData(application.id, customData);
+
+    expect(result.key).toEqual(customData.key);
+
+    await updateApplication(application.id, {
+      customData: { key: 'bar' },
+    });
+
+    const fetchedApplication = await getApplication(application.id);
+    expect(fetchedApplication.customData.key).toEqual('bar');
   });
 });
