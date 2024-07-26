100dba5ec6da6bde474fd08a2e61fb7773f8679f
simeng-li <simeng@silverhand.io>
2024-07-26 12:07:20 +0800

    refactor(core): refactor backup code generate flow
    refactor backup code generate flow

--- packages/core/src/routes/experience/verification-routes/backup-code-verification.ts
+++ packages/core/src/routes/experience/verification-routes/backup-code-verification.ts
@@ -17,6 +17,30 @@ export default function backupCodeVerificationRoutes<T extends WithLogContext>(
 ) {
   const { libraries, queries } = tenantContext;
 
+  router.post(`${experienceRoutes.verification}/backup-code/generate`, async (ctx, next) => {
+    const { experienceInteraction } = ctx;
+
+    assertThat(experienceInteraction.identifiedUserId, 'session.identifier_not_found');
+
+    const backupCodeVerificationRecord = BackupCodeVerification.create(
+      libraries,
+      queries,
+      experienceInteraction.identifiedUserId
+    );
+
+    backupCodeVerificationRecord.generate();
+
+    ctx.experienceInteraction.setVerificationRecord(backupCodeVerificationRecord);
+
+    await ctx.experienceInteraction.save();
+
+    ctx.body = {
+      verificationId: backupCodeVerificationRecord.id,
+    };
+
+    return next();
+  });
+
   router.post(
     `${experienceRoutes.verification}/backup-code/generate`,
     koaGuard({
