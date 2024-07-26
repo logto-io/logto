ec4027d373a675e6772df4e78796cfb2fe4ca316
simeng-li <simeng@silverhand.io>
2024-07-26 16:03:19 +0800

    fix(core): fix rebase issue
    fix rebase issue

--- packages/core/src/routes/experience/verification-routes/backup-code-verification.ts
+++ packages/core/src/routes/experience/verification-routes/backup-code-verification.ts
@@ -27,41 +27,6 @@ export default function backupCodeVerificationRoutes<T extends ExperienceInterac
         codes: z.array(z.string()),
       }),
     }),
-    async (ctx, next) => {
-      const { experienceInteraction } = ctx;
-
-      assertThat(experienceInteraction.identifiedUserId, 'session.identifier_not_found');
-
-      const backupCodeVerificationRecord = BackupCodeVerification.create(
-        libraries,
-        queries,
-        experienceInteraction.identifiedUserId
-      );
-
-      const codes = backupCodeVerificationRecord.generate();
-
-      ctx.experienceInteraction.setVerificationRecord(backupCodeVerificationRecord);
-
-      await ctx.experienceInteraction.save();
-
-      ctx.body = {
-        verificationId: backupCodeVerificationRecord.id,
-        codes,
-      };
-
-      return next();
-    }
-  );
-
-  router.post(
-    `${experienceRoutes.verification}/backup-code/generate`,
-    koaGuard({
-      status: [200, 400],
-      response: z.object({
-        verificationId: z.string(),
-        codes: z.array(z.string()),
-      }),
-    }),
     koaExperienceVerificationsAuditLog({
       type: VerificationType.BackupCode,
       action: Action.Create,
