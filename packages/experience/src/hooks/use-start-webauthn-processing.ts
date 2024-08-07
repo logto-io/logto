3e643f40b301cf86ba5efaf91faa290a2f1a93a0
simeng-li <simeng@silverhand.io>
2024-08-07 18:18:04 +0800

    refactor(experience): migrate mfa binding flow
    migrate mfa binding flow

--- packages/experience/src/hooks/use-start-webauthn-processing.ts
+++ packages/experience/src/hooks/use-start-webauthn-processing.ts
@@ -1,11 +1,9 @@
-import { MfaFactor } from '@logto/schemas';
-import { useCallback } from 'react';
+import { MfaFactor, VerificationType } from '@logto/schemas';
+import { useCallback, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import {
-  createWebAuthnRegistrationOptions,
-  generateWebAuthnAuthnOptions,
-} from '@/apis/interaction';
+import UserInteractionContext from '@/Providers/UserInteractionContextProvider/UserInteractionContext';
+import { createWebAuthnRegistration, createWebAuthnAuthentication } from '@/apis/experience';
 import { UserMfaFlow } from '@/types';
 import { type WebAuthnState, type MfaFlowState } from '@/types/guard';
 
@@ -18,13 +16,14 @@ type Options = {
 
 const useStartWebAuthnProcessing = ({ replace }: Options = {}) => {
   const navigate = useNavigate();
-  const asyncCreateRegistrationOptions = useApi(createWebAuthnRegistrationOptions);
-  const asyncGenerateAuthnOptions = useApi(generateWebAuthnAuthnOptions);
+  const asyncCreateRegistrationOptions = useApi(createWebAuthnRegistration);
+  const asyncGenerateAuthnOptions = useApi(createWebAuthnAuthentication);
   const handleError = useErrorHandler();
+  const { setVerificationId } = useContext(UserInteractionContext);
 
   return useCallback(
     async (flow: UserMfaFlow, flowState: MfaFlowState) => {
-      const [error, options] =
+      const [error, result] =
         flow === UserMfaFlow.MfaBinding
           ? await asyncCreateRegistrationOptions()
           : await asyncGenerateAuthnOptions();
@@ -34,7 +33,10 @@ const useStartWebAuthnProcessing = ({ replace }: Options = {}) => {
         return;
       }
 
-      if (options) {
+      if (result) {
+        const { verificationId, options } = result;
+        setVerificationId(VerificationType.WebAuthn, verificationId);
+
         const state: WebAuthnState = {
           options,
           ...flowState,
@@ -43,7 +45,14 @@ const useStartWebAuthnProcessing = ({ replace }: Options = {}) => {
         navigate({ pathname: `/${flow}/${MfaFactor.WebAuthn}` }, { replace, state });
       }
     },
-    [asyncCreateRegistrationOptions, asyncGenerateAuthnOptions, handleError, navigate, replace]
+    [
+      asyncCreateRegistrationOptions,
+      asyncGenerateAuthnOptions,
+      handleError,
+      navigate,
+      replace,
+      setVerificationId,
+    ]
   );
 };
 
