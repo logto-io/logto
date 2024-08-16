adc1e25524d19d8650acee811b61e852e3746972
Charles Zhao <charleszhao@silverhand.io>
2024-08-16 18:48:40 +0800

    test(cli): add unit tests for tunnel util

--- packages/cli/src/commands/tunnel/utils.ts
+++ packages/cli/src/commands/tunnel/utils.ts
@@ -149,7 +149,7 @@ export const checkExperienceInput = (url?: string, staticPath?: string) => {
 export const isLogtoRequestPath = (requestPath?: string): boolean =>
   ['/oidc/', '/api/'].some((path) => requestPath?.startsWith(path)) || requestPath === '/consent';
 
-const isFileAssetPath = (url: string): boolean => {
+export const isFileAssetPath = (url: string): boolean => {
   // Check if the request URL contains query params. If yes, ignore the params and check the request path
   const pathWithoutQuery = url.split('?')[0];
   return Boolean(pathWithoutQuery?.split('/').at(-1)?.includes('.'));
