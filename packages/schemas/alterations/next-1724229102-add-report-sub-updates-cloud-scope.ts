19d3aa787aafaf7d8910ad3e7acda2843889cd1a
Darcy Ye <darcyye@silverhand.io>
2024-08-22 10:24:55 +0800

    fix: make alteration script compatible for core DB in all regions (#6494)

--- packages/schemas/alterations/next-1724229102-add-report-sub-updates-cloud-scope.ts
+++ packages/schemas/alterations/next-1724229102-add-report-sub-updates-cloud-scope.ts
@@ -40,11 +40,15 @@ const reportSubscriptionUpdatesScopeDescription =
 const alteration: AlterationScript = {
   up: async (pool) => {
     // Get the Cloud API resource
-    const cloudApiResource = await pool.one<Resource>(sql`
+    const cloudApiResource = await pool.maybeOne<Resource>(sql`
       select * from resources
       where tenant_id = ${adminTenantId}
       and indicator = ${cloudApiIndicator}
     `);
+
+    if (!cloudApiResource) {
+      return;
+    }
 
     // Get cloud connection application role
     const tenantApplicationRole = await pool.one<Role>(sql`
@@ -59,6 +63,7 @@ const alteration: AlterationScript = {
       values (${generateStandardId()}, ${adminTenantId}, ${
         cloudApiResource.id
       }, ${reportSubscriptionUpdatesScopeName}, ${reportSubscriptionUpdatesScopeDescription})
+      on conflict (tenant_id, name, resource_id) do nothing
       returning *;
     `);
 
@@ -67,16 +72,20 @@ const alteration: AlterationScript = {
       insert into roles_scopes (id, tenant_id, role_id, scope_id)
       values (${generateStandardId()}, ${adminTenantId}, ${tenantApplicationRole.id}, ${
         reportSubscriptionUpdatesCloudScope.id
-      });
+      }) on conflict (tenant_id, role_id, scope_id) do nothing;
     `);
   },
   down: async (pool) => {
     // Get the Cloud API resource
-    const cloudApiResource = await pool.one<Resource>(sql`
+    const cloudApiResource = await pool.maybeOne<Resource>(sql`
       select * from resources
       where tenant_id = ${adminTenantId}
       and indicator = ${cloudApiIndicator}
     `);
+
+    if (!cloudApiResource) {
+      return;
+    }
 
     // Remove the `report:subscription:updates` scope
     await pool.query(sql`
