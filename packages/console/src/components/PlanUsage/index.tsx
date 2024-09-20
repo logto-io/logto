58c8818b1a1f4044d97887c8cbda1c2097f937c3
Darcy Ye <darcyye@silverhand.io>
2024-09-20 19:36:17 +0800

    fix(console): add comments

--- packages/console/src/components/PlanUsage/index.tsx
+++ packages/console/src/components/PlanUsage/index.tsx
@@ -84,6 +84,8 @@ function PlanUsage({ periodicUsage: rawPeriodicUsage }: Props) {
       (key) =>
         // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
         isAddOnAvailable ||
+        // TODO: design a flow for enterprise tenants onboarding.
+        // Show all usages for Enterprise plan since some of the enterprise tenants does not have Stripe subscription, as a result, the `isAddOnAvailable` will be undefined in this case, even if we will deprecate `isAddOnAvailable` soon, the plan usage will not be automatically fixed for these enterprise tenants.
         isEnterprisePlan ||
         (onlyShowPeriodicUsage && (key === 'mauLimit' || key === 'tokenLimit'))
     )
