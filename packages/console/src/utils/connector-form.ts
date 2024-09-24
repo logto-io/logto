f8fd52b2f8ddc5963f0698bdbee69187bc1bc90f
wangsijie <wangsijie@silverhand.io>
2024-09-24 13:48:35 +0800

    fix(console): fix empty string update of connector config form (#6626)
    Revert "fix(console): fix passwordless connector tester send failed bug (#6268)"
    
    This reverts commit 6963192ac5cf2285826799a34a20c9efaa1f343a.

--- packages/console/src/utils/connector-form.ts
+++ packages/console/src/utils/connector-form.ts
@@ -22,17 +22,11 @@ const initFormData = (formItems: ConnectorConfigFormItem[], config?: Record<stri
 
 export const parseFormConfig = (
   config: Record<string, unknown>,
-  formItems: ConnectorConfigFormItem[],
-  skipFalsyValuesRemoval = false
+  formItems: ConnectorConfigFormItem[]
 ) => {
   return Object.fromEntries(
     Object.entries(config)
       .map(([key, value]) => {
-        // Filter out empty input
-        if (!skipFalsyValuesRemoval && value === '') {
-          return null;
-        }
-
         const formItem = formItems.find((item) => item.key === key);
 
         if (!formItem) {
