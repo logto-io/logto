d4f7d098dc8aa8083cdd867d927066f320e2bb95
simeng-li <simeng@silverhand.io>
2024-10-14 13:34:35 +0800

    feat(schemas): add IdP initiated SAML SSO sessions table (#6663)
    * feat(schemas): add idp-initiated saml sso sessions table
    
    add idp-iniaited saml sso sessions table
    
    * refactor(schemas): remove nameID index
    
    remove nameID index

---
+++ packages/schemas/tables/idp_initiated_saml_sso_sessions.sql
@@ -0,0 +1,16 @@
+/* init_order = 2 */
+create table idp_initiated_saml_sso_sessions (
+  tenant_id varchar(21) not null 
+    references tenants (id) on update cascade on delete cascade,
+  /** The globally unique identifier of the assertion record. */
+  id varchar(21) not null,
+  /** The identifier of the SAML SSO connector. */
+  connector_id varchar(128) not null
+    references sso_connectors (id) on update cascade on delete cascade,
+  /** The SAML assertion. */
+  assertion_content jsonb /* @use SsoSamlAssertionContent */ not null default '{}'::jsonb,
+  created_at timestamptz not null default(now()),
+  /** The expiration time of the assertion. */
+  expires_at timestamptz not null,
+  primary key (tenant_id, id)
+);
