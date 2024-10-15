6c0fe806316f8428b85de613fb205c55c8358e1d
simeng-li <simeng@silverhand.io>
2024-10-15 11:39:07 +0800

    feat(core): add idp-initiated sso client sign-in redirect
    add idp-initiated sso client sign-in redirect

--- packages/phrases/src/locales/en/errors/single-sign-on.ts
+++ packages/phrases/src/locales/en/errors/single-sign-on.ts
@@ -9,6 +9,8 @@ const single_sign_on = {
     'Invalid application type. Only {{type}} applications are allowed.',
   idp_initiated_authentication_redirect_uri_not_registered:
     'The redirect_uri is not registered. Please check the application settings.',
+  idp_initiated_authentication_client_callback_uri_not_found:
+    'The client IdP-initiated authentication callback URI is not found. Please check the connector settings.',
 };
 
 export default Object.freeze(single_sign_on);
