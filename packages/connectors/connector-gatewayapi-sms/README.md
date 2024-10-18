61aa13f8a9aeb0f89b7b64177fa88159c908ec58
wangsijie <wangsijie@silverhand.io>
2024-10-18 19:24:15 +0800

    feat(connector): add gatewayapi connector (#6691)

---
+++ packages/connectors/connector-gatewayapi-sms/README.md
@@ -0,0 +1,32 @@
+# GatewayAPI SMS connector
+
+The official Logto connector for GatewayAPI SMS.
+
+## Get started
+
+GatewayAPI is a cloud service provider in Europe, offering many cloud services, including SMS (short message service). GatewayAPI SMS Connector is a plugin provided by the Logto team to call the GatewayAPI SMS service, with the help of which Logto end-users can register and sign in to their Logto account via SMS verification code.
+
+## Set up in GatewayAPI
+
+> 💡 **Tip**
+> 
+> You can skip some sections if you have already finished.
+
+### Create an GatewayAPI account
+
+Go to the [GatewayAPI website](https://www.gatewayapi.com/) and register your GatewayAPI account if you don't have one.
+
+### Enable account
+
+You may need to enable your account before using the SMS service. You can contact the GatewayAPI customer service to enable your account.
+
+### Get API token
+
+Go to the API Keys page from the GatewayAPI console, and find the API token or create a new API token.
+
+## Set up in Logto
+
+1. **Endpoint**: If your GatewayAPI account is in the EU region, you should use the endpoint `https://gatewayapi.com/rest/mtsms`. If your GatewayAPI account is in the US region, you should use the endpoint `https://gatewayapi.com/rest/mtsms`.
+2. **API Token**: The API token you created in the previous step.
+3. **Sender**: The sender you want to use to send the SMS.
+4. **Templates**: The templates you want to use to send the SMS, you can use the default templates or modify them as needed.
---
+++ packages/connectors/connector-gatewayapi-sms/src/index.ts
@@ -0,0 +1,81 @@
+import { assert } from '@silverhand/essentials';
+import { got, HTTPError } from 'got';
+
+import type {
+  GetConnectorConfig,
+  SendMessageFunction,
+  CreateConnector,
+  SmsConnector,
+} from '@logto/connector-kit';
+import {
+  ConnectorError,
+  ConnectorErrorCodes,
+  validateConfig,
+  ConnectorType,
+  replaceSendMessageHandlebars,
+} from '@logto/connector-kit';
+
+import { defaultMetadata } from './constant.js';
+import { gatewayApiSmsConfigGuard, type GatewayApiSmsPayload } from './types.js';
+
+const sendMessage =
+  (getConfig: GetConnectorConfig): SendMessageFunction =>
+  async (data, inputConfig) => {
+    const { to, type, payload } = data;
+    const config = inputConfig ?? (await getConfig(defaultMetadata.id));
+    validateConfig(config, gatewayApiSmsConfigGuard);
+    const { endpoint, apiToken, sender, templates } = config;
+    const template = templates.find((template) => template.usageType === type);
+
+    assert(
+      template,
+      new ConnectorError(
+        ConnectorErrorCodes.TemplateNotFound,
+        `Cannot find template for type: ${type}`
+      )
+    );
+
+    const encodedAuth = Buffer.from(`${apiToken}:`).toString('base64');
+    const body: GatewayApiSmsPayload = {
+      sender,
+      message: replaceSendMessageHandlebars(template.content, payload),
+      recipients: [{ msisdn: to }],
+    };
+
+    try {
+      return await got.post(endpoint, {
+        headers: {
+          Authorization: `Basic ${encodedAuth}`,
+        },
+        json: body,
+      });
+    } catch (error: unknown) {
+      if (error instanceof HTTPError) {
+        const {
+          response: { body: rawBody },
+        } = error;
+        assert(
+          typeof rawBody === 'string',
+          new ConnectorError(
+            ConnectorErrorCodes.InvalidResponse,
+            `Invalid response raw body type: ${typeof rawBody}`
+          )
+        );
+
+        throw new ConnectorError(ConnectorErrorCodes.General, rawBody);
+      }
+
+      throw error;
+    }
+  };
+
+const createGatewayApiSmsConnector: CreateConnector<SmsConnector> = async ({ getConfig }) => {
+  return {
+    metadata: defaultMetadata,
+    type: ConnectorType.Sms,
+    configGuard: gatewayApiSmsConfigGuard,
+    sendMessage: sendMessage(getConfig),
+  };
+};
+
+export default createGatewayApiSmsConnector;
