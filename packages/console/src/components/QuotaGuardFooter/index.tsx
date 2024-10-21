570a6a9f7205c7ec9005e51e77295c2015d769c3
Darcy Ye <darcyye@silverhand.io>
2024-10-21 19:12:15 +0800

    chore: update code

--- packages/console/src/components/QuotaGuardFooter/index.tsx
+++ packages/console/src/components/QuotaGuardFooter/index.tsx
@@ -1,7 +1,7 @@
 import { type ReactNode } from 'react';
 
 import { contactEmailLink } from '@/consts';
-import Button, { LinkButton } from '@/ds-components/Button';
+import { LinkButton } from '@/ds-components/Button';
 import useTenantPathname from '@/hooks/use-tenant-pathname';
 
 import styles from './index.module.scss';
@@ -19,30 +19,28 @@ function QuotaGuardFooter({ children, isLoading, onClickUpgrade, isContactUsPref
   return (
     <div className={styles.container}>
       <div className={styles.description}>{children}</div>
-      {isContactUsPreferred ? (
-        <LinkButton
-          title="general.contact_us_action"
-          size="large"
-          type="primary"
-          href={contactEmailLink}
-          targetBlank="noopener"
-        />
-      ) : (
-        <Button
-          size="large"
-          type="primary"
-          title="upsell.upgrade_plan"
-          isLoading={isLoading}
-          onClick={() => {
-            if (onClickUpgrade) {
-              onClickUpgrade();
-              return;
+      <LinkButton
+        size="large"
+        type="primary"
+        {...(isContactUsPreferred
+          ? {
+              title: 'general.contact_us_action',
+              href: contactEmailLink,
+              targetBlank: 'noopener',
             }
-            // Navigate to subscription page by default
-            navigate('/tenant-settings/subscription');
-          }}
-        />
-      )}
+          : {
+              title: 'upsell.upgrade_plan',
+              isLoading,
+              onClick: () => {
+                if (onClickUpgrade) {
+                  onClickUpgrade();
+                  return;
+                }
+                // Navigate to subscription page by default
+                navigate('/tenant-settings/subscription');
+              },
+            })}
+      />
     </div>
   );
 }
