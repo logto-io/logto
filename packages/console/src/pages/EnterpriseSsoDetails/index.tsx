dc8263b3309619129dbc8b6ce2c17df575cedb3d
simeng-li <simeng@silverhand.io>
2024-10-22 11:32:06 +0800

    fix(console): add devFeatureGuard
    add devFeatureGuard

--- packages/console/src/pages/EnterpriseSsoDetails/index.tsx
+++ packages/console/src/pages/EnterpriseSsoDetails/index.tsx
@@ -16,7 +16,7 @@ import Skeleton from '@/components/DetailsPage/Skeleton';
 import Drawer from '@/components/Drawer';
 import PageMeta from '@/components/PageMeta';
 import { EnterpriseSsoDetailsTabs } from '@/consts';
-import { isCloud } from '@/consts/env';
+import { isCloud, isDevFeaturesEnabled } from '@/consts/env';
 import { SubscriptionDataContext } from '@/contexts/SubscriptionDataProvider';
 import ConfirmModal from '@/ds-components/ConfirmModal';
 import DynamicT from '@/ds-components/DynamicT';
@@ -69,6 +69,7 @@ function EnterpriseSsoDetails() {
 
   const isIdpInitiatedAuthConfigEnabled = useMemo(
     () =>
+      isDevFeaturesEnabled &&
       isCloud &&
       ssoConnector?.providerType === SsoProviderType.SAML &&
       currentSubscriptionQuota.idpInitiatedSsoEnabled,
