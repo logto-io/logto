95c00199351db7c0b3019895e9c2e6cbcc24244a
Xiao Yijun <xiaoyijun@silverhand.io>
2024-10-23 14:57:23 +0800

    feat(elements): add logto-user-phone element

--- packages/elements/src/account/index.ts
+++ packages/elements/src/account/index.ts
@@ -8,3 +8,4 @@ export * from './providers/logto-account-provider.js';
 export * from './elements/logto-username.js';
 export * from './elements/logto-user-email.js';
 export * from './elements/logto-user-password.js';
+export * from './elements/logto-user-phone.js';
