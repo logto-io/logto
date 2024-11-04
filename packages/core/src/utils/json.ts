40eb8ce9e551e39e28fa18f421af9b49bb2d3c8d
simeng-li <simeng@silverhand.io>
2024-11-04 16:28:50 +0800

    fix(core): cleanup undefined in unknown json
    cleanup undefined in unknown json

--- packages/core/src/utils/json.ts
+++ packages/core/src/utils/json.ts
@@ -1,5 +1,6 @@
 import { type JsonObject, jsonObjectGuard } from '@logto/schemas';
 import { trySafe } from '@silverhand/essentials';
+import cleanDeep from 'clean-deep';
 
 export const safeParseJson = (jsonString: string): unknown =>
   // eslint-disable-next-line no-restricted-syntax
@@ -7,4 +8,14 @@ export const safeParseJson = (jsonString: string): unknown =>
 
 // Safely parse Zod unknown JSON object to JsonObject
 export const safeParseUnknownJson = (unknownJson: Record<string, unknown>): JsonObject =>
-  trySafe(() => jsonObjectGuard.safeParse(unknownJson).data) ?? {};
+  trySafe(
+    () =>
+      jsonObjectGuard.safeParse(
+        cleanDeep(unknownJson, {
+          emptyArrays: false,
+          emptyObjects: false,
+          emptyStrings: false,
+          nullValues: false,
+        })
+      ).data
+  ) ?? {};
