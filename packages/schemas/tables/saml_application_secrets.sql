9bed048d57d31949f98583834087231041e2277b
Darcy Ye <darcyye@silverhand.io>
2024-11-19 14:06:28 +0800

    chore: update schema

--- packages/schemas/tables/saml_application_secrets.sql
+++ packages/schemas/tables/saml_application_secrets.sql
@@ -6,17 +6,17 @@ create table saml_application_secrets (
     references tenants (id) on update cascade on delete cascade,
   application_id varchar(21) not null
     references applications (id) on update cascade on delete cascade,
-  private_key text not null,
-  certificate text not null,
+  private_key varchar not null,
+  certificate varchar not null,
   created_at timestamptz not null default now(),
   expires_at timestamptz not null,
   active boolean not null,
-  primary key (id),
+  primary key (tenant_id, application_id, id),
   constraint application_type
     check (check_application_type(application_id, 'SAML'))
 );
 
 -- Only one active secret per application
 create unique index saml_application_secrets__unique_active_secret
-  on saml_application_secrets (application_id, active)
+  on saml_application_secrets (tenant_id, application_id, active)
   where active;
