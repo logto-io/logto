036ddfecb0c9fcc8ccdffde52212c64a3122916d
simeng-li <simeng@silverhand.io>
2024-12-10 16:44:33 +0800

    fix(console): fix the lint error
    fix the lint error

--- packages/console/src/components/CreateTenantModal/SelectTenantPlanModal/index.tsx
+++ packages/console/src/components/CreateTenantModal/SelectTenantPlanModal/index.tsx
@@ -35,7 +35,7 @@ function SelectTenantPlanModal({ tenantData, onClose }: Props) {
   const { subscribe } = useSubscribe();
   const cloudApi = useCloudApi({ hideErrorToast: true });
 
-  const reservedBasicLogtoSkus = conditional(logtoSkus && pickupFeaturedLogtoSkus(logtoSkus));
+  const reservedBasicLogtoSkus = conditional(pickupFeaturedLogtoSkus(logtoSkus));
 
   if (!reservedBasicLogtoSkus || !tenantData) {
     return null;
