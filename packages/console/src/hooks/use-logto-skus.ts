ad4800fd24b3fd7d7c2e15a1d3d44efb1b2387f9
simeng-li <simeng@silverhand.io>
2024-12-10 18:33:02 +0800

    fix(console): fix enterprise tenant current sku always return dev (#6869)
    * fix(console): fix enterprise tenant current sku always dev
    
    fix the enterprise tenant current sku always dev bug
    
    * fix(console): fix the lint error
    
    fix the lint error

--- packages/console/src/hooks/use-logto-skus.ts
+++ packages/console/src/hooks/use-logto-skus.ts
@@ -5,17 +5,22 @@ import useSWRImmutable from 'swr/immutable';
 import { useCloudApi } from '@/cloud/hooks/use-cloud-api';
 import { type LogtoSkuResponse } from '@/cloud/types/router';
 import { isCloud } from '@/consts/env';
-import { featuredPlanIdOrder } from '@/consts/subscriptions';
 // Used in the docs
 // eslint-disable-next-line unused-imports/no-unused-imports
 import TenantAccess from '@/containers/TenantAccess';
+// eslint-disable-next-line unused-imports/no-unused-imports -- for jsDoc use
+import type { SubscriptionDataContext } from '@/contexts/SubscriptionDataProvider';
 import { LogtoSkuType } from '@/types/skus';
-import { sortBy } from '@/utils/sort';
-import { addSupportQuota } from '@/utils/subscription';
+import { formatLogtoSkusResponses } from '@/utils/subscription';
 
 /**
- * Fetch Logto SKUs from the cloud API.
- * Note: If you want to retrieve Logto SKUs under the {@link TenantAccess} component, use `SubscriptionDataContext` instead.
+ * Fetch public Logto SKUs from the cloud API.
+ *
+ * @remarks
+ * Note: This hook is used for retrieving public available Logto SKUs for all the users.
+ * If you want to retrieve tenant specific available Logto SKUs under the {@link TenantAccess} component,
+ * e.g. For enterprise tenant who have their own private SKUs, and all grandfathered plan tenants,
+ * use the logtoSkus from the {@link SubscriptionDataContext} instead.
  */
 const useLogtoSkus = () => {
   const cloudApi = useCloudApi();
@@ -30,18 +35,10 @@ const useLogtoSkus = () => {
 
   const { data: logtoSkuResponse } = useSwrResponse;
 
-  const logtoSkus: Optional<LogtoSkuResponse[]> = useMemo(() => {
-    if (!logtoSkuResponse) {
-      return;
-    }
-
-    return logtoSkuResponse
-      .map((logtoSku) => addSupportQuota(logtoSku))
-      .slice()
-      .sort(({ id: previousId }, { id: nextId }) =>
-        sortBy(featuredPlanIdOrder)(previousId, nextId)
-      );
-  }, [logtoSkuResponse]);
+  const logtoSkus: Optional<LogtoSkuResponse[]> = useMemo(
+    () => formatLogtoSkusResponses(logtoSkuResponse),
+    [logtoSkuResponse]
+  );
 
   return {
     ...useSwrResponse,
