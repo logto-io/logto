23ffec81b054f1dca421fc882457c8883ecd3253
Darcy Ye <darcyye@silverhand.io>
2024-12-27 12:41:01 +0800

    feat: support SAML app encryption and nameIdFormat config

--- packages/schemas/src/foundations/jsonb-types/saml-application-configs.ts
+++ packages/schemas/src/foundations/jsonb-types/saml-application-configs.ts
@@ -21,3 +21,35 @@ export const samlAcsUrlGuard = z.object({
   binding: z.nativeEnum(BindingType),
   url: z.string().url(),
 }) satisfies ToZodObject<SamlAcsUrl>;
+
+export const samlEncryptionGuard = z
+  .object({
+    encryptAssertion: z.boolean().optional(),
+    encryptThenSign: z.boolean().optional(),
+    certificate: z.string().optional(),
+  })
+  .superRefine(({ encryptAssertion, encryptThenSign, certificate }, ctx) => {
+    if (encryptAssertion && (encryptThenSign === undefined || certificate === undefined)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message:
+          '`encryptThenSign` and `certificate` are required when `encryptAssertion` is `true`',
+      });
+      return z.NEVER;
+    }
+  });
+
+export type SamlEncryption = z.input<typeof samlEncryptionGuard>;
+
+export enum NameIdFormat {
+  /** The Identity Provider can determine the format. */
+  Unspecified = 'urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified',
+  /** Returns the email address of the user. */
+  EmailAddress = 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
+  /** Uses unique and persistent identifiers for the user. */
+  Persistent = 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+  /** Uses unique and transient identifiers for the user, which can be different for each session. */
+  Transient = 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+}
+
+export const nameIdFormatGuard = z.nativeEnum(NameIdFormat);
