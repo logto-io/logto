28b31470727b44e7d18b67aaaba6f1be0fcbbf25
simeng-li <simeng@silverhand.io>
2025-02-19 10:32:27 +0800

    fix(core): remove unnessesary ternary
    remove unnessesary ternary

--- packages/core/src/caches/base-cache.ts
+++ packages/core/src/caches/base-cache.ts
@@ -51,10 +51,7 @@ export abstract class BaseCache<CacheMapT extends Record<string, unknown>> {
   ): Promise<Optional<CacheMapT[Type]>> {
     return trySafe(async () => {
       const data = await this.cacheStore.get(this.cacheKey(type, key));
-      // Allow `null` value to be stored in the cache.
-      // If the value is `null`, we should return `null` instead of parsing it as JSON.
-      // Otherwise, treat the value as JSON and parse it.
-      return this.getValueGuard(type).parse(data === 'null' ? null : JSON.parse(data ?? ''));
+      return this.getValueGuard(type).parse(JSON.parse(data ?? ''));
     });
   }
 
