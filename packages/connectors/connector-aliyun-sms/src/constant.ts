b2a77d79c1c9c56c1320c4954dd5099420605bb1
Darcy Ye <darcyye@silverhand.io>
2025-02-19 17:17:32 +0800

    fix(connector): update Aliyun SMS connector config (#7052)

--- packages/connectors/connector-aliyun-sms/src/constant.ts
+++ packages/connectors/connector-aliyun-sms/src/constant.ts
@@ -91,5 +91,13 @@ export const defaultMetadata: ConnectorMetadata = {
         },
       ],
     },
+    {
+      key: 'strictPhoneRegionNumberCheck',
+      label: 'Strict Phone Region Number Check',
+      type: ConnectorConfigFormItemType.Switch,
+      defaultValue: false,
+      description:
+        "When this configuration is enabled, the connector will assume by default that all phone numbers include a valid region code and rely on this to determine whether the phone number belongs to mainland China. If your users' phone numbers do not include a region code due to historical reasons, their sign-in processes may be affected. Please enable this setting with caution.",
+    },
   ],
 };
