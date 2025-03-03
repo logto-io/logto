b1c6237d266ae427013ce1be443c33dd6da46380
simeng-li <simeng@silverhand.io>
2025-03-03 18:26:12 +0800

    refactor(core): add password encryption to worker pool
    add password encryption to worker pool

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -4,6 +4,7 @@ import { defaultConfig } from '../../tsup.shared.config.js';
 
 export const config = Object.freeze({
   ...defaultConfig,
+  entry: ['src/index.ts', 'src/workers/tasks/**/*.ts'],
   outDir: 'build',
   onSuccess: 'pnpm run copy:apidocs',
 } satisfies Options);
