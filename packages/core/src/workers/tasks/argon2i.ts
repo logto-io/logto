e6cbaeedbeaf08843db7cede4008c83f1aea97f2
simeng-li <simeng@silverhand.io>
2025-03-05 18:52:48 +0800

    refactor(core): update password encryption method
    update the aragon2i password encryption method

--- packages/core/src/workers/tasks/argon2i.ts
+++ packages/core/src/workers/tasks/argon2i.ts
@@ -2,14 +2,13 @@ import crypto from 'node:crypto';
 
 import { argon2i } from 'hash-wasm';
 
-// eslint-disable-next-line import/no-unused-modules
 export default async function argon2iEncrypt(password: string): Promise<string> {
   return argon2i({
     password,
     salt: crypto.randomBytes(16),
-    iterations: 256,
+    iterations: 8,
     parallelism: 1,
-    memorySize: 4096,
+    memorySize: 8 * 1024,
     hashLength: 32,
     outputType: 'encoded',
   });
