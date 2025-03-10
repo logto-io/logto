2bb02e380f747506c4b26ee5687e64b4cda9bad1
wangsijie <wangsijie@silverhand.io>
2025-03-10 09:30:04 +0800

    feat(core): add crud routes for captcha provider

---
+++ packages/integration-tests/src/tests/api/captcha-provider.test.ts
@@ -0,0 +1,73 @@
+import { CaptchaType } from '@logto/schemas';
+
+import {
+  deleteCaptchaProvider,
+  getCaptchaProvider,
+  updateCaptchaProvider,
+} from '#src/api/captcha-provider.js';
+import { expectRejects } from '#src/helpers/index.js';
+
+describe('captcha provider', () => {
+  it('should update and get captcha provider successfully', async () => {
+    await updateCaptchaProvider({
+      config: {
+        type: CaptchaType.Turnstile,
+        siteKey: 'site_key',
+        secretKey: 'secret_key',
+      },
+    });
+    const captchaProvider = await getCaptchaProvider();
+
+    expect(captchaProvider).toMatchObject({
+      config: {
+        type: CaptchaType.Turnstile,
+        siteKey: 'site_key',
+        secretKey: 'secret_key',
+      },
+    });
+
+    await updateCaptchaProvider({
+      config: {
+        type: CaptchaType.RecaptchaEnterprise,
+        siteKey: 'site_key',
+        secretKey: 'secret_key',
+      },
+    });
+
+    const updatedCaptchaProvider = await getCaptchaProvider();
+
+    expect(updatedCaptchaProvider).toMatchObject({
+      config: {
+        type: CaptchaType.RecaptchaEnterprise,
+        siteKey: 'site_key',
+        secretKey: 'secret_key',
+      },
+    });
+  });
+
+  it('should delete captcha provider successfully', async () => {
+    await updateCaptchaProvider({
+      config: {
+        type: CaptchaType.Turnstile,
+        siteKey: 'site_key',
+        secretKey: 'secret_key',
+      },
+    });
+    const captchaProvider = await getCaptchaProvider();
+
+    expect(captchaProvider).toMatchObject({
+      config: {
+        type: CaptchaType.Turnstile,
+        siteKey: 'site_key',
+        secretKey: 'secret_key',
+      },
+    });
+
+    await deleteCaptchaProvider();
+
+    await expectRejects(getCaptchaProvider(), {
+      code: 'entity.not_found',
+      status: 404,
+    });
+  });
+});
