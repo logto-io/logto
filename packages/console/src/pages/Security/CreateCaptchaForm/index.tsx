061e7fa860c02ad13b6ac4391618e4f8e2c1e62b
wangsijie <wangsijie@silverhand.io>
2025-03-12 10:44:22 +0800

    feat(console): add captcha modal

---
+++ packages/console/src/pages/Security/CreateCaptchaForm/index.tsx
@@ -0,0 +1,52 @@
+import { type CaptchaType } from '@logto/schemas';
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+import Button from '@/ds-components/Button';
+import ModalLayout from '@/ds-components/ModalLayout';
+import modalStyles from '@/scss/modal.module.scss';
+
+import ProviderRadioGroup from './ProviderRadioGroup';
+
+type Props = {
+  readonly isOpen: boolean;
+  readonly onClose?: (type?: CaptchaType) => void;
+};
+
+function CreateCaptchaForm({ onClose, isOpen: isFormOpen }: Props) {
+  const [provider, setProvider] = useState<CaptchaType>();
+
+  const handleClose = () => {
+    onClose?.();
+    setProvider(undefined);
+  };
+
+  return (
+    <Modal
+      shouldCloseOnEsc
+      isOpen={isFormOpen}
+      className={modalStyles.content}
+      overlayClassName={modalStyles.overlay}
+      onRequestClose={handleClose}
+    >
+      <ModalLayout
+        title="security.create_captcha.setup_captcha"
+        footer={
+          <Button
+            title="general.next"
+            type="primary"
+            disabled={!provider}
+            onClick={() => {
+              // TODO: Implement captcha
+            }}
+          />
+        }
+        onClose={handleClose}
+      >
+        <ProviderRadioGroup value={provider} onChange={setProvider} />
+      </ModalLayout>
+    </Modal>
+  );
+}
+
+export default CreateCaptchaForm;
