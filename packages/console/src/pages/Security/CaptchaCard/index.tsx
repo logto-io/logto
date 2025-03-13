38ef05a4877a45620d45d167a0295914733141af
wangsijie <wangsijie@silverhand.io>
2025-03-13 11:20:18 +0800

    feat(console): captchar list page

---
+++ packages/console/src/pages/Security/CaptchaCard/index.tsx
@@ -0,0 +1,36 @@
+import { type CaptchaProvider } from '@logto/schemas';
+
+import Gear from '@/assets/icons/gear.svg?react';
+import Button from '@/ds-components/Button';
+import DynamicT from '@/ds-components/DynamicT';
+
+import CaptchaLogo from '../CaptchaLogo';
+import { captchaProviders } from '../CreateCaptchaForm/constants';
+
+import styles from './index.module.scss';
+
+type Props = {
+  readonly captchaProvider: CaptchaProvider;
+};
+
+function CaptchaCard({ captchaProvider }: Props) {
+  const metadata = captchaProviders.find(
+    (provider) => provider.type === captchaProvider.config.type
+  );
+
+  if (!metadata) {
+    return null;
+  }
+
+  return (
+    <div className={styles.container}>
+      <CaptchaLogo Logo={metadata.logo} LogoDark={metadata.logoDark} />
+      <div className={styles.name}>
+        <DynamicT forKey={metadata.name} />
+      </div>
+      <Button type="text" icon={<Gear />} title="security.bot_protection.settings" />
+    </div>
+  );
+}
+
+export default CaptchaCard;
