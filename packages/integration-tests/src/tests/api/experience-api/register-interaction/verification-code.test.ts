0f7413c16a24107f9dcec73d3e1875afee00177b
simeng-li <simeng@silverhand.io>
2025-03-14 17:24:30 +0800

    feat(core): parse mandatory profile
    parse mandatory profile

--- packages/integration-tests/src/tests/api/experience-api/register-interaction/verification-code.test.ts
+++ packages/integration-tests/src/tests/api/experience-api/register-interaction/verification-code.test.ts
@@ -5,17 +5,21 @@ import {
 } from '@logto/schemas';
 
 import { deleteUser } from '#src/api/admin-user.js';
+import { updateSignInExperience } from '#src/api/sign-in-experience.js';
 import { initExperienceClient, logoutClient, processSession } from '#src/helpers/client.js';
 import { setEmailConnector, setSmsConnector } from '#src/helpers/connector.js';
-import { registerNewUserWithVerificationCode } from '#src/helpers/experience/index.js';
+import {
+  registerNewUserWithVerificationCode,
+  signInWithPassword,
+} from '#src/helpers/experience/index.js';
 import {
   successfullySendVerificationCode,
   successfullyVerifyVerificationCode,
 } from '#src/helpers/experience/verification-code.js';
 import { expectRejects } from '#src/helpers/index.js';
 import { enableAllVerificationCodeSignInMethods } from '#src/helpers/sign-in-experience.js';
-import { generateNewUser } from '#src/helpers/user.js';
-import { generateEmail, generatePhone } from '#src/utils.js';
+import { generateNewUser, generateNewUserProfile } from '#src/helpers/user.js';
+import { devFeatureTest, generateEmail, generatePhone } from '#src/utils.js';
 
 const verificationIdentifierType: readonly [SignInIdentifier.Email, SignInIdentifier.Phone] =
   Object.freeze([SignInIdentifier.Email, SignInIdentifier.Phone]);
@@ -188,4 +192,84 @@ describe('Register interaction with verification code happy path', () => {
       }
     );
   });
+});
+
+devFeatureTest.describe('username as secondary identifier', () => {
+  beforeAll(async () => {
+    await updateSignInExperience({
+      signUp: {
+        identifiers: [SignInIdentifier.Email, SignInIdentifier.Phone],
+        password: true,
+        verify: true,
+        secondaryIdentifiers: [
+          {
+            identifier: SignInIdentifier.Username,
+          },
+        ],
+      },
+    });
+  });
+
+  it.each(verificationIdentifierType)(
+    'Should register with verification code using %p and fulfill the password and username successfully',
+    async (identifierType) => {
+      const identifier = {
+        type: identifierType,
+        value: identifierType === SignInIdentifier.Email ? generateEmail() : generatePhone(),
+      };
+
+      const client = await initExperienceClient(InteractionEvent.Register);
+      const { verificationId, code } = await successfullySendVerificationCode(client, {
+        identifier,
+        interactionEvent: InteractionEvent.Register,
+      });
+      await successfullyVerifyVerificationCode(client, {
+        identifier,
+        verificationId,
+        code,
+      });
+      await expectRejects(client.identifyUser({ verificationId }), {
+        code: 'user.missing_profile',
+        status: 422,
+      });
+
+      const { username, password } = generateNewUserProfile({
+        username: true,
+        password: true,
+      });
+
+      await client.updateProfile({ type: 'password', value: password });
+
+      await expectRejects(client.identifyUser({ verificationId }), {
+        code: 'user.missing_profile',
+        status: 422,
+      });
+
+      await client.updateProfile({ type: SignInIdentifier.Username, value: username });
+
+      await client.identifyUser();
+
+      const { redirectTo } = await client.submitInteraction();
+
+      const userId = await processSession(client, redirectTo);
+      await logoutClient(client);
+
+      // Should able to sign-in with username password
+      await signInWithPassword({
+        identifier: {
+          type: SignInIdentifier.Username,
+          value: username,
+        },
+        password,
+      });
+
+      // Should able to sign-in with email password
+      await signInWithPassword({
+        identifier,
+        password,
+      });
+
+      void deleteUser(userId);
+    }
+  );
 });
