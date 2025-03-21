4ba3f2d50dae225ee881edd55212bb73a37e2eaa
simeng-li <simeng@silverhand.io>
2025-03-21 14:20:33 +0800

    feat(console): add multi sign-up identifiers
    update SIE page to support multi sign-up identifiers

--- packages/console/src/pages/SignInExperience/PageContent/SignUpAndSignIn/index.tsx
+++ packages/console/src/pages/SignInExperience/PageContent/SignUpAndSignIn/index.tsx
@@ -1,10 +1,12 @@
 import PageMeta from '@/components/PageMeta';
+import { isDevFeaturesEnabled } from '@/consts/env';
 
 import SignInExperienceTabWrapper from '../components/SignInExperienceTabWrapper';
 
 import AdvancedOptions from './AdvancedOptions';
 import SignInForm from './SignInForm';
 import SignUpForm from './SignUpForm';
+import NewSignUpFrom from './SignUpForm/SignUpForm';
 import SocialSignInForm from './SocialSignInForm';
 
 type Props = {
@@ -17,7 +19,7 @@ function SignUpAndSignIn({ isActive }: Props) {
       {isActive && (
         <PageMeta titleKey={['sign_in_exp.tabs.sign_up_and_sign_in', 'sign_in_exp.page_title']} />
       )}
-      <SignUpForm />
+      {isDevFeaturesEnabled ? <NewSignUpFrom /> : <SignUpForm />}
       <SignInForm />
       <SocialSignInForm />
       <AdvancedOptions />
