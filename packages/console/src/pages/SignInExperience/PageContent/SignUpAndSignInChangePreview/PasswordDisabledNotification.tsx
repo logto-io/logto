1f38b26e7b7186efea95c7df30a946cb55cd389d
simeng-li <simeng@silverhand.io>
2025-03-27 16:13:31 +0800

    feat(console): add password not enabled notification
    add password not enabled notificaiton

---
+++ packages/console/src/pages/SignInExperience/PageContent/SignUpAndSignInChangePreview/PasswordDisabledNotification.tsx
@@ -0,0 +1,43 @@
+import { SignInIdentifier, type SignInExperience } from '@logto/schemas';
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { isDevFeaturesEnabled } from '@/consts/env';
+import InlineNotification from '@/ds-components/InlineNotification';
+
+import { signUpFormDataParser } from '../utils/parser';
+
+type Props = {
+  readonly after: SignInExperience;
+  readonly className?: string;
+};
+
+function PasswordDisabledNotification({ after, className }: Props) {
+  const { signUp } = after;
+  const { identifiers, password } = signUpFormDataParser.fromSignUp(signUp);
+  const { t } = useTranslation(undefined, { keyPrefix: 'admin_console' });
+
+  const shouldShowNotification = useMemo(() => {
+    if (!isDevFeaturesEnabled) {
+      return false;
+    }
+
+    return (
+      identifiers.length === 1 &&
+      identifiers[0]?.identifier === SignInIdentifier.Username &&
+      !password
+    );
+  }, [identifiers, password]);
+
+  if (!shouldShowNotification) {
+    return null;
+  }
+
+  return (
+    <InlineNotification className={className}>
+      {t('sign_in_exp.sign_up_and_sign_in.tip.password_disabled_notification')}
+    </InlineNotification>
+  );
+}
+
+export default PasswordDisabledNotification;
