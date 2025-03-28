3455aec5069fb8cd778087e5ecba6d7a3eaef538
HighError <hierrorua@gmail.com>
2025-03-28 09:59:29 +0200

    feat(phrases): added uk-ua locale

---
+++ packages/phrases-experience/src/locales/uk-UA/user-scopes.ts
@@ -0,0 +1,15 @@
+const user_scopes = {
+	descriptions: {
+		custom_data: "Ваші власні дані",
+		email: "Ваша електронна пошта",
+		phone: "Ваш номер телефону",
+		profile: "Ваше ім'я, ім'я користувача, аватар та інша інформація профілю",
+		roles: "Ваші ролі",
+		identities: "Ваші пов’язані соціальні ідентичності",
+		"urn:logto:scope:organizations": "Інформація про ваші організації",
+		"urn:logto:scope:organization_roles": "Ваші ролі в організації",
+		address: "Ваша адреса",
+	},
+};
+
+export default Object.freeze(user_scopes);
