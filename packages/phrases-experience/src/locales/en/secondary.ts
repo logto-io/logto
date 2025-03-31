048f6ab63b9a6396215c67758c3ec51a0fabb63b
Nick Daugherty <ndaugherty987@gmail.com>
2025-03-30 23:02:44 -0600

    fix(phrases): improve wording of "existing account?" question (#7172)
    fix(phrases): improve en i18n content
    
    "have" reads better in English

--- packages/phrases-experience/src/locales/en/secondary.ts
+++ packages/phrases-experience/src/locales/en/secondary.ts
@@ -1,6 +1,6 @@
 const secondary = {
   social_bind_with:
-    'Already had an account? Sign in to link {{methods, list(type: disjunction;)}} with your social identity.',
+    'Already have an account? Sign in to link {{methods, list(type: disjunction;)}} with your social identity.',
 };
 
 export default Object.freeze(secondary);
