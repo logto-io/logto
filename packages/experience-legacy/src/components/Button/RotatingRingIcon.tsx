8850e5cb6e9b509de6ff30612a3152f7c2d66dc5
simeng-li <simeng@silverhand.io>
2025-04-01 13:44:44 +0800

    refactor(experience): remove experience-legacy
    officially remove the experience-legay package

--- packages/experience-legacy/src/components/Button/RotatingRingIcon.tsx
+++
@@ -1,7 +0,0 @@
-import Ring from '@/assets/icons/loading-ring.svg?react';
-
-import RotatingRingIconStyles from './RotatingRingIcon.module.scss';
-
-const RotatingRingIcon = () => <Ring className={RotatingRingIconStyles.icon} />;
-
-export default RotatingRingIcon;
--- packages/experience-legacy/src/utils/consts.ts
+++
@@ -1,7 +0,0 @@
-export const layoutClassNames = Object.freeze({
-  pageContainer: 'logto_page-container',
-  mainContent: 'logto_main-content',
-  customContent: 'logto_custom-content',
-  signature: 'logto_signature',
-  brandingHeader: 'logto_branding-header',
-});
