1c1c52a452d24a6fc5ebd8537071032c326a6100
wangsijie <wangsijie@silverhand.io>
2025-04-07 11:16:49 +0800

    fix(console): add recaptcha readme

--- packages/console/src/pages/Security/CreateCaptchaForm/constants.ts
+++ packages/console/src/pages/Security/CreateCaptchaForm/constants.ts
@@ -29,6 +29,38 @@ Turnstile is a CAPTCHA service that helps protect your website from spam and abu
 2. Scroll down to the bottom and copy the **Site key** and **Secret key**.
 `;
 
+const reCAPTCHAEnterpriseReadme = `
+# reCAPTCHA Enterprise
+
+reCAPTCHA Enterprise is a Google service that protects websites from fraud and abuse using advanced bot detection without disrupting user experience. This guide will walk you through the process of setting up reCAPTCHA Enterprise with Logto.
+
+## Prerequisites
+
+- A Google Cloud project
+
+## Setup a reCAPTCHA key
+
+1. Go to the [reCAPTCHA page of Google Cloud Console](https://console.cloud.google.com/security/recaptcha).
+2. Click **Create key** button near "reCAPTCHA keys".
+3. Fill out the form with the following details:
+   - **Display name**: Any name you want to give to the key
+   - **Application type**: Website
+   - **Domain list**: Add Logto's endpoint domain
+4. After creating the key, you will be redirected to the key details page, copy the **ID**.
+
+## Setup an API key
+
+1. Go to the [Credentials page of Google Cloud Console](https://console.cloud.google.com/apis/credentials).
+2. Click **Create credentials** button and select **API key**.
+3. Copy the API key.
+4. Optionally, you can restrict the API key to **reCAPTCHA Enterprise API** to make it more secure.
+5. Remember to leave "Application restrictions" to **None** if you don't understand what it is.
+
+## Get project ID
+
+1. Copy the **Project ID** from the [home page of Google Cloud Console](https://console.cloud.google.com/welcome).
+`;
+
 export const captchaProviders: CaptchaProviderMetadata[] = [
   {
     name: 'security.captcha_providers.recaptcha_enterprise.name',
@@ -36,7 +68,7 @@ export const captchaProviders: CaptchaProviderMetadata[] = [
     logo: recaptchaEnterprise,
     logoDark: recaptchaEnterprise,
     description: 'security.captcha_providers.recaptcha_enterprise.description',
-    readme: 'readme',
+    readme: reCAPTCHAEnterpriseReadme,
     requiredFields: [
       {
         field: 'siteKey',
