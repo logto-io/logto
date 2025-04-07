08db143430f55cbf0ed91491a2c3841ce6a73de8
wangsijie <wangsijie@silverhand.io>
2025-04-07 14:12:20 +0800

    fix(console): add turnstile readme (#7244)

--- packages/console/src/pages/Security/CreateCaptchaForm/constants.ts
+++ packages/console/src/pages/Security/CreateCaptchaForm/constants.ts
@@ -5,6 +5,30 @@ import turnstile from '@/assets/images/turnstile.svg?react';
 
 import { type CaptchaProviderMetadata } from './types';
 
+const turnstileReadme = `
+# Cloudflare Turnstile
+
+Turnstile is a CAPTCHA service that helps protect your website from spam and abuse. This guide will walk you through the process of setting up Turnstile with Logto.
+
+## Prerequisites
+
+- A Cloudflare account
+
+## Setup
+
+1. Go to the [Cloudflare Dashboard](https://dash.cloudflare.com/login) and select your account.
+2. Navigate to **Turnstile** > **Add widget**.
+3. Fill out the form with the following details:
+   - **Widget name**: Any name you want to give to the widget
+   - **Hostname**: Logto's endpoint domain, e.g. https://[tenant-id].logto.app
+   - **Widget Mode**: Leave as default
+
+## Get the site key and secret key
+
+1. Navigate to a widget you just created, and click **Manage widget**.
+2. Scroll down to the bottom and copy the **Site key** and **Secret key**.
+`;
+
 export const captchaProviders: CaptchaProviderMetadata[] = [
   {
     name: 'security.captcha_providers.recaptcha_enterprise.name',
@@ -34,7 +58,7 @@ export const captchaProviders: CaptchaProviderMetadata[] = [
     logo: turnstile,
     logoDark: turnstile,
     description: 'security.captcha_providers.turnstile.description',
-    readme: 'readme',
+    readme: turnstileReadme,
     requiredFields: [
       {
         field: 'siteKey',
