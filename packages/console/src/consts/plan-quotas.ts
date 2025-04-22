603ac6425267322c0bab36b4dfddc642ddb3de2c
simeng-li <simeng@silverhand.io>
2025-04-22 11:59:57 +0800

    fix(core): fix the lint error
    fix the lint error

--- packages/console/src/consts/plan-quotas.ts
+++ packages/console/src/consts/plan-quotas.ts
@@ -37,5 +37,5 @@ export const skuQuotaItemOrder: Array<keyof LogtoSkuQuota> = [
   'ticketSupportResponseTime',
 ];
 
-//TODO: @sijie remove this after the captcha quota is removed from the plan
+// TODO: @sijie remove this after the captcha quota is removed from the plan
 export const comingSoonSkuQuotaKeys: Array<keyof LogtoSkuQuota> = ['captchaEnabled'];
