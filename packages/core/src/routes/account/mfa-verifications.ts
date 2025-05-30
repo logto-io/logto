ff8d2043a75273a0b7297e1936f1444789aa8678
wangsijie <wangsijie@silverhand.io>
2025-05-30 12:24:44 +0800

    feat(core): update webauthn passkey name (#7415)

--- packages/core/src/routes/account/mfa-verifications.ts
+++ packages/core/src/routes/account/mfa-verifications.ts
@@ -26,6 +26,7 @@ export default function mfaVerificationsRoutes<T extends UserRouter>(
       body: z.object({
         type: z.literal(MfaFactor.WebAuthn),
         newIdentifierVerificationRecordId: z.string(),
+        name: z.string().optional(),
       }),
       status: [204, 400, 401],
     }),
@@ -35,7 +36,7 @@ export default function mfaVerificationsRoutes<T extends UserRouter>(
         identityVerified,
         new RequestError({ code: 'verification_record.permission_denied', status: 401 })
       );
-      const { newIdentifierVerificationRecordId } = ctx.guard.body;
+      const { newIdentifierVerificationRecordId, name } = ctx.guard.body;
       const { fields } = ctx.accountCenter;
       assertThat(
         fields.mfa === AccountCenterControlValue.Edit,
@@ -71,6 +72,7 @@ export default function mfaVerificationsRoutes<T extends UserRouter>(
             ...bindMfa,
             id: generateStandardId(),
             createdAt: new Date().toISOString(),
+            name,
           },
         ],
       });
@@ -78,6 +80,57 @@ export default function mfaVerificationsRoutes<T extends UserRouter>(
       ctx.appendDataHookContext('User.Data.Updated', { user: updatedUser });
 
       ctx.status = 204;
+
+      return next();
+    }
+  );
+
+  // Update mfa verification name, only support webauthn
+  router.patch(
+    `${accountApiPrefix}/mfa-verifications/:verificationId/name`,
+    koaGuard({
+      params: z.object({
+        verificationId: z.string(),
+      }),
+      body: z.object({
+        name: z.string(),
+      }),
+      status: [200, 400, 401],
+    }),
+    async (ctx, next) => {
+      const { id: userId, scopes, identityVerified } = ctx.auth;
+      assertThat(
+        identityVerified,
+        new RequestError({ code: 'verification_record.permission_denied', status: 401 })
+      );
+      const { name } = ctx.guard.body;
+      const { fields } = ctx.accountCenter;
+      assertThat(
+        fields.mfa === AccountCenterControlValue.Edit,
+        'account_center.filed_not_editable'
+      );
+
+      assertThat(scopes.has(UserScope.Identities), 'auth.unauthorized');
+
+      const user = await findUserById(userId);
+      const mfaVerification = user.mfaVerifications.find(
+        (mfaVerification) =>
+          mfaVerification.id === ctx.guard.params.verificationId &&
+          mfaVerification.type === MfaFactor.WebAuthn
+      );
+      assertThat(mfaVerification, 'verification_record.not_found');
+
+      const updatedUser = await updateUserById(userId, {
+        mfaVerifications: user.mfaVerifications.map((mfaVerification) =>
+          mfaVerification.id === ctx.guard.params.verificationId
+            ? { ...mfaVerification, name }
+            : mfaVerification
+        ),
+      });
+
+      ctx.appendDataHookContext('User.Data.Updated', { user: updatedUser });
+
+      ctx.status = 200;
 
       return next();
     }
