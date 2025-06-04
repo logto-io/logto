5a6ed96071b721afa88b29e35262cfde650b73f8
wangsijie <wangsijie@silverhand.io>
2025-06-04 10:14:59 +0800

    feat(core): delete passkey (#7416)

--- packages/phrases/src/locales/tr-tr/errors/account-center.ts
+++ packages/phrases/src/locales/tr-tr/errors/account-center.ts
@@ -1,6 +1,6 @@
 const account_center = {
   not_enabled: 'Hesap merkezi etkin değil.',
-  filed_not_editable: 'Alan düzenlenemez.',
+  field_not_editable: 'Alan düzenlenemez.',
 };
 
 export default Object.freeze(account_center);
