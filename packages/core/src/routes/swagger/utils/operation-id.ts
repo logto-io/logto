e347bd9561949b5c27d8dc1ba73c2bb8281efe5b
Charles Zhao <charleszhao@silverhand.io>
2025-06-11 19:57:07 +0800

    fix(core): openapi operation id

--- packages/core/src/routes/swagger/utils/operation-id.ts
+++ packages/core/src/routes/swagger/utils/operation-id.ts
@@ -28,7 +28,12 @@ const methodToVerb = Object.freeze({
 type RouteDictionary = Record<`${OpenAPIV3.HttpMethods} ${string}`, string>;
 
 const devFeatureCustomRoutes: RouteDictionary = Object.freeze({
+  'get /custom-profile-fields': 'FindAllCustomProfileFields',
+  'get /custom-profile-fields/{id}': 'FindCustomProfileFieldById',
   'post /custom-profile-fields': 'CreateCustomProfileField',
+  'patch /custom-profile-fields/{id}': 'UpdateCustomProfileFieldById',
+  'delete /custom-profile-fields/{id}': 'DeleteCustomProfileFieldById',
+  'put /custom-profile-fields/sie-order': 'UpdateCustomProfileFieldsSieOrder',
 });
 
 export const customRoutes: Readonly<RouteDictionary> = Object.freeze({
