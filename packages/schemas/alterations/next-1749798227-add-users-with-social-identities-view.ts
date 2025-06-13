c4804cfa576a40e9cd3b0d8f207a0ed3c92ebd49
simeng-li <simeng@silverhand.io>
2025-06-13 15:15:19 +0800

    feat(schemas, cli): add users_with_social_identities view
    add users_with_social_identities view

---
+++ packages/schemas/alterations/next-1749798227-add-users-with-social-identities-view.ts
@@ -0,0 +1,56 @@
+import { sql } from '@silverhand/slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      CREATE OR REPLACE VIEW users_with_social_identities AS
+      --- select all columns from users table except identities,
+      SELECT
+        u.tenant_id,
+        u.id,
+        u.username,
+        u.primary_email,
+        u.primary_phone,
+        u.password_encrypted,
+        u.password_encryption_method,
+        u.name,
+        u.avatar,
+        u.profile,
+        u.application_id,
+        u.custom_data,
+        u.logto_config,
+        u.mfa_verifications,
+        u.is_suspended,
+        u.last_sign_in_at,
+        u.created_at,
+        u.updated_at,
+        -- custom merged identities column here
+        u.identities || COALESCE(
+          (
+            -- Aggregate social identities from user_social_identities table
+            SELECT jsonb_object_agg(
+              usi.target,
+              jsonb_build_object(
+                'userId', usi.identity_id,
+                'details', usi.details
+              )
+            )
+            FROM user_social_identities usi
+            WHERE usi.user_id = u.id AND usi.tenant_id = u.tenant_id
+          ),
+          '{}'::jsonb
+          -- join with existing identities
+        )  AS identities
+      FROM users u;
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      DROP VIEW IF EXISTS users_with_social_identities;
+    `);
+  },
+};
+
+export default alteration;
