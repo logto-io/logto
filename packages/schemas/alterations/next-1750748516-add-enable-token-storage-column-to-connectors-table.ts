44f30652a3bf85b5a79aa2ededb6587ab3de73ce
simeng-li <simeng@silverhand.io>
2025-06-24 15:07:50 +0800

    feat(schemas): add enable_token_storage column to connectors
    add enable_token_storage column to connectors

---
+++ packages/schemas/alterations/next-1750748516-add-enable-token-storage-column-to-connectors-table.ts
@@ -0,0 +1,20 @@
+import { sql } from '@silverhand/slonik';
+
+import type { AlterationScript } from '../lib/types/alteration.js';
+
+const alteration: AlterationScript = {
+  up: async (pool) => {
+    await pool.query(sql`
+      alter table connectors
+      add column enable_token_storage enable_token_storage boolean not null default FALSE,
+    `);
+  },
+  down: async (pool) => {
+    await pool.query(sql`
+      alter table connectors
+      drop column if exists enable_token_storage;
+    `);
+  },
+};
+
+export default alteration;
