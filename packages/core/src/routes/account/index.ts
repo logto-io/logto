70e6926224c1d99dbfe630254750b1ea19c76cf5
wangsijie <wangsijie@silverhand.io>
2025-07-04 21:40:44 +0800

    feat(core): manage custom data in account api

--- packages/core/src/routes/account/index.ts
+++ packages/core/src/routes/account/index.ts
@@ -6,7 +6,9 @@ import {
   SignInIdentifier,
   userMfaDataGuard,
   userMfaDataKey,
+  jsonObjectGuard,
 } from '@logto/schemas';
+import { conditional } from '@silverhand/essentials';
 import { z } from 'zod';
 
 import koaGuard from '#src/middleware/koa-guard.js';
@@ -59,6 +61,7 @@ export default function accountRoutes<T extends UserRouter>(...args: RouterInitA
         name: z.string().nullable().optional(),
         avatar: z.string().url().nullable().optional(),
         username: z.string().regex(usernameRegEx).nullable().optional(),
+        customData: jsonObjectGuard.optional(),
       }),
       response: userProfileResponseGuard.partial(),
       status: [200, 400, 422],
@@ -66,7 +69,7 @@ export default function accountRoutes<T extends UserRouter>(...args: RouterInitA
     async (ctx, next) => {
       const { id: userId, scopes } = ctx.auth;
       const { body } = ctx.guard;
-      const { name, avatar, username } = body;
+      const { name, avatar, username, customData } = body;
       const { fields } = ctx.accountCenter;
 
       assertThat(
@@ -81,7 +84,14 @@ export default function accountRoutes<T extends UserRouter>(...args: RouterInitA
         username === undefined || fields.username === AccountCenterControlValue.Edit,
         'account_center.field_not_editable'
       );
+      assertThat(
+        customData === undefined || fields.customData === AccountCenterControlValue.Edit,
+        'account_center.field_not_editable'
+      );
       assertThat(scopes.has(UserScope.Profile), 'auth.unauthorized');
+      if (customData !== undefined) {
+        assertThat(scopes.has(UserScope.CustomData), 'auth.unauthorized');
+      }
 
       if (username !== undefined) {
         if (username === null) {
@@ -95,11 +105,16 @@ export default function accountRoutes<T extends UserRouter>(...args: RouterInitA
         }
       }
 
-      const updatedUser = await updateUserById(userId, {
-        name,
-        avatar,
-        username,
-      });
+      const updatedUser = await updateUserById(
+        userId,
+        {
+          name,
+          avatar,
+          username,
+          ...conditional(customData !== undefined && { customData }),
+        },
+        'replace'
+      );
 
       ctx.appendDataHookContext('User.Data.Updated', { user: updatedUser });
 
