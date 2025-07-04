6f0b307c1018ea7c9ec28843699c6ac643b533bf
Darcy Ye <darcyye@silverhand.io>
2025-07-04 14:25:07 +0800

    feat(core): add google identity support for OTT

--- packages/schemas/src/foundations/jsonb-types/one-time-tokens.ts
+++ packages/schemas/src/foundations/jsonb-types/one-time-tokens.ts
@@ -1,14 +1,17 @@
-import { type ToZodObject } from '@logto/connector-kit';
+import { type ToZodObject, GoogleConnector } from '@logto/connector-kit';
 import { z } from 'zod';
 
 export type OneTimeTokenContext = {
   // Used for organization JIT provisioning.
   jitOrganizationIds?: string[];
+  // Used for Google One Tap to carry the Google Identity Sub.
+  [GoogleConnector.oneTimeTokenContextKey]?: string;
 };
 
 export const oneTimeTokenContextGuard = z
   .object({
     jitOrganizationIds: z.string().array(),
+    [GoogleConnector.oneTimeTokenContextKey]: z.string(),
   })
   .partial() satisfies ToZodObject<OneTimeTokenContext>;
 
