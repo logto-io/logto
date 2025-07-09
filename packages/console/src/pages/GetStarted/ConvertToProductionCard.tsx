3ebe925244b247d4b13a74cc0fbc4ca340a6eaa4
Xiao Yijun <xiaoyijun@silverhand.io>
2025-07-09 12:56:02 +0800

    style: add dark icon for tenant converting header

--- packages/console/src/pages/GetStarted/ConvertToProductionCard.tsx
+++ packages/console/src/pages/GetStarted/ConvertToProductionCard.tsx
@@ -1,6 +1,8 @@
+import { Theme } from '@logto/schemas';
 import { useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 
+import ConvertTenantHeaderDarkIcon from '@/assets/icons/convert-tenant-header-dark.svg?react';
 import ConvertTenantHeaderIcon from '@/assets/icons/convert-tenant-header.svg?react';
 import ConvertToProductionModal from '@/components/ConvertToProductionModal';
 import { contactEmailLink } from '@/consts';
@@ -8,12 +10,20 @@ import Button from '@/ds-components/Button';
 import Card from '@/ds-components/Card';
 import Spacer from '@/ds-components/Spacer';
 import TextLink from '@/ds-components/TextLink';
+import useTheme from '@/hooks/use-theme';
 
 import styles from './index.module.scss';
+
+const icons = {
+  [Theme.Light]: ConvertTenantHeaderIcon,
+  [Theme.Dark]: ConvertTenantHeaderDarkIcon,
+};
 
 function ConvertToProductionCard() {
   const { t } = useTranslation(undefined, { keyPrefix: 'admin_console.get_started' });
   const [isConvertModalOpen, setIsConvertModalOpen] = useState(false);
+  const theme = useTheme();
+  const ConvertTenantIcon = icons[theme];
 
   return (
     <Card className={styles.card}>
@@ -21,7 +31,7 @@ function ConvertToProductionCard() {
       <div className={styles.borderBox}>
         <div className={styles.rowWrapper}>
           <div className={styles.icon}>
-            <ConvertTenantHeaderIcon />
+            <ConvertTenantIcon />
           </div>
           <div className={styles.columnWrapper}>
             <div className={styles.title}>{t('convert_to_production.title')}</div>
