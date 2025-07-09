3bc3d80098fbc52ce5b9b1826518c2a20b6cc755
wangsijie <wangsijie@silverhand.io>
2025-07-09 11:05:44 +0800

    Revert "feat(core): verify user totp (#7505)"
    This reverts commit 2ecea1f8d180cab883ff672088ac880f45f824f3.

--- packages/integration-tests/src/api/my-account.ts
+++ packages/integration-tests/src/api/my-account.ts
@@ -122,8 +122,3 @@ export const updateMfaSettings = async (
     json: { skipMfaOnSignIn },
     headers: { [verificationRecordIdHeader]: verificationRecordId },
   });
-
-export const verifyTotpMfa = async (api: KyInstance, code: string) =>
-  api.post('api/my-account/mfa-verifications/totp/verify', {
-    json: { code },
-  });
