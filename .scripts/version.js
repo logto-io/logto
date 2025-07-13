d0b17755ed98aad508b05e98217d8da5d2140acc
Gao Sun <gao@silverhand.io>
2025-07-13 16:55:43 -0700

    ci: fix script order

--- .scripts/version.js
+++ .scripts/version.js
@@ -20,10 +20,17 @@ await execAsync(`pnpm -r version`).catch(catchCmdError);
 
 // Sanity check for prepublish scripts
 await execAsync(`pnpm -r prepack`).catch(catchCmdError);
-await execAsync(`pnpm -r prepublishOnly`).catch(catchCmdError);
+
+// Note: We deliberately move the lockfile update before `prepublishOnly` to ensure the lockfile
+// is always updated after version bump and before publishing, so that Docker build and other CI
+// tasks can always get the latest lockfile. This is required for `@logto/api` package which has
+// Docker Compose file and needs to be built in CI.
 
 // Update lockfile
 await execAsync(`pnpm i --no-frozen-lockfile`).catch(catchCmdError);
+
+// Sanity check for prepublish scripts
+await execAsync(`pnpm -r prepublishOnly`).catch(catchCmdError);
 
 // Show Git changes
 await execAsync(`git status`).catch(catchCmdError);
