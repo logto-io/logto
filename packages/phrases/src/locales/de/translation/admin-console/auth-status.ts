948e1b1225434d0228601469c8d0899efc0ac630
Darcy Ye <darcyye@silverhand.io>
2025-07-31 14:12:32 +0800

    chore: remove related phrases

--- packages/phrases/src/locales/de/translation/admin-console/auth-status.ts
+++
@@ -1,7 +0,0 @@
-const auth_status = {
-  page_title: 'Authentifizierungsstatus',
-  title: 'Authentifizierungsstatus',
-  description: 'Überprüfen Sie den Status der Authentifizierung.',
-};
-
-export default Object.freeze(auth_status);
