0dd77923771059ebc60ee967ad3c2d5618b339c5
simeng-li <simeng@silverhand.io>
2025-08-18 11:55:40 +0800

    feat(core): enable third-party app usage report
    enable third-party app usage report

--- packages/console/src/consts/subscriptions.ts
+++ packages/console/src/consts/subscriptions.ts
@@ -1,5 +1,7 @@
 import { ReservedPlanId } from '@logto/schemas';
 
+import { isDevFeaturesEnabled } from './env';
+
 /**
  * Shared quota limits between the featured plan content in the `CreateTenantModal` and the `PlanComparisonTable`.
  */
@@ -25,15 +27,6 @@ export const securityFeaturesAddOnUnitPrice = 48;
 /* === Add-on unit price (in USD) === */
 
 /**
- * In console, only featured plans are shown in the plan selection component.
- * we will this to filter out the public visible featured plans.
- */
-export const featuredPlanIds: readonly string[] = Object.freeze([
-  ReservedPlanId.Free,
-  ReservedPlanId.Pro202411,
-]);
-
-/**
  * The order of plans in the plan selection content component.
  * Unlike the `featuredPlanIds`, include both grandfathered plans and public visible featured plans.
  * We need to properly identify the order of the grandfathered plans compared to the new public visible featured plans.
@@ -48,4 +41,15 @@ export const planIdOrder: Record<string, number> = Object.freeze({
 export const checkoutStateQueryKey = 'checkout-state';
 
 /** The latest pro plan id we are using. */
-export const latestProPlanId = ReservedPlanId.Pro202411;
+export const latestProPlanId = isDevFeaturesEnabled
+  ? ReservedPlanId.Pro202509
+  : ReservedPlanId.Pro202411;
+
+/**
+ * In console, only featured plans are shown in the plan selection component.
+ * we will this to filter out the public visible featured plans.
+ */
+export const featuredPlanIds: readonly string[] = Object.freeze([
+  ReservedPlanId.Free,
+  latestProPlanId,
+]);
