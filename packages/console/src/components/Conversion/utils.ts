0776f55aac930c3816c5e4d6f4e273aa66457efe
Gao Sun <gao@silverhand.io>
2025-11-15 21:03:04 -0800

    refactor(console): implement privacy-friendly conversion reporting

--- packages/console/src/components/Conversion/utils.ts
+++ packages/console/src/components/Conversion/utils.ts
@@ -15,7 +15,6 @@ export enum GtagConversionId {
   PurchaseProPlan = 'AW-11192640559/WjCtCKHCtpgZEK_IiNkp',
 }
 
-export const redditPixelId = 't2_ggt11omdo';
 /** The data domain to aggregate the data for Plausible. */
 export const plausibleDataDomain = 'logto.io';
 
@@ -34,53 +33,17 @@ const sha256 = async (message: string) => {
   return [...new Uint8Array(hash)].map((value) => value.toString(16).padStart(2, '0')).join('');
 };
 
-/**
- * This function will do the following things:
- *
- * 1. Canonicalize the given email by Reddit's rule: lowercase, trim,
- * remove dots, remove everything after the first '+'.
- * 2. Hash the canonicalized email by SHA256.
- */
-export const hashEmail = async (email?: string) => {
-  if (!email) {
-    return;
-  }
-
-  const splitEmail = email.toLocaleLowerCase().trim().split('@');
-  const [localPart, domain] = splitEmail;
-
-  if (!localPart || !domain || splitEmail.length > 2) {
-    return;
-  }
-
-  // eslint-disable-next-line unicorn/prefer-string-replace-all
-  const canonicalizedEmail = `${localPart.replace(/\./g, '').replace(/\+.*/, '')}@${domain}`;
-  return sha256(canonicalizedEmail);
-};
-
-/**
- * Add more if needed: https://reddit.my.site.com/helpcenter/s/article/Install-the-Reddit-Pixel-on-your-website
- */
-type RedditReportType = 'PageVisit' | 'ViewContent' | 'Search' | 'Purchase' | 'Lead' | 'SignUp';
-
-const reportToReddit = (redditType: RedditReportType) => {
-  if (!window.rdt) {
-    log('report:', 'window.rdt is not available');
-    return false;
-  }
-
-  log('report:', 'redditType =', redditType);
-  window.rdt('track', redditType);
-
-  return true;
-};
-
 export const reportToGoogle = (
   gtagId: GtagConversionId,
   { transactionId }: { transactionId?: string } = {}
 ) => {
   if (!window.gtag) {
     log('report:', 'window.gtag is not available');
+    return false;
+  }
+
+  if (!shouldReport) {
+    log('skip reporting to Google:', { gtagId, transactionId });
     return false;
   }
 
@@ -102,24 +65,4 @@ export const reportToGoogle = (
 type ReportConversionOptions = {
   transactionId?: string;
   gtagId?: GtagConversionId;
-  redditType?: RedditReportType;
-};
-
-export const reportConversion = ({
-  gtagId,
-  redditType,
-  transactionId,
-}: ReportConversionOptions) => {
-  if (!shouldReport) {
-    log('skip reporting conversion:', { gtagId, redditType, transactionId });
-    return;
-  }
-
-  if (gtagId) {
-    reportToGoogle(gtagId, { transactionId });
-  }
-
-  if (redditType) {
-    reportToReddit(redditType);
-  }
 };
