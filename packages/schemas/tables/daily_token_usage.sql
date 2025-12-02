d30e2336cd90a8895ba79d2d0ca1182468906710
Darcy Ye <darcyye@silverhand.io>
2025-12-02 13:33:53 +0800

    refactor: add m2m and user token usage cols to daily token usage table

--- packages/schemas/tables/daily_token_usage.sql
+++ packages/schemas/tables/daily_token_usage.sql
@@ -1,11 +1,18 @@
 create table daily_token_usage (
   id varchar(21) not null,
-  tenant_id varchar(21) not null
-    references tenants (id) on update cascade on delete cascade,
+  tenant_id varchar(21) not null,
   usage bigint not null default(0),
+  user_token_usage bigint not null default(0),
+  m2m_token_usage bigint not null default(0),
   date timestamptz not null,
   primary key (id)
 );
 
 create unique index daily_token_usage__date
   on daily_token_usage (tenant_id, date);
+
+create index daily_token_usage__user_token_usage
+  on daily_token_usage (tenant_id, date, user_token_usage);
+
+create index daily_token_usage__m2m_token_usage
+  on daily_token_usage (tenant_id, date, m2m_token_usage);
