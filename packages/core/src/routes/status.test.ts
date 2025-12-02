8d9f535b62626f041277a29aea3bc71f9853bd59
Gao Sun <gao@silverhand.io>
2025-12-02 13:29:57 -0800

    feat: status api with key auth

--- packages/core/src/routes/status.test.ts
+++ packages/core/src/routes/status.test.ts
@@ -1,10 +1,34 @@
 import { createRequester } from '#src/utils/test-utils.js';
 
+import { EnvSet } from '../env-set/index.js';
+
 import statusRoutes from './status.js';
 
 describe('status router', () => {
   const requester = createRequester({ anonymousRoutes: statusRoutes });
-  it('GET /status', async () => {
-    await expect(requester.get('/status')).resolves.toHaveProperty('status', 204);
+
+  it('should respond with status 204', async () => {
+    const response = await requester.get('/status');
+
+    expect(response.status).toBe(204);
+    expect(response.headers).not.toHaveProperty('logto-tenant-id');
+  });
+
+  it('should respond with tenant ID when valid API key is provided', async () => {
+    const testApiKey = 'test-status-api-key';
+    const originalApiKey = EnvSet.values.statusApiKey;
+    // eslint-disable-next-line @silverhand/fp/no-mutating-methods
+    Object.defineProperty(EnvSet.values, 'statusApiKey', {
+      value: testApiKey,
+    });
+
+    const response = await requester.get('/status').set('logto-status-api-key', testApiKey);
+    expect(response.headers).toHaveProperty('logto-tenant-id', 'mock_id');
+
+    // Restore original API key
+    // eslint-disable-next-line @silverhand/fp/no-mutating-methods
+    Object.defineProperty(EnvSet.values, 'statusApiKey', {
+      value: originalApiKey,
+    });
   });
 });
--- packages/core/src/routes/status.ts
+++ packages/core/src/routes/status.ts
@@ -1,10 +1,21 @@
 import koaGuard from '#src/middleware/koa-guard.js';
 
+import { EnvSet } from '../env-set/index.js';
+
 import type { AnonymousRouter, RouterInitArgs } from './types.js';
 
-export default function statusRoutes<T extends AnonymousRouter>(...[router]: RouterInitArgs<T>) {
+export default function statusRoutes<T extends AnonymousRouter>(
+  ...[router, tenant]: RouterInitArgs<T>
+) {
   router.get('/status', koaGuard({ status: 204 }), async (ctx, next) => {
     ctx.status = 204;
+
+    if (
+      EnvSet.values.statusApiKey &&
+      ctx.headers['logto-status-api-key'] === EnvSet.values.statusApiKey
+    ) {
+      ctx.set('Logto-Tenant-ID', tenant.id);
+    }
 
     return next();
   });
