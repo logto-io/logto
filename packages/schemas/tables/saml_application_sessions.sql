b94c61e33e898e531fd9b2db8176815c6369667e
Darcy Ye <darcyye@silverhand.io>
2025-12-08 18:25:34 +0800

    chore: update code

--- packages/schemas/tables/saml_application_sessions.sql
+++ packages/schemas/tables/saml_application_sessions.sql
@@ -8,11 +8,11 @@ create table saml_application_sessions (
   application_id varchar(21) not null
     references applications (id) on update cascade on delete cascade,
   /** The identifier of the SAML SSO auth request ID, SAML request ID is pretty long. */
-  saml_request_id text not null,
+  saml_request_id varchar(128) not null,
   /** The identifier of the OIDC auth request state. */
   oidc_state varchar(32),
   /** The relay state of the SAML auth request. */
-  relay_state text,
+  relay_state varchar(512),
   /** The raw request of the SAML auth request. */
   raw_auth_request text not null,
   created_at timestamptz not null default(now()),
