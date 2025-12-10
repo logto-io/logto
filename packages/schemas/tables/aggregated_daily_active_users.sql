ab8a6431d3c89e0ad469b1a4ea3524280f5f5be2
Darcy Ye <darcyye@silverhand.io>
2025-12-10 20:31:19 +0800

    fix: fix aggregated-daily-active-user table comments (#8059)

--- packages/schemas/tables/aggregated_daily_active_users.sql
+++ packages/schemas/tables/aggregated_daily_active_users.sql
@@ -1,6 +1,4 @@
--- This table is used to store aggregated data of daily active users for each tenant.
--- A daily job summarizes data from the daily active users table and inserts it into this table, or removes expired data.
--- Therefore, we should not directly manipulate this table, except for "read" operations.
+/** This table is used to store aggregated data of daily active users for each tenant. A daily job summarizes data from the daily active users table and inserts it into this table, or removes expired data. Therefore, we should not directly manipulate this table, except for "read" operations. */
 create table aggregated_daily_active_users (
   tenant_id varchar(21) not null,
   activity_date date not null,
