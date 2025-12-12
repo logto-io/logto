843e983dc9d343187b2fc4682f1ecf14db2936a8
Gao Sun <14722250+gao-sun@users.noreply.github.com>
2025-12-11 19:45:45 -0800

    refactor: remove access token issued event

--- packages/core/src/event-listeners/utils.ts
+++ packages/core/src/event-listeners/utils.ts
@@ -1,6 +1,5 @@
-import { ProductAccessTokenType } from '@logto/schemas';
 import type { IRouterParamContext } from 'koa-router';
-import type { KoaContextWithOIDC, Provider } from 'oidc-provider';
+import type { KoaContextWithOIDC } from 'oidc-provider';
 
 import { type WithAppSecretContext } from '#src/middleware/koa-app-secret-transpilation.js';
 import type { LogPayload } from '#src/middleware/koa-audit-log.js';
@@ -21,19 +20,4 @@ export const extractInteractionContext = (
     userId: Account?.accountId,
     params,
   };
-};
-
-export const getAccessTokenEventPayload = (
-  token: unknown,
-  provider: Provider
-): Record<string, unknown> => {
-  if (token instanceof provider.AccessToken) {
-    return { type: ProductAccessTokenType.User, accountId: token.accountId };
-  }
-
-  if (token instanceof provider.ClientCredentials) {
-    return { type: ProductAccessTokenType.ClientCredentials, clientId: token.clientId };
-  }
-
-  return { type: ProductAccessTokenType.Unknown };
 };
