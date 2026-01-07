b11923a0f0837220936886e13f5674d0db5432c8
scarlettsokolova-a11y <scarlett.sokolova@petsy.pl>
2026-01-07 13:37:27 +0100

    Update input.ts

--- packages/phrases-experience/src/locales/cs/input.ts
+++ packages/phrases-experience/src/locales/cs/input.ts
@@ -1,11 +1,11 @@
 const input = {
-  username: 'Username',
-  password: 'Password',
-  email: 'Email',
-  phone_number: 'Phone number',
-  confirm_password: 'Confirm password',
-  search_region_code: 'Search region code',
-  backup_code: 'Backup code',
+  username: 'Uživatelské jméno',
+  password: 'Heslo',
+  email: 'E-mailová adresa',
+  phone_number: 'Telefonní číslo',
+  confirm_password: 'Potvrď heslo',
+  search_region_code: 'Hledat PSČ',
+  backup_code: 'Záložní kód',
   label_with_optional: '{{label}} (Optional)',
 };
 
