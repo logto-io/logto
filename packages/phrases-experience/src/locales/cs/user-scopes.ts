ebe51ae892f048080ac700000cfe5beb9d972c38
scarlettsokolova-a11y <scarlett.sokolova@petsy.pl>
2026-01-07 13:12:42 +0100

    Update user-scopes.ts

--- packages/phrases-experience/src/locales/cs/user-scopes.ts
+++ packages/phrases-experience/src/locales/cs/user-scopes.ts
@@ -1,14 +1,14 @@
 const user_scopes = {
   descriptions: {
-    custom_data: 'Your custom data',
-    email: 'Your email address',
-    phone: 'Your phone number',
-    profile: 'Your name, username, avatar, and other profile info',
-    roles: 'Your roles',
-    identities: 'Your linked social identities',
-    'urn:logto:scope:organizations': 'Your organizations info',
-    'urn:logto:scope:organization_roles': 'Your organization roles',
-    address: 'Your address',
+    custom_data: 'Tvé osobní údaje',
+    email: 'Tvá e-mailová adresa',
+    phone: 'Tvé telefonní číslo',
+    profile: 'Tvé jméno, uživatelské jméno, profilovka a další údaje o profilu',
+    roles: 'Tvá role',
+    identities: 'Tvé propojené sociální účty',
+    'urn:logto:scope:organizations': 'Informace o tvé organizaci',
+    'urn:logto:scope:organization_roles': 'Role v tvé organizaci',
+    address: 'Tvá adresa',
   },
 };
 
