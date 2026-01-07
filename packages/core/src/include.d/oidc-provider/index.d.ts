eeef44583d976bf997ac85a165e35d92bbdf2b8b
wangsijie <sijiewg@gmail.com>
2026-01-07 11:47:15 +0800

    feat(core): enable allowWildcardRedirectUris in oidc-provider

--- packages/core/src/include.d/oidc-provider/index.d.ts
+++ packages/core/src/include.d/oidc-provider/index.d.ts
@@ -2,4 +2,8 @@ import type { CustomClientMetadata } from '@logto/schemas';
 
 declare module 'oidc-provider' {
   export interface AllClientMetadata extends CustomClientMetadata {}
+
+  export interface Configuration {
+    allowWildcardRedirectUris?: boolean;
+  }
 }
