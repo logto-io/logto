366eecbf3cb74fce373f664e1bfa73c6e1f512ca
wangsijie <wangsijie@silverhand.io>
2026-01-28 16:24:17 +0800

    fix(phrases): use single quotes for fr and it locale strings

--- packages/phrases-experience/src/locales/it/mfa.ts
+++ packages/phrases-experience/src/locales/it/mfa.ts
@@ -54,7 +54,7 @@ const mfa = {
     'Puoi utilizzare uno di questi codici di backup per accedere al tuo account in caso di problemi durante la verifica in due passaggi in altri modi. Ogni codice può essere utilizzato solo una volta.',
   backup_code_hint: 'Assicurati di copiarli e salvarli in un luogo sicuro.',
   new_backup_codes_generated:
-    "I nuovi codici di backup hanno sostituito i tuoi vecchi codici. Salvali in un luogo sicuro il prima possibile.",
+    'I nuovi codici di backup hanno sostituito i tuoi vecchi codici. Salvali in un luogo sicuro il prima possibile.',
   enter_a_backup_code: 'Inserisci un codice di backup',
   enter_backup_code_description:
     'Inserisci il codice di backup salvato quando è stata abilitata la verifica in due passaggi.',
