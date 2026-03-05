9a24a7ab6d172f1234a42e8f9fd9f2b60d5c82c9
simeng-li <simeng@silverhand.io>
2026-03-05 10:32:36 +0800

    refactor(schemas): update alteration script
    update alteration script

--- packages/schemas/tables/oidc_model_instances.sql
+++ packages/schemas/tables/oidc_model_instances.sql
@@ -27,6 +27,7 @@ create index oidc_model_instances__model_name_payload_uid
     (payload->>'uid')
   );
 
+/* TODO: Consider dropping this full data index if the partial index proves to be effective and safe. */
 create index oidc_model_instances__model_name_payload_grant_id
   on oidc_model_instances (
     tenant_id,
