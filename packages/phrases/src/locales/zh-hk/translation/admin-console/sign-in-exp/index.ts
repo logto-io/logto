292b876919bea473454ef282f5edd9740e9f1915
wangsijie <wangsijie@silverhand.io>
2026-03-16 11:22:06 +0800

    chore(phrases): translate flows_description for all locales

--- packages/phrases/src/locales/zh-hk/translation/admin-console/sign-in-exp/index.ts
+++ packages/phrases/src/locales/zh-hk/translation/admin-console/sign-in-exp/index.ts
@@ -173,7 +173,7 @@ const sign_in_exp = {
         '要整合這些預建流程，請在下方設定中將相關的帳戶 API 權限設為<strong>編輯</strong>。',
       flows_title: '整合開箱即用的安全設置流程',
       flows_description:
-        '結合你的域名與路徑形成你的帳戶設置 URL（例如，https://auth.foo.com/account/email）。可以選擇性地添加一個 `redirect=` URL 參數來在成功更新後返回用戶至你的應用程式。',
+        '結合你的域名與路徑形成你的帳戶設置 URL（例如，https://auth.foo.com/account/email）。可以選擇性地添加 `redirect=` 在成功更新後返回用戶至你的應用程式，添加 `show_success=true` 以保持成功頁面可見，或添加 `ui_locales=` 以覆蓋預設語言。',
       tooltips: {
         email: '更新你的主電郵地址',
         phone: '更新你的主電話號碼',
