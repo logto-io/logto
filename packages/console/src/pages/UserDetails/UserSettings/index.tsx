0a62660aa6fc386bbae6936496d0db7b1fc7aed6
simeng-li <simeng@silverhand.io>
2026-03-17 15:34:38 +0800

    chore(console): add dev feature guard
    add dev feature guard

--- packages/console/src/pages/UserDetails/UserSettings/index.tsx
+++ packages/console/src/pages/UserDetails/UserSettings/index.tsx
@@ -13,6 +13,7 @@ import FormCard from '@/components/FormCard';
 import LearnMore from '@/components/LearnMore';
 import UnsavedChangesAlertModal from '@/components/UnsavedChangesAlertModal';
 import { profilePropertyReferenceLink, userCustomData } from '@/consts';
+import { isDevFeaturesEnabled } from '@/consts/env';
 import CodeEditor from '@/ds-components/CodeEditor';
 import FormField from '@/ds-components/FormField';
 import TextInput from '@/ds-components/TextInput';
@@ -172,7 +173,7 @@ function UserSettings() {
         </FormCard>
         <UserConnections userId={user.id} />
         <UserSessions userId={user.id} />
-        <UserThirdPartyApps userId={user.id} />
+        {isDevFeaturesEnabled && <UserThirdPartyApps userId={user.id} />}
         <FormCard title="user_details.user_profile">
           <FormField title="user_details.field_name">
             <TextInput {...register('name')} placeholder={t('users.placeholder_name')} />
