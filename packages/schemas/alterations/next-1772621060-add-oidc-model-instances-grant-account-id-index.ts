3f63209e244bddd3663be79af9faebadaefc38fe
simeng-li <simeng@silverhand.io>
2026-03-18 12:16:12 +0800

    fix(schemas): should throw if index exists
    should throw if index exists

--- packages/schemas/alterations/next-1772621060-add-oidc-model-instances-grant-account-id-index.ts
+++ packages/schemas/alterations/next-1772621060-add-oidc-model-instances-grant-account-id-index.ts
@@ -5,7 +5,7 @@ import type { AlterationScript } from '../lib/types/alteration.js';
 const alteration: AlterationScript = {
   beforeUp: async (pool) => {
     await pool.query(sql`
-      create index concurrently if not exists oidc_model_instances__grant_payload_account_id_expires_at
+      create index concurrently oidc_model_instances__grant_payload_account_id_expires_at
         on oidc_model_instances (tenant_id, (payload->>'accountId'), expires_at)
         WHERE model_name = 'Grant';
     `);
@@ -15,7 +15,7 @@ const alteration: AlterationScript = {
   },
   beforeDown: async (pool) => {
     await pool.query(sql`
-      drop index concurrently if exists oidc_model_instances__grant_payload_account_id_expires_at;
+      drop index concurrently oidc_model_instances__grant_payload_account_id_expires_at;
     `);
   },
   down: async () => {
