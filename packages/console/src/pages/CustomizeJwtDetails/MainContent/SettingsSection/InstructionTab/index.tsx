5898bdc589d9ec5486b5ef51202a8246d8838239
Darcy Ye <darcyye@silverhand.io>
2026-03-28 12:38:59 +0800

    refactor(console): merge jwt data source section guards

--- packages/console/src/pages/CustomizeJwtDetails/MainContent/SettingsSection/InstructionTab/index.tsx
+++ packages/console/src/pages/CustomizeJwtDetails/MainContent/SettingsSection/InstructionTab/index.tsx
@@ -59,161 +59,155 @@ function InstructionTab({ isActive, section }: Props) {
   return (
     <div className={classNames(tabContentStyles.tabContent, isActive && tabContentStyles.active)}>
       {isDataSourceSection && (
-        <GuideCard
-          name={CardType.TokenData}
-          isExpanded={expendCard === CardType.TokenData}
-          setExpanded={(expand) => {
-            setExpendCard(expand ? CardType.TokenData : undefined);
-          }}
-        >
-          <Editor
-            language="typescript"
-            className={styles.sampleCode}
-            value={
-              tokenType === LogtoJwtTokenKeyType.AccessToken
-                ? accessTokenPayloadTypeDefinition
-                : clientCredentialsPayloadTypeDefinition
-            }
-            // ClientCredentials token payload has only a few fields, so it doesn't need to be as tall as the AccessToken payload.
-            height={tokenType === LogtoJwtTokenKeyType.AccessToken ? '320px' : '200px'}
-            theme="logto-dark"
-            options={typeDefinitionCodeEditorOptions}
-          />
-        </GuideCard>
-      )}
-      {isDataSourceSection && tokenType === LogtoJwtTokenKeyType.AccessToken && (
-        <GuideCard
-          name={CardType.UserData}
-          isExpanded={expendCard === CardType.UserData}
-          setExpanded={(expand) => {
-            setExpendCard(expand ? CardType.UserData : undefined);
-          }}
-        >
-          <Editor
-            language="typescript"
-            className={styles.sampleCode}
-            value={jwtCustomizerUserContextTypeDefinition}
-            height="400px"
-            theme="logto-dark"
-            options={typeDefinitionCodeEditorOptions}
-          />
-        </GuideCard>
-      )}
-      {isDataSourceSection && tokenType === LogtoJwtTokenKeyType.AccessToken && (
-        <GuideCard
-          name={CardType.GrantData}
-          isExpanded={expendCard === CardType.GrantData}
-          setExpanded={(expand) => {
-            setExpendCard(expand ? CardType.GrantData : undefined);
-          }}
-        >
-          <Editor
-            language="typescript"
-            className={styles.sampleCode}
-            value={jwtCustomizerGrantContextTypeDefinition}
-            height="180px"
-            theme="logto-dark"
-            options={typeDefinitionCodeEditorOptions}
-          />
-        </GuideCard>
-      )}
-      {isDataSourceSection && tokenType === LogtoJwtTokenKeyType.AccessToken && (
-        <GuideCard
-          name={CardType.InteractionData}
-          isExpanded={expendCard === CardType.InteractionData}
-          setExpanded={(expand) => {
-            setExpendCard(expand ? CardType.InteractionData : undefined);
-          }}
-        >
-          <Editor
-            language="typescript"
-            className={styles.sampleCode}
-            value={`declare ${jwtCustomizerUserInteractionContextTypeDefinition}`}
-            height="400px"
-            theme="logto-dark"
-            options={typeDefinitionCodeEditorOptions}
-          />
-        </GuideCard>
-      )}
-      {isDataSourceSection && (
-        <GuideCard
-          name={CardType.ApplicationData}
-          isExpanded={expendCard === CardType.ApplicationData}
-          setExpanded={(expand) => {
-            setExpendCard(expand ? CardType.ApplicationData : undefined);
-          }}
-        >
-          <Editor
-            language="typescript"
-            className={styles.sampleCode}
-            value={`declare ${jwtCustomizerApplicationContextTypeDefinition}`}
-            height="400px"
-            theme="logto-dark"
-            options={typeDefinitionCodeEditorOptions}
-          />
-        </GuideCard>
-      )}
-      {isDataSourceSection && (
-        <GuideCard
-          name={CardType.FetchExternalData}
-          isExpanded={expendCard === CardType.FetchExternalData}
-          setExpanded={(expand) => {
-            setExpendCard(expand ? CardType.FetchExternalData : undefined);
-          }}
-        >
-          <div className={tabContentStyles.description}>
-            {t('jwt_claims.fetch_external_data.description')}
-          </div>
-          <Editor
-            language="typescript"
-            className={styles.sampleCode}
-            value={fetchExternalDataCodeExample}
-            height="300px"
-            theme="logto-dark"
-            options={sampleCodeEditorOptions}
-          />
-        </GuideCard>
-      )}
-      {isDataSourceSection && (
-        <GuideCard
-          name={CardType.EnvironmentVariables}
-          isExpanded={expendCard === CardType.EnvironmentVariables}
-          setExpanded={(expand) => {
-            setExpendCard(expand ? CardType.EnvironmentVariables : undefined);
-          }}
-        >
-          <EnvironmentVariablesField className={styles.envVariablesField} />
-          <div className={tabContentStyles.description}>
-            {t('jwt_claims.environment_variables.sample_code')}
-          </div>
-          <Editor
-            language="typescript"
-            className={styles.sampleCode}
-            value={environmentVariablesCodeExample}
-            path="file:///env-variables-sample.js"
-            height="400px"
-            theme="logto-dark"
-            options={sampleCodeEditorOptions}
-          />
-        </GuideCard>
-      )}
-      {isDataSourceSection && (
-        <GuideCard
-          name={CardType.ApiContext}
-          isExpanded={expendCard === CardType.ApiContext}
-          setExpanded={(expand) => {
-            setExpendCard(expand ? CardType.ApiContext : undefined);
-          }}
-        >
-          <Editor
-            language="typescript"
-            className={styles.sampleCode}
-            value={denyAccessCodeExample}
-            height="240px"
-            theme="logto-dark"
-            options={sampleCodeEditorOptions}
-          />
-        </GuideCard>
+        <>
+          <GuideCard
+            name={CardType.TokenData}
+            isExpanded={expendCard === CardType.TokenData}
+            setExpanded={(expand) => {
+              setExpendCard(expand ? CardType.TokenData : undefined);
+            }}
+          >
+            <Editor
+              language="typescript"
+              className={styles.sampleCode}
+              value={
+                tokenType === LogtoJwtTokenKeyType.AccessToken
+                  ? accessTokenPayloadTypeDefinition
+                  : clientCredentialsPayloadTypeDefinition
+              }
+              // ClientCredentials token payload has only a few fields, so it doesn't need to be as tall as the AccessToken payload.
+              height={tokenType === LogtoJwtTokenKeyType.AccessToken ? '320px' : '200px'}
+              theme="logto-dark"
+              options={typeDefinitionCodeEditorOptions}
+            />
+          </GuideCard>
+          {tokenType === LogtoJwtTokenKeyType.AccessToken && (
+            <GuideCard
+              name={CardType.UserData}
+              isExpanded={expendCard === CardType.UserData}
+              setExpanded={(expand) => {
+                setExpendCard(expand ? CardType.UserData : undefined);
+              }}
+            >
+              <Editor
+                language="typescript"
+                className={styles.sampleCode}
+                value={jwtCustomizerUserContextTypeDefinition}
+                height="400px"
+                theme="logto-dark"
+                options={typeDefinitionCodeEditorOptions}
+              />
+            </GuideCard>
+          )}
+          {tokenType === LogtoJwtTokenKeyType.AccessToken && (
+            <GuideCard
+              name={CardType.GrantData}
+              isExpanded={expendCard === CardType.GrantData}
+              setExpanded={(expand) => {
+                setExpendCard(expand ? CardType.GrantData : undefined);
+              }}
+            >
+              <Editor
+                language="typescript"
+                className={styles.sampleCode}
+                value={jwtCustomizerGrantContextTypeDefinition}
+                height="180px"
+                theme="logto-dark"
+                options={typeDefinitionCodeEditorOptions}
+              />
+            </GuideCard>
+          )}
+          {tokenType === LogtoJwtTokenKeyType.AccessToken && (
+            <GuideCard
+              name={CardType.InteractionData}
+              isExpanded={expendCard === CardType.InteractionData}
+              setExpanded={(expand) => {
+                setExpendCard(expand ? CardType.InteractionData : undefined);
+              }}
+            >
+              <Editor
+                language="typescript"
+                className={styles.sampleCode}
+                value={`declare ${jwtCustomizerUserInteractionContextTypeDefinition}`}
+                height="400px"
+                theme="logto-dark"
+                options={typeDefinitionCodeEditorOptions}
+              />
+            </GuideCard>
+          )}
+          <GuideCard
+            name={CardType.ApplicationData}
+            isExpanded={expendCard === CardType.ApplicationData}
+            setExpanded={(expand) => {
+              setExpendCard(expand ? CardType.ApplicationData : undefined);
+            }}
+          >
+            <Editor
+              language="typescript"
+              className={styles.sampleCode}
+              value={`declare ${jwtCustomizerApplicationContextTypeDefinition}`}
+              height="400px"
+              theme="logto-dark"
+              options={typeDefinitionCodeEditorOptions}
+            />
+          </GuideCard>
+          <GuideCard
+            name={CardType.FetchExternalData}
+            isExpanded={expendCard === CardType.FetchExternalData}
+            setExpanded={(expand) => {
+              setExpendCard(expand ? CardType.FetchExternalData : undefined);
+            }}
+          >
+            <div className={tabContentStyles.description}>
+              {t('jwt_claims.fetch_external_data.description')}
+            </div>
+            <Editor
+              language="typescript"
+              className={styles.sampleCode}
+              value={fetchExternalDataCodeExample}
+              height="300px"
+              theme="logto-dark"
+              options={sampleCodeEditorOptions}
+            />
+          </GuideCard>
+          <GuideCard
+            name={CardType.EnvironmentVariables}
+            isExpanded={expendCard === CardType.EnvironmentVariables}
+            setExpanded={(expand) => {
+              setExpendCard(expand ? CardType.EnvironmentVariables : undefined);
+            }}
+          >
+            <EnvironmentVariablesField className={styles.envVariablesField} />
+            <div className={tabContentStyles.description}>
+              {t('jwt_claims.environment_variables.sample_code')}
+            </div>
+            <Editor
+              language="typescript"
+              className={styles.sampleCode}
+              value={environmentVariablesCodeExample}
+              path="file:///env-variables-sample.js"
+              height="400px"
+              theme="logto-dark"
+              options={sampleCodeEditorOptions}
+            />
+          </GuideCard>
+          <GuideCard
+            name={CardType.ApiContext}
+            isExpanded={expendCard === CardType.ApiContext}
+            setExpanded={(expand) => {
+              setExpendCard(expand ? CardType.ApiContext : undefined);
+            }}
+          >
+            <Editor
+              language="typescript"
+              className={styles.sampleCode}
+              value={denyAccessCodeExample}
+              height="240px"
+              theme="logto-dark"
+              options={sampleCodeEditorOptions}
+            />
+          </GuideCard>
+        </>
       )}
       {isErrorHandlingSection && isDevFeaturesEnabled && (
         <GuideCard
