d7b1311185039b85181a4c5c000667af5999fc7f
Sijie Wang <sijiewg@gmail.com>
2026-06-08 08:41:12 +0000

    fix(core): add signInExperiences mock to passcode unit tests

--- packages/core/src/libraries/passcode.test.ts
+++ packages/core/src/libraries/passcode.test.ts
@@ -3,7 +3,12 @@ import { ConnectorType, TemplateType } from '@logto/connector-kit';
 import { type Passcode } from '@logto/schemas';
 import { any } from 'zod';
 
-import { mockConnector, mockMetadata, mockUser } from '#src/__mocks__/index.js';
+import {
+  mockConnector,
+  mockMetadata,
+  mockSignInExperience,
+  mockUser,
+} from '#src/__mocks__/index.js';
 import RequestError from '#src/errors/RequestError/index.js';
 import { MockQueries } from '#src/test-utils/tenant.js';
 
@@ -43,7 +48,12 @@ const getMessageConnector = jest.fn();
 const getI18nEmailTemplate = jest.fn();
 
 const { createPasscode, sendPasscode, verifyPasscode } = createPasscodeLibrary(
-  new MockQueries({ passcodes: passcodeQueries }),
+  new MockQueries({
+    passcodes: passcodeQueries,
+    signInExperiences: {
+      findDefaultSignInExperience: jest.fn().mockResolvedValue(mockSignInExperience),
+    },
+  }),
   // @ts-expect-error
   { getI18nEmailTemplate, getMessageConnector }
 );
@@ -355,6 +365,9 @@ describe('buildVerificationCodeContext', () => {
   const { buildVerificationCodeContext } = createPasscodeLibrary(
     new MockQueries({
       passcodes: passcodeQueries,
+      signInExperiences: {
+        findDefaultSignInExperience: jest.fn().mockResolvedValue(mockSignInExperience),
+      },
       users: {
         findUserById: mockFindUserById,
       },
