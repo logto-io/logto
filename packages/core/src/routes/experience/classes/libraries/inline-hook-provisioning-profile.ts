06938b39a5d831490740a7c3df73f04834a93b69
wangsijie <wangsijie@silverhand.io>
2026-07-02 09:30:34 +0800

    feat(core): add inline hook user provisioning

--- packages/core/src/routes/experience/classes/libraries/inline-hook-provisioning-profile.ts
+++ packages/core/src/routes/experience/classes/libraries/inline-hook-provisioning-profile.ts
@@ -1,4 +1,26 @@
 import { hookProvisioningProfileGuard, type HookProvisioningProfile } from '@logto/schemas';
+import { conditional } from '@silverhand/essentials';
+
+import { type InteractionProfile } from '../../types.js';
 
 export const toHookProvisioningProfile = (user: unknown): HookProvisioningProfile =>
   hookProvisioningProfileGuard.parse(user);
+
+export const getProfileIdentifierCollisionPayload = ({
+  socialIdentity,
+  username,
+  primaryEmail,
+  primaryPhone,
+}: InteractionProfile) => ({
+  username,
+  primaryEmail,
+  primaryPhone,
+  ...conditional(
+    socialIdentity && {
+      identity: {
+        target: socialIdentity.target,
+        id: socialIdentity.userInfo.id,
+      },
+    }
+  ),
+});
